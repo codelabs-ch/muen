039fb76c00f842e9dff426db70290c3255e9b5ba
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-02-14 11:20:00 +0100

    Extend annotations of Main procedure

--- kernel/src/sk-kernel.ads
+++ kernel/src/sk-kernel.ads
@@ -1,16 +1,22 @@
 --# inherit
---#    SK.Interrupts;
+--#    X86_64,
+--#    SK.Interrupts,
+--#    SK.System_State,
+--#    SK.VMX;
 package SK.Kernel
 is
 
    --  Kernel entry point.
    procedure Main;
    --# global
+   --#    in out X86_64.State;
+   --#    in     VMX.VMXON_Address;
    --#    in     Interrupts.ISR_List;
    --#       out Interrupts.IDT;
    --#       out Interrupts.IDT_Pointer;
    --# derives
-   --#    Interrupts.IDT, Interrupts.IDT_Pointer from Interrupts.ISR_List;
+   --#    Interrupts.IDT, Interrupts.IDT_Pointer from Interrupts.ISR_List &
+   --#    X86_64.State                           from *, VMX.VMXON_Address;
    pragma Export (C, Main, "kmain");
 
 end SK.Kernel;

87e4080c4a99457e5a68e4f666e18063774c58ae
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-02-21 17:30:44 +0100

    Subject: Display progress bar
    The subject does not provoke an exit because it is displaying a progress bar in
    an endless loop. The progress bar shows that the guest is actually running and
    not suspended.

--- kernel/src/sk-subject.adb
+++ kernel/src/sk-subject.adb
@@ -14,13 +14,42 @@ is
       Output_New_Line => Console_VGA.New_Line,
       Output_Char     => Console_VGA.Put_Char);
 
+   Greeter : constant String := "Hello from guest world";
+
    -------------------------------------------------------------------------
 
    procedure Main
    is
+      Counter : SK.Word32 := 0;
+      Idx     : Positive  := 1;
+      Dlt     : Integer   := -1;
    begin
       Text_IO.Init;
-      Text_IO.Put_Line (Item => "Hello from guest world");
+      Text_IO.Put_Line (Item => Greeter);
+      Text_IO.New_Line;
+
+      for I in Greeter'Range loop
+         Text_IO.Put_Char (Item => Character'Val (176));
+      end loop;
+
+      while True loop
+         if Counter mod 2**19 = 0 then
+            Console_VGA.Set_Position (X => Integer (Idx - Dlt),
+                                      Y => 3);
+            Text_IO.Put_Char (Item => Character'Val (176));
+            if Idx = Greeter'Last then
+               Dlt := -1;
+            elsif Idx = Greeter'First then
+               Dlt := 1;
+            end if;
+            Console_VGA.Set_Position (X => Integer (Idx),
+                                      Y => 3);
+            Text_IO.Put_Char (Item => Character'Val (178));
+            Idx := Idx + Dlt;
+         end if;
+         Counter := Counter + 1;
+      end loop;
+
       CPU.Panic;
    end Main;
 

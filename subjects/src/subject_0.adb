107b22e08c3562a90d16aea1261f9fc777293bac
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-03-13 21:50:53 +0100

    Let subject0 switch scheduling plans

--- subjects/src/subject_0.adb
+++ subjects/src/subject_0.adb
@@ -26,6 +26,10 @@ is
    Counter : SK.Word32       := 0;
    Idx     : Positive        := 1;
    Dlt     : Integer         := -1;
+
+   New_Major : SK.Major_Frame_Range;
+   for New_Major'Address use System'To_Address (16#4000#);
+   pragma Atomic (New_Major);
 begin
    Text_IO.Init;
    Text_IO.Put_Line (Item => Name);
@@ -42,8 +46,10 @@ begin
          Text_IO.Put_Char (Item => Character'Val (176));
          if Idx = Name'Last then
             Dlt := -1;
+            New_Major := 1;
          elsif Idx = Name'First then
             Dlt := 1;
+            New_Major := 0;
          end if;
          VGA.Set_Position (X => Integer (Idx),
                            Y => 3);

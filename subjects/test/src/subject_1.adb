54661b06ece353885012e55685a600c4935acb9e
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-03-14 10:50:58 +0100

    Rename test subjects
    Subject_0 is called Subject_1 and Subject_1 is now Subject_2.

--- subjects/test/src/subject_1.adb
+++ subjects/test/src/subject_1.adb
@@ -15,7 +15,7 @@ is
    package VGA is new SK.Console_VGA
      (Width_Type   => Width_Type,
       Height_Type  => Height_Type,
-      Base_Address => System'To_Address (16#000b_8820#));
+      Base_Address => System'To_Address (16#000b_8000#));
 
    package Text_IO is new SK.Console
      (Initialize      => VGA.Init,
@@ -26,6 +26,10 @@ is
    Counter : SK.Word32       := 0;
    Idx     : Positive        := 1;
    Dlt     : Integer         := -1;
+
+   New_Major : SK.Major_Frame_Range;
+   for New_Major'Address use System'To_Address (16#4000#);
+   pragma Atomic (New_Major);
 begin
    Text_IO.Init;
    Text_IO.Put_Line (Item => Name);
@@ -36,14 +40,16 @@ begin
    end loop;
 
    while True loop
-      if Counter mod 2**18 = 0 then
+      if Counter mod 2**16 = 0 then
          VGA.Set_Position (X => Integer (Idx - Dlt),
                            Y => 3);
          Text_IO.Put_Char (Item => Character'Val (176));
          if Idx = Name'Last then
             Dlt := -1;
+            New_Major := 1;
          elsif Idx = Name'First then
             Dlt := 1;
+            New_Major := 0;
          end if;
          VGA.Set_Position (X => Integer (Idx),
                            Y => 3);
--- subjects/test/src/subject_2.adb
+++ subjects/test/src/subject_2.adb
@@ -3,7 +3,7 @@ with System;
 with SK.Console;
 with SK.Console_VGA;
 
-procedure Subject_0
+procedure Subject_2
 is
 
    use type SK.Word32;
@@ -15,21 +15,17 @@ is
    package VGA is new SK.Console_VGA
      (Width_Type   => Width_Type,
       Height_Type  => Height_Type,
-      Base_Address => System'To_Address (16#000b_8000#));
+      Base_Address => System'To_Address (16#000b_8820#));
 
    package Text_IO is new SK.Console
      (Initialize      => VGA.Init,
       Output_New_Line => VGA.New_Line,
       Output_Char     => VGA.Put_Char);
 
-   Name    : constant String := "Subject 0";
+   Name    : constant String := "Subject 2";
    Counter : SK.Word32       := 0;
    Idx     : Positive        := 1;
    Dlt     : Integer         := -1;
-
-   New_Major : SK.Major_Frame_Range;
-   for New_Major'Address use System'To_Address (16#4000#);
-   pragma Atomic (New_Major);
 begin
    Text_IO.Init;
    Text_IO.Put_Line (Item => Name);
@@ -40,16 +36,14 @@ begin
    end loop;
 
    while True loop
-      if Counter mod 2**16 = 0 then
+      if Counter mod 2**18 = 0 then
          VGA.Set_Position (X => Integer (Idx - Dlt),
                            Y => 3);
          Text_IO.Put_Char (Item => Character'Val (176));
          if Idx = Name'Last then
             Dlt := -1;
-            New_Major := 1;
          elsif Idx = Name'First then
             Dlt := 1;
-            New_Major := 0;
          end if;
          VGA.Set_Position (X => Integer (Idx),
                            Y => 3);
@@ -59,4 +53,4 @@ begin
       Counter := Counter + 1;
    end loop;
 
-end Subject_0;
+end Subject_2;

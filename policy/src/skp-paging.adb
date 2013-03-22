ca04aebca1f9a960bb35e4403f18b631dd71d569
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-03-22 10:25:52 +0100

    Add Skp.Paging package
    This package provides data types and subprograms representing the IA-32e paging
    structures, as specified by Intel SDM Vol. 3A, section 4.5.

---
+++ policy/src/skp-paging.adb
@@ -0,0 +1,419 @@
+package body Skp.Paging
+is
+
+   Present_Flag   : constant := 0;
+   RW_Flag        : constant := 1;
+   US_Flag        : constant := 2;
+   PWT_Flag       : constant := 3;
+   PCD_Flag       : constant := 4;
+   Accessed_Flag  : constant := 5;
+   Dirty_Flag     : constant := 6;
+   Page_Size_Flag : constant := 7;
+   PTE_PAT_Flag   : constant := 7;
+   Global_Flag    : constant := 8;
+   PD_PAT_Flag    : constant := 12;
+   NXE_Flag       : constant := 63;
+
+   --  Table entry address range is bits 12 .. 47.
+   Address_Mask : constant Table_Entry_Type := 16#0000fffffffff000#;
+
+   --  PDPTE address range is bits 30 .. 47; entry maps a 1 GB page.
+   PDPT_Address_Mask : constant PDPT_Entry_Type := 16#0000ffffc0000000#;
+
+   --  PDE address range is bits 21 .. 47; entry maps a 2 MB page.
+   PD_Address_Mask : constant PD_Entry_Type := 16#0000ffffffe00000#;
+
+   --  PML4 index is bits 39 .. 47 of the linear address.
+   PML4_Index_Mask : constant SK.Word64 := 16#0000ff8000000000#;
+
+   --  PDPT index is bits 30 .. 38 of the linear address.
+   PDPT_Index_Mask : constant SK.Word64 := 16#0000007fc0000000#;
+
+   --  PD index is bits 21 .. 29 of the linear address.
+   PD_Index_Mask : constant SK.Word64 := 16#000000003fe00000#;
+
+   --  PT index is bits 12 .. 20 of the linear address.
+   PT_Index_Mask : constant SK.Word64 := 16#00000000001ff000#;
+
+   --  Set specified flag.
+   procedure Set_Flag
+     (E    : in out Table_Entry_Type;
+      Flag :        SK.Word64_Pos);
+
+   --  Create paging structure entry with specified parameters.
+   function Create_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Exec_Disable  : Boolean)
+      return Table_Entry_Type;
+
+   --  Create paging directory entry (PDPT or PD) with specified parameters.
+   function Create_Directory_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Map_Page      : Boolean;
+      Global        : Boolean;
+      PAT           : Boolean;
+      Exec_Disable  : Boolean)
+      return Directory_Entry_Type;
+
+   -------------------------------------------------------------------------
+
+   function Create_Directory_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Map_Page      : Boolean;
+      Global        : Boolean;
+      PAT           : Boolean;
+      Exec_Disable  : Boolean)
+      return Directory_Entry_Type
+   is
+      DE : Directory_Entry_Type;
+   begin
+      DE := Create_Entry (Address       => Address,
+                          Writable      => Writable,
+                          User_Access   => User_Access,
+                          Writethrough  => Writethrough,
+                          Cache_Disable => Cache_Disable,
+                          Exec_Disable  => Exec_Disable);
+
+      if Map_Page then
+         Set_Flag (E    => DE,
+                   Flag => Page_Size_Flag);
+
+         if PAT then
+            Set_Flag (E    => DE,
+                      Flag => PD_PAT_Flag);
+         end if;
+
+         if Global then
+            Set_Flag (E    => DE,
+                      Flag => Global_Flag);
+         end if;
+      end if;
+
+      return DE;
+   end Create_Directory_Entry;
+
+   -------------------------------------------------------------------------
+
+   function Create_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Exec_Disable  : Boolean)
+      return Table_Entry_Type
+   is
+      New_Entry : Table_Entry_Type;
+   begin
+      New_Entry := Table_Entry_Type (Address) and Address_Mask;
+
+      Set_Flag (E    => New_Entry,
+                Flag => Present_Flag);
+
+      if Writable then
+         Set_Flag (E    => New_Entry,
+                   Flag => RW_Flag);
+      end if;
+
+      if User_Access then
+         Set_Flag (E    => New_Entry,
+                   Flag => US_Flag);
+      end if;
+
+      if Writethrough then
+         Set_Flag (E    => New_Entry,
+                   Flag => PWT_Flag);
+      end if;
+
+      if Cache_Disable then
+         Set_Flag (E    => New_Entry,
+                   Flag => PCD_Flag);
+      end if;
+
+      if Exec_Disable then
+         Set_Flag (E    => New_Entry,
+                   Flag => NXE_Flag);
+      end if;
+
+      return New_Entry;
+   end Create_Entry;
+
+   -------------------------------------------------------------------------
+
+   function Create_PD_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Map_Page      : Boolean;
+      Global        : Boolean;
+      PAT           : Boolean;
+      Exec_Disable  : Boolean)
+      return PD_Entry_Type renames Create_Directory_Entry;
+
+   -------------------------------------------------------------------------
+
+   function Create_PDPT_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Map_Page      : Boolean;
+      Global        : Boolean;
+      PAT           : Boolean;
+      Exec_Disable  : Boolean)
+      return PDPT_Entry_Type renames Create_Directory_Entry;
+
+   -------------------------------------------------------------------------
+
+   function Create_PML4_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Exec_Disable  : Boolean)
+      return PML4_Entry_Type renames Create_Entry;
+
+   -------------------------------------------------------------------------
+
+   function Create_PT_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Global        : Boolean;
+      PAT           : Boolean;
+      Exec_Disable  : Boolean)
+      return PT_Entry_Type
+   is
+      PTE : PT_Entry_Type;
+   begin
+      PTE := Create_Entry (Address       => Address,
+                           Writable      => Writable,
+                           User_Access   => User_Access,
+                           Writethrough  => Writethrough,
+                           Cache_Disable => Cache_Disable,
+                           Exec_Disable  => Exec_Disable);
+
+      if PAT then
+         Set_Flag (E    => PTE,
+                   Flag => PTE_PAT_Flag);
+      end if;
+
+      if Global then
+         Set_Flag (E    => PTE,
+                   Flag => Global_Flag);
+      end if;
+
+      return PTE;
+   end Create_PT_Entry;
+
+   -------------------------------------------------------------------------
+
+   function Get_Address (E : Table_Entry_Type) return SK.Word64
+   is
+   begin
+      return SK.Word64 (E and Address_Mask);
+   end Get_Address;
+
+   -------------------------------------------------------------------------
+
+   procedure Get_Indexes
+     (Address    :     SK.Word64;
+      PML4_Index : out Table_Range;
+      PDPT_Index : out Table_Range;
+      PD_Index   : out Table_Range;
+      PT_Index   : out Table_Range)
+   is
+      use type SK.Word64;
+   begin
+
+      --  Add 1 since the table range starts at 1 instead of 0.
+
+      PML4_Index := Table_Range ((Address and PML4_Index_Mask) / 2 ** 39 + 1);
+      PDPT_Index := Table_Range ((Address and PDPT_Index_Mask) / 2 ** 30 + 1);
+      PD_Index   := Table_Range ((Address and PD_Index_Mask) / 2 ** 21 + 1);
+      PT_Index   := Table_Range ((Address and PT_Index_Mask) / 2 ** 12 + 1);
+   end Get_Indexes;
+
+   -------------------------------------------------------------------------
+
+   function Get_PD_Address (E : PDPT_Entry_Type) return SK.Word64
+   is
+      Address : SK.Word64;
+   begin
+      if SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => Page_Size_Flag)
+      then
+         Address := SK.Word64 (E and PDPT_Address_Mask);
+      else
+         Address := SK.Word64 (E and Address_Mask);
+      end if;
+
+      return Address;
+   end Get_PD_Address;
+
+   -------------------------------------------------------------------------
+
+   function Get_PDPT_Address
+     (E : PML4_Entry_Type) return SK.Word64
+      renames Get_Address;
+
+   -------------------------------------------------------------------------
+
+   function Get_PT_Address (E : PD_Entry_Type) return SK.Word64
+   is
+      Address : SK.Word64;
+   begin
+      if SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => Page_Size_Flag)
+      then
+         Address := SK.Word64 (E and PD_Address_Mask);
+      else
+         Address := SK.Word64 (E and Address_Mask);
+      end if;
+
+      return Address;
+   end Get_PT_Address;
+
+   -------------------------------------------------------------------------
+
+   function Has_Execute_Disable (E : Table_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => NXE_Flag);
+   end Has_Execute_Disable;
+
+   -------------------------------------------------------------------------
+
+   function Has_PAT (E : Directory_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => PD_PAT_Flag);
+   end Has_PAT;
+
+   -------------------------------------------------------------------------
+
+   function Is_Cache_Disabled (E : Table_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => PCD_Flag);
+   end Is_Cache_Disabled;
+
+   -------------------------------------------------------------------------
+
+   function Is_Dirty (E : PT_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => Dirty_Flag);
+   end Is_Dirty;
+
+   -------------------------------------------------------------------------
+
+   function Is_Global (E : PT_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => Global_Flag);
+   end Is_Global;
+
+   -------------------------------------------------------------------------
+
+   function Is_Present (E : Table_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => Present_Flag);
+   end Is_Present;
+
+   -------------------------------------------------------------------------
+
+   function Is_User_Accessible (E : Table_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => US_Flag);
+   end Is_User_Accessible;
+
+   -------------------------------------------------------------------------
+
+   function Is_Writable (E : Table_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => RW_Flag);
+   end Is_Writable;
+
+   -------------------------------------------------------------------------
+
+   function Is_Writethrough (E : Table_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => PWT_Flag);
+   end Is_Writethrough;
+
+   -------------------------------------------------------------------------
+
+   function PT_Has_PAT (E : PT_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => PTE_PAT_Flag);
+   end PT_Has_PAT;
+
+   -------------------------------------------------------------------------
+
+   procedure Set_Flag
+     (E    : in out Table_Entry_Type;
+      Flag :        SK.Word64_Pos)
+   is
+   begin
+      E := Table_Entry_Type
+        (SK.Bit_Set (Value => SK.Word64 (E),
+                     Pos   => Flag));
+   end Set_Flag;
+
+   -------------------------------------------------------------------------
+
+   function Was_Accessed (E : Table_Entry_Type) return Boolean
+   is
+   begin
+      return SK.Bit_Test
+        (Value => SK.Word64 (E),
+         Pos   => Accessed_Flag);
+   end Was_Accessed;
+
+end Skp.Paging;
---
+++ policy/src/skp-paging.ads
@@ -0,0 +1,186 @@
+with SK;
+
+package Skp.Paging
+is
+
+   --  Implementation of IA-32e paging structures, as specified by Intel SDM
+   --  Vol. 3A, section 4.5.
+
+   --  All paging structure types (PML4, PDPT, PD, PT) have 512 entries.
+   type Table_Range is range 1 .. 512;
+
+   --  Table entry type. Used as basis for all other entry types since they all
+   --  include the same fields but interpret some in a different way. See Intel
+   --  SDM 3a, page 4-28.
+   type Table_Entry_Type is private;
+
+   --  Returns True if entry is present in memory.
+   function Is_Present (E : Table_Entry_Type) return Boolean;
+
+   --  Returns True if entry is writable.
+   function Is_Writable (E : Table_Entry_Type) return Boolean;
+
+   --  Returns True if entry allows user-mode access.
+   function Is_User_Accessible (E : Table_Entry_Type) return Boolean;
+
+   --  Returns True if page-level write-through is enabled.
+   function Is_Writethrough (E : Table_Entry_Type) return Boolean;
+
+   --  Returns True if page-level cache is disabled.
+   function Is_Cache_Disabled (E : Table_Entry_Type) return Boolean;
+
+   --  Returns True if entry has been used for linear-address translation.
+   function Was_Accessed (E : Table_Entry_Type) return Boolean;
+
+   --  Returns True if execute-disable (NXE) is set, meaning instruction
+   --  fetches are not allowed from the memory region controlled by this entry.
+   function Has_Execute_Disable (E : Table_Entry_Type) return Boolean;
+
+   --  Page Map Level 4 table entry, see Intel SDM Vol. 3a, page 4-28.
+   subtype PML4_Entry_Type is Table_Entry_Type;
+
+   --  Create a new PML4 entry with specified attributes.
+   function Create_PML4_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Exec_Disable  : Boolean)
+      return PML4_Entry_Type;
+
+   --  Returns address pointed to by the given entry.
+   function Get_PDPT_Address (E : PML4_Entry_Type) return SK.Word64;
+
+   --  A Page Map Level 4 table comprises 512 64-bit entries (PML4Es), see
+   --  Intel SDM Vol 3a, page 4-22.
+   type PML4_Table_Type is array (Table_Range) of PML4_Entry_Type;
+
+   PML4_Null_Entry : constant PML4_Entry_Type;
+   Null_PML4_Table : constant PML4_Table_Type;
+
+   --  Directory entry type. Used as basis for all other directory entry types.
+   subtype Directory_Entry_Type is Table_Entry_Type;
+
+   --  Returns True if given directory entry has page attribute tables
+   --  activated.
+   function Has_PAT (E : Directory_Entry_Type) return Boolean;
+
+   --  Page directory pointer table entry, see Intel SDM Vol. 3a, page 4-28.
+   subtype PDPT_Entry_Type is Directory_Entry_Type;
+
+   --  Create a new PDPT entry with specified attributes. The map page
+   --  parameter specifies if the entry maps a 1 GB page.
+   function Create_PDPT_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Map_Page      : Boolean;
+      Global        : Boolean;
+      PAT           : Boolean;
+      Exec_Disable  : Boolean)
+      return PDPT_Entry_Type;
+
+   --  Returns address pointed to by the given entry.
+   function Get_PD_Address (E : PDPT_Entry_Type) return SK.Word64;
+
+   --  A page-directory pointer table comprises 512 64-bit entries (PDPTEs),
+   --  see Intel SDM Vol 3a, page 4-22.
+   type PDP_Table_Type is array (Table_Range) of PDPT_Entry_Type;
+
+   PDPT_Null_Entry : constant PDPT_Entry_Type;
+   Null_PDP_Table  : constant PDP_Table_Type;
+
+   --  Page directory entry, see Intel SDM Vol. 3a, page 4-28.
+   subtype PD_Entry_Type is Directory_Entry_Type;
+
+   --  Create a new PD entry with specified attributes. The map page parameter
+   --  specifies if the entry maps a 2 MB page.
+   function Create_PD_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Map_Page      : Boolean;
+      Global        : Boolean;
+      PAT           : Boolean;
+      Exec_Disable  : Boolean)
+      return PDPT_Entry_Type;
+
+   --  Returns address pointed to by the given entry.
+   function Get_PT_Address (E : PD_Entry_Type) return SK.Word64;
+
+   --  A page directory comprises 512 64-bit entries (PDEs), see Intel SDM Vol
+   --  3a, 4 - 22.
+   type PD_Table_Type is array (Table_Range) of PD_Entry_Type;
+
+   PD_Null_Entry : constant PD_Entry_Type;
+   Null_PD_Table : constant PD_Table_Type;
+
+   --  Page-table entry, see Intel SDM Vol. 3a, page 4-28.
+   subtype PT_Entry_Type is Table_Entry_Type;
+
+   --  Create a new PT entry with specified attributes.
+   function Create_PT_Entry
+     (Address       : SK.Word64;
+      Writable      : Boolean;
+      User_Access   : Boolean;
+      Writethrough  : Boolean;
+      Cache_Disable : Boolean;
+      Global        : Boolean;
+      PAT           : Boolean;
+      Exec_Disable  : Boolean)
+      return PT_Entry_Type;
+
+   --  Returns True if software has accessed the 4-KByte page referenced by the
+   --  given entry.
+   function Is_Dirty (E : PT_Entry_Type) return Boolean;
+
+   --  Returns True if address translations using this entry are cached
+   --  globally.
+   function Is_Global (E : PT_Entry_Type) return Boolean;
+
+   --  Returns True if given entry has corresponding page attribute table.
+   function PT_Has_PAT (E : PT_Entry_Type) return Boolean;
+
+   --  Returns address pointed to by the given entry.
+   function Get_Address (E : PT_Entry_Type) return SK.Word64;
+
+   --  A page table comprises 512 64-bit entries (PTEs), see Intel SDM Vol 3a,
+   --  page 4-22.
+   type Page_Table_Type is array (Table_Range) of PT_Entry_Type;
+
+   PT_Null_Entry   : constant PT_Entry_Type;
+   Null_Page_Table : constant Page_Table_Type;
+
+   --  Return the paging structure indexes for a given linear address, see
+   --  Intel SDM Vol. 3a, page 4-22:
+   --   * PML4 index is formed by bits 39 .. 47
+   --   * PDPT index is formed by bits 30 .. 38
+   --   * PD   index is formed by bits 21 .. 29
+   --   * PT   index is formed by bits 12 .. 20
+   procedure Get_Indexes
+     (Address    :     SK.Word64;
+      PML4_Index : out Table_Range;
+      PDPT_Index : out Table_Range;
+      PD_Index   : out Table_Range;
+      PT_Index   : out Table_Range);
+
+private
+
+   type Table_Entry_Type is new SK.Word64;
+
+   PML4_Null_Entry : constant PML4_Entry_Type := 0;
+   PDPT_Null_Entry : constant PDPT_Entry_Type := 0;
+   PD_Null_Entry   : constant PD_Entry_Type   := 0;
+   PT_Null_Entry   : constant PT_Entry_Type   := 0;
+
+   Null_PML4_Table : constant PML4_Table_Type := (others => PML4_Null_Entry);
+   Null_PDP_Table  : constant PDP_Table_Type  := (others => PDPT_Null_Entry);
+   Null_PD_Table   : constant PD_Table_Type   := (others => PD_Null_Entry);
+   Null_Page_Table : constant Page_Table_Type := (others => PT_Null_Entry);
+
+end Skp.Paging;

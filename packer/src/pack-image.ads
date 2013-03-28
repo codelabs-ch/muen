c93c3461bac22866c0c42694a1c49304a3470476
Reto Buerki <reet@codelabs.ch>
2013-03-28 17:17:31 +0100

    Provide Pack.Image package
    This package provides the Add_Section procedure which can be used to add
    a file to the kernel image.
    
    The file is added as new section with the specified physical address.
    The section name is derived from the filename.

---
+++ packer/src/pack-image.ads
@@ -0,0 +1,13 @@
+with SK;
+
+package Pack.Image
+is
+
+   --  Add file to kernel image. The file is added as new section with
+   --  the specified physical address. The section name is derived from the
+   --  filename.
+   procedure Add_Section
+     (Filename : String;
+      Address  : SK.Word64);
+
+end Pack.Image;

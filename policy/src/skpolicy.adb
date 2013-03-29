0633565b1fbda231477de32dd605de29000538e8
Reto Buerki <reet@codelabs.ch>
2013-03-29 10:38:03 +0100

    Write packer policy to 'pack' directory
    This policy is not relevant for the kernel so put it in a separate
    directory.

--- policy/src/skpolicy.adb
+++ policy/src/skpolicy.adb
@@ -8,25 +8,32 @@ procedure Skpolicy
 is
    Data    : Skp.Xml.XML_Data_Type;
    Policy  : Skp.Policy_Type;
-   Out_Dir : constant String := "include";
+   Inc_Dir : constant String := "include";
+   Pac_Dir : constant String := "pack";
 begin
    Skp.Xml.Parse (Data   => Data,
                   File   => Ada.Command_Line.Argument (Number => 1),
                   Schema => "schema/system.xsd");
 
    Policy := Skp.Xml.To_Policy (Data => Data);
-   Skp.Writers.Write_Kernel (Dir_Name => Out_Dir,
+   Skp.Writers.Write_Kernel (Dir_Name => Inc_Dir,
                              Policy   => Policy);
-   Ada.Text_IO.Put_Line (Item => "Wrote kernel policy to '" & Out_Dir
+
+   Ada.Text_IO.Put_Line (Item => "Wrote kernel policy to '" & Inc_Dir
                          & "' directory");
 
-   Skp.Writers.Write_Subjects (Dir_Name => Out_Dir,
+   Skp.Writers.Write_Subjects (Dir_Name => Inc_Dir,
                                Policy   => Policy);
-   Ada.Text_IO.Put_Line (Item => "Wrote subject policy to '" & Out_Dir
+   Ada.Text_IO.Put_Line (Item => "Wrote subject policy to '" & Inc_Dir
                          & "' directory");
 
-   Skp.Writers.Write_System (Dir_Name => Out_Dir,
+   Skp.Writers.Write_System (Dir_Name => Inc_Dir,
                              Policy   => Policy);
-   Ada.Text_IO.Put_Line (Item => "Wrote system policy to '" & Out_Dir
+   Ada.Text_IO.Put_Line (Item => "Wrote system policy to '" & Inc_Dir
+                         & "' directory");
+
+   Skp.Writers.Write_Binaries (Dir_Name => Pac_Dir,
+                               Policy   => Policy);
+   Ada.Text_IO.Put_Line (Item => "Wrote packer policy to '" & Pac_Dir
                          & "' directory");
 end Skpolicy;

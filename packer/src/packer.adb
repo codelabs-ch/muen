9a1064fdd2dcdb1a5bd0111c87666d5f9bb1b024
Reto Buerki <reet@codelabs.ch>
2013-03-29 11:12:40 +0100

    Make image name a packer command line argument

--- packer/src/packer.adb
+++ packer/src/packer.adb
@@ -24,14 +24,14 @@ is
    procedure Print_Usage
    is
    begin
-      Ada.Text_IO.Put_Line (Ada.Command_Line.Command_Name & " <kernel_elf>");
+      Ada.Text_IO.Put_Line
+        (Ada.Command_Line.Command_Name & " <kernel_elf> <image>");
    end Print_Usage;
 
-   Knl_Elf    : constant String := "obj/kernel";
    Top_Dir    : constant String := "..";
    Policy_Dir : constant String := Top_Dir & "/policy/include";
 begin
-   if Ada.Command_Line.Argument_Count /= 1 then
+   if Ada.Command_Line.Argument_Count /= 2 then
       Print_Usage;
       Ada.Command_Line.Set_Exit_Status (Code => Ada.Command_Line.Failure);
       return;
@@ -43,9 +43,9 @@ begin
 
    Ada.Directories.Copy_File
      (Source_Name => Ada.Command_Line.Argument (1),
-      Target_Name => Knl_Elf);
+      Target_Name => Ada.Command_Line.Argument (2));
    Image.Add_Section
-     (Image    => Knl_Elf,
+     (Image    => Ada.Command_Line.Argument (2),
       Filename => Policy_Dir & "/kernel_pt",
       Address  => Kernel.PML4_Address);
    Ada.Text_IO.Put_Line (SK.Utils.To_Hex (Item => Kernel.PML4_Address)
@@ -55,6 +55,7 @@ begin
 
    for S in Subject_Specs'Range loop
       declare
+         Img  : constant String := Ada.Command_Line.Argument (2);
          Fn   : constant String := Top_Dir & "/" & To_String
            (Binary_Specs (S).Path);
          Name : constant String := Ada.Directories.Base_Name (Name => Fn);
@@ -70,15 +71,15 @@ begin
             & " [IOBM] " & Name);
 
          Image.Add_Section
-           (Image    => Knl_Elf,
+           (Image    => Img,
             Filename => "obj/" & Name,
             Address  => Binary_Specs (S).Physical_Address);
          Image.Add_Section
-           (Image    => Knl_Elf,
+           (Image    => Img,
             Filename => Policy_Dir & "/" & Name & "_pt",
             Address  => Subject_Specs (S).PML4_Address);
          Image.Add_Section
-           (Image    => Knl_Elf,
+           (Image    => Img,
             Filename => Policy_Dir & "/" & Name & "_iobm",
             Address  => Subject_Specs (S).IO_Bitmap_Address);
       end;

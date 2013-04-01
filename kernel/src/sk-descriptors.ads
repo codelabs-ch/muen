4902201c32e0a889f32f3b9e308f70bfc7b1680f
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-04-01 23:15:21 +0200

    Move IDT related types to Descriptors package
    These types can be shared with subjects wanting to implement their own interrupt
    handling.

--- kernel/src/sk-descriptors.ads
+++ kernel/src/sk-descriptors.ads
@@ -3,11 +3,27 @@
 package SK.Descriptors
 is
 
-   --  Pseudo Descriptor type, see Intel SDM vol. 3A, chapter 3.5.1.
+   --  Pseudo Descriptor type, see Intel SDM Vol. 3A, chapter 3.5.1.
    type Pseudo_Descriptor_Type is record
       Limit : SK.Word16;
       Base  : SK.Word64;
    end record;
+
+   --  Interrupt/Trap gate descriptor, see Intel SDM Vol. 3A, chapter 6.14.1.
+   type Gate_Type is record
+      Offset_15_00     : SK.Word16;
+      Segment_Selector : SK.Word16;
+      Flags            : SK.Word16;
+      Offset_31_16     : SK.Word16;
+      Offset_63_32     : SK.Word32;
+      Reserved         : SK.Word32;
+   end record;
+
+   --  Interrupt vector number, see Intel SDM Vol. 3A, chapter 6.2.
+   subtype Vector_Type is SK.Byte range 0 .. 255;
+
+   --  Interrupt descriptor table, see Intel SDM Vol. 3A, chapter 6.10.
+   type IDT_Type is array (Vector_Type) of Gate_Type;
 
 private
 
@@ -16,5 +32,17 @@ private
       Base  at 2 range 0 .. 63;
    end record;
    for Pseudo_Descriptor_Type'Size use 80;
+
+   for Gate_Type use record
+      Offset_15_00     at  0 range 0 .. 15;
+      Segment_Selector at  2 range 0 .. 15;
+      Flags            at  4 range 0 .. 15;
+      Offset_31_16     at  6 range 0 .. 15;
+      Offset_63_32     at  8 range 0 .. 31;
+      Reserved         at 12 range 0 .. 31;
+   end record;
+   for Gate_Type'Size use 16 * 8;
+
+   for IDT_Type'Alignment use 8;
 
 end SK.Descriptors;
--- kernel/src/sk-interrupts.adb
+++ kernel/src/sk-interrupts.adb
@@ -4,45 +4,16 @@ with System.Storage_Elements;
 package body SK.Interrupts
 is
 
-   --  Interrupt vector number.
-   subtype Vector_Type is SK.Byte range 0 .. 255;
-
-   --  Address of ISR.
-   subtype ISR_Address_Type is SK.Word64;
-
    --  ISR list type.
-   type ISR_List_Type is array (Vector_Type) of ISR_Address_Type;
+   type ISR_List_Type is array (Descriptors.Vector_Type) of SK.Word64;
 
    --  ISR trampoline list.
    ISR_List : ISR_List_Type;
    pragma Import (C, ISR_List, "isrlist");
    --# assert ISR_List'Always_Valid;
 
-   --  Interrupt gate descriptor.
-   type Gate_Type is record
-      Offset_15_00     : SK.Word16;
-      Segment_Selector : SK.Word16;
-      Flags            : SK.Word16;
-      Offset_31_16     : SK.Word16;
-      Offset_63_32     : SK.Word32;
-      Reserved         : SK.Word32;
-   end record;
-
-   for Gate_Type use record
-      Offset_15_00     at  0 range 0 .. 15;
-      Segment_Selector at  2 range 0 .. 15;
-      Flags            at  4 range 0 .. 15;
-      Offset_31_16     at  6 range 0 .. 15;
-      Offset_63_32     at  8 range 0 .. 31;
-      Reserved         at 12 range 0 .. 31;
-   end record;
-   for Gate_Type'Size use 16 * 8;
-
-   type IDT_Type is array (Vector_Type) of Gate_Type;
-
    --  IDT, see Intel SDM 3A, chapter 6.10.
-   IDT : IDT_Type;
-   for IDT'Alignment use 8;
+   IDT : Descriptors.IDT_Type;
 
    --  Interrupt table pointer, loaded into IDTR
    IDT_Pointer : Descriptors.Pseudo_Descriptor_Type;
@@ -74,8 +45,8 @@ begin
    declare
       Temp : SK.Word64;
    begin
-      IDT := IDT_Type'
-        (others => Gate_Type'
+      IDT := Descriptors.IDT_Type'
+        (others => Descriptors.Gate_Type'
            (Offset_15_00     => 0,
             Segment_Selector => 0,
             Flags            => 0,
@@ -83,10 +54,10 @@ begin
             Offset_63_32     => 0,
             Reserved         => 0));
 
-      for I in Vector_Type range IDT'Range loop
+      for I in Descriptors.Vector_Type range IDT'Range loop
          Temp := ISR_List (I);
 
-         IDT (I) := Gate_Type'
+         IDT (I) := Descriptors.Gate_Type'
            (Offset_15_00     => SK.Word16
               (Temp and 16#0000_0000_0000_ffff#),
             Segment_Selector => 16#0008#,

b7d07c50d243520081091603c83fdf589d139f70
Reto Buerki <reet@codelabs.ch>
2013-04-04 00:49:39 +0200

    Move segment descriptor defines to separate file

---
+++ kernel/src/asm/gdt.h
@@ -0,0 +1,10 @@
+// segment descriptors: See Intel SDM 3A, chapter 3.4.5
+
+#define GDTE_TYPE_DATA_A (1 <<  8)
+#define GDTE_TYPE_DATA_W (1 <<  9)
+#define GDTE_CODE        (1 << 11)
+#define GDTE_NON_SYSTEM  (1 << 12)
+#define GDTE_PRESENT     (1 << 15)
+#define GDTE_LONG        (1 << 21)
+
+#define GDTE_SYS_TSS 0x900

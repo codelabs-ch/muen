ba2a7e3a0316a480eb52bdf984724dbd9d4aba9e
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-04-03 11:49:27 +0200

    Config: Extend read binary information tests

--- config/tests/binary_tests.adb
+++ config/tests/binary_tests.adb
@@ -1,3 +1,5 @@
+with Ada.Exceptions;
+
 with Skc.Subjects;
 
 package body Binary_Tests
@@ -15,6 +17,21 @@ is
       T.Add_Test_Routine
         (Routine => Read_Binary'Access,
          Name    => "Read binary information");
+      T.Add_Test_Routine
+        (Routine => Read_Nonexistent_File'Access,
+         Name    => "Read nonexistent file");
+      T.Add_Test_Routine
+        (Routine => Read_Invalid_File'Access,
+         Name    => "Read invalid file");
+      T.Add_Test_Routine
+        (Routine => Read_No_Symbols'Access,
+         Name    => "Read file without symbols");
+      T.Add_Test_Routine
+        (Routine => Read_No_Global_Symbols'Access,
+         Name    => "Read file without global symbols");
+      T.Add_Test_Routine
+        (Routine => Read_Undefined_Symbols'Access,
+         Name    => "Read file with undefined symbols");
    end Initialize;
 
    -------------------------------------------------------------------------
@@ -33,5 +50,100 @@ is
       Assert (Condition => Bin = Ref_Bin,
               Message   => "Binary info mismatch");
    end Read_Binary;
+
+   -------------------------------------------------------------------------
+
+   procedure Read_Invalid_File
+   is
+      Bin : Subjects.Binary_Type;
+      pragma Unreferenced (Bin);
+   begin
+      Bin := Subjects.Read (Binary => "data/bin1.S");
+      Fail (Message => "Exception expected");
+
+   exception
+      when E : Subjects.Binary_Error =>
+         Assert (Condition => Ada.Exceptions.Exception_Message
+                 (X => E) = "File 'data/bin1.S' has invalid format",
+                 Message   => "Exception message mismatch");
+   end Read_Invalid_File;
+
+   -------------------------------------------------------------------------
+
+   procedure Read_No_Global_Symbols
+   is
+      Bin : Subjects.Binary_Type;
+      pragma Unreferenced (Bin);
+   begin
+      Bin := Subjects.Read (Binary => "data/noglobalmainsymbol");
+      Fail (Message => "Exception expected");
+
+   exception
+      when E : Subjects.Binary_Error =>
+         Assert (Condition => Ada.Exceptions.Exception_Message
+                 (X => E) = "main is not a global symbol",
+                 Message   => "Exception message mismatch");
+   end Read_No_Global_Symbols;
+
+   -------------------------------------------------------------------------
+
+   procedure Read_No_Symbols
+   is
+      Bin : Subjects.Binary_Type;
+      pragma Unreferenced (Bin);
+   begin
+      begin
+         Bin := Subjects.Read (Binary => "data/nomainsymbol");
+         Fail (Message => "Exception expected");
+
+      exception
+         when E : Subjects.Binary_Error =>
+            Assert (Condition => Ada.Exceptions.Exception_Message
+                    (X => E) = "No main symbol",
+                    Message   => "Exception message mismatch");
+      end;
+
+      begin
+         Bin := Subjects.Read (Binary => "data/nostacksymbol");
+         Fail (Message => "Exception expected");
+
+      exception
+         when E : Subjects.Binary_Error =>
+            Assert (Condition => Ada.Exceptions.Exception_Message
+                    (X => E) = "No stack symbol",
+                    Message   => "Exception message mismatch");
+      end;
+   end Read_No_Symbols;
+
+   -------------------------------------------------------------------------
+
+   procedure Read_Nonexistent_File
+   is
+      Bin : Subjects.Binary_Type;
+      pragma Unreferenced (Bin);
+   begin
+      Bin := Subjects.Read (Binary => "nonexistent");
+      Fail (Message => "Exception expected");
+
+   exception
+      when Subjects.Open_Error => null;
+   end Read_Nonexistent_File;
+
+   -------------------------------------------------------------------------
+
+   procedure Read_Undefined_Symbols
+   is
+      Bin : Subjects.Binary_Type;
+      pragma Unreferenced (Bin);
+   begin
+      Bin := Subjects.Read (Binary => "data/undefmain");
+      Fail (Message => "Exception expected");
+
+   exception
+      when E : Subjects.Binary_Error =>
+         Assert (Condition => Ada.Exceptions.Exception_Message
+                 (X => E) = "Undefined symbol main",
+                 Message   => "Exception message mismatch");
+   end Read_Undefined_Symbols;
 
 end Binary_Tests;
--- config/tests/binary_tests.ads
+++ config/tests/binary_tests.ads
@@ -11,4 +11,19 @@ is
    --  Read information from a binary file.
    procedure Read_Binary;
 
+   --  Try to read a nonexistent file.
+   procedure Read_Nonexistent_File;
+
+   --  Try to read non-binary file.
+   procedure Read_Invalid_File;
+
+   --  Try to read a binary file which contains no symbols.
+   procedure Read_No_Symbols;
+
+   --  Try to read a binary file which contains no global symbols.
+   procedure Read_No_Global_Symbols;
+
+   --  Try to read a binary file which contains undefined symbols.
+   procedure Read_Undefined_Symbols;
+
 end Binary_Tests;

c2c79740438b566aa79a7fcd7b057b0d7c572c8b
Reto Buerki <reet@codelabs.ch>
2013-04-05 17:33:39 +0200

    Put APs to sleep after jump into kernel

--- kernel/src/sk-kernel.adb
+++ kernel/src/sk-kernel.adb
@@ -5,6 +5,7 @@ with SK.System_State;
 with SK.VMX;
 with SK.Scheduler;
 with SK.Apic;
+with SK.CPU;
 
 package body SK.Kernel
 is
@@ -13,12 +14,20 @@ is
 
    procedure Main
    is
-      Success : Boolean;
+      Success, Is_Bsp : Boolean;
    begin
-      pragma Debug (KC.Init);
-      pragma Debug
-        (KC.Put_Line (Item => "Booting Separation Kernel ("
-                      & SK.Version.Version_String & ") ..."));
+      Is_Bsp := Apic.Is_BSP;
+      if Is_Bsp then
+         pragma Debug (KC.Init);
+         pragma Debug (KC.Put_Line
+           (Item => "Booting Separation Kernel ("
+            & SK.Version.Version_String & ") ..."));
+         null;
+      else
+         pragma Debug (KC.Put_Line
+                       (Item => "AP online -> hlt"));
+         CPU.Hlt;
+      end if;
 
       --  Setup IDT.
 
@@ -27,12 +36,9 @@ is
 
       Success := System_State.Is_Valid;
       if Success then
-         if Apic.Is_BSP then
-            pragma Debug (KC.Put_Line
-                          (Item => "BSP online, waking AP processors"));
-            Apic.Enable;
-            Apic.Start_AP_Processors;
-         end if;
+         pragma Debug (KC.Put_Line (Item => "Starting AP processors"));
+         Apic.Enable;
+         Apic.Start_AP_Processors;
 
          VMX.Enable;
          Scheduler.Schedule;

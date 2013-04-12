22e11e3e557d92c7e57b44f087336d2e1f576d46
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-04-12 10:29:54 +0200

    Add scheduling spec template

---
+++ tools/policy/templates/skp-scheduling.ads
@@ -0,0 +1,31 @@
+with SK;
+
+--# inherit SK, Skp;
+package Skp.Scheduling is
+
+   type Minor_Frame_Type is record
+      Subject_Id : Skp.Subject_Id_Type;
+      Ticks      : SK.Word32;
+   end record;
+
+   Null_Minor_Frame : constant Minor_Frame_Type := Minor_Frame_Type'
+     (Subject_Id => 0,
+      Ticks      => 0);
+
+   type Minor_Frame_Range is range __minor_range__;
+
+   type Minor_Frame_Array is array (Minor_Frame_Range) of Minor_Frame_Type;
+
+   type Major_Frame_Type is record
+      Length       : Minor_Frame_Range;
+      Minor_Frames : Minor_Frame_Array;
+   end record;
+
+   type Major_Frame_Range is range __major_range__;
+
+   type Scheduling_Plan_Type is array (Major_Frame_Range) of Major_Frame_Type;
+
+   Scheduling_Plans : constant Scheduling_Plan_Type := Scheduling_Plan_Type'(
+__scheduling_plans__);
+
+end Skp.Scheduling;

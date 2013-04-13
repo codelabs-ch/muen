8a5f07eb5b064d6d80a1b72db5142780293ea2b1
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-04-14 01:07:07 +0200

    Add annotations to SK.IO package
    This allows the port IO procedures to be used in kernel code.

--- common/debug/sk-io.ads
+++ common/debug/sk-io.ads
@@ -1,14 +1,25 @@
+--# inherit
+--#    X86_64,
+--#    SK;
 package SK.IO
 is
 
    --  Receive byte from given port.
    procedure Inb
-     (Port  :     Word16;
-      Value : out Byte);
+     (Port  :     SK.Word16;
+      Value : out SK.Byte);
+   --# global
+   --#    in X86_64.State;
+   --# derives
+   --#    Value from Port, X86_64.State;
 
    --  Send byte to given port.
    procedure Outb
-     (Port  : Word16;
-      Value : Byte);
+     (Port  : SK.Word16;
+      Value : SK.Byte);
+   --# global
+   --#    in out X86_64.State;
+   --# derives
+   --#    X86_64.State from *, Port, Value;
 
 end SK.IO;

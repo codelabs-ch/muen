2f8df3ca3508777fc9775b10f32e84cc71bafde6
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-05-03 16:47:38 +0200

    Add static PAT value to policy.h file
    Writing the PAT value to the IA32_PAT MSR will result in the following
    page attribute table:
    
    PAT0 -> UC
    PAT1 -> WC
    PAT2 -> WT
    PAT3 -> WP
    PAT4 -> WB
    
    [ PAT5..PAT7 -> UC ]

--- tools/policy/templates/policy.h
+++ tools/policy/templates/policy.h
@@ -4,3 +4,5 @@
 #define SUBJECT_COUNT __subj_count__
 #define VMXON_ADDRESS 0x__vmxon_addr__
 #define VMCS_ADDRESS  0x__vmcs_addr__
+#define PAT_HIGH      0x00000006
+#define PAT_LOW       0x05040100

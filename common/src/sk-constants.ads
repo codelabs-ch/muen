b992deca4967de5a8b35cbdfdcb4ee073ca42130
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-05-16 17:35:48 +0200

    Kernel: Drop unneeded VM-Execution control constants

--- common/src/sk-constants.ads
+++ common/src/sk-constants.ads
@@ -118,30 +118,6 @@ is
    GUEST_RIP                : constant := 16#681e#;
    GUEST_RFLAGS             : constant := 16#6820#;
 
-   --  VMX control flags
-
-   VM_CTRL_EXIT_EXT_INT     : constant := 16#0000_0001#;
-   VM_CTRL_EXIT_DT          : constant := 16#0000_0004#;
-   VM_CTRL_EXIT_WBINVD      : constant := 16#0000_0040#;
-   VM_CTRL_PREEMPT_TIMER    : constant := 16#0000_0040#;
-   VM_CTRL_EXIT_HLT         : constant := 16#0000_0080#;
-   VM_CTRL_IA32E_MODE       : constant := 16#0000_0200#;
-   VM_CTRL_EXIT_INVLPG      : constant := 16#0000_0200#;
-   VM_CTRL_EXIT_MWAIT       : constant := 16#0000_0400#;
-   VM_CTRL_EXIT_ACK_INT     : constant := 16#0000_8000#;
-   VM_CTRL_EXIT_RDPMC       : constant := 16#0000_0800#;
-   VM_CTRL_EXIT_RDTSC       : constant := 16#0000_1000#;
-   VM_CTRL_EXIT_CR3_LOAD    : constant := 16#0000_8000#;
-   VM_CTRL_EXIT_CR3_STORE   : constant := 16#0001_0000#;
-   VM_CTRL_EXIT_CR8_LOAD    : constant := 16#0008_0000#;
-   VM_CTRL_EXIT_CR8_STORE   : constant := 16#0010_0000#;
-   VM_CTRL_EXIT_SAVE_TIMER  : constant := 16#0040_0000#;
-   VM_CTRL_EXIT_MOV_DR      : constant := 16#0080_0000#;
-   VM_CTRL_IO_BITMAPS       : constant := 16#0200_0000#;
-   VM_CTRL_MSR_BITMAPS      : constant := 16#1000_0000#;
-   VM_CTRL_EXIT_MONITOR     : constant := 16#2000_0000#;
-   VM_CTRL_SECONDARY_PROC   : constant := 16#8000_0000#;
-
    --  VM entry/exit interruption-information flags
 
    VM_INTERRUPT_INFO_VALID  : constant := 16#8000_0000#;

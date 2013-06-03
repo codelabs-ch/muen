2609cbaf32e5b92233bd3f57578801e2192be808
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-06-03 12:06:46 +0200

    Crypter: Add SPARK annotations to Crypt.Sender package

--- subjects/crypter/src/crypt-sender.adb
+++ subjects/crypter/src/crypt-sender.adb
@@ -1,18 +1,23 @@
 with System;
 
 package body Crypt.Sender
+--# own
+--#    State is out Response;
 is
 
-   Response : Message_Type;
+   Response : Crypt.Message_Type;
    for Response'Address use System'To_Address (16#20000#);
    pragma Volatile (Response);
 
    -------------------------------------------------------------------------
 
-   procedure Send (Res : Message_Type)
+   procedure Send (Res : Crypt.Message_Type)
+   --# global
+   --#    out Response;
+   --# derives
+   --#    Response from Res;
    is
    begin
-      Response := Null_Message;
       Response := Res;
    end Send;
 
--- subjects/crypter/src/crypt-sender.ads
+++ subjects/crypter/src/crypt-sender.ads
@@ -1,7 +1,15 @@
+--# inherit
+--#    Crypt;
 package Crypt.Sender
+--# own
+--#    out State;
 is
 
    --  Copies the given response message into the crypter response page.
-   procedure Send (Res : Message_Type);
+   procedure Send (Res : Crypt.Message_Type);
+   --# global
+   --#    out State;
+   --# derives
+   --#    State from Res;
 
 end Crypt.Sender;

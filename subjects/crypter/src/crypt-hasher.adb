2e737493fff45877c53a65318c2aeedbe099677e
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-06-03 12:07:48 +0200

    Crypter: Rewrite Crypt.Hasher package in SPARK
    Simplify implementation and add SPARK annotations.

--- subjects/crypter/src/crypt-hasher.adb
+++ subjects/crypter/src/crypt-hasher.adb
@@ -1,66 +1,52 @@
 with Ada.Unchecked_Conversion;
 
-with LSC.SHA256;
-with LSC.Types;
-
 package body Crypt.Hasher
 is
 
-   use type LSC.SHA256.Message_Index;
-
    subtype SHA256_Message_Range is LSC.SHA256.Message_Index range
-     0 .. LSC.SHA256.Message_Index (Data_Range'Last) / 64 - 1;
+     0 .. LSC.SHA256.Message_Index (Crypt.Data_Range'Last) / 64 - 1;
    subtype SHA256_Message_Type is LSC.SHA256.Message_Type
      (SHA256_Message_Range);
 
    --  Convert crypter message data to SHA256 message.
    function To_SHA256_Msg is new Ada.Unchecked_Conversion
-     (Source => Data_Array,
+     (Source => Crypt.Data_Array,
       Target => SHA256_Message_Type);
-
-   --  Convert SHA256 hash type to data array.
-   procedure To_Data
-     (Input  :     LSC.SHA256.SHA256_Hash_Type;
-      Output : out Data_Array);
 
    -------------------------------------------------------------------------
 
    procedure SHA256_Hash
-     (Input  :     Message_Type;
-      Output : out Message_Type)
+     (Input  :     Crypt.Message_Type;
+      Output : out Crypt.Message_Type)
    is
+      LSC_Hash : LSC.SHA256.SHA256_Hash_Type;
    begin
+      --# accept Warning, 13, To_SHA256_Msg, "Message to SHA256 conversion";
+      LSC_Hash := LSC.SHA256.Hash
+        (Message => To_SHA256_Msg (S => Input.Data),
+         Length  => LSC.Types.Word64 (Input.Size * 8));
+      --# end accept;
+
+      Output      := Crypt.Null_Message;
       Output.Size := 32;
-
-      To_Data (Input  => LSC.SHA256.Hash
-               (Message => To_SHA256_Msg (S => Input.Data),
-                Length  => LSC.Types.Word64 (Input.Size * 8)),
-               Output => Output.Data);
-   end SHA256_Hash;
-
-   -------------------------------------------------------------------------
-
-   procedure To_Data
-     (Input  :     LSC.SHA256.SHA256_Hash_Type;
-      Output : out Data_Array)
-   is
-      use type LSC.SHA256.SHA256_Hash_Index;
-      use type LSC.Types.Word32;
-   begin
-      Output := Null_Data;
-
       for I in LSC.SHA256.SHA256_Hash_Index loop
-         Output (Data_Range (I) * 4 + 1) := SK.Byte'Mod (Input (I));
-         Output (Data_Range (I) * 4 + 2) := SK.Byte'Mod
-           (LSC.Types.SHR32 (Value  => Input (I),
+
+         --# accept Warning, 444, "LSC_Hash is an array of LSC.Types.Word32.";
+         --# assume LSC_Hash (I) in LSC.Types.Word32;
+         --# end accept;
+
+         Output.Data (Crypt.Data_Range (I * 4 + 1)) := SK.Byte'Mod
+           (LSC_Hash (I));
+         Output.Data (Crypt.Data_Range (I * 4 + 2)) := SK.Byte'Mod
+           (LSC.Types.SHR32 (Value  => LSC_Hash (I),
                              Amount => 8));
-         Output (Data_Range (I) * 4 + 3) := SK.Byte'Mod
-           (LSC.Types.SHR32 (Value  => Input (I),
+         Output.Data (Crypt.Data_Range (I * 4 + 3)) := SK.Byte'Mod
+           (LSC.Types.SHR32 (Value  => LSC_Hash (I),
                              Amount => 16));
-         Output (Data_Range (I) * 4 + 4) := SK.Byte'Mod
-           (LSC.Types.SHR32 (Value  => Input (I),
+         Output.Data (Crypt.Data_Range (I * 4 + 4)) := SK.Byte'Mod
+           (LSC.Types.SHR32 (Value  => LSC_Hash (I),
                              Amount => 24));
       end loop;
-   end To_Data;
+   end SHA256_Hash;
 
 end Crypt.Hasher;
--- subjects/crypter/src/crypt-hasher.ads
+++ subjects/crypter/src/crypt-hasher.ads
@@ -1,9 +1,22 @@
+with LSC.SHA256;
+with LSC.Types;
+
+use type LSC.SHA256.Message_Index;
+use type LSC.SHA256.SHA256_Hash_Index;
+use type LSC.Types.Word32;
+
+--# inherit
+--#    Ada,
+--#    SK,
+--#    LSC.SHA256,
+--#    LSC.Types,
+--#    Crypt;
 package Crypt.Hasher
 is
 
    --  Calculate SHA256 hash of given input message and store in output.
    procedure SHA256_Hash
-     (Input  :     Message_Type;
-      Output : out Message_Type);
+     (Input  :     Crypt.Message_Type;
+      Output : out Crypt.Message_Type);
 
 end Crypt.Hasher;

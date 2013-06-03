fec722e01cf9c7ffd743a805ed243d6eb702e56e
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-06-03 12:04:55 +0200

    Crypter: Make Handler package SPARK compliant

--- subjects/crypter/src/handler.adb
+++ subjects/crypter/src/handler.adb
@@ -1,3 +1,5 @@
+with Crypt.Text_IO;
+
 package body Handler
 is
 
@@ -6,7 +8,18 @@ is
    procedure Handle_Interrupt (Vector : SK.Byte)
    is
    begin
-      Requesting_Subject := Integer (Vector) - 32;
+      if Vector >= SK.Byte (Skp.Subject_Id_Type'First) + 32
+        and then Vector <= SK.Byte (Skp.Subject_Id_Type'Last) + 32
+      then
+         Requesting_Subject := Integer (Vector) - 32;
+      else
+         Requesting_Subject := Skp.Subject_Id_Type'First;
+      end if;
+
+      pragma Debug (Vector < 32,
+                    Crypt.Text_IO.Put_String ("Ignoring spurious interrupt "));
+      pragma Debug (Vector < 32, Crypt.Text_IO.Put_Byte (Item => Vector));
+      pragma Debug (Vector < 32, Crypt.Text_IO.New_Line);
    end Handle_Interrupt;
 
 end Handler;
--- subjects/crypter/src/handler.ads
+++ subjects/crypter/src/handler.ads
@@ -2,14 +2,27 @@ with Skp;
 
 with SK;
 
+use type SK.Byte;
+
+--# inherit
+--#    SK,
+--#    Skp;
 package Handler
+--# own
+--#    Requesting_Subject : Skp.Subject_Id_Type;
+--# initializes
+--#    Requesting_Subject;
 is
 
    --  Interrupt handler.
    procedure Handle_Interrupt (Vector : SK.Byte);
+   --# global
+   --#    out Requesting_Subject;
+   --# derives
+   --#    Requesting_Subject from Vector;
    pragma Export (C, Handle_Interrupt, "dispatch_interrupt");
 
-   Requesting_Subject : Skp.Subject_Id_Type;
+   Requesting_Subject : Skp.Subject_Id_Type := Skp.Subject_Id_Type'First;
    pragma Atomic (Requesting_Subject);
 
 end Handler;

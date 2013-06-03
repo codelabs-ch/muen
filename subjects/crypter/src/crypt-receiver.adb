f9881d180ee79b1a8c8a11e6f3080fea08e6996c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-06-03 12:05:39 +0200

    Crypter: Add SPARK annotations to Crypt.Receiver

--- subjects/crypter/src/crypt-receiver.adb
+++ subjects/crypter/src/crypt-receiver.adb
@@ -1,15 +1,21 @@
 with System;
 
 package body Crypt.Receiver
+--# own State is in Request;
 is
 
-   Request : Message_Type;
+   Request : Crypt.Message_Type;
    for Request'Address use System'To_Address (16#10000#);
    pragma Volatile (Request);
+   --# assert Request'Always_Valid;
 
    -------------------------------------------------------------------------
 
-   procedure Receive (Req : out Message_Type)
+   procedure Receive (Req : out Crypt.Message_Type)
+   --# global
+   --#    in Request;
+   --# derives
+   --#    Req from Request;
    is
    begin
       Req := Request;
--- subjects/crypter/src/crypt-receiver.ads
+++ subjects/crypter/src/crypt-receiver.ads
@@ -1,7 +1,15 @@
+--# inherit
+--#    Crypt;
 package Crypt.Receiver
+--# own
+--#    in State;
 is
 
    --  Fills the current crypter request into the given req parameter.
-   procedure Receive (Req : out Message_Type);
+   procedure Receive (Req : out Crypt.Message_Type);
+   --# global
+   --#    in State;
+   --# derives
+   --#    Req from State;
 
 end Crypt.Receiver;

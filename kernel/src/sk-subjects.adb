a749dd679e83d7c9f719a49073315700e6f9195e
Reto Buerki <reet@codelabs.ch>
2013-10-01 14:47:33 +0200

    Drop event list from Subjects package

--- kernel/src/sk-subjects.adb
+++ kernel/src/sk-subjects.adb
@@ -18,12 +18,9 @@
 
 with System;
 
-with SK.KC;
-with SK.Locks;
-
 package body SK.Subjects
 --# own
---#    State is Descriptors, Events;
+--#    State is Descriptors;
 is
 
    subtype Descriptor_Array is SK.Subject_State_Array (Skp.Subject_Id_Type);
@@ -33,49 +30,6 @@ is
    Descriptors : Descriptor_Array;
    for Descriptors'Address use System'To_Address (16#001fe000#);
    --# end accept;
-
-   --  Subject events.
-   type Subject_Events_Length is range 0 .. 32;
-   subtype Subject_Event_Index is Subject_Events_Length range 1 .. 32;
-   type Subject_Event_Array is array (Subject_Event_Index) of SK.Byte;
-
-   type Subject_Events_Type is record
-      Length : Subject_Events_Length;
-      Data   : Subject_Event_Array;
-   end record;
-
-   type Events_Array is array (Skp.Subject_Id_Type) of Subject_Events_Type;
-
-   Events : Events_Array;
-
-   -------------------------------------------------------------------------
-
-   procedure Get_Pending_Event
-     (Id    :     Skp.Subject_Id_Type;
-      Event : out SK.Byte)
-   --# global
-   --#    in out Events;
-   --#    in out Locks.State;
-   --# derives
-   --#    Events      from *, Id      &
-   --#    Event       from Events, Id &
-   --#    Locks.State from *;
-   --# post
-   --#    (Events~ (Id).Length not in Subject_Event_Index ->
-   --#       (Event = 0 and Events = Events~)) and
-   --#    (Events~ (Id).Length in     Subject_Event_Index ->
-   --#       (Event = Events (Id).Data (Events~ (Id).Length) and
-   --#        Events (Id).Length = Events~ (Id).Length - 1));
-   is
-   begin
-      Event := 0;
-      Locks.Spin_Lock;
-      if Events (Id).Length in Subject_Event_Index then
-         Event := Events (Id).Data (Events (Id).Length);
-         Events (Id).Length := Events (Id).Length - 1;
-      end if;
-      Locks.Unlock;
-   end Get_Pending_Event;
 
    -------------------------------------------------------------------------
 
@@ -88,18 +42,6 @@ is
    begin
       return Descriptors (Id);
    end Get_State;
-
-   -------------------------------------------------------------------------
-
-   function Has_Pending_Events (Id : Skp.Subject_Id_Type) return Boolean
-   --# global
-   --#    Events;
-   --# return
-   --#    Events (Id).Length > Subject_Events_Length'First;
-   is
-   begin
-      return Events (Id).Length > Subject_Events_Length'First;
-   end Has_Pending_Events;
 
    -------------------------------------------------------------------------
 
@@ -149,45 +91,7 @@ is
       Descriptors (Id) := Subject_State;
    end Set_State;
 
-   -------------------------------------------------------------------------
-
-   procedure Set_Pending_Event
-     (Id     : Skp.Subject_Id_Type;
-      Vector : SK.Byte)
-   --# global
-   --#    in out Events;
-   --#    in out Locks.State;
-   --# derives
-   --#    Events      from *, Id, Vector &
-   --#    Locks.State from *;
-   --# post
-   --#    (Events~ (Id).Length < Subject_Event_Index'Last ->
-   --#       (Events (Id).Length = Events~ (Id).Length + 1 and
-   --#        Events (Id).Data (Events (Id).Length) = Vector)) and
-   --#    (Events~ (Id).Length = Subject_Event_Index'Last -> Events = Events~);
-   is
-      Length : Subject_Events_Length;
-   begin
-      Locks.Spin_Lock;
-      Length := Events (Id).Length;
-      if Length < Subject_Event_Index'Last then
-         Events (Id).Length := Events (Id).Length + 1;
-         Events (Id).Data (Events (Id).Length) := Vector;
-      end if;
-      Locks.Unlock;
-      pragma Debug (Length = Subject_Event_Index'Last,
-                    KC.Put_String (Item => "Subject "));
-      pragma Debug (Length = Subject_Event_Index'Last,
-                    KC.Put_Byte (Item => SK.Byte (Id)));
-      pragma Debug (Length = Subject_Event_Index'Last,
-                    KC.Put_Line (Item => " - Event queue overflow"));
-   end Set_Pending_Event;
-
 begin
-   Events      := Events_Array'
-     (others => Subject_Events_Type'
-        (Length => Subject_Events_Length'First,
-         Data   => Subject_Event_Array'(others => 0)));
    Descriptors := Descriptor_Array'
      (others => SK.Subject_State_Type'
         (SK.Null_Subject_State));
--- kernel/src/sk-subjects.ads
+++ kernel/src/sk-subjects.ads
@@ -20,8 +20,7 @@ with Skp;
 
 --# inherit
 --#    Skp,
---#    SK.CPU,
---#    SK.Locks;
+--#    SK.CPU;
 package SK.Subjects
 --# own
 --#    State;
@@ -42,35 +41,6 @@ is
    --#    State;
    --# derives
    --#    State from *, Id, Subject_State;
-
-   --  Return True if the subject identified by id has events pending.
-   function Has_Pending_Events (Id : Skp.Subject_Id_Type) return Boolean;
-   --# global
-   --#    State;
-
-   --  Set pending event of subject specified by id to given vector.
-   procedure Set_Pending_Event
-     (Id     : Skp.Subject_Id_Type;
-      Vector : SK.Byte);
-   --# global
-   --#    in out State;
-   --#    in out Locks.State;
-   --# derives
-   --#    State       from *, Id, Vector &
-   --#    Locks.State from *;
-
-   --  Return pending event of subject identified by id. If no event is
-   --  pending, zero is returned.
-   procedure Get_Pending_Event
-     (Id    :     Skp.Subject_Id_Type;
-      Event : out SK.Byte);
-   --# global
-   --#    in out State;
-   --#    in out Locks.State;
-   --# derives
-   --#    State       from *, Id     &
-   --#    Event       from State, Id &
-   --#    Locks.State from *;
 
    --  Set RIP of subject specified by id to given value.
    procedure Set_RIP

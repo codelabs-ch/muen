33259f4ec3b45711cdbc5b3aeec5ca9a82bf7f52
Reto Buerki <reet@codelabs.ch>
2013-10-10 16:55:13 +0200

    SM: Implement minimal CPUID emulation
    The code is inspired by the emulation done by the lguest hypervisor in
    the Linux kernel (see arch/x86/lguest/boot.c, lguest_cpuid function).

--- subjects/sm/src/sm.adb
+++ subjects/sm/src/sm.adb
@@ -32,8 +32,9 @@ procedure Sm
 is
    use type SK.Word64;
 
-   Id    : Skp.Subject_Id_Type;
-   State : SK.Subject_State_Type;
+   Id            : Skp.Subject_Id_Type;
+   Dump_And_Halt : Boolean := False;
+   State         : SK.Subject_State_Type;
    for State'Address use System'To_Address (16#1e0000#);
 begin
    Subject.Text_IO.Init;
@@ -46,17 +47,57 @@ begin
    loop
       Id := Handler.Current_Subject;
 
-      if State.Exit_Reason = 30 then
-         Subject.Text_IO.Put_String (Item => "Subject ");
-         Subject.Text_IO.Put_Byte   (Item => SK.Byte (Id));
-         Subject.Text_IO.Put_String (Item => ": I/O instruction on port ");
+      if State.Exit_Reason = 10 then
+
+         --  Minimal CPUID emulation. Code inspired by the emulation done by
+         --  the lguest hypervisor in the Linux kernel
+         --  (see arch/x86/lguest/boot.c, lguest_cpuid function).
+         --  For reference values see e.g.
+         --  http://www.cpu-world.com/cgi-bin/CPUID.pl?CPUID=26937&RAW_DATA=1
+
+         case State.Regs.RAX is
+            when 0 =>
+
+               --  Get vendor ID.
+
+               --  Return the vendor ID for a GenuineIntel processor and set
+               --  the highest valid CPUID number to 5.
+
+               State.Regs.RAX := 5;
+               State.Regs.RBX := 16#756e_6547#;
+               State.Regs.RCX := 16#6c65_746e#;
+               State.Regs.RDX := 16#4965_6e69#;
+            when 1 =>
+
+               --  Processor Info and Feature Bits.
+               --  We currently provide no features.
+
+               State.Regs.RDX := 0;
+            when 16#8000_0000# =>
+
+               --  Get Highest Extended Function Supported.
+               --  Disabled for now.
+
+               State.Regs.RAX := 16#8000_0000#;
+            when others =>
+               Subject.Text_IO.Put_String (Item => "Unknown CPUID function ");
+               Subject.Text_IO.Put_Word64 (Item => State.Regs.RAX);
+               Subject.Text_IO.New_Line;
+               Dump_And_Halt := True;
+         end case;
+
+         if not Dump_And_Halt then
+            State.RIP := State.RIP + State.Instruction_Len;
+            SK.Hypercall.Trigger_Event (Number => SK.Byte (Id));
+         end if;
+
+      elsif State.Exit_Reason = 30 then
+         Subject.Text_IO.Put_String (Item => "I/O instruction on port ");
          Subject.Text_IO.Put_Word16
            (Item => SK.Word16 (State.Exit_Qualification / 2 ** 16));
          Subject.Text_IO.New_Line;
-         State.RIP := State.RIP + State.Instruction_Len;
-         SK.Hypercall.Trigger_Event (Number => SK.Byte (Id));
+         Dump_And_Halt := True;
       else
-         Subject.Text_IO.New_Line;
          Subject.Text_IO.Put_String (Item => "Unhandled trap for subject ");
          Subject.Text_IO.Put_Byte   (Item => SK.Byte (Id));
          Subject.Text_IO.Put_String (Item => " EXIT (");
@@ -68,6 +109,10 @@ begin
          Subject.Text_IO.Put_Word32 (Item => SK.Word32 (State.Interrupt_Info));
          Subject.Text_IO.Put_Line   (Item => ")");
 
+         Dump_And_Halt := True;
+      end if;
+
+      if Dump_And_Halt then
          Subject.Text_IO.Put_String ("EIP: ");
          Subject.Text_IO.Put_Word32 (Item => SK.Word32 (State.RIP));
          Subject.Text_IO.Put_String (" CS : ");

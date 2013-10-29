509795180d3ec48d9cc9f75418a37cb0a3cc4960
Nico Huber <nico.huber@secunet.com>
2013-10-29 15:12:05 +0100

    Subjects: Add a simple `time` counting subject
    Current implementations uses port 80 writes to delay for about 1ms. It
    should be replaced by a real clock, some time... But it serves for a
    proof of concept.
    
    A constant rate TSC would be adequate if you know the rate.

---
+++ subjects/time/src/time.adb
@@ -0,0 +1,41 @@
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with System;
+
+with SK.IO;
+
+with Subject.Text_IO;
+
+procedure Time
+is
+   --  Time page
+   Time : SK.Word64;
+   for Time'Address use System'To_Address (16#20000#);
+   pragma Volatile (Time);
+
+   use type SK.Word64;
+begin
+   Subject.Text_IO.Init;
+   Subject.Text_IO.Put_Line (Item => "Time subject running");
+
+   loop
+      for J in 1 .. 998 loop
+         SK.IO.Outb (Port  => 16#80#,
+                     Value => 16#00#);
+      end loop;
+      Time := Time + 1;
+   end loop;
+end Time;

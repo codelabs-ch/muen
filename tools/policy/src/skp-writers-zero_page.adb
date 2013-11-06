d32d1b7f5db0523e41a5eec0c72abd6ccf23eb7e
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2013-11-06 17:34:58 +0100

    Linux: Map HVC -> VT channel

--- tools/policy/src/skp-writers-zero_page.adb
+++ tools/policy/src/skp-writers-zero_page.adb
@@ -54,12 +54,12 @@ is
                 N => bootparam_h.boot_params'Object_Size / 8);
 
       Params.e820_entries := 4;
-      --  Zero page, Time page
+      --  Zero page, Time page, HVC channel
       Params.e820_map (0) := (addr   => 16#000000#,
-                              size   => 16#002000#,
+                              size   => 16#003000#,
                               c_type => E820_RESERVED);
       --  Usable lower memory
-      Params.e820_map (1) := (addr   => 16#002000#,
+      Params.e820_map (1) := (addr   => 16#003000#,
                               size   => 16#09e000#,
                               c_type => E820_RAM);
       --  VGA memory, OPROMs, BIOS extension (ACPI tables), System BIOS

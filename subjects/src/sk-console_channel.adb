ce1f0a67d71a2ce106b8211626a35819d0ca2795
Reto Buerki <reet@codelabs.ch>
2013-11-15 17:40:30 +0100

    Implement Channel_Console
    This console implementation writes characters into a muchannel for
    consumption by a terminal emulator.

---
+++ subjects/src/sk-console_channel.adb
@@ -0,0 +1,56 @@
+--
+--  Copyright (C) 2013  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2013  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with VT_Channels;
+
+package body SK.Console_Channel
+is
+
+   use VT_Channels;
+
+   Channel : VT_Channel.Channel_Type;
+   for Channel'Address use Channel_Address;
+
+   -------------------------------------------------------------------------
+
+   procedure Init
+   is
+   begin
+      VT_Channel_Wtr.Initialize (Channel => Channel,
+                                 Epoch   => 1);
+   end Init;
+
+   -------------------------------------------------------------------------
+
+   procedure New_Line
+   is
+   begin
+      VT_Channel_Wtr.Write (Channel => Channel,
+                            Element => ASCII.LF);
+   end New_Line;
+
+   -------------------------------------------------------------------------
+
+   procedure Put_Char (Item : Character)
+   is
+   begin
+      VT_Channel_Wtr.Write (Channel => Channel,
+                            Element => Item);
+   end Put_Char;
+
+end SK.Console_Channel;

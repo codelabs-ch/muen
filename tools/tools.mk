8fbcc24bd9a16da5c67a9a5db2db2c126fc673a4
Reto Buerki <reet@codelabs.ch>
2014-01-06 18:41:42 +0100

    Add test targets to tools.mk

--- tools/tools.mk
+++ tools/tools.mk
@@ -2,8 +2,14 @@ include ../../Makeconf
 
 all: $(TOOL)
 
+tests: test_$(TOOL)
+	@obj/tests/test_runner
+
 $(TOOL):
 	@gprbuild $(BUILD_OPTS) -P$@
+
+test_$(TOOL):
+	@gprbuild $(BUILD_OPTS) -P$@ -XBUILD=tests
 
 clean:
 	@rm -rf bin obj

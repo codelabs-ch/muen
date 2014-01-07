78f24a38893b255434993ad19596da7da93c6a36
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-01-07 17:16:53 +0100

    mumsrbmgen: Add skeleton for MSR bitmap generator
    The mumsrbmgen tool generates model-specific register (MSR) bitmap files
    according to a given system policy.

---
+++ tools/mumsrbmgen/src/mumsrbmgen.adb
@@ -0,0 +1,51 @@
+--
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Ada.Command_Line;
+with Ada.Exceptions;
+
+with Mugen.Command_Line;
+
+with Mulog;
+with Muxml;
+
+with Msrbm;
+
+procedure Mumsrbmgen
+is
+begin
+   Mugen.Command_Line.Init
+     (Description => "Generate MSR bitmaps according to given system policy");
+
+   Msrbm.Process (Policy     => Mugen.Command_Line.Get_Policy,
+                  Output_Dir => Mugen.Command_Line.Get_Output_Dir);
+
+exception
+   when E : Muxml.Processing_Error =>
+      Mulog.Log (Level => Mulog.Error,
+                 Msg   => "Processing failed, aborting");
+      Mulog.Log (Level => Mulog.Error,
+                 Msg   => Ada.Exceptions.Exception_Message (X => E));
+      Ada.Command_Line.Set_Exit_Status (Code => Ada.Command_Line.Failure);
+   when E : others =>
+      Mulog.Log (Level => Mulog.Error,
+                 Msg   => "Unexpected exception");
+      Mulog.Log (Level => Mulog.Error,
+                 Msg   => Ada.Exceptions.Exception_Information (X => E));
+      Ada.Command_Line.Set_Exit_Status (Code => Ada.Command_Line.Failure);
+end Mumsrbmgen;

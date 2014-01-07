b29f8e055f1e1794b7eacaecce8d8964943a671d
Reto Buerki <reet@codelabs.ch>
2014-01-07 21:12:57 +0100

    libmugen: Add Mugen.Generators package
    The Generators.Run procedure uses the Command_Line package to read the
    output directory and the path of the policy file. It then parses the
    policy and passes the output directory and XML data on to the given
    generator procedure.
    
    This can be used by the generator tools to easily run their own
    generators.

--- tools/libmugen/src/mugen-generators.ads
+++ tools/libmugen/src/mugen-generators.ads
@@ -1,6 +1,6 @@
 --
---  Copyright (C) 2013  Reto Buerki <reet@codelabs.ch>
---  Copyright (C) 2013  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
@@ -16,13 +16,18 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-with "../libmulog/libmulog";
-with "../libmuxml/libmuxml";
+with Muxml;
 
-library project Libmugen is
-   for Source_Dirs use ("src");
-   for Object_Dir use "obj";
-   for Library_Dir use "lib";
-   for Library_Name use "mugen";
-   for Library_Kind use "static";
-end Libmugen;
+package Mugen.Generators
+is
+
+   --  The Run procedure uses the Command_Line package to read the output
+   --  directory and the path of the policy file. It then parses the policy and
+   --  passes the output directory and XML data on to the given generator
+   --  procedure.
+   procedure Run
+     (Process : not null access procedure
+        (Output_Dir : String;
+         Policy     : Muxml.XML_Data_Type));
+
+end Mugen.Generators;

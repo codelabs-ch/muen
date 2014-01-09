07486b0f2db9969ce7667623cb5143dafddba373
Reto Buerki <reet@codelabs.ch>
2014-01-09 17:35:33 +0100

    mupack: Add tool-specific command line package

---
+++ tools/mupack/src/pack-command_line.ads
@@ -0,0 +1,37 @@
+--
+--  Copyright (C) 2013, 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2013, 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+package Pack.Command_Line
+is
+
+   --  Init command line, use given tool description in usage output.
+   procedure Init (Description : String);
+
+   --  Return policy filename.
+   function Get_Policy return String;
+
+   --  Return output directory.
+   function Get_Output_Dir return String;
+
+   --  Retun input directory.
+   function Get_Input_Dir return String;
+
+   --  Return kernel filename.
+   function Get_Kernel_Filename return String;
+
+end Pack.Command_Line;

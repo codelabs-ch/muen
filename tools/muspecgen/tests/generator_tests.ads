da56870cdaf3436312d9afe71402f6706638a783
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-01-09 11:46:18 +0100

    muspecgen: Add test for scheduling spec generation
    The reference data is copied from the existing skpolicy test suite.

--- tools/muspecgen/tests/generator_tests.ads
+++ tools/muspecgen/tests/generator_tests.ads
@@ -16,24 +16,17 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-with Ahven.Text_Runner;
 with Ahven.Framework;
 
-with Templates_Tests;
-with Generator_Tests;
-
-procedure Test_Runner
+package Generator_Tests
 is
-   use Ahven.Framework;
 
-   S : constant Test_Suite_Access := Create_Suite
-     (Suite_Name => "Muspecgen tests");
-begin
-   Add_Test (Suite => S.all,
-             T     => new Generator_Tests.Testcase);
-   Add_Test (Suite => S.all,
-             T     => new Templates_Tests.Testcase);
+   type Testcase is new Ahven.Framework.Test_Case with null record;
 
-   Ahven.Text_Runner.Run (Suite => S);
-   Release_Suite (T => S);
-end Test_Runner;
+   --  Initialize testcase.
+   procedure Initialize (T : in out Testcase);
+
+   --  Write SPARK spec and C header files.
+   procedure Write_Specs;
+
+end Generator_Tests;

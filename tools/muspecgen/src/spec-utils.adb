310d54002361c3e870afc0a1ba302fc4940297b9
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-01-13 14:32:34 +0100

    muspecgen: Add Spec.Utils package
    The package contains a generic function which computes the numeric value
    for a given list of bitfield nodes. The generic must be instantiated
    with an enumeration type encompassing all fields and an array mapping
    the bitfields to their position.
    
    This can be used to convert, e.g. Pin-Base VM-Execution controls to their
    numeric representation.

---
+++ tools/muspecgen/src/spec-utils.adb
@@ -0,0 +1,49 @@
+--
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with DOM.Core.Nodes;
+
+package body Spec.Utils
+is
+
+   -------------------------------------------------------------------------
+
+   function To_Number (Fields : DOM.Core.Node_List) return Long_Long_Integer
+   is
+      Result : Long_Long_Integer := 0;
+   begin
+      for I in 0 .. DOM.Core.Nodes.Length (List => Fields) - 1 loop
+         declare
+            Flag_Node : constant DOM.Core.Node := DOM.Core.Nodes.Item
+              (List  => Fields,
+               Index => I);
+            Flag_Name : constant Bitfield_Type := Bitfield_Type'Value
+              (DOM.Core.Nodes.Node_Name (N => Flag_Node));
+            Flag_Value : constant String       := DOM.Core.Nodes.Node_Value
+              (N => DOM.Core.Nodes.First_Child (N => Flag_Node));
+         begin
+            if Flag_Value = "1" then
+               Result := Result + 2 ** Map (Flag_Name);
+            end if;
+         end;
+      end loop;
+
+      return Result;
+   end To_Number;
+
+end Spec.Utils;
---
+++ tools/muspecgen/src/spec-utils.ads
@@ -0,0 +1,35 @@
+--
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with DOM.Core;
+
+package Spec.Utils
+is
+
+   --  Converts a given list of bitfields to their corresponding numeric value.
+   generic
+      --  Enumration type specifying all bitfields.
+      type Bitfield_Type is (<>);
+
+      type Mapping_Type is array (Bitfield_Type) of Natural;
+
+      --  Mapping of fields to bit position.
+      Map : Mapping_Type;
+   function To_Number (Fields : DOM.Core.Node_List) return Long_Long_Integer;
+
+end Spec.Utils;

cf37d38c5d9f76f44dab5dfca100102d6566b2bb
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-01-23 13:54:05 +0100

    libpaging: Add test for table iteration

---
+++ tools/libpaging/tests/tables_tests.adb
@@ -0,0 +1,85 @@
+--
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Paging.Entries;
+with Paging.Tables;
+
+package body Tables_Tests
+is
+
+   use Ahven;
+   use Paging;
+   use Paging.Tables;
+
+   -------------------------------------------------------------------------
+
+   procedure Initialize (T : in out Testcase)
+   is
+   begin
+      T.Set_Name (Name => "Pagetable tests");
+      T.Add_Test_Routine
+        (Routine => Iteration'Access,
+         Name    => "Table iteration");
+   end Initialize;
+
+   -------------------------------------------------------------------------
+
+   procedure Iteration
+   is
+      Ref_Entry : constant Entries.PT_Entry_Type := Entries.Create
+        (Dst_Offset  => 12,
+         Dst_Address => 16#deadbeef0000#,
+         Readable    => True,
+         Writable    => True,
+         Executable  => False,
+         Maps_Page   => True,
+         Global      => False,
+         Caching     => Paging.UC);
+
+      Table   : PT.Page_Table_Type;
+      Counter : Natural := 0;
+
+      --  Increment counter.
+      procedure Inc_Counter (TEntry : Entries.PT_Entry_Type);
+
+      ----------------------------------------------------------------------
+
+      procedure Inc_Counter (TEntry : Entries.PT_Entry_Type)
+      is
+         use type Entries.PT_Entry_Type;
+      begin
+         Assert (Condition => TEntry = Ref_Entry,
+                 Message   => "Table entry mismatch");
+         Counter := Counter + 1;
+      end Inc_Counter;
+   begin
+      PT.Add_Entry (Table => Table,
+                    Index => 12,
+                    E     => Ref_Entry);
+      PT.Add_Entry (Table => Table,
+                    Index => 13,
+                    E     => Ref_Entry);
+
+      PT.Iterate (Table   => Table,
+                  Process => Inc_Counter'Access);
+
+      Assert (Condition => Counter = 2,
+              Message   => "Iteration counter mismatch");
+   end Iteration;
+
+end Tables_Tests;

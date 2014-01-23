8a4eaff7be82a200f59e4850d2310f0761691930
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-01-23 15:30:45 +0100

    Add table handling procedures to Paging.Memory body
    These convenience procedures simplify the addition of new PDPT, PD or PT
    entries to specific structure tables identified by number.

--- tools/libpaging/src/paging-memory.adb
+++ tools/libpaging/src/paging-memory.adb
@@ -16,8 +16,169 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+with Paging.Entries;
+
 package body Paging.Memory
 is
+
+   --  Add entry with given index to page directory specified by number.
+   procedure Add_PD_Entry
+     (Mem_Layout  : in out Memory_Layout_Type;
+      Table_Nr    :        Table_Range;
+      Entry_Index :        Table_Range;
+      TEntry      :        Entries.PD_Entry_Type);
+
+   --  Add entry with given index to PDP table specified by number.
+   procedure Add_PDPT_Entry
+     (Mem_Layout  : in out Memory_Layout_Type;
+      Table_Nr    :        Table_Range;
+      Entry_Index :        Table_Range;
+      TEntry      :        Entries.PDPT_Entry_Type);
+
+   --  Add entry with given index to page table specified by table number.
+   procedure Add_PT_Entry
+     (Mem_Layout  : in out Memory_Layout_Type;
+      Table_Nr    :        Table_Range;
+      Entry_Index :        Table_Range;
+      TEntry      :        Entries.PT_Entry_Type);
+
+   ----------------------------------------------------------------------
+
+   procedure Add_PD_Entry
+     (Mem_Layout  : in out Memory_Layout_Type;
+      Table_Nr    :        Table_Range;
+      Entry_Index :        Table_Range;
+      TEntry      :        Entries.PD_Entry_Type)
+   is
+      use type PD_Map_Package.Cursor;
+
+      --  Add entry to given table.
+      procedure Add_Entry
+        (Number :        Table_Range;
+         Table  : in out Tables.PD.Page_Table_Type);
+
+      ----------------------------------------------------------------------
+
+      procedure Add_Entry
+        (Number :        Table_Range;
+         Table  : in out Tables.PD.Page_Table_Type)
+      is
+         pragma Unreferenced (Number);
+      begin
+         Tables.PD.Add_Entry
+           (Table => Table,
+            Index => Entry_Index,
+            E     => TEntry);
+      end Add_Entry;
+
+      Pos : PD_Map_Package.Cursor := Mem_Layout.PDs.Find (Key => Table_Nr);
+      Ins : Boolean;
+   begin
+
+      if Pos = PD_Map_Package.No_Element then
+         Mem_Layout.PDs.Insert
+           (Key      => Table_Nr,
+            New_Item => Tables.PD.Create_Table (Number => Table_Nr),
+            Position => Pos,
+            Inserted => Ins);
+      end if;
+
+      Mem_Layout.PDs.Update_Element
+        (Position => Pos,
+         Process  => Add_Entry'Access);
+   end Add_PD_Entry;
+
+   -------------------------------------------------------------------------
+
+   procedure Add_PDPT_Entry
+     (Mem_Layout  : in out Memory_Layout_Type;
+      Table_Nr    :        Table_Range;
+      Entry_Index :        Table_Range;
+      TEntry      :        Entries.PDPT_Entry_Type)
+   is
+      use type PDPT_Map_Package.Cursor;
+
+      --  Add entry to given table.
+      procedure Add_Entry
+        (Number :        Table_Range;
+         Table  : in out Tables.PDPT.Page_Table_Type);
+
+      ----------------------------------------------------------------------
+
+      procedure Add_Entry
+        (Number :        Table_Range;
+         Table  : in out Tables.PDPT.Page_Table_Type)
+      is
+         pragma Unreferenced (Number);
+      begin
+         Tables.PDPT.Add_Entry
+           (Table => Table,
+            Index => Entry_Index,
+            E     => TEntry);
+      end Add_Entry;
+
+      Pos : PDPT_Map_Package.Cursor := Mem_Layout.PDPTs.Find (Key => Table_Nr);
+      Ins : Boolean;
+   begin
+
+      if Pos = PDPT_Map_Package.No_Element then
+         Mem_Layout.PDPTs.Insert
+           (Key      => Table_Nr,
+            New_Item => Tables.PDPT.Create_Table (Number => Table_Nr),
+            Position => Pos,
+            Inserted => Ins);
+      end if;
+
+      Mem_Layout.PDPTs.Update_Element
+        (Position => Pos,
+         Process  => Add_Entry'Access);
+   end Add_PDPT_Entry;
+
+   -------------------------------------------------------------------------
+
+   procedure Add_PT_Entry
+     (Mem_Layout  : in out Memory_Layout_Type;
+      Table_Nr    :        Table_Range;
+      Entry_Index :        Table_Range;
+      TEntry      :        Entries.PT_Entry_Type)
+   is
+      use type PT_Map_Package.Cursor;
+
+      --  Add entry to given table.
+      procedure Add_Entry
+        (Number :        Table_Range;
+         Table  : in out Tables.PT.Page_Table_Type);
+
+      ----------------------------------------------------------------------
+
+      procedure Add_Entry
+        (Number :        Table_Range;
+         Table  : in out Tables.PT.Page_Table_Type)
+      is
+         pragma Unreferenced (Number);
+      begin
+         Tables.PT.Add_Entry
+           (Table => Table,
+            Index => Entry_Index,
+            E     => TEntry);
+      end Add_Entry;
+
+      Pos : PT_Map_Package.Cursor := Mem_Layout.PTs.Find (Key => Table_Nr);
+      Ins : Boolean;
+   begin
+
+      if Pos = PT_Map_Package.No_Element then
+         Mem_Layout.PTs.Insert
+           (Key      => Table_Nr,
+            New_Item => Tables.PT.Create_Table (Number => Table_Nr),
+            Position => Pos,
+            Inserted => Ins);
+      end if;
+
+      Mem_Layout.PTs.Update_Element
+        (Position => Pos,
+         Process  => Add_Entry'Access);
+   end Add_PT_Entry;
 
    -------------------------------------------------------------------------
 

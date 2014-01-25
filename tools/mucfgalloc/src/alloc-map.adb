436284d000a59de74cc18ff64509d7aaec779edf
Alexander Senier <mail@senier.net>
2014-01-25 15:32:55 +0100

    Implement adjacent empty region merging

--- tools/mucfgalloc/src/alloc-map.adb
+++ tools/mucfgalloc/src/alloc-map.adb
@@ -24,6 +24,22 @@ is
    is
       use Region_List_Package;
       Curr, Prev : Cursor;
+
+      --  Update the First_Address field of an region with First_Address
+      procedure Set_First_Address (Element : in out Region_Type);
+      procedure Set_First_Address (Element : in out Region_Type)
+      is
+      begin
+         Element.First_Address := First_Address;
+      end Set_First_Address;
+
+      --  Update the Last_Address field of a region with Last_Address
+      procedure Set_Last_Address (Element : in out Region_Type);
+      procedure Set_Last_Address (Element : in out Region_Type)
+      is
+      begin
+         Element.Last_Address := Last_Address;
+      end Set_Last_Address;
    begin
       Curr := First (Map.Data);
       while Curr /= No_Element and then
@@ -38,19 +54,52 @@ is
              Last_Address < Element (Curr).First_Address);
       end loop;
 
+      --  Overlap check
       if Curr /= No_Element and then
          Element (Curr).First_Address <= Last_Address
       then
          raise Overlapping_Empty_Region;
       end if;
 
-      Insert
-         (Container => Map.Data,
-          Before    => Curr,
-          New_Item  => Region_Type'
-                        (Kind          => Empty,
-                         First_Address => First_Address,
-                         Last_Address  => Last_Address));
+      --  Check for adjacent areas and merge them. There can be 4 scenarios:
+      --
+      --    (1) Last_Address + 1 = Curr.First_Address and
+      --        Prev.Last_Address + 1 = First_Address =>
+      --        Prev.Last_Address := Curr.Last_Address, Remove Curr.
+      --    (2) Last_Address + 1 = Curr.First_Address =>
+      --        Curr.First_Address := First_Address
+      --    (3) Prev.Last_Address + 1 = First_Address =>
+      --        Prev.Last_Address := Last_Address
+      --    (4) Otherwise =>
+      --        Insert new element before Curr
+
+      declare
+         Adjacent_Right : constant Boolean := Curr /= No_Element and then
+            Last_Address + 1 = Element (Curr).First_Address;
+         Adjacent_Left : constant Boolean := Prev /= No_Element and then
+            Element (Prev).Last_Address + 1 = First_Address;
+      begin
+         if Adjacent_Right and Adjacent_Left then
+            --  (1)
+            Update_Element (Map.Data, Prev, Set_Last_Address'Access);
+            Delete (Map.Data, Curr);
+         elsif Adjacent_Right then
+            --  (2)
+            Update_Element (Map.Data, Curr, Set_First_Address'Access);
+         elsif Adjacent_Left then
+            --  (3)
+            Update_Element (Map.Data, Prev, Set_Last_Address'Access);
+         else
+            --  (4)
+            Insert
+               (Container => Map.Data,
+                Before    => Curr,
+                New_Item  => Region_Type'
+                              (Kind          => Empty,
+                               First_Address => First_Address,
+                               Last_Address  => Last_Address));
+         end if;
+      end;
 
    end Insert_Empty_Region;
 

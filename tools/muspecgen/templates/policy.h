437a2b60b0a1ca20820d7cda7b858db49276c06b
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-01-28 15:19:57 +0100

    Update PAT to closer match default PAT settings
    The Page Attribute Table (PAT) is reordered as follows:
    
       PAT0 -> WB
       PAT1 -> WT
       PAT2 -> WC
       PAT3 -> UC
       PAT4 -> WP
       PAT5 -> WT
       PAT6 -> UC-
       PAT7 -> UC
    
    The differences to the default setting, specified in Intel SDM Vol.
    3A, section 11.12.4, are PAT2 (WC instead of UC-) and PAT4 (WP instead
    of WB).

--- tools/muspecgen/templates/policy.h
+++ tools/muspecgen/templates/policy.h
@@ -5,5 +5,5 @@
 #define SUBJECT_COUNT __subj_count__
 #define VMXON_ADDRESS 0x__vmxon_addr__
 #define VMCS_ADDRESS  0x__vmcs_addr__
-#define PAT_HIGH      0x00000006
-#define PAT_LOW       0x05040100
+#define PAT_HIGH      0x00070405
+#define PAT_LOW       0x00010406

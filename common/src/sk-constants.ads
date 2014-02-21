dd6c3ccdf8843a24dffcf2b12c5d19dea4589703
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-02-21 14:24:57 +0100

    Add EPT violation exit reason constant

--- common/src/sk-constants.ads
+++ common/src/sk-constants.ads
@@ -185,6 +185,7 @@ is
    EXIT_REASON_IO_INSTRUCTION   : constant := 30;
    EXIT_REASON_RDMSR            : constant := 31;
    EXIT_REASON_WRMSR            : constant := 32;
+   EXIT_REASON_EPT_VIOLATION    : constant := 48;
    EXIT_REASON_TIMER_EXPIRY     : constant := 52;
 
    --  VMX exit reason flags

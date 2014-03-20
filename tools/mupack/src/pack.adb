e93bc6c21dec5a3509185cca1aa7694e9f08e8b1
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-03-20 11:46:08 +0100

    mupack: Switch to content providers for image creation

--- tools/mupack/src/pack.adb
+++ tools/mupack/src/pack.adb
@@ -16,69 +16,21 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-with Ada.Text_IO;
 with Ada.Streams;
 
 with Interfaces;
 
+with Muxml;
 with Mulog;
 with Mutools.Utils;
 
 with Pack.Image;
-with Pack.Parser;
 with Pack.Command_Line;
+with Pack.Content_Providers;
+with Pack.Utils;
 
 package body Pack
 is
-
-   --  Write memory map to given file.
-   procedure Print_Layout
-     (File    : String;
-      Entries : Parser.File_Array);
-
-   --  Initialize file paths.
-   procedure Set_Initial_Filepath (Files : in out Parser.File_Array);
-
-   -------------------------------------------------------------------------
-
-   procedure Print_Layout
-     (File    : String;
-      Entries : Parser.File_Array)
-   is
-      Fd : Ada.Text_IO.File_Type;
-   begin
-      Ada.Text_IO.Create (File => Fd,
-                          Mode => Ada.Text_IO.Out_File,
-                          Name => File);
-      Ada.Text_IO.Put_Line
-        (File => Fd,
-         Item => "[Name;PhysicalAddress;Offset;Size;Type  Path]");
-      for I in Entries'Range loop
-         Ada.Text_IO.Set_Col (File => Fd,
-                              To   => 1);
-         Ada.Text_IO.Put (File => Fd,
-                          Item => S (Entries (I).Mem_Name) & ";");
-         Ada.Text_IO.Put (File => Fd,
-                          Item => Mutools.Utils.To_Hex
-                            (Number => Entries (I).Address) & ";");
-         Ada.Text_IO.Put (File => Fd,
-                          Item => Mutools.Utils.To_Hex
-                            (Number => Entries (I).Offset) & ";");
-         Ada.Text_IO.Put (File => Fd,
-                          Item => Mutools.Utils.To_Hex
-                            (Number => Entries (I).Size) & ";");
-         Ada.Text_IO.Put (File => Fd,
-                          Item => Entries (I).Format'Img);
-         Ada.Text_IO.Set_Col (File => Fd,
-                              To   => 60);
-         Ada.Text_IO.Put
-           (File => Fd,
-            Item => S (Entries (I).Path) & "/" & S (Entries (I).Filename));
-         Ada.Text_IO.New_Line (Fd);
-      end loop;
-
-      Ada.Text_IO.Close (File => Fd);
-   end Print_Layout;
 
    -------------------------------------------------------------------------
 
@@ -87,58 +39,39 @@ is
       Out_Dir     : constant String := Command_Line.Get_Output_Dir;
       In_Dir      : constant String := Command_Line.Get_Input_Dir;
       Policy_File : constant String := Command_Line.Get_Policy;
-      Mmap        : constant String := Out_Dir & "/mmap";
+      Policy      : Muxml.XML_Data_Type;
    begin
       Mulog.Log (Msg => "Looking for input files in '" & In_Dir & "'");
       Mulog.Log (Msg => "Using output directory '" & Out_Dir & "'");
       Mulog.Log (Msg => "Processing policy '" & Policy_File & "'");
 
+      Muxml.Parse (Data => Policy,
+                   Kind => Muxml.Format_B,
+                   File => Policy_File);
+
+      Pack_Image :
       declare
-         Files : Parser.File_Array := Parser.Parse (Policy => Policy_File);
+         Size   : constant Interfaces.Unsigned_64
+           := Utils.Get_Image_Size (Policy => Policy);
+         Sysimg : constant String := Out_Dir & "/muen.img";
+         Data   : Content_Providers.Param_Type
+           (Ada.Streams.Stream_Element_Offset (Size));
       begin
-         Set_Initial_Filepath (Files => Files);
+         Data.Mmap_File := U (Out_Dir & "/mmap");
+         Data.XML_Doc   := Policy.Doc;
 
-         Mulog.Log (Msg => "Found" & Files'Length'Img & " file(s) to process");
+         Content_Providers.Register_All;
+         Mulog.Log (Msg => "Registered content provider(s)"
+                    & Content_Providers.Get_Count'Img);
 
-         Pack_Image :
-         declare
-            Size    : constant Interfaces.Unsigned_64
-              := Parser.Get_Image_Size (Files => Files);
-            Img     : Pack.Image.Image_Type
-              (End_Address => Ada.Streams.Stream_Element_Offset (Size));
-            Sysimg  : constant String := Out_Dir & "/muen.img";
-         begin
-            Mulog.Log (Msg => "Creating system image '" & Sysimg & "' of size "
-                       & Mutools.Utils.To_Hex (Number => Size) & " bytes");
+         Content_Providers.Run (Data => Data);
 
-            for I in Files'Range loop
-               Image.Add_File
-                 (Image   => Img,
-                  Path    => S (Files (I).Path) & "/" & S (Files (I).Filename),
-                  Address => Files (I).Address,
-                  Size    => Files (I).Size,
-                  Offset  => Files (I).Offset);
-            end loop;
-
-            Image.Write (Image    => Img,
-                         Filename => Sysimg);
-            Mulog.Log (Msg => "Successfully created system image '"
-                       & Sysimg & "'");
-            Print_Layout (File    => Mmap,
-                          Entries => Files);
-            Mulog.Log (Msg => "Memory map written to file '" & Mmap & "'");
-         end Pack_Image;
-      end;
+         Image.Write (Image    => Data.Image,
+                      Filename => Sysimg);
+         Mulog.Log (Msg => "Successfully created system image '" & Sysimg
+                    & "' of size " & Mutools.Utils.To_Hex (Number => Size)
+                    & " bytes");
+      end Pack_Image;
    end Run;
-
-   -------------------------------------------------------------------------
-
-   procedure Set_Initial_Filepath (Files : in out Parser.File_Array)
-   is
-   begin
-      for I in Files'Range loop
-         Files (I).Path := U (Command_Line.Get_Input_Dir);
-      end loop;
-   end Set_Initial_Filepath;
 
 end Pack;

4d82a97304110d4fd0a61935ce1173f3ce6a3b02
Reto Buerki <reet@codelabs.ch>
2014-03-27 17:07:55 +0100

    mugenpt: Switch to Cmd_Line.Infile_Outdir profile

--- tools/mugenpt/src/mugenpt.adb
+++ tools/mugenpt/src/mugenpt.adb
@@ -19,23 +19,21 @@
 with Ada.Command_Line;
 with Ada.Exceptions;
 
-with Mugen.Generators;
-with Mugen.Command_Line;
-
 with Mulog;
 with Muxml;
+with Mutools.Cmd_Line.Infile_Outdir;
 
 with Pt.Generator;
 
 procedure Mugenpt
 is
 begin
-   Mugen.Command_Line.Init
+   Mutools.Cmd_Line.Infile_Outdir.Init
      (Description => "Generate page table structures according to given " &
         "system policy");
-   Mugen.Generators.Run
-      (Kind    => Muxml.Format_B,
-       Process => Pt.Generator.Write'Access);
+   Mutools.Cmd_Line.Infile_Outdir.Run
+     (Kind    => Muxml.Format_B,
+      Process => Pt.Generator.Write'Access);
 
 exception
    when E : Muxml.Processing_Error =>

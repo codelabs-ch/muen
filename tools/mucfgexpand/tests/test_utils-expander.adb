8426404ad50afac9a1f2e4d4d4aa884390c4e0e2
Reto Buerki <reet@codelabs.ch>
2014-04-01 11:45:41 +0200

    Provide an Expander specific Test_Utils package
    The package provides a convenience procedure to easily test expanders.

---
+++ tools/mucfgexpand/tests/test_utils-expander.adb
@@ -0,0 +1,53 @@
+--
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Ada.Directories;
+
+with Ahven;
+
+package body Test_Utils.Expander
+is
+
+   -------------------------------------------------------------------------
+
+   procedure Run_Test
+     (Filename     : String;
+      Ref_Filename : String;
+      Expander     : not null access procedure
+        (Data : in out Muxml.XML_Data_Type))
+   is
+      Policy : Muxml.XML_Data_Type;
+   begin
+      Muxml.Parse (Data => Policy,
+                   Kind => Muxml.Format_Src,
+                   File => "data/test_policy.xml");
+      Expander (Data => Policy);
+
+      Muxml.Write (Data => Policy,
+                   Kind => Muxml.Format_Src,
+                   File => Filename);
+
+      Ahven.Assert (Condition => Test_Utils.Equal_Files
+                    (Filename1 => Filename,
+                     Filename2 => Ref_Filename),
+                    Message   => "Policy mismatch");
+
+      Ada.Directories.Delete_File (Name => Filename);
+   end Run_Test;
+
+end Test_Utils.Expander;

0eed9d5e5b4337a2095f6332d7d9ee9a32152e75
Reto Buerki <reet@codelabs.ch>
2014-04-03 21:56:44 +0200

    Add Expand.Post_Checks package
    The package is used to register and run post-expansion policy validation
    procedures. Currently, the Physical_Memory_Name_Uniqueness check
    provided by libmucfgcheck is registered.

---
+++ tools/mucfgexpand/src/expand-post_checks.ads
@@ -0,0 +1,33 @@
+--
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Muxml;
+
+package Expand.Post_Checks
+is
+
+   --  Register all post-checks.
+   procedure Register_All;
+
+   --  Run registered post-checks.
+   procedure Run (Data : Muxml.XML_Data_Type);
+
+   --  Return number of registered post-checks.
+   function Get_Count return Natural;
+
+end Expand.Post_Checks;

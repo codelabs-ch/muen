45740218844801d528627b38107ed55f4664150c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-04 12:36:49 +0200

    Implement subject binary mapping expander

--- tools/mucfgexpand/src/expanders-subjects.adb
+++ tools/mucfgexpand/src/expanders-subjects.adb
@@ -17,8 +17,11 @@
 --
 
 with DOM.Core.Nodes;
+with DOM.Core.Elements;
 
 with McKae.XML.XPath.XIA;
+
+with Mulog;
 
 with Expanders.XML_Utils;
 
@@ -40,9 +43,52 @@ is
               := DOM.Core.Nodes.Item
                 (List  => Nodes,
                  Index => I);
+            Filename : constant String
+              := DOM.Core.Elements.Get_Attribute
+                (Elem => Bin_Node,
+                 Name => "filename");
+            Filesize : constant String
+              := DOM.Core.Elements.Get_Attribute
+                (Elem => Bin_Node,
+                 Name => "size");
+            Virtual_Address : constant String
+              := DOM.Core.Elements.Get_Attribute
+                (Elem => Bin_Node,
+                 Name => "virtualAddress");
             Subj_Node : constant DOM.Core.Node
               := DOM.Core.Nodes.Parent_Node (N => Bin_Node);
+            Subj_Mem_Node : constant DOM.Core.Node := DOM.Core.Nodes.Item
+              (List  =>  McKae.XML.XPath.XIA.XPath_Query
+                 (N     => Subj_Node,
+                  XPath => "memory"),
+               Index => 0);
+            Subj_Name : constant String := DOM.Core.Elements.Get_Attribute
+              (Elem => Subj_Node,
+               Name => "name");
          begin
+            Mulog.Log (Msg => "Mapping binary '" & Filename & "' with size "
+                       & Filesize & " at virtual address " & Virtual_Address
+                       & " of subject '" & Subj_Name & "'");
+            XML_Utils.Add_Memory_Region
+              (Policy      => Data,
+               Name        => Subj_Name & "|bin",
+               Address     => "",
+               Size        => Filesize,
+               Caching     => "WB",
+               Alignment   => "16#1000#",
+               File_Name   => Filename,
+               File_Format => "bin_raw",
+               File_Offset => "none");
+            XML_Utils.Append_Child
+              (Node      => Subj_Mem_Node,
+               New_Child => XML_Utils.Create_Virtual_Memory_Node
+                 (Policy        => Data,
+                  Logical_Name  => "binary",
+                  Physical_Name => Subj_Name & "|bin",
+                  Address       => Virtual_Address,
+                  Writable      => True,
+                  Executable    => True));
+
             XML_Utils.Remove_Child
               (Node       => Subj_Node,
                Child_Name => "binary");

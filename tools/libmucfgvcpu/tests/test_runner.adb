e52595bcdc2b9c9bae59bd4c9d700aba9fa6bcee
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-04 16:30:46 +0200

    Add libmucfgvcpu library skeleton

---
+++ tools/libmucfgvcpu/tests/test_runner.adb
@@ -0,0 +1,31 @@
+--
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Ahven.Text_Runner;
+with Ahven.Framework;
+
+procedure Test_Runner
+is
+   use Ahven.Framework;
+
+   S : constant Test_Suite_Access := Create_Suite
+     (Suite_Name => "Libmucfgvcpu tests");
+begin
+   Ahven.Text_Runner.Run (Suite => S);
+   Release_Suite (T => S);
+end Test_Runner;

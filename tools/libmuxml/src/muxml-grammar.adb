4cb78fdcdc359d0d68b5a3960c22135656b8abef
Reto Buerki <reet@codelabs.ch>
2014-04-08 11:00:50 +0200

    Libmuxml: Simplify Get_Grammar function
    Use constant array to map schema kind to XSD, XSD_Id string constants.

--- tools/libmuxml/src/muxml-grammar.adb
+++ tools/libmuxml/src/muxml-grammar.adb
@@ -31,6 +31,20 @@ with Muxml.VCPU_Profile_Schema;
 package body Muxml.Grammar
 is
 
+   type Schema_Info_Type is record
+      Id, XSD : access constant String;
+   end record;
+
+   Schema_Map : constant array (Schema_Kind) of Schema_Info_Type
+     := (Format_Src   => (Id  => Format_src_Schema.XSD_Id'Access,
+                          XSD => Format_src_Schema.XSD'Access),
+         Format_A     => (Id  => Format_a_Schema.XSD_Id'Access,
+                          XSD => Format_a_Schema.XSD'Access),
+         Format_B     => (Id  => Format_b_Schema.XSD_Id'Access,
+                          XSD => Format_b_Schema.XSD'Access),
+         VCPU_Profile => (Id  => VCPU_Profile_Schema.XSD_Id'Access,
+                          XSD => VCPU_Profile_Schema.XSD'Access));
+
    -------------------------------------------------------------------------
 
    function Get_Grammar
@@ -39,35 +53,15 @@ is
    is
       use type Schema.Validators.XML_Grammar;
 
-      Reader    : Schema.Schema_Readers.Schema_Reader;
-      Str_Input : Input_Sources.Strings.String_Input;
+      Reader      : Schema.Schema_Readers.Schema_Reader;
+      Str_Input   : Input_Sources.Strings.String_Input;
+      Schema_Info : constant Schema_Info_Type := Schema_Map (Kind);
    begin
-      case Kind is
-         when Format_Src =>
-            Str_Input.Set_Public_Id (Id => Format_src_Schema.XSD_Id);
-            Input_Sources.Strings.Open
-              (Str      => Format_src_Schema.XSD,
-               Encoding => Unicode.CES.Utf8.Utf8_Encoding,
-               Input    => Str_Input);
-         when Format_A =>
-            Str_Input.Set_Public_Id (Id => Format_a_Schema.XSD_Id);
-            Input_Sources.Strings.Open
-              (Str      => Format_a_Schema.XSD,
-               Encoding => Unicode.CES.Utf8.Utf8_Encoding,
-               Input    => Str_Input);
-         when Format_B =>
-            Str_Input.Set_Public_Id (Id => Format_b_Schema.XSD_Id);
-            Input_Sources.Strings.Open
-              (Str      => Format_b_Schema.XSD,
-               Encoding => Unicode.CES.Utf8.Utf8_Encoding,
-               Input    => Str_Input);
-         when VCPU_Profile =>
-            Str_Input.Set_Public_Id (Id => VCPU_Profile_Schema.XSD_Id);
-            Input_Sources.Strings.Open
-              (Str      => VCPU_Profile_Schema.XSD,
-               Encoding => Unicode.CES.Utf8.Utf8_Encoding,
-               Input    => Str_Input);
-      end case;
+      Str_Input.Set_Public_Id (Id => Schema_Info.Id.all);
+      Input_Sources.Strings.Open
+        (Str      => Schema_Info.XSD,
+         Encoding => Unicode.CES.Utf8.Utf8_Encoding,
+         Input    => Str_Input);
 
       begin
          Schema.Schema_Readers.Parse

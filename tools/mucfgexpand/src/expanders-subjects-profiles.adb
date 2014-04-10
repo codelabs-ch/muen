c7a5395064f3f456c255641b4ae0457abcf68590
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-10 18:40:12 +0200

    Implement expansion of Linux ACPI FADT memory region

--- tools/mucfgexpand/src/expanders-subjects-profiles.adb
+++ tools/mucfgexpand/src/expanders-subjects-profiles.adb
@@ -108,6 +108,25 @@ is
             Address       => "16#000e_1000#",
             Writable      => False,
             Executable    => False));
+      XML_Utils.Add_Memory_Region
+        (Policy      => Data,
+         Name        => Subj_Name & "|acpi_fadt",
+         Address     => "",
+         Size        => "16#1000#",
+         Caching     => "WB",
+         Alignment   => "16#1000#",
+         File_Name   => Subj_Name & "_fadt",
+         File_Format => "acpi_fadt",
+         File_Offset => "none");
+      Muxml.Utils.Append_Child
+        (Node      => Subj_Mem_Node,
+         New_Child => XML_Utils.Create_Virtual_Memory_Node
+           (Policy        => Data,
+            Logical_Name  => "acpi_fadt",
+            Physical_Name => Subj_Name & "|acpi_fadt",
+            Address       => "16#000e_2000#",
+            Writable      => False,
+            Executable    => False));
    end Handle_Linux_Profile;
 
 end Expanders.Subjects.Profiles;

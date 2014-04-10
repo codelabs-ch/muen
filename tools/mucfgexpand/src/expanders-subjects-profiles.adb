30d498eb3a28aaa27bf17a791a3829c58f855263
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-10 18:36:45 +0200

    Implement expansion of Linux ACPI XSDT memory region

--- tools/mucfgexpand/src/expanders-subjects-profiles.adb
+++ tools/mucfgexpand/src/expanders-subjects-profiles.adb
@@ -89,6 +89,25 @@ is
             Address       => "16#000e_0000#",
             Writable      => False,
             Executable    => False));
+      XML_Utils.Add_Memory_Region
+        (Policy      => Data,
+         Name        => Subj_Name & "|acpi_xsdt",
+         Address     => "",
+         Size        => "16#1000#",
+         Caching     => "WB",
+         Alignment   => "16#1000#",
+         File_Name   => Subj_Name & "_xsdt",
+         File_Format => "acpi_xsdt",
+         File_Offset => "none");
+      Muxml.Utils.Append_Child
+        (Node      => Subj_Mem_Node,
+         New_Child => XML_Utils.Create_Virtual_Memory_Node
+           (Policy        => Data,
+            Logical_Name  => "acpi_xsdt",
+            Physical_Name => Subj_Name & "|acpi_xsdt",
+            Address       => "16#000e_1000#",
+            Writable      => False,
+            Executable    => False));
    end Handle_Linux_Profile;
 
 end Expanders.Subjects.Profiles;

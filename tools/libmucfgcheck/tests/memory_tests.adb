31f9189e215ff0c2bf68621465d389d482af6927
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-22 21:46:42 +0200

    Rework VMXON region size test

--- tools/libmucfgcheck/tests/memory_tests.adb
+++ tools/libmucfgcheck/tests/memory_tests.adb
@@ -892,16 +892,25 @@ is
    begin
       Muxml.Parse (Data => Data,
                    Kind => Muxml.Format_B,
-                   File => "data/validators.xml");
+                   File => "data/test_policy.xml");
 
+      declare
+         Node : constant DOM.Core.Node := Muxml.Utils.Get_Element
+           (Doc   => Data.Doc,
+            XPath => "/system/memory/memory[@name='kernel_0|vmxon']");
       begin
+         DOM.Core.Elements.Set_Attribute
+           (Elem  => Node,
+            Name  => "size",
+            Value => "16#0001_0012#");
+
          Mucfgcheck.Memory.VMXON_Region_Size (XML_Data => Data);
          Fail (Message => "Exception expected");
 
       exception
          when E : Mucfgcheck.Validation_Error =>
             Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
-                    = "Attribute 'size => 16#0001_0012#' of 'invalid_0|vmxon' "
+                    = "Attribute 'size => 16#0001_0012#' of 'kernel_0|vmxon' "
                     & "VMXON memory element not 4K",
                     Message   => "Exception mismatch");
       end;

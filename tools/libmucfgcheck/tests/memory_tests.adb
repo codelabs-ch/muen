32c5aaa12c412abfc0bd906a9db7c0c4d19a03c0
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-22 21:34:07 +0200

    Rework VMCS memory consecutiveness test

--- tools/libmucfgcheck/tests/memory_tests.adb
+++ tools/libmucfgcheck/tests/memory_tests.adb
@@ -669,16 +669,25 @@ is
    begin
       Muxml.Parse (Data => Data,
                    Kind => Muxml.Format_B,
-                   File => "data/validators.xml");
+                   File => "data/test_policy.xml");
 
+      declare
+         Node : constant DOM.Core.Node := Muxml.Utils.Get_Element
+           (Doc   => Data.Doc,
+            XPath => "/system/memory/memory[@name='linux|vmcs']");
       begin
+         DOM.Core.Elements.Set_Attribute
+           (Elem  => Node,
+            Name  => "physicalAddress",
+            Value => "16#a000#");
+
          Mucfgcheck.Memory.VMCS_Consecutiveness (XML_Data => Data);
          Fail (Message => "Exception expected");
 
       exception
          when E : Mucfgcheck.Validation_Error =>
             Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
-                    = "Memory region 'invalid|vmcs' not adjacent to other"
+                    = "Memory region 'linux|vmcs' not adjacent to other"
                     & " VMCS regions",
                     Message   => "Exception mismatch");
       end;

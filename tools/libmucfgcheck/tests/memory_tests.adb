0aa163badc9ec616f8d9e0eb370622e8d185a40a
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-22 18:28:10 +0200

    Rework memory entity name encoding test

--- tools/libmucfgcheck/tests/memory_tests.adb
+++ tools/libmucfgcheck/tests/memory_tests.adb
@@ -154,9 +154,18 @@ is
    begin
       Muxml.Parse (Data => Data,
                    Kind => Muxml.Format_B,
-                   File => "data/validators.xml");
+                   File => "data/test_policy.xml");
 
+      declare
+         Node : constant DOM.Core.Node := Muxml.Utils.Get_Element
+           (Doc   => Data.Doc,
+            XPath => "/system/memory/memory[@name='kernel_0|vmxon']");
       begin
+         DOM.Core.Elements.Set_Attribute
+           (Elem  => Node,
+            Name  => "name",
+            Value => "invalid_0|vmxon");
+
          Mucfgcheck.Memory.Entity_Name_Encoding (XML_Data => Data);
          Fail (Message => "Exception expected");
 

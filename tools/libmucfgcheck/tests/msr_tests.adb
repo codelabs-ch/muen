87c2adf5b24cdeb7a585f89931e982fd654057b8
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-22 21:51:27 +0200

    Rework MSR start larger than end test

--- tools/libmucfgcheck/tests/msr_tests.adb
+++ tools/libmucfgcheck/tests/msr_tests.adb
@@ -84,16 +84,26 @@ is
    begin
       Muxml.Parse (Data => Data,
                    Kind => Muxml.Format_B,
-                   File => "data/validators.xml");
+                   File => "data/test_policy.xml");
 
+      declare
+         Node : constant DOM.Core.Node := Muxml.Utils.Get_Element
+           (Doc   => Data.Doc,
+            XPath => "/system/subjects/subject/vcpu/registers/msrs/"
+            & "msr[@end='16#0176#']");
       begin
+         DOM.Core.Elements.Set_Attribute
+           (Elem  => Node,
+            Name  => "end",
+            Value => "16#0170#");
+
          Mucfgcheck.MSR.Start_Smaller_End (XML_Data => Data);
          Fail (Message => "Exception expected");
 
       exception
          when E : Mucfgcheck.Validation_Error =>
             Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
-                    = "MSR start 16#0902# larger than end 16#0800#"
+                    = "MSR start 16#0174# larger than end 16#0170#"
                     & " (Subject 'linux')",
                     Message   => "Exception mismatch");
       end;

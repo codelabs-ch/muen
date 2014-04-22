1c3bd79437965808d5105d60da1614560abd40a5
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-22 16:44:30 +0200

    Rework device I/O port range test

--- tools/libmucfgcheck/tests/device_tests.adb
+++ tools/libmucfgcheck/tests/device_tests.adb
@@ -248,16 +248,13 @@ is
    begin
       Muxml.Parse (Data => Data,
                    Kind => Muxml.Format_B,
-                   File => "data/validators.xml");
+                   File => "data/test_policy.xml");
 
       declare
          Node : constant DOM.Core.Node := Muxml.Utils.Get_Element
            (Doc   => Data.Doc,
             XPath => "/system/platform/device/ioPort[@name='ports']");
       begin
-
-         --  Set invalid port range.
-
          DOM.Core.Elements.Set_Attribute
            (Elem  => Node,
             Name  => "start",

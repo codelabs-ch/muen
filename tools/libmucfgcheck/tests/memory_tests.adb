b305efa0311af73bef57e9a0b82f1075dcae5397
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-04-22 21:41:44 +0200

    Rework VMXON memory consecutiveness test

--- tools/libmucfgcheck/tests/memory_tests.adb
+++ tools/libmucfgcheck/tests/memory_tests.adb
@@ -796,16 +796,25 @@ is
    begin
       Muxml.Parse (Data => Data,
                    Kind => Muxml.Format_B,
-                   File => "data/validators.xml");
+                   File => "data/test_policy.xml");
 
+      declare
+         Node : constant DOM.Core.Node := Muxml.Utils.Get_Element
+           (Doc   => Data.Doc,
+            XPath => "/system/memory/memory[@name='kernel_1|vmxon']");
       begin
+         DOM.Core.Elements.Set_Attribute
+           (Elem  => Node,
+            Name  => "physicalAddress",
+            Value => "16#a000#");
+
          Mucfgcheck.Memory.VMXON_Consecutiveness (XML_Data => Data);
          Fail (Message => "Exception expected");
 
       exception
          when E : Mucfgcheck.Validation_Error =>
             Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
-                    = "Memory region 'invalid_0|vmxon' not adjacent to other"
+                    = "Memory region 'kernel_0|vmxon' not adjacent to other"
                     & " VMXON regions",
                     Message   => "Exception mismatch");
       end;

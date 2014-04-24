a76399b21377cb6d6c55e4ed27cbb6055d887775
Reto Buerki <reet@codelabs.ch>
2014-04-24 15:25:59 +0200

    Rework Process_Files test

--- tools/mupack/tests/content_provider_tests.adb
+++ tools/mupack/tests/content_provider_tests.adb
@@ -21,6 +21,7 @@ with Ada.Directories;
 with DOM.Core.Elements;
 
 with Muxml.Utils;
+with Mutools.XML_Utils;
 
 with Pack.Image;
 with Pack.Manifest;
@@ -59,28 +60,59 @@ is
       Command_Line.Test.Set_Input_Dir  (Path => "data");
       Command_Line.Test.Set_Output_Dir (Path => "obj");
       Command_Line.Test.Set_Policy     (Path => "data/test_policy.xml");
+
       Muxml.Parse (Data => Policy,
                    Kind => Muxml.Format_B,
                    File => "data/test_policy.xml");
-      Data.XML_Doc := Policy.Doc;
+      Mutools.XML_Utils.Add_Memory_Region
+        (Policy      => Policy,
+         Name        => "mboot",
+         Address     => "16#0010_0000#",
+         Size        => "16#1000#",
+         Caching     => "WB",
+         Alignment   => "16#1000#",
+         File_Name   => "mboot",
+         File_Format => "bin_raw",
+         File_Offset => "none");
+      Mutools.XML_Utils.Add_Memory_Region
+        (Policy      => Policy,
+         Name        => "linux|acpi_rsdp",
+         Address     => "16#0010_1000#",
+         Size        => "16#0001_3000#",
+         Caching     => "WB",
+         Alignment   => "16#1000#",
+         File_Name   => "sections.ref",
+         File_Format => "acpi_rsdp",
+         File_Offset => "none");
+      Mutools.XML_Utils.Add_Memory_Region
+        (Policy      => Policy,
+         Name        => "linux|bin",
+         Address     => "16#0011_4000#",
+         Size        => "16#0001_3000#",
+         Caching     => "WB",
+         Alignment   => "16#1000#",
+         File_Name   => "obj1.o",
+         File_Format => "bin_raw",
+         File_Offset => "16#0004#");
 
+      Data.XML_Doc := Policy.Doc;
       Content_Providers.Process_Files (Data => Data);
 
       Image.Write (Image    => Data.Image,
-                   Filename => "obj/muen.img");
+                   Filename => "obj/process_files.img");
       Manifest.Write (Manifest => Data.Manifest,
-                      Filename => "obj/manifest");
+                      Filename => "obj/process_files.manifest");
       Assert (Condition => Test_Utils.Equal_Files
-              (Filename1 => "obj/muen.img",
-               Filename2 => "data/muen.file.ref"),
+              (Filename1 => "obj/process_files.img",
+               Filename2 => "data/process_files.img"),
               Message   => "Image file differs");
       Assert (Condition => Test_Utils.Equal_Files
-              (Filename1 => "obj/manifest",
-               Filename2 => "data/manifest.ref"),
+              (Filename1 => "obj/process_files.manifest",
+               Filename2 => "data/process_files.manifest"),
               Message   => "Manifest file differs");
 
-      Ada.Directories.Delete_File (Name => "obj/muen.img");
-      Ada.Directories.Delete_File (Name => "obj/manifest");
+      Ada.Directories.Delete_File (Name => "obj/process_files.img");
+      Ada.Directories.Delete_File (Name => "obj/process_files.manifest");
    end Process_Files;
 
    -------------------------------------------------------------------------

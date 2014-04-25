9b6fee74e03e6a91fd24717de3695a858ab93a33
Robert Dorn <robert.dorn@secunet.com>
2014-04-25 14:16:17 +0200

    Transform SK.Subjects
    TODO:
    - Subject_State_Array is accessed by different cores.
      - Needs Independent_Components
      - Needs access model or volatile
      - Needs better handling for initialization
    
    - Fix for [N425-012]

--- kernel/src/sk-subjects.ads
+++ kernel/src/sk-subjects.ads
@@ -17,108 +17,88 @@
 --
 
 with Skp;
+with X86_64;
 
---# inherit
---#    Skp,
---#    SK.Constants,
---#    SK.CPU,
---#    SK.VMX,
---#    X86_64;
 package SK.Subjects
---# own
---#    State;
---# initializes
---#    State;
+with SPARK_Mode,
+   Abstract_State => State,
+   Initializes    => State
 is
 
    --  Get state of subject with given ID.
-   function Get_State (Id : Skp.Subject_Id_Type) return SK.Subject_State_Type;
-   --# global
-   --#    State;
+   function Get_State (Id : Skp.Subject_Id_Type) return SK.Subject_State_Type
+   with
+      Global => (Input => State);
 
    --  Get RFLAGS of subject with given ID.
-   function Get_RFLAGS (Id : Skp.Subject_Id_Type) return SK.Word64;
-   --# global
-   --#    State;
+   function Get_RFLAGS (Id : Skp.Subject_Id_Type) return SK.Word64
+   with
+      Global => (Input => State);
 
    --  Set CR0 of subject specified by id to given value.
    procedure Set_CR0
      (Id    : Skp.Subject_Id_Type;
-      Value : SK.Word64);
-   --# global
-   --#    State;
-   --# derives
-   --#    State from *, Id, Value;
+      Value : SK.Word64)
+   with
+      Global  => (In_Out => State),
+      Depends => (State =>+ (Id, Value));
 
    --  Get RIP of subject with given ID.
-   function Get_RIP (Id : Skp.Subject_Id_Type) return SK.Word64;
-   --# global
-   --#    State;
+   function Get_RIP (Id : Skp.Subject_Id_Type) return SK.Word64
+   with
+      Global => (Input => State);
 
    --  Set RIP of subject specified by id to given value.
    procedure Set_RIP
      (Id    : Skp.Subject_Id_Type;
-      Value : SK.Word64);
-   --# global
-   --#    State;
-   --# derives
-   --#    State from *, Id, Value;
+      Value : SK.Word64)
+   with
+      Global  => (In_Out => State),
+      Depends => (State =>+ (Id, Value));
 
    --  Set RSP of subject specified by id to given value.
    procedure Set_RSP
      (Id    : Skp.Subject_Id_Type;
-      Value : SK.Word64);
-   --# global
-   --#    State;
-   --# derives
-   --#    State from *, Id, Value;
+      Value : SK.Word64)
+   with
+      Global  => (In_Out => State),
+      Depends => (State =>+ (Id, Value));
 
    --  Get instruction length of subject with given ID.
-   function Get_Instruction_Length (Id : Skp.Subject_Id_Type) return SK.Word64;
-   --# global
-   --#    State;
+   function Get_Instruction_Length (Id : Skp.Subject_Id_Type) return SK.Word64
+   with
+      Global => (Input => State);
 
    --  Get interrupt information of subject with given ID.
-   function Get_Interrupt_Info (Id : Skp.Subject_Id_Type) return SK.Word64;
-   --# global
-   --#    State;
+   function Get_Interrupt_Info (Id : Skp.Subject_Id_Type) return SK.Word64
+   with
+      Global => (Input => State);
 
    --  Restore non-GPR guest registers from the subject state identified by ID.
    --  GPRs are returned in Regs.
    procedure Restore_State
      (Id   :     Skp.Subject_Id_Type;
-      GPRs : out SK.CPU_Registers_Type);
-   --# global
-   --#    in     State;
-   --#    in out X86_64.State;
-   --# derives
-   --#    X86_64.State from
-   --#       *,
-   --#       Id,
-   --#       State &
-   --#    GPRs from State, Id;
+      GPRs : out SK.CPU_Registers_Type)
+   with
+      Global  => (Input  => State,
+                  In_Out => X86_64.State),
+      Depends => (X86_64.State =>+ (Id, State),
+                  GPRs         =>  (Id, State));
 
    --  Save GPRs and VMCS guest data to the state of the subject identified by
    --  ID.
    procedure Save_State
      (Id   : Skp.Subject_Id_Type;
-      GPRs : SK.CPU_Registers_Type);
-   --# global
-   --#    in out X86_64.State;
-   --#    in out State;
-   --# derives
-   --#    State from
-   --#       *,
-   --#       Id,
-   --#       GPRs,
-   --#       X86_64.State &
-   --#    X86_64.State from *;
+      GPRs : SK.CPU_Registers_Type)
+   with
+      Global  => (In_Out => (State, X86_64.State)),
+      Depends => (State        =>+ (Id, GPRs, X86_64.State),
+                  X86_64.State =>+ null);
 
    --  Clear state of subject with given ID.
-   procedure Clear_State (Id : Skp.Subject_Id_Type);
-   --# global
-   --#    in out State;
-   --# derives
-   --#    State from *, Id;
+   procedure Clear_State (Id : Skp.Subject_Id_Type)
+   with
+      Global  => (In_Out => State),
+      Depends => (State =>+ Id);
 
 end SK.Subjects;

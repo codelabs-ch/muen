da16e91f94e4529c48fcc865b8d0150563a506fa
Reto Buerki <reet@codelabs.ch>
2014-05-01 14:18:17 +0200

    Move GNATtest build logic to component.mk

--- tools/component.mk
+++ tools/component.mk
@@ -1,7 +1,12 @@
 include ../../Makeconf
 
-TEST_RUNNER = @obj/tests/test_runner $(TEST_OPTS)
-TOOLS_DIR   = $(TOP_DIR)/tools
+TEST_RUNNER     = $(OBJ_DIR)/tests/test_runner $(TEST_OPTS)
+GNATTEST_RUNNER = $(OBJ_DIR)/tests/gnattest/harness/test_runner
+GNATTEST_DRIVER = $(OBJ_DIR)/tests/gnattest/harness/test_driver
+TESTS_DIR       = $(CURDIR)/tests-ng
+TOOLS_DIR       = $(TOP_DIR)/tools
+
+SRC_FILES = $(wildcard $(SRC_DIR)/*)
 
 all: $(COMPONENT)
 
@@ -9,7 +14,7 @@ $(DEPENDS) $(TDEPENDS):
 	@$(MAKE) -s -C $(TOOLS_DIR)/$@
 
 tests: test_$(COMPONENT)
-	$(TEST_RUNNER)
+	@$(TEST_RUNNER)
 
 $(COMPONENT): $(DEPENDS) $(COMPONENT_TARGETS)
 	@gprbuild $(BUILD_OPTS) -P$@
@@ -19,6 +24,17 @@ test_$(COMPONENT): $(DEPENDS) $(TDEPENDS) $(TEST_TARGETS)
 
 build_cov: $(DEPENDS) $(TDEPENDS) $(COV_TARGETS)
 	@gprbuild $(BUILD_OPTS) -Ptest_$(COMPONENT) -XBUILD=coverage
+
+$(OBJ_DIR)/.harness_stamp: $(SRC_FILES)
+	@mkdir -p $(OBJ_DIR)/tests ../libtest/obj/tests ../libtest/lib
+	gnattest --tests-dir=$(TESTS_DIR) -Pgnattest_$(COMPONENT)
+	@touch $@
+
+build_gnattests: $(DEPENDS) $(TDEPENDS) $(TEST_TARGETS) $(OBJ_DIR)/.harness_stamp
+	gprbuild $(BUILD_OPTS) -P$(GNATTEST_DRIVER) -XBUILD=tests
+
+gnattests: build_gnattests
+	$(GNATTEST_RUNNER)
 
 clean:
 	@rm -rf bin obj $(ADDITIONAL_CLEAN)

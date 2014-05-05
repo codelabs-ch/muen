a1aa6b04f4ce2e14a143e0354ec082e7a59ffb21
Reto Buerki <reet@codelabs.ch>
2014-05-05 23:42:37 +0200

    Convert events Self_References test

--- tools/libmucfgcheck/tests-ng/mucfgcheck-events-test_data-tests.adb
+++ tools/libmucfgcheck/tests-ng/mucfgcheck-events-test_data-tests.adb
@@ -138,12 +138,30 @@ package body Mucfgcheck.Events.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      Data : Muxml.XML_Data_Type;
    begin
+      Muxml.Parse (Data => Data,
+                   Kind => Muxml.Format_B,
+                   File => "data/test_policy.xml");
+      Muxml.Utils.Set_Attribute
+        (Doc   => Data.Doc,
+         XPath => "/system/subjects/subject/events/target/event"
+         & "[@physical='linux_console']",
+         Name  => "physical",
+         Value => "linux_keyboard");
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
+      begin
+         Self_References (XML_Data => Data);
+         Assert (Condition => False,
+                 Message   => "Exception expected");
 
+      exception
+         when E : Validation_Error =>
+            Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
+                    = "Reference to self in event 'linux_keyboard' of subject "
+                    & "'vt'",
+                    Message   => "Exception mismatch");
+      end;
 --  begin read only
    end Test_Self_References;
 --  end read only

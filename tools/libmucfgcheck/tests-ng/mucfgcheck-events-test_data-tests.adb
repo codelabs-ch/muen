f3c042996e0510717743e8d243e2be0550b062bc
Reto Buerki <reet@codelabs.ch>
2014-05-05 23:47:30 +0200

    Convert Source_Group_Event_ID_Uniqueness test

--- tools/libmucfgcheck/tests-ng/mucfgcheck-events-test_data-tests.adb
+++ tools/libmucfgcheck/tests-ng/mucfgcheck-events-test_data-tests.adb
@@ -256,12 +256,30 @@ package body Mucfgcheck.Events.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      Data : Muxml.XML_Data_Type;
    begin
+      Muxml.Parse (Data => Data,
+                   Kind => Muxml.Format_B,
+                   File => "data/test_policy.xml");
+      Muxml.Utils.Set_Attribute
+        (Doc   => Data.Doc,
+         XPath => "/system/subjects/subject/events/source/group/event"
+         & "[@logical='resume_linux']",
+         Name  => "id",
+         Value => "1");
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
+      begin
+         Source_Group_Event_ID_Uniqueness (XML_Data => Data);
+         Assert (Condition => False,
+                 Message   => "Exception expected");
 
+      exception
+         when E : Validation_Error =>
+            Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
+                    = "Subject 'sm' source events 'resume_linux' and "
+                    & "'channel_event_sm_console' share ID 1",
+                    Message   => "Exception mismatch");
+      end;
 --  begin read only
    end Test_Source_Group_Event_ID_Uniqueness;
 --  end read only

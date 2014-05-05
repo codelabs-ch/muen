ed81902bb77adfe77d746a572fb15ed95e0ab1b3
Reto Buerki <reet@codelabs.ch>
2014-05-06 00:15:57 +0200

    Convert memory VMCS_Region_Size test

--- tools/libmucfgcheck/tests-ng/mucfgcheck-memory-test_data-tests.adb
+++ tools/libmucfgcheck/tests-ng/mucfgcheck-memory-test_data-tests.adb
@@ -209,12 +209,29 @@ package body Mucfgcheck.Memory.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      Data : Muxml.XML_Data_Type;
    begin
+      Muxml.Parse (Data => Data,
+                   Kind => Muxml.Format_B,
+                   File => "data/test_policy.xml");
+      Muxml.Utils.Set_Attribute
+        (Doc   => Data.Doc,
+         XPath => "/system/memory/memory[@name='sm|vmcs']",
+         Name  => "size",
+         Value => "16#0001_0013#");
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
+      begin
+         VMCS_Region_Size (XML_Data => Data);
+         Assert (Condition => False,
+                 Message   => "Exception expected");
 
+      exception
+         when E : Validation_Error =>
+            Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
+                    = "Attribute 'size => 16#0001_0013#' of 'sm|vmcs' "
+                    & "VMCS memory element not 4K",
+                    Message   => "Exception mismatch");
+      end;
 --  begin read only
    end Test_VMCS_Region_Size;
 --  end read only

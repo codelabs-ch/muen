90391f827c545baa045734af1502937fa7d352bf
Reto Buerki <reet@codelabs.ch>
2014-05-06 00:19:52 +0200

    Convert Physical_Memory_References test

--- tools/libmucfgcheck/tests-ng/mucfgcheck-memory-test_data-tests.adb
+++ tools/libmucfgcheck/tests-ng/mucfgcheck-memory-test_data-tests.adb
@@ -361,12 +361,29 @@ package body Mucfgcheck.Memory.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      Data : Muxml.XML_Data_Type;
    begin
+      Muxml.Parse (Data => Data,
+                   Kind => Muxml.Format_B,
+                   File => "data/test_policy.xml");
+      Muxml.Utils.Set_Attribute
+        (Doc   => Data.Doc,
+         XPath => "/system/memory/memory[@name='linux|ram']",
+         Name  => "name",
+         Value => "foobar");
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
+      begin
+         Physical_Memory_References (XML_Data => Data);
+         Assert (Condition => False,
+                 Message   => "Exception expected");
 
+      exception
+         when E : Validation_Error =>
+            Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
+                    = "Physical memory 'linux|ram' referenced by logical "
+                    & "memory 'ram' not found",
+                    Message   => "Exception mismatch");
+      end;
 --  begin read only
    end Test_Physical_Memory_References;
 --  end read only

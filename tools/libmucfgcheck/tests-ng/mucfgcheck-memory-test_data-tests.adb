a5cd708f4331c1deb28d4f1b17c267bbee7c72a7
Reto Buerki <reet@codelabs.ch>
2014-05-06 00:21:12 +0200

    Convert memory Entity_Name_Encoding test

--- tools/libmucfgcheck/tests-ng/mucfgcheck-memory-test_data-tests.adb
+++ tools/libmucfgcheck/tests-ng/mucfgcheck-memory-test_data-tests.adb
@@ -513,12 +513,29 @@ package body Mucfgcheck.Memory.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      Data : Muxml.XML_Data_Type;
    begin
+      Muxml.Parse (Data => Data,
+                   Kind => Muxml.Format_B,
+                   File => "data/test_policy.xml");
+      Muxml.Utils.Set_Attribute
+        (Doc   => Data.Doc,
+         XPath => "/system/memory/memory[@name='kernel_0|vmxon']",
+         Name  => "name",
+         Value => "kernel_5|vmxon");
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
+      begin
+         Entity_Name_Encoding (XML_Data => Data);
+         Assert (Condition => False,
+                 Message   => "Exception expected");
 
+      exception
+         when E : Validation_Error =>
+            Assert (Condition => Ada.Exceptions.Exception_Message (X => E)
+                    = "Entity 'kernel_5' encoded in memory region "
+                    & "'kernel_5|vmxon' does not exist or is invalid",
+                    Message   => "Exception mismatch");
+      end;
 --  begin read only
    end Test_Entity_Name_Encoding;
 --  end read only

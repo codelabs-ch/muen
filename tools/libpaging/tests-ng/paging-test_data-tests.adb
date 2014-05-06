a851194a7a84a625c1a2c699960b6683a3684527
Reto Buerki <reet@codelabs.ch>
2014-05-06 17:34:05 +0200

    Convert test for Get_Indexes procedure

--- tools/libpaging/tests-ng/paging-test_data-tests.adb
+++ tools/libpaging/tests-ng/paging-test_data-tests.adb
@@ -20,12 +20,49 @@ package body Paging.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      PML4, PDPT, PD, PT : Table_Range;
    begin
+      Get_Indexes (Address    => 0,
+                   PML4_Index => PML4,
+                   PDPT_Index => PDPT,
+                   PD_Index   => PD,
+                   PT_Index   => PT);
+      Assert (Condition => PML4 = 0,
+              Message   => "PML4 index mismatch (1)");
+      Assert (Condition => PDPT = 0,
+              Message   => "PDPT index mismatch (1)");
+      Assert (Condition => PD = 0,
+              Message   => "PD index mismatch (1)");
+      Assert (Condition => PT = 0,
+              Message   => "PT index mismatch (1)");
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
+      Get_Indexes (Address    => Interfaces.Unsigned_64'Last,
+                   PML4_Index => PML4,
+                   PDPT_Index => PDPT,
+                   PD_Index   => PD,
+                   PT_Index   => PT);
+      Assert (Condition => PML4 = Table_Range'Last,
+              Message   => "PML4 index mismatch (2)");
+      Assert (Condition => PDPT = Table_Range'Last,
+              Message   => "PDPT index mismatch (2)");
+      Assert (Condition => PD = Table_Range'Last,
+              Message   => "PD index mismatch (2)");
+      Assert (Condition => PT = Table_Range'Last,
+              Message   => "PT index mismatch (2)");
 
+      Get_Indexes (Address    => 16#fffc80200f000#,
+                   PML4_Index => PML4,
+                   PDPT_Index => PDPT,
+                   PD_Index   => PD,
+                   PT_Index   => PT);
+      Assert (Condition => PML4 = 511,
+              Message   => "PML4 index mismatch (3)");
+      Assert (Condition => PDPT = 288,
+              Message   => "PDPT index mismatch (3)");
+      Assert (Condition => PD = 16,
+              Message   => "PD index mismatch (3)");
+      Assert (Condition => PT = 15,
+              Message   => "PT index mismatch (3)");
 --  begin read only
    end Test_Get_Indexes;
 --  end read only

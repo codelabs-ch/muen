34e85a00ad153a8478ae61aed4bd3b5f89b47683
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-05-06 11:29:12 +0200

    Add 'kernel_interface' memory type
    This type will be used for the tau0/kernel interface memory region.

--- tools/libmutools/src/mutools-types.ads
+++ tools/libmutools/src/mutools-types.ads
@@ -25,13 +25,13 @@ is
    --  Types of physical memory.
    type Memory_Kind is
      (System, System_Vmxon, System_Vmcs, System_Iobm, System_Msrbm, System_Pt,
-      Kernel, Kernel_Binary,
+      Kernel, Kernel_Binary, Kernel_Interface,
       Subject, Subject_Binary, Subject_Zeropage, Subject_Initrd,
       Subject_Channel, Subject_State, Subject_Bios, Subject_Acpi_Rsdp,
       Subject_Acpi_Xsdt, Subject_Acpi_Fadt, Subject_Acpi_Dsdt);
 
    subtype System_Memory  is Memory_Kind range System  .. System_Pt;
-   subtype Kernel_Memory  is Memory_Kind range Kernel  .. Kernel_Binary;
+   subtype Kernel_Memory  is Memory_Kind range Kernel  .. Kernel_Interface;
    subtype Subject_Memory is Memory_Kind range Subject .. Subject_Acpi_Dsdt;
 
 end Mutools.Types;

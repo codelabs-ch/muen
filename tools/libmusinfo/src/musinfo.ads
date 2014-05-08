50633a5a7b4a1a201cb5ff23ba58393a9ae1577c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-05-08 12:07:11 +0200

    Add subject info type specifications (Ref T84)

--- tools/libmusinfo/src/musinfo.ads
+++ tools/libmusinfo/src/musinfo.ads
@@ -16,6 +16,150 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+with Interfaces;
+
 package Musinfo
 is
+
+   --  Unique identifier used to designate a subject info memory region.
+   --  Generated as follows:
+   --  echo -n "muinfo" | hexdump -C, 2 bytes for counter.
+   Muen_Subject_Info_Magic : constant := 16#6d75_696e_666f_0001#;
+
+   type Bit_Type is range 0 .. 1
+     with
+       Size => 1;
+
+   type Bit_Array is array (Positive range <>) of Bit_Type
+     with
+       Pack;
+
+   type Name_Size_Type is range 0 .. 63
+     with
+       Size => 6;
+
+   subtype Name_Index_Type is Positive range 1 .. 63;
+
+   type Name_Data_Type is new String
+     (Name_Index_Type'First .. Name_Index_Type'Last)
+     with
+       Size => Name_Index_Type'Last * 8;
+
+   --  A name is a string with an explicit length field and maximum size of 63
+   --  characters.
+   type Name_Type is record
+      Length  : Name_Size_Type;
+      Padding : Bit_Array (1 .. 2);
+      Data    : Name_Data_Type;
+   end record
+     with
+       Size => (1 + Name_Index_Type'Last) * 8;
+
+   for Name_Type use record
+      Length  at 0 range 0 .. 5;
+      Padding at 0 range 6 .. 7;
+      Data    at 1 range 0 .. 503;
+   end record;
+
+   Null_Name : constant Name_Type
+     := (Length  => 0,
+         Padding => (others => 0),
+         Data    => (others => ASCII.NUL));
+
+   type Event_Number_Range is range 0 .. 255
+     with
+       Size => 8;
+
+   type Vector_Range is range 0 .. 255
+     with
+       Size => 8;
+
+   Channel_Type_Size : constant := 88;
+
+   --  A channel is described by its name, memory address and size. The
+   --  Writable flag specifies if the memory region can be written to. Channels
+   --  can optionally have an assigned vector or event which is indicated by
+   --  the Has_Event/Vector flags.
+   type Channel_Type is record
+      Name       : Name_Type;
+      Address    : Interfaces.Unsigned_64;
+      Size       : Interfaces.Unsigned_64;
+      Writable   : Boolean;
+      Has_Event  : Boolean;
+      Has_Vector : Boolean;
+      Padding_1  : Bit_Array (1 .. 5);
+      Event      : Event_Number_Range;
+      Vector     : Vector_Range;
+      Padding_2  : Bit_Array (1 .. 40);
+   end record
+     with
+       Alignment => 8,
+       Size      => Channel_Type_Size * 8;
+
+   for Channel_Type use record
+      Name       at  0 range 0 .. 511;
+      Address    at 64 range 0 .. 63;
+      Size       at 72 range 0 .. 63;
+      Writable   at 80 range 0 .. 0;
+      Has_Event  at 80 range 1 .. 1;
+      Has_Vector at 80 range 2 .. 2;
+      Padding_1  at 80 range 3 .. 7;
+      Event      at 81 range 0 .. 7;
+      Vector     at 82 range 0 .. 7;
+      Padding_2  at 83 range 0 .. 39;
+   end record;
+
+   Null_Channel : constant Channel_Type
+     := (Name       => Null_Name,
+         Address    => 0,
+         Size       => 0,
+         Writable   => False,
+         Has_Event  => False,
+         Has_Vector => False,
+         Padding_1  => (others => 0),
+         Event      => 0,
+         Vector     => 0,
+         Padding_2  => (others => 0));
+
+   type Channel_Count_Type is range 0 .. 255
+     with
+       Size => 8;
+
+   subtype Channel_Index_Type is Channel_Count_Type range
+     1 .. Channel_Count_Type'Last;
+
+   Channel_Array_Size : constant := Channel_Index_Type'Last
+     * Channel_Type_Size;
+
+   type Channel_Array is array (Channel_Index_Type) of Channel_Type
+     with
+       Pack,
+       Alignment => 8;
+
+   Subject_Info_Type_Size : constant := 8 + 1 + 7 + Channel_Array_Size;
+
+   --  Subject info records enable subjects to determine what resources are
+   --  provided to them at runtime.
+   type Subject_Info_Type is record
+      Magic         : Interfaces.Unsigned_64;
+      Channel_Count : Channel_Count_Type;
+      Padding       : Bit_Array (1 .. 56);
+      Channels      : Channel_Array;
+   end record
+     with
+       Size => Subject_Info_Type_Size * 8;
+
+   for Subject_Info_Type use record
+      Magic         at 0  range 0 .. 63;
+      Channel_Count at 8  range 0 .. 7;
+      Padding       at 9  range 0 .. 55;
+      Channels      at 16 range 0 .. (Channel_Array_Size * 8) - 1;
+   end record;
+
+   Null_Subject_Info : constant Subject_Info_Type
+     := (Magic         => Muen_Subject_Info_Magic,
+         Channel_Count => Channel_Count_Type'First,
+         Padding       => (others => 0),
+         Channels      => (others => Null_Channel));
+
 end Musinfo;

82840ff6d43183aef5e61fe642373129feb9c71b
Reto Buerki <reet@codelabs.ch>
2014-05-14 23:57:29 +0200

    Mugenacpi: Add auto-generated test stubs

---
+++ tools/mugenacpi/tests-ng/acpi-generator-test_data-tests.adb
@@ -0,0 +1,33 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Acpi.Generator.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Acpi.Generator.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Write (Gnattest_T : in out Test);
+   procedure Test_Write_23ab15 (Gnattest_T : in out Test) renames Test_Write;
+--  id:2.2/23ab1562ae4604fa/Write/1/0/
+   procedure Test_Write (Gnattest_T : in out Test) is
+   --  acpi-generator.ads:26:4:Write
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Write;
+--  end read only
+
+end Acpi.Generator.Test_Data.Tests;
---
+++ tools/mugenacpi/tests-ng/acpi-xsdt-test_data-tests.adb
@@ -0,0 +1,33 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Acpi.XSDT.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Acpi.XSDT.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Write (Gnattest_T : in out Test);
+   procedure Test_Write_c33834 (Gnattest_T : in out Test) renames Test_Write;
+--  id:2.2/c3383472bb920369/Write/1/0/
+   procedure Test_Write (Gnattest_T : in out Test) is
+   --  acpi-xsdt.ads:20:4:Write
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Write;
+--  end read only
+
+end Acpi.XSDT.Test_Data.Tests;

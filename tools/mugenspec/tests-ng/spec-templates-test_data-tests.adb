bd524f034f948287e9de9630211c0a8638414f8f
Reto Buerki <reet@codelabs.ch>
2014-05-16 17:20:25 +0200

    Mugenspec: Add auto-generated test stubs

---
+++ tools/mugenspec/tests-ng/spec-templates-test_data-tests.adb
@@ -0,0 +1,138 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Spec.Templates.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Spec.Templates.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Load (Gnattest_T : in out Test);
+   procedure Test_Load_bffd3b (Gnattest_T : in out Test) renames Test_Load;
+--  id:2.2/bffd3bbcacc394fb/Load/1/0/
+   procedure Test_Load (Gnattest_T : in out Test) is
+   --  spec-templates.ads:29:4:Load
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Load;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Create (Gnattest_T : in out Test);
+   procedure Test_Create_fbd165 (Gnattest_T : in out Test) renames Test_Create;
+--  id:2.2/fbd165d850fee6bf/Create/1/0/
+   procedure Test_Create (Gnattest_T : in out Test) is
+   --  spec-templates.ads:35:4:Create
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Create;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Get_Size (Gnattest_T : in out Test);
+   procedure Test_Get_Size_8e9311 (Gnattest_T : in out Test) renames Test_Get_Size;
+--  id:2.2/8e9311b18dc4da9f/Get_Size/1/0/
+   procedure Test_Get_Size (Gnattest_T : in out Test) is
+   --  spec-templates.ads:38:4:Get_Size
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Get_Size;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Set_Template_Dir (Gnattest_T : in out Test);
+   procedure Test_Set_Template_Dir_2c1dd5 (Gnattest_T : in out Test) renames Test_Set_Template_Dir;
+--  id:2.2/2c1dd572e76981b0/Set_Template_Dir/1/0/
+   procedure Test_Set_Template_Dir (Gnattest_T : in out Test) is
+   --  spec-templates.ads:41:4:Set_Template_Dir
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Set_Template_Dir;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Replace (Gnattest_T : in out Test);
+   procedure Test_Replace_d85dde (Gnattest_T : in out Test) renames Test_Replace;
+--  id:2.2/d85dde1373bd8ec9/Replace/1/0/
+   procedure Test_Replace (Gnattest_T : in out Test) is
+   --  spec-templates.ads:45:4:Replace
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Replace;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Write (Gnattest_T : in out Test);
+   procedure Test_Write_220c26 (Gnattest_T : in out Test) renames Test_Write;
+--  id:2.2/220c26b6a1044317/Write/1/0/
+   procedure Test_Write (Gnattest_T : in out Test) is
+   --  spec-templates.ads:51:4:Write
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Write;
+--  end read only
+
+end Spec.Templates.Test_Data.Tests;

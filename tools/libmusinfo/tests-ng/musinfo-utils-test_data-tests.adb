0ef82fb5b5471abbdeee8d087c651f52f2624091
Reto Buerki <reet@codelabs.ch>
2014-05-16 17:52:31 +0200

    Convert util tests

--- tools/libmusinfo/tests-ng/musinfo-utils-test_data-tests.adb
+++ tools/libmusinfo/tests-ng/musinfo-utils-test_data-tests.adb
@@ -20,12 +20,14 @@ package body Musinfo.Utils.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      Ref_Str  : constant String := "foobar";
+      Ref_Name : Name_Type       := Null_Name;
    begin
+      Ref_Name.Length := Ref_Str'Length;
+      Ref_Name.Data (1 .. Ref_Str'Length) := Name_Data_Type (Ref_Str);
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
-
+      Assert (Condition => Create_Name (Str => Ref_Str) = Ref_Name,
+              Message   => "Name mismatch");
 --  begin read only
    end Test_Create_Name;
 --  end read only
@@ -41,12 +43,41 @@ package body Musinfo.Utils.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      use type Interfaces.Unsigned_64;
+
+      Ref_Name   : constant Name_Type := Create_Name (Str => "foo");
+      Ref_Addr   : constant Interfaces.Unsigned_64 := 16#8000_cafe_beef_0000#;
+      Ref_Size   : constant Interfaces.Unsigned_64 := 16#2000#;
+      Ref_Event  : constant Event_Number_Range     := 234;
+      Ref_Vector : constant Vector_Range           := 123;
+      Channel    : Channel_Type;
    begin
+      Channel := Create_Channel
+        (Name       => Ref_Name,
+         Address    => Ref_Addr,
+         Size       => Ref_Size,
+         Writable   => False,
+         Has_Event  => True,
+         Has_Vector => False,
+         Event      => Ref_Event,
+         Vector     => Ref_Vector);
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
-
+      Assert (Condition => Channel.Name = Ref_Name,
+              Message   => "Name mismatch");
+      Assert (Condition => Channel.Address = Ref_Addr,
+              Message   => "Address mismatch");
+      Assert (Condition => Channel.Size = Ref_Size,
+              Message   => "Size mismatch");
+      Assert (Condition => not Channel.Writable,
+              Message   => "Writable");
+      Assert (Condition => Channel.Has_Event,
+              Message   => "Has no event");
+      Assert (Condition => not Channel.Has_Vector,
+              Message   => "Has vector");
+      Assert (Condition => Channel.Event = Ref_Event,
+              Message   => "Event mismatch");
+      Assert (Condition => Channel.Vector = Ref_Vector,
+              Message   => "Vector mismatch");
 --  begin read only
    end Test_Create_Channel;
 --  end read only
@@ -62,12 +93,32 @@ package body Musinfo.Utils.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      Ref_Channel : constant Channel_Type := Create_Channel
+        (Name       => Create_Name (Str => "foobar"),
+         Address    => 16#3000#,
+         Size       => 16#1000#,
+         Writable   => True,
+         Has_Event  => False,
+         Has_Vector => False,
+         Event      => 12,
+         Vector     => 21);
+      Info : Subject_Info_Type := Null_Subject_Info;
    begin
+      Assert (Condition => Info.Channel_Count = Channel_Count_Type'First,
+              Message   => "Channel present");
 
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
+      Append_Channel (Info    => Info,
+                      Channel => Ref_Channel);
 
+      Assert (Condition => Info.Channel_Count = 1,
+              Message   => "Channel not appended");
+      Assert (Condition => Info.Channels (1) = Ref_Channel,
+              Message   => "Channel mismatch");
+
+      Utils.Append_Channel (Info    => Info,
+                            Channel => Ref_Channel);
+      Assert (Condition => Info.Channel_Count = 2,
+              Message   => "Channel not appended (2)");
 --  begin read only
    end Test_Append_Channel;
 --  end read only

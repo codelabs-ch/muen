a5cc9bd5e1a8517c2ce579f29e5529b819de4642
Reto Buerki <reet@codelabs.ch>
2014-05-16 17:48:32 +0200

    Libmusinfo: Add auto-generated test stubs

---
+++ tools/libmusinfo/tests-ng/musinfo-utils-test_data-tests.adb
@@ -0,0 +1,75 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Musinfo.Utils.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Musinfo.Utils.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Create_Name (Gnattest_T : in out Test);
+   procedure Test_Create_Name_3d4022 (Gnattest_T : in out Test) renames Test_Create_Name;
+--  id:2.2/3d402238b556bfe4/Create_Name/1/0/
+   procedure Test_Create_Name (Gnattest_T : in out Test) is
+   --  musinfo-utils.ads:23:4:Create_Name
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Create_Name;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Create_Channel (Gnattest_T : in out Test);
+   procedure Test_Create_Channel_a2c081 (Gnattest_T : in out Test) renames Test_Create_Channel;
+--  id:2.2/a2c0814d7c272b3d/Create_Channel/1/0/
+   procedure Test_Create_Channel (Gnattest_T : in out Test) is
+   --  musinfo-utils.ads:28:4:Create_Channel
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Create_Channel;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Append_Channel (Gnattest_T : in out Test);
+   procedure Test_Append_Channel_6c9a46 (Gnattest_T : in out Test) renames Test_Append_Channel;
+--  id:2.2/6c9a465a2a59b39c/Append_Channel/1/0/
+   procedure Test_Append_Channel (Gnattest_T : in out Test) is
+   --  musinfo-utils.ads:40:4:Append_Channel
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Append_Channel;
+--  end read only
+
+end Musinfo.Utils.Test_Data.Tests;

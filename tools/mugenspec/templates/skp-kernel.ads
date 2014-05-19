5942ae4d10fa1a87b85580d836117e6055832d5b
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-05-19 18:14:56 +0200

    Add IO_Apic_Address constant to skp-kernel.ads
    The IO_Apic_Address constant will be used to designate the memory
    address of the I/O APIC.

--- tools/mugenspec/templates/skp-kernel.ads
+++ tools/mugenspec/templates/skp-kernel.ads
@@ -5,5 +5,6 @@ is
    CPU_Store_Address   : constant := __cpu_store_addr__;
    Tau0_Iface_Address  : constant := __tau0_iface_addr__;
    Subj_States_Address : constant := __subj_states_addr__;
+   IO_Apic_Address     : constant := __ioapic_addr__;
 
 end Skp.Kernel;

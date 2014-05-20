8c5aa45d34d273f1a2e44e2e33c03a7abc9765dc
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-05-20 23:51:31 +0200

    Kernel: Set trigger mode/level as specified by IRQ route
    When routing hardware interrupts honor the IRQ mode and level specified
    by the policy.

--- kernel/src/sk-interrupts.adb
+++ kernel/src/sk-interrupts.adb
@@ -131,8 +131,8 @@ is
               (IRQ            => Skp.Interrupts.IRQ_Routing (I).IRQ,
                Vector         =>
                  SK.Byte (Skp.Interrupts.IRQ_Routing (I).Vector),
-               Trigger_Mode   => Skp.Interrupts.Edge,
-               Trigger_Level  => Skp.Interrupts.High,
+               Trigger_Mode   => Skp.Interrupts.IRQ_Routing (I).IRQ_Mode,
+               Trigger_Level  => Skp.Interrupts.IRQ_Routing (I).IRQ_Level,
                Destination_Id => APIC_ID);
          end if;
       end loop;

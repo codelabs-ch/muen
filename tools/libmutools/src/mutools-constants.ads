3bb8568df9eabcd943f3b41d431adf616b08b098
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-06-05 12:28:58 +0200

    Add MSR address constants
    The specified Model-Specific Register (MSR) addresses will be used to
    determine which MSRs need to be handled via the MSR store mechanism.
    
    NOTE: The constants are placed in Libmutools to avoid the use of the
          SK.Constants package. This may lead to some level of duplication
          but it will simplify the future split of the Muen repository into
          multiple separate repositories.

--- tools/libmutools/src/mutools-constants.ads
+++ tools/libmutools/src/mutools-constants.ads
@@ -22,4 +22,19 @@ is
    --  Size of one memory page in bytes.
    Page_Size : constant := 4096;
 
+   --  Model-Specific Registers, see Intel SDM Vol. 3C, chapter 35.
+   IA32_SYSENTER_CS      : constant := 16#0000_0174#;
+   IA32_SYSENTER_ESP     : constant := 16#0000_0175#;
+   IA32_SYSENTER_EIP     : constant := 16#0000_0176#;
+   IA32_DEBUGCTL         : constant := 16#0000_01d9#;
+   IA32_PAT              : constant := 16#0000_0277#;
+   IA32_PERF_GLOBAL_CTRL : constant := 16#0000_038f#;
+   IA32_EFER             : constant := 16#c000_0080#;
+   IA32_STAR             : constant := 16#c000_0081#;
+   IA32_LSTAR            : constant := 16#c000_0082#;
+   IA32_FMASK            : constant := 16#c000_0084#;
+   IA32_FS_BASE          : constant := 16#c000_0100#;
+   IA32_GS_BASE          : constant := 16#c000_0101#;
+   IA32_KERNEL_GS_BASE   : constant := 16#c000_0102#;
+
 end Mutools.Constants;

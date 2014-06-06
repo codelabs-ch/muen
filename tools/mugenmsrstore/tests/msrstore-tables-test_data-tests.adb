87bb4d9543969d2f3856204a005fc834b53f344f
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-06-06 11:30:10 +0200

    Add generated GNATtest skeleton

---
+++ tools/mugenmsrstore/tests/msrstore-tables-test_data-tests.adb
@@ -0,0 +1,54 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Msrstore.Tables.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Msrstore.Tables.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Is_Full (Gnattest_T : in out Test);
+   procedure Test_Is_Full_aaff6f (Gnattest_T : in out Test) renames Test_Is_Full;
+--  id:2.2/aaff6f63df362f53/Is_Full/1/0/
+   procedure Test_Is_Full (Gnattest_T : in out Test) is
+   --  msrstore-tables.ads:30:4:Is_Full
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Is_Full;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Append_Entry (Gnattest_T : in out Test);
+   procedure Test_Append_Entry_ce511a (Gnattest_T : in out Test) renames Test_Append_Entry;
+--  id:2.2/ce511a06b30ed5cd/Append_Entry/1/0/
+   procedure Test_Append_Entry (Gnattest_T : in out Test) is
+   --  msrstore-tables.ads:33:4:Append_Entry
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Append_Entry;
+--  end read only
+
+end Msrstore.Tables.Test_Data.Tests;
---
+++ tools/mugenmsrstore/tests/msrstore-tables-test_data-tests.ads
@@ -0,0 +1,19 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Msrstore.Tables.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Msrstore.Tables.Test_Data.Test
+   with null record;
+
+   procedure Test_Is_Full_aaff6f (Gnattest_T : in out Test);
+   --  msrstore-tables.ads:30:4:Is_Full
+
+   procedure Test_Append_Entry_ce511a (Gnattest_T : in out Test);
+   --  msrstore-tables.ads:33:4:Append_Entry
+
+end Msrstore.Tables.Test_Data.Tests;
+--  end read only

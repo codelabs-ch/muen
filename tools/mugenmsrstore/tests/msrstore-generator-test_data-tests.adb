ca03449f1edf39075e66dd42cd0722419388d1d8
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-06-09 14:29:36 +0200

    Implement test for Generator.Write procedure

---
+++ tools/mugenmsrstore/tests/msrstore-generator-test_data-tests.adb
@@ -0,0 +1,47 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Msrstore.Generator.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Msrstore.Generator.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Write (Gnattest_T : in out Test);
+   procedure Test_Write_23ab15 (Gnattest_T : in out Test) renames Test_Write;
+--  id:2.2/23ab1562ae4604fa/Write/1/0/
+   procedure Test_Write (Gnattest_T : in out Test) is
+   --  msrstore-generator.ads:26:4:Write
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+      Policy : Muxml.XML_Data_Type;
+
+      Sub1_Msrstore : constant String := "obj/subject1_msrstore";
+      Sub2_Msrstore : constant String := "obj/subject2_msrstore";
+   begin
+      Muxml.Parse (Data => Policy,
+                   Kind => Muxml.Format_B,
+                   File => "data/test_policy.xml");
+
+      Write (Output_Dir => "obj",
+             Policy     => Policy);
+
+      Assert (Condition => Test_Utils.Equal_Files
+              (Filename1 => "data/subject1_msrstore.ref",
+               Filename2 => Sub1_Msrstore),
+              Message   => "Subject 1 MSR store mismatch");
+      Assert (Condition => Test_Utils.Equal_Files
+              (Filename1 => "data/subject2_msrstore.ref",
+               Filename2 => Sub2_Msrstore),
+              Message   => "Subject 2 MSR store mismatch");
+--  begin read only
+   end Test_Write;
+--  end read only
+
+end Msrstore.Generator.Test_Data.Tests;

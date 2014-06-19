e5d312b0d117c1a06337a8c657fc5bfd889215c1
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-06-19 11:46:44 +0200

    Add GNATtest skeleton for new Paging.Maps package
    Since the tests of the Paging.Pagetable package cannot be copied as-is,
    they will be migrated incrementally.

---
+++ tools/libpaging/tests/paging-maps-test_data-tests.adb
@@ -0,0 +1,159 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Paging.Maps.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Paging.Maps.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Contains (Gnattest_T : in out Test);
+   procedure Test_Contains_d2cb11 (Gnattest_T : in out Test) renames Test_Contains;
+--  id:2.2/d2cb11e5cb82bccc/Contains/1/0/
+   procedure Test_Contains (Gnattest_T : in out Test) is
+   --  paging-maps.ads:34:4:Contains
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Contains;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Add_Entry (Gnattest_T : in out Test);
+   procedure Test_Add_Entry_5e31fc (Gnattest_T : in out Test) renames Test_Add_Entry;
+--  id:2.2/5e31fcf20e811abf/Add_Entry/1/0/
+   procedure Test_Add_Entry (Gnattest_T : in out Test) is
+   --  paging-maps.ads:42:4:Add_Entry
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Add_Entry;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Get_Table_Address (Gnattest_T : in out Test);
+   procedure Test_Get_Table_Address_8b3274 (Gnattest_T : in out Test) renames Test_Get_Table_Address;
+--  id:2.2/8b3274220cfac2b3/Get_Table_Address/1/0/
+   procedure Test_Get_Table_Address (Gnattest_T : in out Test) is
+   --  paging-maps.ads:49:4:Get_Table_Address
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Get_Table_Address;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Length (Gnattest_T : in out Test);
+   procedure Test_Length_f363cb (Gnattest_T : in out Test) renames Test_Length;
+--  id:2.2/f363cb6cb38c39e2/Length/1/0/
+   procedure Test_Length (Gnattest_T : in out Test) is
+   --  paging-maps.ads:55:4:Length
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Length;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Update (Gnattest_T : in out Test);
+   procedure Test_Update_7b49c2 (Gnattest_T : in out Test) renames Test_Update;
+--  id:2.2/7b49c2e8c677cb11/Update/1/0/
+   procedure Test_Update (Gnattest_T : in out Test) is
+   --  paging-maps.ads:59:4:Update
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Update;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Iterate (Gnattest_T : in out Test);
+   procedure Test_Iterate_ffea48 (Gnattest_T : in out Test) renames Test_Iterate;
+--  id:2.2/ffea48126aa59663/Iterate/1/0/
+   procedure Test_Iterate (Gnattest_T : in out Test) is
+   --  paging-maps.ads:67:4:Iterate
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Iterate;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Clear (Gnattest_T : in out Test);
+   procedure Test_Clear_ed2306 (Gnattest_T : in out Test) renames Test_Clear;
+--  id:2.2/ed2306f05183c90b/Clear/1/0/
+   procedure Test_Clear (Gnattest_T : in out Test) is
+   --  paging-maps.ads:74:4:Clear
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Clear;
+--  end read only
+
+end Paging.Maps.Test_Data.Tests;

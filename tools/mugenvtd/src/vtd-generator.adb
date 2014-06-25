8636cc31f0a13e519a2542e6a6fa14b5f767bd0a
Reto Buerki <reet@codelabs.ch>
2014-06-25 14:27:22 +0200

    Perform IOMMU check in Generator.Write procedure

--- tools/mugenvtd/src/vtd-generator.adb
+++ tools/mugenvtd/src/vtd-generator.adb
@@ -57,7 +57,17 @@ is
      (Output_Dir : String;
       Policy     : Muxml.XML_Data_Type)
    is
+      IOMMUs : constant DOM.Core.Node_List
+        := McKae.XML.XPath.XIA.XPath_Query
+          (N     => Policy.Doc,
+           XPath => "/system/platform/devices/device[starts-with"
+           & "(string(@name),'iommu')]");
    begin
+      if DOM.Core.Nodes.Length (List => IOMMUs) = 0 then
+         Mulog.Log (Msg => "No IOMMU device found, not adding VT-d tables");
+         return;
+      end if;
+
       Write_Root_Table
         (Output_Dir => Output_Dir,
          Policy     => Policy);
@@ -201,69 +211,56 @@ is
    is
       package MX renames Mutools.XML_Utils;
 
-      IOMMUs : constant DOM.Core.Node_List
-        := McKae.XML.XPath.XIA.XPath_Query
-          (N     => Policy.Doc,
-           XPath => "/system/platform/devices/device[starts-with"
-           & "(string(@name),'iommu')]");
+      Buses     : constant MX.PCI_Bus_Set.Set := MX.Get_Occupied_PCI_Buses
+        (Data => Policy);
+      Ctx_Pos   : MX.PCI_Bus_Set.Cursor       := Buses.First;
+      Root      : Tables.Root_Table_Type;
+      Root_File : constant String
+        := DOM.Core.Elements.Get_Attribute
+          (Elem => Muxml.Utils.Get_Element
+             (Doc   => Policy.Doc,
+              XPath => "/system/memory/memory/file[@filename='vtd_root']"),
+           Name => "filename");
    begin
-      if DOM.Core.Nodes.Length (List => IOMMUs) = 0 then
-         Mulog.Log (Msg => "No IOMMU device found, not adding VT-d tables");
-         return;
-      end if;
-
-      declare
-         Buses     : constant MX.PCI_Bus_Set.Set := MX.Get_Occupied_PCI_Buses
-           (Data => Policy);
-         Ctx_Pos   : MX.PCI_Bus_Set.Cursor       := Buses.First;
-         Root      : Tables.Root_Table_Type;
-         Root_File : constant String
-           := DOM.Core.Elements.Get_Attribute
-             (Elem => Muxml.Utils.Get_Element
+      while MX.PCI_Bus_Set.Has_Element (Position => Ctx_Pos) loop
+         declare
+            Ctx_Bus   : constant MX.PCI_Bus_Range
+              := MX.PCI_Bus_Set.Element (Position => Ctx_Pos);
+            Bus_Str   : constant String
+              := Mutools.Utils.To_Hex
+                (Number    => Interfaces.Unsigned_64 (Ctx_Bus),
+                 Normalize => False);
+            Bus_Str_N : constant String
+              := Mutools.Utils.To_Hex
+                (Number     => Interfaces.Unsigned_64 (Ctx_Bus),
+                 Byte_Short => True);
+            Filename  : constant String := "vtd_context_bus_" & Bus_Str;
+            Mem_Node  : constant DOM.Core.Node
+              := Muxml.Utils.Get_Element
                 (Doc   => Policy.Doc,
-                 XPath => "/system/memory/memory/file[@filename='vtd_root']"),
-              Name => "filename");
-      begin
-         while MX.PCI_Bus_Set.Has_Element (Position => Ctx_Pos) loop
-            declare
-               Ctx_Bus   : constant MX.PCI_Bus_Range
-                 := MX.PCI_Bus_Set.Element (Position => Ctx_Pos);
-               Bus_Str   : constant String
-                 := Mutools.Utils.To_Hex
-                   (Number    => Interfaces.Unsigned_64 (Ctx_Bus),
-                    Normalize => False);
-               Bus_Str_N : constant String
-                 := Mutools.Utils.To_Hex
-                   (Number     => Interfaces.Unsigned_64 (Ctx_Bus),
-                    Byte_Short => True);
-               Filename  : constant String := "vtd_context_bus_" & Bus_Str;
-               Mem_Node  : constant DOM.Core.Node
-                 := Muxml.Utils.Get_Element
-                   (Doc   => Policy.Doc,
-                    XPath => "/system/memory/memory/file[@filename='"
-                    & Filename & "']/..");
-               Ctx_Addr  : constant Tables.Table_Pointer_Type
-                 := Tables.Table_Pointer_Type'Value
-                   (DOM.Core.Elements.Get_Attribute
-                      (Elem => Mem_Node,
-                       Name => "physicalAddress"));
-            begin
-               Mulog.Log (Msg => "Adding root entry for PCI bus " & Bus_Str_N
-                          & ": " & Mutools.Utils.To_Hex
-                            (Number => Interfaces.Unsigned_64 (Ctx_Addr)));
-               Tables.Add_Entry (RT  => Root,
-                                 Bus => Tables.Table_Index_Type (Ctx_Bus),
-                                 CTP => Ctx_Addr);
-            end;
+                 XPath => "/system/memory/memory/file[@filename='"
+                 & Filename & "']/..");
+            Ctx_Addr  : constant Tables.Table_Pointer_Type
+              := Tables.Table_Pointer_Type'Value
+                (DOM.Core.Elements.Get_Attribute
+                   (Elem => Mem_Node,
+                    Name => "physicalAddress"));
+         begin
+            Mulog.Log (Msg => "Adding root entry for PCI bus " & Bus_Str_N
+                       & ": " & Mutools.Utils.To_Hex
+                         (Number => Interfaces.Unsigned_64 (Ctx_Addr)));
+            Tables.Add_Entry (RT  => Root,
+                              Bus => Tables.Table_Index_Type (Ctx_Bus),
+                              CTP => Ctx_Addr);
+         end;
 
-            MX.PCI_Bus_Set.Next (Position => Ctx_Pos);
-         end loop;
+         MX.PCI_Bus_Set.Next (Position => Ctx_Pos);
+      end loop;
 
-         Mulog.Log (Msg => "Writing VT-d root table to file '" & Output_Dir
-                    & "/" & Root_File & "'");
-         Tables.Serialize (RT       => Root,
-                           Filename => Output_Dir & "/" & Root_File);
-      end;
+      Mulog.Log (Msg => "Writing VT-d root table to file '" & Output_Dir & "/"
+                 & Root_File & "'");
+      Tables.Serialize (RT       => Root,
+                        Filename => Output_Dir & "/" & Root_File);
    end Write_Root_Table;
 
 end VTd.Generator;

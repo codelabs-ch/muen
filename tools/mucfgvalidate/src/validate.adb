badf50585374a492c7b1c0b85009cd9735618255
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-06-26 17:26:46 +0200

    Mucfgvalidate: Register PCI device BDF validator

--- tools/mucfgvalidate/src/validate.adb
+++ tools/mucfgvalidate/src/validate.adb
@@ -119,6 +119,8 @@ is
       XML_Processors.Register
         (Process => Device.Device_Sharing'Access);
       XML_Processors.Register
+        (Process => Device.PCI_Device_BDF_Uniqueness'Access);
+      XML_Processors.Register
         (Process => Scheduling.CPU_Element_Count'Access);
       XML_Processors.Register
         (Process => Scheduling.Subject_References'Access);

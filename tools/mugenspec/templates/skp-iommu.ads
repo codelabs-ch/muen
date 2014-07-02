464c920656b89ea56445f1b4643c916b016be4b7
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-07-02 11:29:14 +0200

    Mugenspec: Add register base address to IOMMU template
    The IOMMU register base address specifies the location of the
    memory-mapped IOMMU registers.

--- tools/mugenspec/templates/skp-iommu.ads
+++ tools/mugenspec/templates/skp-iommu.ads
@@ -4,6 +4,7 @@ package Skp.IOMMU
 is
 
    Root_Table_Address : constant := __root_table_addr__;
+   Base_Address       : constant := __base_addr__;
 
    type IOMMU_Device_Range is range __iommu_device_range__;
 

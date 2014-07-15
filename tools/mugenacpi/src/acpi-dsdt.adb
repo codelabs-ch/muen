2717211ca17b7744c0e289d43e1ea016b02f2f7c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-07-15 15:41:23 +0200

    Set reserved memory in DSDT according to policy
    Add subject device memory to current resource settings (_CRS) of PCI0
    device.

--- tools/mugenacpi/src/acpi-dsdt.adb
+++ tools/mugenacpi/src/acpi-dsdt.adb
@@ -16,15 +16,31 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+with Ada.Strings.Unbounded;
+
+with DOM.Core.Nodes;
+with DOM.Core.Elements;
+
+with McKae.XML.XPath.XIA;
+
 with Mutools.Utils;
 with Mutools.Templates;
 
 with Muxml.Utils;
 
+with Acpi.Asl;
+
 with String_Templates;
 
 package body Acpi.DSDT
 is
+
+   use Ada.Strings.Unbounded;
+
+   function Indent
+     (N         : Positive := 1;
+      Unit_Size : Positive := 4)
+      return String renames Mutools.Utils.Indent;
 
    -------------------------------------------------------------------------
 
@@ -33,10 +49,61 @@ is
       Subject  : DOM.Core.Node;
       Filename : String)
    is
-      pragma Unreferenced (Subject);
-
+      Devices  : constant DOM.Core.Node_List
+        := McKae.XML.XPath.XIA.XPath_Query
+             (N     => Subject,
+              XPath => "/system/platform/devices/device[memory]");
       Dsl_File : String := Filename;
       Tmpl     : Mutools.Templates.Template_Type;
+      Buffer   : Unbounded_String;
+
+      --  Add resources of given subject device memory to string buffer.
+      procedure Add_Device_Memory_Resources (Dev_Mem : DOM.Core.Node);
+
+      ----------------------------------------------------------------------
+
+      procedure Add_Device_Memory_Resources (Dev_Mem : DOM.Core.Node)
+      is
+         Log_Mem_Name  : constant String
+           := DOM.Core.Elements.Get_Attribute
+             (Elem => Dev_Mem,
+              Name => "logical");
+         Phys_Mem_Name : constant String
+           := DOM.Core.Elements.Get_Attribute
+             (Elem => Dev_Mem,
+              Name => "physical");
+         Virtual_Addr  : constant Interfaces.Unsigned_32
+           := Interfaces.Unsigned_32'Value
+             (DOM.Core.Elements.Get_Attribute
+                (Elem => Dev_Mem,
+                 Name => "virtualAddress"));
+         Log_Dev_Name  : constant String
+           := DOM.Core.Elements.Get_Attribute
+           (Elem => DOM.Core.Nodes.Parent_Node (N => Dev_Mem),
+            Name => "logical");
+         Phys_Dev_Name : constant String
+           := DOM.Core.Elements.Get_Attribute
+           (Elem => DOM.Core.Nodes.Parent_Node (N => Dev_Mem),
+            Name => "physical");
+         Physical_Dev  : constant DOM.Core.Node
+           := Muxml.Utils.Get_Element
+             (Nodes     => Devices,
+              Ref_Attr  => "name",
+              Ref_Value => Phys_Dev_Name);
+         Mem_Size      : constant Interfaces.Unsigned_32
+           := Interfaces.Unsigned_32'Value
+             (Muxml.Utils.Get_Attribute
+                (Doc   => Physical_Dev,
+                 XPath => "memory[@name='" & Phys_Mem_Name & "']",
+                 Name  => "size"));
+      begin
+         Buffer := Buffer & Indent (N => 5)
+           & "/* " & Log_Dev_Name & "->" & Log_Mem_Name & " */";
+         Buffer := Buffer & ASCII.LF & Indent (N => 5);
+         Buffer := Buffer & Asl.DWordMemory
+           (Base_Address => Virtual_Addr,
+            Size         => Mem_Size) & ASCII.LF;
+      end Add_Device_Memory_Resources;
    begin
       Dsl_File (Dsl_File'Last - 3 .. Dsl_File'Last) := ".dsl";
 
@@ -62,6 +129,30 @@ is
               (Number     => PCI_Cfg_Addr,
                Normalize  => False));
       end Set_PCI_Cfg_Space_Address;
+
+      Add_Device_Memory :
+      declare
+         Dev_Mem : constant DOM.Core.Node_List
+           := McKae.XML.XPath.XIA.XPath_Query
+             (N     => Subject,
+              XPath => "devices/device/memory[@physical!='mmconf']");
+         Count   : constant Natural
+           := DOM.Core.Nodes.Length (List => Dev_Mem);
+      begin
+         for I in 0 .. Count - 1 loop
+            Add_Device_Memory_Resources
+              (Dev_Mem => DOM.Core.Nodes.Item
+                 (List  => Dev_Mem,
+                  Index => I));
+         end loop;
+
+         Buffer := Buffer & Indent (N => 4);
+
+         Mutools.Templates.Replace
+           (Template => Tmpl,
+            Pattern  => "__reserved_memory__",
+            Content  => To_String (Buffer));
+      end Add_Device_Memory;
 
       Mutools.Templates.Write (Template => Tmpl,
                                Filename => Dsl_File);

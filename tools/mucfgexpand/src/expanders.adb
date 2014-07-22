be855a613ea4b24e5babeac2edfd606f6c9d247d
Reto Buerki <reet@codelabs.ch>
2014-07-22 19:28:06 +0200

    Mucfgexpand: Register Remove_Component_Attr expander

--- tools/mucfgexpand/src/expanders.adb
+++ tools/mucfgexpand/src/expanders.adb
@@ -57,6 +57,7 @@ is
       Procs.Register (Process => Subjects.Add_CPU_Ids'Access);
 
       Procs.Register (Process => Components.Remove_Components'Access);
+      Procs.Register (Process => Components.Remove_Component_Attr'Access);
       Procs.Register (Process => Platform.Add_PCI_Config_Space'Access);
       Procs.Register (Process => Memory.Add_Missing_Attributes'Access);
       Procs.Register (Process => Memory.Add_Kernel_Binary'Access);

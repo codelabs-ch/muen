ccfb3f3a326dd8daa8ef82cd5ffb4e8921ca308e
Reto Buerki <reet@codelabs.ch>
2014-07-25 14:40:01 +0200

    Register subject logical channel exports pre-check

--- tools/mucfgexpand/src/expand-pre_checks.adb
+++ tools/mucfgexpand/src/expand-pre_checks.adb
@@ -475,6 +475,7 @@ is
       Check_Procs.Register (Process => Subject_Monitor_References'Access);
       Check_Procs.Register (Process => Subject_Channel_References'Access);
       Check_Procs.Register (Process => Subject_Component_References'Access);
+      Check_Procs.Register (Process => Subject_Channel_Exports'Access);
       Check_Procs.Register (Process => Channel_Reader_Writer'Access);
       Check_Procs.Register (Process => Channel_Writer_Has_Event_ID'Access);
       Check_Procs.Register (Process => Channel_Reader_Has_Event_Vector'Access);

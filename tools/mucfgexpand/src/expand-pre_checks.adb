7d092c76f0d98adc6295f7f2f7a4a3502b16bf20
Reto Buerki <reet@codelabs.ch>
2014-08-28 15:22:11 +0200

    Register Subject_Resource_Maps_Physical_Uniqueness pre-check

--- tools/mucfgexpand/src/expand-pre_checks.adb
+++ tools/mucfgexpand/src/expand-pre_checks.adb
@@ -544,6 +544,8 @@ is
       Check_Procs.Register (Process => Tau0_Presence_In_Scheduling'Access);
       Check_Procs.Register
         (Process => Subject_Resource_Maps_Logical_Uniqueness'Access);
+      Check_Procs.Register
+        (Process => Subject_Resource_Maps_Physical_Uniqueness'Access);
       Check_Procs.Register (Process => Subject_Monitor_References'Access);
       Check_Procs.Register (Process => Subject_Channel_References'Access);
       Check_Procs.Register (Process => Subject_Component_References'Access);

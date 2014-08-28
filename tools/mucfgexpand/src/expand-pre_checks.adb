5ae9d1da0655814a4268f117ee2b33988c88ff60
Reto Buerki <reet@codelabs.ch>
2014-08-28 15:06:57 +0200

    Register Subject_Resource_Maps_Logical_Uniqueness pre-check

--- tools/mucfgexpand/src/expand-pre_checks.adb
+++ tools/mucfgexpand/src/expand-pre_checks.adb
@@ -478,6 +478,8 @@ is
         (Process => Mucfgcheck.Subject.Runnability'Access);
 
       Check_Procs.Register (Process => Tau0_Presence_In_Scheduling'Access);
+      Check_Procs.Register
+        (Process => Subject_Resource_Maps_Logical_Uniqueness'Access);
       Check_Procs.Register (Process => Subject_Monitor_References'Access);
       Check_Procs.Register (Process => Subject_Channel_References'Access);
       Check_Procs.Register (Process => Subject_Component_References'Access);

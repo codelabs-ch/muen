38cafbab2f73893faf841c129cc52d4df19de61b
Reto Buerki <reet@codelabs.ch>
2014-09-16 21:26:53 +0200

    Simplify command line args of muen-integ script
    Only the configuration file is needed for the script to work.

--- tools/scripts/integ/args.sh
+++ tools/scripts/integ/args.sh
@@ -1,11 +1,4 @@
-if [ ! $# -eq 3 ]; then
-	echo "Usage: $0 <config_file> <commit> <build ID>"
-	exit 1
-fi
-
 CONFIG=$1
-COMMIT=$2
-BUILDID=$3
 
 if [ ! -f $CONFIG ]; then
 	echo "Unable to read config file '$CONFIG'"

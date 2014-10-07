a561a8e40c1fd4626038cdcd76597aed226d6db3
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-10-07 17:52:02 +0200

    Crypter: Convert Crypt.Hasher package to SPARK 2014
    SPARK is currently deactivated for the Hash procedure since
    libsparkcrypto has not completely transitioned to SPARK 2014.

--- components/crypter/src/crypt-hasher.adb
+++ components/crypter/src/crypt-hasher.adb
@@ -36,23 +36,18 @@ is
    procedure SHA256_Hash
      (Input  :     Crypt.Message_Type;
       Output : out Crypt.Message_Type)
+   with
+      SPARK_Mode => Off
    is
       LSC_Hash : LSC.SHA256.SHA256_Hash_Type;
    begin
-      --# accept Warning, 13, To_SHA256_Msg, "Message to SHA256 conversion";
       LSC_Hash := LSC.SHA256.Hash
         (Message => To_SHA256_Msg (S => Input.Data),
          Length  => LSC.Types.Word64 (Input.Size * 8));
-      --# end accept;
 
       Output      := Crypt.Null_Message;
       Output.Size := 32;
       for I in LSC.SHA256.SHA256_Hash_Index loop
-
-         --# accept Warning, 444, "LSC_Hash is an array of LSC.Types.Word32.";
-         --# assume LSC_Hash (I) in LSC.Types.Word32;
-         --# end accept;
-
          Output.Data (Crypt.Data_Range (I * 4 + 1)) := SK.Byte'Mod
            (LSC_Hash (I));
          Output.Data (Crypt.Data_Range (I * 4 + 2)) := SK.Byte'Mod
--- components/crypter/src/crypt-hasher.ads
+++ components/crypter/src/crypt-hasher.ads
@@ -23,18 +23,15 @@ use type LSC.SHA256.Message_Index;
 use type LSC.SHA256.SHA256_Hash_Index;
 use type LSC.Types.Word32;
 
---# inherit
---#    Ada,
---#    SK,
---#    LSC.SHA256,
---#    LSC.Types,
---#    Crypt;
 package Crypt.Hasher
 is
 
    --  Calculate SHA256 hash of given input message and store in output.
    procedure SHA256_Hash
      (Input  :     Crypt.Message_Type;
-      Output : out Crypt.Message_Type);
+      Output : out Crypt.Message_Type)
+   with
+      Global  => null,
+      Depends => (Output => Input);
 
 end Crypt.Hasher;

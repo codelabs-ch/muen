bcebaf27289920854198ff5da38cd020e01b4b40
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-10-09 18:47:09 +0200

    Components: Add support for limited/complete proofs
    This is the same mechanism used by the kernel to disable certain proofs
    if not supported by the currently used gnatprove tool.

--- components/component_spark.mk
+++ components/component_spark.mk
@@ -1,13 +1,23 @@
+GNATPROVE_DATE  := $(shell gnatprove --version | cut -d "(" -f2 | cut -d ")" -f1)
+COMPLETE_PROOFS := $(shell expr $(GNATPROVE_DATE) \>= 20140901)
+ifeq ($(COMPLETE_PROOFS),1)
+PROOF_OPTS = -Xproofs=complete
+else
+PROOF_OPTS = -Xproofs=limited
+endif
+
 include ../../Makeconf
 include ../../Makespark
+
+SPARK_OPTS += $(PROOF_OPTS)
 
 all: $(ALL) install
 
 $(OBJ_DIR)/debug/$(COMPONENT):
-	gprbuild $(BUILD_OPTS) -P$(COMPONENT) -Xbuild=debug
+	gprbuild $(BUILD_OPTS) -P$(COMPONENT) -Xbuild=debug $(PROOF_OPTS)
 
 $(OBJ_DIR)/release/$(COMPONENT):
-	gprbuild $(BUILD_OPTS) -P$(COMPONENT) -Xbuild=release
+	gprbuild $(BUILD_OPTS) -P$(COMPONENT) -Xbuild=release $(PROOF_OPTS)
 
 $(OBJ_DIR)/$(COMPONENT): $(OBJ_DIR)/debug/$(COMPONENT) $(OBJ_DIR)/release/$(COMPONENT)
 	@cp $< $@

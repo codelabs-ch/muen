e5540213b4e19c3d98468a89bc084d4dd6b0a607
Reto Buerki <reet@codelabs.ch>
2014-10-14 10:35:37 +0200

    Convert Exit_Handlers.RDTSC package to SPARK 2014

--- components/sm/src/exit_handlers-rdtsc.ads
+++ components/sm/src/exit_handlers-rdtsc.ads
@@ -1,6 +1,6 @@
 --
---  Copyright (C) 2013  Reto Buerki <reet@codelabs.ch>
---  Copyright (C) 2013  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--  Copyright (C) 2013, 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2013, 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
@@ -16,10 +16,18 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+with Subject_Info;
+
 package Exit_Handlers.RDTSC
+with
+   Abstract_State => State,
+   Initializes    => State
 is
 
    --  Emulate RDTSC instruction.
-   procedure Process (Halt : out Boolean);
+   procedure Process (Halt : out Boolean)
+   with
+      Global  => (In_Out => (State, Subject_Info.State)),
+      Depends => (Halt => null, State =>+ null, Subject_Info.State =>+ State);
 
 end Exit_Handlers.RDTSC;

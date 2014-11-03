3328e5e693112697ee48b02cc9ce7ba5f44d2bf9
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-11-03 11:54:14 +0100

    Mucfgexpand: Implement major frame barriers expander
    This new expansion step adds barrier configurations to each major frame.
    A barrier config consists of a number of barriers, each barrier
    specifying its size (i.e. the number of CPUs synchronizing on that
    barrier).
    
    The implementation works as follows: Insert the exit time of each minor
    frame into an ordered list. The exit time of a minor frame is defined as
    the number of ticks from the beginning of the major frame until the
    minor frame ends. After all minor frames are inserted into the list,
    chronologically traverse the ordered list and add a barrier whenever
    multiple, consecutive exit times are identical. The size of the barrier
    is given by the number of list elements with the same exit time.

--- tools/mucfgexpand/src/expanders-scheduling.adb
+++ tools/mucfgexpand/src/expanders-scheduling.adb
@@ -16,15 +16,139 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+with Ada.Strings.Fixed;
+with Ada.Containers.Ordered_Multisets;
+
+with Interfaces;
+
+with DOM.Core.Nodes;
+with DOM.Core.Elements;
+with DOM.Core.Documents;
+
+with McKae.XML.XPath.XIA;
+
+with Mulog;
+with Muxml.Utils;
+
 package body Expanders.Scheduling
 is
+
+   use type Interfaces.Unsigned_64;
+
+   package Map_Of_Minor_Frame_Deadlines is new Ada.Containers.Ordered_Multisets
+     (Element_Type => Interfaces.Unsigned_64);
+
+   package MOMFD renames Map_Of_Minor_Frame_Deadlines;
 
    -------------------------------------------------------------------------
 
    procedure Add_Barrier_Configs (Data : in out Muxml.XML_Data_Type)
    is
+      Major_Frames : constant DOM.Core.Node_List
+        := McKae.XML.XPath.XIA.XPath_Query
+          (N     => Data.Doc,
+           XPath => "/system/scheduling/majorFrame");
    begin
-      null;
+      for I in 0 .. DOM.Core.Nodes.Length (List => Major_Frames) - 1 loop
+         declare
+            Major_Frame      : constant DOM.Core.Node
+              := DOM.Core.Nodes.Item
+                (List  => Major_Frames,
+                 Index => I);
+            CPU_Nodes        : constant DOM.Core.Node_List
+              := McKae.XML.XPath.XIA.XPath_Query
+                (N     => Major_Frame,
+                 XPath => "cpu");
+            Barriers_Node    : constant DOM.Core.Node
+              := DOM.Core.Documents.Create_Element
+                (Doc      => Data.Doc,
+                 Tag_Name => "barriers");
+            Minor_Exit_Times : MOMFD.Set;
+         begin
+            for J in 0 .. DOM.Core.Nodes.Length (List => CPU_Nodes) - 1 loop
+               declare
+                  CPU_Node      : constant DOM.Core.Node
+                    := DOM.Core.Nodes.Item
+                      (List  => CPU_Nodes,
+                       Index => J);
+                  Minor_Frames  : constant DOM.Core.Node_List
+                    := McKae.XML.XPath.XIA.XPath_Query
+                      (N     => CPU_Node,
+                       XPath => "minorFrame");
+                  Current_Ticks : Interfaces.Unsigned_64 := 0;
+               begin
+                  for K in 0 .. DOM.Core.Nodes.Length
+                    (List => Minor_Frames) - 1
+                  loop
+                     declare
+                        Minor_Frame : constant DOM.Core.Node
+                          := DOM.Core.Nodes.Item
+                            (List  => Minor_Frames,
+                             Index => K);
+                        Minor_Ticks : constant Interfaces.Unsigned_64
+                          := Interfaces.Unsigned_64'Value
+                            (DOM.Core.Elements.Get_Attribute
+                               (Elem => Minor_Frame,
+                                Name => "ticks"));
+                     begin
+                        Current_Ticks := Current_Ticks + Minor_Ticks;
+                        Minor_Exit_Times.Insert (New_Item => Current_Ticks);
+                     end;
+                  end loop;
+               end;
+            end loop;
+
+            declare
+               Cur_Ticks        : Interfaces.Unsigned_64 := 0;
+               Cur_Barrier_Idx  : Positive               := 1;
+               Cur_Barrier_Size : Positive               := 1;
+               Pos              : MOMFD.Cursor
+                 := MOMFD.First (Container => Minor_Exit_Times);
+            begin
+               while MOMFD.Has_Element (Position => Pos) loop
+                  declare
+                     Cur_Deadline : constant Interfaces.Unsigned_64
+                       := MOMFD.Element (Position => Pos);
+                  begin
+                     if Cur_Deadline = Cur_Ticks then
+                        Cur_Barrier_Size := Cur_Barrier_Size + 1;
+                     elsif Cur_Barrier_Size > 1 then
+                        declare
+                           Size_Str : constant String
+                             := Ada.Strings.Fixed.Trim
+                               (Source => Cur_Barrier_Size'Img,
+                                Side   => Ada.Strings.Left);
+                           Barrier_Node : constant DOM.Core.Node
+                             := DOM.Core.Documents.Create_Element
+                               (Doc      => Data.Doc,
+                                Tag_Name => "barrier");
+                        begin
+                           Mulog.Log
+                             (Msg => "Adding barrier to major frame" & I'Img
+                              & ": size " & Size_Str
+                              & ", ticks" & Cur_Ticks'Img);
+                           DOM.Core.Elements.Set_Attribute
+                             (Elem  => Barrier_Node,
+                              Name  => "size",
+                              Value => Size_Str);
+                           Muxml.Utils.Append_Child
+                             (Node      => Barriers_Node,
+                              New_Child => Barrier_Node);
+                        end;
+                        Cur_Barrier_Idx  := Cur_Barrier_Idx + 1;
+                        Cur_Barrier_Size := 1;
+                     end if;
+
+                     Cur_Ticks := Cur_Deadline;
+                     Pos       := MOMFD.Next (Position => Pos);
+                  end;
+               end loop;
+            end;
+            Muxml.Utils.Append_Child
+              (Node      => Major_Frame,
+               New_Child => Barriers_Node);
+         end;
+      end loop;
    end Add_Barrier_Configs;
 
 end Expanders.Scheduling;

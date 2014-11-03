353e7a067a4e12c4ed21e8db8c936e69e739232b
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-11-03 11:53:17 +0100

    Mucfgexpand: Add test for major frame barriers expander

---
+++ tools/mucfgexpand/src/expanders-scheduling.adb
@@ -0,0 +1,30 @@
+--
+--  Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+package body Expanders.Scheduling
+is
+
+   -------------------------------------------------------------------------
+
+   procedure Add_Barrier_Configs (Data : in out Muxml.XML_Data_Type)
+   is
+   begin
+      null;
+   end Add_Barrier_Configs;
+
+end Expanders.Scheduling;
---
+++ tools/mucfgexpand/tests/expanders-scheduling-test_data-tests.adb
@@ -0,0 +1,32 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Expanders.Scheduling.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Expanders.Scheduling.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Add_Barrier_Configs (Gnattest_T : in out Test);
+   procedure Test_Add_Barrier_Configs_057525 (Gnattest_T : in out Test) renames Test_Add_Barrier_Configs;
+--  id:2.2/057525ba5aaf3024/Add_Barrier_Configs/1/0/
+   procedure Test_Add_Barrier_Configs (Gnattest_T : in out Test) is
+   --  expanders-scheduling.ads:25:4:Add_Barrier_Configs
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+      Test_Utils.Expander.Run_Test
+        (Filename     => "obj/scheduling_barrier_configs.xml",
+         Ref_Filename => "data/scheduling_barrier_configs.ref.xml",
+         Expander     => Add_Barrier_Configs'Access);
+--  begin read only
+   end Test_Add_Barrier_Configs;
+--  end read only
+
+end Expanders.Scheduling.Test_Data.Tests;

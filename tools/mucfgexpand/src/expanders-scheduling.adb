88a26e344bcbef43f1bd185069d7767875451152
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-11-03 15:23:11 +0100

    Mucfgexpand: Extend barrier config expansion
    Set barrier attribute of minor frames. Note that the last minor frame(s)
    of a major frame do not specify a barrier since all CPUs are
    synchronized when a major frame ends (or starts).

--- tools/mucfgexpand/src/expanders-scheduling.adb
+++ tools/mucfgexpand/src/expanders-scheduling.adb
@@ -35,8 +35,21 @@ is
 
    use type Interfaces.Unsigned_64;
 
+   --  Minor frame and its exit time in ticks measured from the start of the
+   --  corresponding major frame.
+   type Deadline_Type is record
+      Exit_Time   : Interfaces.Unsigned_64;
+      Minor_Frame : DOM.Core.Node;
+   end record;
+
+   --  Chronologicaly orders deadlines according to exit time.
+   function "<"
+     (Left, Right : Deadline_Type)
+      return Boolean
+   is (Left.Exit_Time < Right.Exit_Time);
+
    package Map_Of_Minor_Frame_Deadlines is new Ada.Containers.Ordered_Multisets
-     (Element_Type => Interfaces.Unsigned_64);
+     (Element_Type => Deadline_Type);
 
    package MOMFD renames Map_Of_Minor_Frame_Deadlines;
 
@@ -92,55 +105,83 @@ is
                                 Name => "ticks"));
                      begin
                         Current_Ticks := Current_Ticks + Minor_Ticks;
-                        Minor_Exit_Times.Insert (New_Item => Current_Ticks);
+                        Minor_Exit_Times.Insert
+                          (New_Item => (Exit_Time   => Current_Ticks,
+                                        Minor_Frame => Minor_Frame));
                      end;
                   end loop;
                end;
             end loop;
 
             declare
-               Cur_Ticks        : Interfaces.Unsigned_64 := 0;
-               Cur_Barrier_Idx  : Positive               := 1;
-               Cur_Barrier_Size : Positive               := 1;
+               Major_End_Ticks  : constant Interfaces.Unsigned_64
+                 := Minor_Exit_Times.Last_Element.Exit_Time;
+               Cur_Barrier_Idx  : Positive := 1;
+               Cur_Barrier_Size : Positive := 1;
+               Prev_Deadline    : Deadline_Type
+                 := (Exit_Time   => 0,
+                     Minor_Frame => null);
                Pos              : MOMFD.Cursor
                  := MOMFD.First (Container => Minor_Exit_Times);
             begin
                while MOMFD.Has_Element (Position => Pos) loop
                   declare
-                     Cur_Deadline : constant Interfaces.Unsigned_64
+                     Cur_Deadline : constant Deadline_Type
                        := MOMFD.Element (Position => Pos);
                   begin
-                     if Cur_Deadline = Cur_Ticks then
+                     if Cur_Deadline.Exit_Time = Prev_Deadline.Exit_Time
+                       and then Cur_Deadline.Exit_Time /= Major_End_Ticks
+                     then
                         Cur_Barrier_Size := Cur_Barrier_Size + 1;
-                     elsif Cur_Barrier_Size > 1 then
-                        declare
-                           Size_Str : constant String
-                             := Ada.Strings.Fixed.Trim
-                               (Source => Cur_Barrier_Size'Img,
-                                Side   => Ada.Strings.Left);
-                           Barrier_Node : constant DOM.Core.Node
-                             := DOM.Core.Documents.Create_Element
-                               (Doc      => Data.Doc,
-                                Tag_Name => "barrier");
-                        begin
-                           Mulog.Log
-                             (Msg => "Adding barrier to major frame" & I'Img
-                              & ": size " & Size_Str
-                              & ", ticks" & Cur_Ticks'Img);
+                        if Cur_Barrier_Size = 2 then
                            DOM.Core.Elements.Set_Attribute
-                             (Elem  => Barrier_Node,
-                              Name  => "size",
-                              Value => Size_Str);
-                           Muxml.Utils.Append_Child
-                             (Node      => Barriers_Node,
-                              New_Child => Barrier_Node);
-                        end;
-                        Cur_Barrier_Idx  := Cur_Barrier_Idx + 1;
-                        Cur_Barrier_Size := 1;
+                             (Elem  => Prev_Deadline.Minor_Frame,
+                              Name  => "barrier",
+                              Value => Ada.Strings.Fixed.Trim
+                                (Source => Cur_Barrier_Idx'Img,
+                                 Side   => Ada.Strings.Left));
+                        end if;
+                        DOM.Core.Elements.Set_Attribute
+                          (Elem  => Cur_Deadline.Minor_Frame,
+                           Name  => "barrier",
+                           Value => Ada.Strings.Fixed.Trim
+                             (Source => Cur_Barrier_Idx'Img,
+                              Side   => Ada.Strings.Left));
+                     else
+                        if Cur_Barrier_Size > 1 then
+                           declare
+                              Size_Str : constant String
+                                := Ada.Strings.Fixed.Trim
+                                  (Source => Cur_Barrier_Size'Img,
+                                   Side   => Ada.Strings.Left);
+                              Barrier_Node : constant DOM.Core.Node
+                                := DOM.Core.Documents.Create_Element
+                                  (Doc      => Data.Doc,
+                                   Tag_Name => "barrier");
+                           begin
+                              Mulog.Log
+                                (Msg => "Adding barrier to major frame"
+                                 & I'Img & ": size " & Size_Str
+                                 & ", ticks" & Prev_Deadline.Exit_Time'Img);
+                              DOM.Core.Elements.Set_Attribute
+                                (Elem  => Barrier_Node,
+                                 Name  => "size",
+                                 Value => Size_Str);
+                              Muxml.Utils.Append_Child
+                                (Node      => Barriers_Node,
+                                 New_Child => Barrier_Node);
+                           end;
+                           Cur_Barrier_Size := 1;
+                           Cur_Barrier_Idx  := Cur_Barrier_Idx + 1;
+                        end if;
+                        DOM.Core.Elements.Set_Attribute
+                          (Elem  => Cur_Deadline.Minor_Frame,
+                           Name  => "barrier",
+                           Value => "none");
                      end if;
 
-                     Cur_Ticks := Cur_Deadline;
-                     Pos       := MOMFD.Next (Position => Pos);
+                     Prev_Deadline := Cur_Deadline;
+                     Pos           := MOMFD.Next (Position => Pos);
                   end;
                end loop;
             end;

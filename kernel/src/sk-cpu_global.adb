66faa252580b2aeb73630ac858fdaf20cd6dfca2
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-11-27 16:34:31 +0100

    Kernel: Remove scheduling plans from CPU_Global storage

--- kernel/src/sk-cpu_global.adb
+++ kernel/src/sk-cpu_global.adb
@@ -32,7 +32,6 @@ is
 
    --  Record used to store per-CPU global data.
    type Storage_Type is record
-      Scheduling_Plan     : Skp.Scheduling.Major_Frame_Array;
       Scheduling_Groups   : Skp.Scheduling.Scheduling_Group_Array;
       Current_Minor_Frame : Active_Minor_Frame_Type;
    end record;
@@ -120,8 +119,7 @@ is
    begin
       Current_Major_Frame := Skp.Scheduling.Major_Frame_Range'First;
       Per_CPU_Storage     := Storage_Type'
-        (Scheduling_Plan     => Skp.Scheduling.Null_Major_Frames,
-         Scheduling_Groups   => (others => Skp.Subject_Id_Type'First),
+        (Scheduling_Groups   => (others => Skp.Subject_Id_Type'First),
          Current_Minor_Frame => Active_Minor_Frame_Type'
            (Minor_Id   => Skp.Scheduling.Minor_Frame_Range'First));
    end Init;

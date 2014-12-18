0a217eb60622dc9cbdbac6fafaa4ac68cd62ef62
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2014-12-18 14:07:24 +0100

    Libmucfgcheck: Factor out subject region check
    Add new Check_Subject_Region_Mappings procedure that checks kernel mappings of
    subject memory regions with specified region type. The following checks
    are performed:
    
     * Each region is mapped by exactly one kernel
     * CPU IDs of kernel and subject are identical
     * Virtual addresses of kernel mappings are relative to the same base address
    
    If Check_Subject_Mappings is True the following additional check is performed:
    
     * Each region is mapped by exactly one subject
    
    The new utility procedure will be used in the upcoming subject state
    mappings validator.

--- tools/libmucfgcheck/src/mucfgcheck-memory.adb
+++ tools/libmucfgcheck/src/mucfgcheck-memory.adb
@@ -42,6 +42,175 @@ is
    --  Returns True if the left and right memory regions are adjacent.
    function Is_Adjacent_Region (Left, Right : DOM.Core.Node) return Boolean;
 
+   --  Checks kernel mappings of subject memory regions with specified region
+   --  type. The following checks are performed:
+   --   * Each region is mapped by exactly one kernel
+   --   * CPU IDs of kernel and subject are identical
+   --   * Virtual addresses of kernel mappings are relative to the same base
+   --     address
+   --  If Check_Subject_Mappings is True the following additional check is
+   --  performed:
+   --   * Each region is mapped by exactly one subject
+   procedure Check_Subject_Region_Mappings
+     (Data                   : Muxml.XML_Data_Type;
+      Mapping_Name           : String;
+      Region_Type            : String;
+      Check_Subject_Mappings : Boolean);
+
+   -------------------------------------------------------------------------
+
+   procedure Check_Subject_Region_Mappings
+     (Data                   : Muxml.XML_Data_Type;
+      Mapping_Name           : String;
+      Region_Type            : String;
+      Check_Subject_Mappings : Boolean)
+   is
+      Nodes           : constant DOM.Core.Node_List
+        := XPath_Query
+          (N     => Data.Doc,
+           XPath => "/system/memory/memory[@type='" & Region_Type & "']");
+      Kernel_Mappings : constant DOM.Core.Node_List
+        := XPath_Query
+          (N     => Data.Doc,
+           XPath => "/system/kernel/memory/cpu/memory");
+      Subj_Mappings   : constant DOM.Core.Node_List
+        := XPath_Query
+          (N     => Data.Doc,
+           XPath => "/system/subjects/subject/memory/memory");
+      Subjects        : constant DOM.Core.Node_List
+        := XPath_Query
+          (N     => Data.Doc,
+           XPath => "/system/subjects/subject");
+
+      Virt_Timer_Base_Addr : Interfaces.Unsigned_64 := 0;
+   begin
+      Mulog.Log (Msg => "Checking mapping of" & DOM.Core.Nodes.Length
+                 (List => Nodes)'Img & " " & Mapping_Name
+                 & " memory region(s)");
+
+      for I in 0 .. DOM.Core.Nodes.Length (List => Nodes) - 1 loop
+         declare
+            use type DOM.Core.Node;
+
+            Phys_Mem         : constant DOM.Core.Node
+              := DOM.Core.Nodes.Item
+                (List  => Nodes,
+                 Index => I);
+            Phys_Name        : constant String
+              := DOM.Core.Elements.Get_Attribute
+                (Elem => Phys_Mem,
+                 Name => "name");
+            Kernel_Mem       : constant DOM.Core.Node_List
+              := Muxml.Utils.Get_Elements
+                (Nodes     => Kernel_Mappings,
+                 Ref_Attr  => "physical",
+                 Ref_Value => Phys_Name);
+            Kernel_Map_Count : constant Natural
+              := DOM.Core.Nodes.Length (List => Kernel_Mem);
+         begin
+            if Kernel_Map_Count = 0 then
+               raise Validation_Error with Mutools.Utils.Capitalize
+                 (Str => Mapping_Name) & " memory region '" & Phys_Name
+                 & "' is not mapped by any kernel";
+            elsif Kernel_Map_Count > 1 then
+               raise Validation_Error with Mutools.Utils.Capitalize
+                 (Str => Mapping_Name) & " memory region '" & Phys_Name
+                 & "' has multiple kernel mappings:" & Kernel_Map_Count'Img;
+            end if;
+
+            if Check_Subject_Mappings then
+               declare
+                  Subj_Mem       : constant DOM.Core.Node_List
+                    := Muxml.Utils.Get_Elements
+                      (Nodes     => Subj_Mappings,
+                       Ref_Attr  => "physical",
+                       Ref_Value => Phys_Name);
+                  Subj_Map_Count : constant Natural
+                    := DOM.Core.Nodes.Length (List => Subj_Mem);
+               begin
+                  if Subj_Map_Count = 0 then
+                     raise Validation_Error with Mutools.Utils.Capitalize
+                       (Str => Mapping_Name) & " memory region '" & Phys_Name
+                       & "' is not mapped by any subject";
+                  elsif Subj_Map_Count > 1 then
+                     raise Validation_Error with Mutools.Utils.Capitalize
+                       (Str => Mapping_Name) & " memory region '" & Phys_Name
+                       & "' has multiple subject mappings:"
+                       & Subj_Map_Count'Img;
+                  end if;
+               end;
+            end if;
+
+            declare
+               Kernel_Mem_Node : constant DOM.Core.Node
+                 := DOM.Core.Nodes.Item (List  => Kernel_Mem,
+                                         Index => 0);
+               Kernel_Mem_Addr : constant Interfaces.Unsigned_64
+                 := Interfaces.Unsigned_64'Value
+                   (DOM.Core.Elements.Get_Attribute
+                      (Elem => Kernel_Mem_Node,
+                       Name => "virtualAddress"));
+               Kernel_CPU_ID   : constant Natural
+                 := Natural'Value
+                   (DOM.Core.Elements.Get_Attribute
+                      (Elem => DOM.Core.Nodes.Parent_Node
+                         (N => Kernel_Mem_Node),
+                       Name => "id"));
+               Subj_Node       : constant DOM.Core.Node
+                 := Muxml.Utils.Get_Element
+                   (Nodes     => Subjects,
+                    Ref_Attr  => "name",
+                    Ref_Value => Mutools.Utils.Decode_Entity_Name
+                      (Encoded_Str => Phys_Name));
+               Subj_CPU_ID     : constant Natural
+                 := Natural'Value
+                   (DOM.Core.Elements.Get_Attribute
+                      (Elem => Subj_Node,
+                       Name => "cpu"));
+               Subj_ID         : constant Natural
+                 := Natural'Value
+                   (DOM.Core.Elements.Get_Attribute
+                      (Elem => Subj_Node,
+                       Name => "id"));
+
+               --  The expected virtual address of the kernel timer mapping is
+               --  Base_Address + (Subject_ID * Page_Size).
+
+               Cur_Base_Addr   : constant Interfaces.Unsigned_64
+                 := Kernel_Mem_Addr - Interfaces.Unsigned_64
+                   (Subj_ID * Mutools.Constants.Page_Size);
+            begin
+               if Kernel_CPU_ID /= Subj_CPU_ID then
+                  raise Validation_Error with Mutools.Utils.Capitalize
+                    (Str => Mapping_Name) & " memory region '" & Phys_Name
+                    & "' mapped by kernel and subject '"
+                    & DOM.Core.Elements.Get_Attribute (Elem => Subj_Node,
+                                                       Name => "name")
+                    & "' with different CPU ID:" & Kernel_CPU_ID'Img & " /="
+                    & Subj_CPU_ID'Img;
+               end if;
+
+               if Virt_Timer_Base_Addr = 0 then
+                  Virt_Timer_Base_Addr := Cur_Base_Addr;
+               elsif Virt_Timer_Base_Addr /= Cur_Base_Addr then
+                  declare
+                     Expected_Addr : constant Interfaces.Unsigned_64
+                       := Virt_Timer_Base_Addr + Interfaces.Unsigned_64
+                         (Subj_ID * Mutools.Constants.Page_Size);
+                  begin
+                     raise Validation_Error with Mutools.Utils.Capitalize
+                       (Str => Mapping_Name) & " memory region '" & Phys_Name
+                       & "' mapped at unexpected kernel virtual address "
+                       & Mutools.Utils.To_Hex (Number => Kernel_Mem_Addr)
+                       & ", should be "
+                       & Mutools.Utils.To_Hex (Number => Expected_Addr);
+                  end;
+               end if;
+            end;
+         end;
+      end loop;
+   end Check_Subject_Region_Mappings;
+
    -------------------------------------------------------------------------
 
    procedure Entity_Name_Encoding (XML_Data : Muxml.XML_Data_Type)
@@ -569,139 +738,12 @@ is
 
    procedure Timer_Memory_Mappings (XML_Data : Muxml.XML_Data_Type)
    is
-      Nodes           : constant DOM.Core.Node_List
-        := XPath_Query
-          (N     => XML_Data.Doc,
-           XPath => "/system/memory/memory[@type='subject_timer']");
-      Kernel_Mappings : constant DOM.Core.Node_List
-        := XPath_Query
-          (N     => XML_Data.Doc,
-           XPath => "/system/kernel/memory/cpu/memory");
-      Subj_Mappings   : constant DOM.Core.Node_List
-        := XPath_Query
-          (N     => XML_Data.Doc,
-           XPath => "/system/subjects/subject/memory/memory");
-      Subjects        : constant DOM.Core.Node_List
-        := XPath_Query
-          (N     => XML_Data.Doc,
-           XPath => "/system/subjects/subject");
-
-      Virt_Timer_Base_Addr : Interfaces.Unsigned_64 := 0;
    begin
-      Mulog.Log (Msg => "Checking mapping of" & DOM.Core.Nodes.Length
-                 (List => Nodes)'Img & " timer memory region(s)");
-
-      for I in 0 .. DOM.Core.Nodes.Length (List => Nodes) - 1 loop
-         declare
-            use type DOM.Core.Node;
-
-            Phys_Mem         : constant DOM.Core.Node
-              := DOM.Core.Nodes.Item
-                (List  => Nodes,
-                 Index => I);
-            Phys_Name        : constant String
-              := DOM.Core.Elements.Get_Attribute
-                (Elem => Phys_Mem,
-                 Name => "name");
-            Kernel_Mem       : constant DOM.Core.Node_List
-              := Muxml.Utils.Get_Elements
-                (Nodes     => Kernel_Mappings,
-                 Ref_Attr  => "physical",
-                 Ref_Value => Phys_Name);
-            Kernel_Map_Count : constant Natural
-              := DOM.Core.Nodes.Length (List => Kernel_Mem);
-            Subj_Mem         : constant DOM.Core.Node_List
-              := Muxml.Utils.Get_Elements
-                (Nodes     => Subj_Mappings,
-                 Ref_Attr  => "physical",
-                 Ref_Value => Phys_Name);
-            Subj_Map_Count   : constant Natural
-              := DOM.Core.Nodes.Length (List => Subj_Mem);
-         begin
-            if Kernel_Map_Count = 0 then
-               raise Validation_Error with "Timer memory region '"
-                 & Phys_Name & "' is not mapped by any kernel";
-            elsif Kernel_Map_Count > 1 then
-               raise Validation_Error with "Timer memory region '"
-                 & Phys_Name & "' has multiple kernel mappings:"
-                 & Kernel_Map_Count'Img;
-            end if;
-
-            if Subj_Map_Count = 0 then
-               raise Validation_Error with "Timer memory region '"
-                 & Phys_Name & "' is not mapped by any subject";
-            elsif Subj_Map_Count > 1 then
-               raise Validation_Error with "Timer memory region '"
-                 & Phys_Name & "' has multiple subject mappings:"
-                 & Subj_Map_Count'Img;
-            end if;
-
-            declare
-               Kernel_Mem_Node : constant DOM.Core.Node
-                 := DOM.Core.Nodes.Item (List  => Kernel_Mem,
-                                         Index => 0);
-               Kernel_Mem_Addr : constant Interfaces.Unsigned_64
-                 := Interfaces.Unsigned_64'Value
-                   (DOM.Core.Elements.Get_Attribute
-                      (Elem => Kernel_Mem_Node,
-                       Name => "virtualAddress"));
-               Kernel_CPU_ID   : constant Natural
-                 := Natural'Value
-                   (DOM.Core.Elements.Get_Attribute
-                      (Elem => DOM.Core.Nodes.Parent_Node
-                         (N => Kernel_Mem_Node),
-                       Name => "id"));
-               Subj_Node       : constant DOM.Core.Node
-                 := Muxml.Utils.Get_Element
-                   (Nodes     => Subjects,
-                    Ref_Attr  => "name",
-                    Ref_Value => Mutools.Utils.Decode_Entity_Name
-                      (Encoded_Str => Phys_Name));
-               Subj_CPU_ID     : constant Natural
-                 := Natural'Value
-                   (DOM.Core.Elements.Get_Attribute
-                      (Elem => Subj_Node,
-                       Name => "cpu"));
-               Subj_ID         : constant Natural
-                 := Natural'Value
-                   (DOM.Core.Elements.Get_Attribute
-                      (Elem => Subj_Node,
-                       Name => "id"));
-
-               --  The expected virtual address of the kernel timer mapping is
-               --  Base_Address + (Subject_ID * Page_Size).
-
-               Cur_Base_Addr   : constant Interfaces.Unsigned_64
-                 := Kernel_Mem_Addr - Interfaces.Unsigned_64
-                   (Subj_ID * Mutools.Constants.Page_Size);
-            begin
-               if Kernel_CPU_ID /= Subj_CPU_ID then
-                  raise Validation_Error with "Timer memory region '"
-                    & Phys_Name & "' mapped by kernel and subject '"
-                    & DOM.Core.Elements.Get_Attribute (Elem => Subj_Node,
-                                                       Name => "name")
-                    & "' with different CPU ID:" & Kernel_CPU_ID'Img & " /="
-                    & Subj_CPU_ID'Img;
-               end if;
-
-               if Virt_Timer_Base_Addr = 0 then
-                  Virt_Timer_Base_Addr := Cur_Base_Addr;
-               elsif Virt_Timer_Base_Addr /= Cur_Base_Addr then
-                  declare
-                     Expected_Addr : constant Interfaces.Unsigned_64
-                       := Virt_Timer_Base_Addr + Interfaces.Unsigned_64
-                         (Subj_ID * Mutools.Constants.Page_Size);
-                  begin
-                     raise Validation_Error with "Timer memory region '"
-                       & Phys_Name & "' mapped at unexpected kernel virtual "
-                       & "address " & Mutools.Utils.To_Hex
-                       (Number => Kernel_Mem_Addr) & ", should be "
-                       & Mutools.Utils.To_Hex (Number => Expected_Addr);
-                  end;
-               end if;
-            end;
-         end;
-      end loop;
+      Check_Subject_Region_Mappings
+        (Data                   => XML_Data,
+         Mapping_Name           => "timer",
+         Region_Type            => "subject_timer",
+         Check_Subject_Mappings => True);
    end Timer_Memory_Mappings;
 
    -------------------------------------------------------------------------

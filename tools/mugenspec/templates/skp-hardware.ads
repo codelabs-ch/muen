c2aea075d1298e69f6180fb98719fc7839c9d3eb
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-01-22 23:53:21 +0100

    Mugenspec: Add MMConf base addr pattern to Skp.Hardware
    The MMConf_Base_Address constant specifies the start address of the PCI
    config space.

--- tools/mugenspec/templates/skp-hardware.ads
+++ tools/mugenspec/templates/skp-hardware.ads
@@ -1,6 +1,7 @@
 package Skp.Hardware
 is
 
-   Debugconsole_Port : constant := __debug_console_port__;
+   Debugconsole_Port   : constant := __debug_console_port__;
+   MMConf_Base_Address : constant := __mmconf_base_addr__;
 
 end Skp.Hardware;

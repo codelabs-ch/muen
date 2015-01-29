974f95405c51edca2cf33cc586ba9e8d91eb1199
Reto Buerki <reet@codelabs.ch>
2015-01-29 16:24:27 +0100

    Rename Port variable to Base_Address

--- common/debug/sk-console_serial.adb
+++ common/debug/sk-console_serial.adb
@@ -23,10 +23,10 @@ with SK.IO;
 package body SK.Console_Serial
 is
 
-   --  Serial output address.
-   Port    : constant := Skp.Hardware.Debugconsole_Port;
+   --  Serial output base address.
+   Base_Address : constant := Skp.Hardware.Debugconsole_Port;
    --  Baud rate: 115200
-   Divisor : constant := 1;
+   Divisor      : constant := 1;
 
    --  Return True if the send buffer is empty.
    function Empty_Send_Buffer return Boolean;
@@ -37,7 +37,7 @@ is
    is
       Data : Byte;
    begin
-      IO.Inb (Port  => Port + 5,
+      IO.Inb (Port  => Base_Address + 5,
               Value => Data);
       return (Data and 16#20#) /= 0;
    end Empty_Send_Buffer;
@@ -50,34 +50,34 @@ is
 
       --  Disable interrupts.
 
-      IO.Outb (Port  => Port + 1,
+      IO.Outb (Port  => Base_Address + 1,
                Value => 0);
 
       --  Enable DLAB.
 
-      IO.Outb (Port  => Port + 3,
+      IO.Outb (Port  => Base_Address + 3,
                Value => 16#80#);
 
       --  Set divisor (least/most significant byte).
 
-      IO.Outb (Port  => Port,
+      IO.Outb (Port  => Base_Address,
                Value => Divisor);
-      IO.Outb (Port  => Port + 1,
+      IO.Outb (Port  => Base_Address + 1,
                Value => 0);
 
       --  Clear DLAB and set 8 bits, no parity, one stop bit (8N1).
 
-      IO.Outb (Port  => Port + 3,
+      IO.Outb (Port  => Base_Address + 3,
                Value => 3);
 
       --  Enable FIFO.
 
-      IO.Outb (Port  => Port + 2,
+      IO.Outb (Port  => Base_Address + 2,
                Value => 16#c7#);
 
       --  IRQS enabled, RTS/DSR set.
 
-      IO.Outb (Port  => Port + 4,
+      IO.Outb (Port  => Base_Address + 4,
                Value => 16#0b#);
    end Init;
 
@@ -99,7 +99,7 @@ is
          null;
       end loop;
 
-      IO.Outb (Port  => Port,
+      IO.Outb (Port  => Base_Address,
                Value => Character'Pos (Item));
    end Put_Char;
 

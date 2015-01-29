f06a79dbccf7e67267657a67fcbb17a1433279f7
Reto Buerki <reet@codelabs.ch>
2015-01-29 16:20:43 +0100

    Drop SPARK_Mode => Off from SK.Console_Serial

--- common/debug/sk-console_serial.adb
+++ common/debug/sk-console_serial.adb
@@ -1,6 +1,6 @@
 --
---  Copyright (C) 2013  Reto Buerki <reet@codelabs.ch>
---  Copyright (C) 2013  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--  Copyright (C) 2013-2015  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2013-2015  Adrian-Ken Rueegsegger <ken@codelabs.ch>
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
@@ -21,8 +21,6 @@ with Skp.Hardware;
 with SK.IO;
 
 package body SK.Console_Serial
-with
-   SPARK_Mode => Off
 is
 
    --  Serial output address.
--- common/debug/sk-console_serial.ads
+++ common/debug/sk-console_serial.ads
@@ -1,6 +1,6 @@
 --
---  Copyright (C) 2013  Reto Buerki <reet@codelabs.ch>
---  Copyright (C) 2013  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--  Copyright (C) 2013-2015  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2013-2015  Adrian-Ken Rueegsegger <ken@codelabs.ch>
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
@@ -17,8 +17,6 @@
 --
 
 package SK.Console_Serial
-with
-   SPARK_Mode => Off
 is
 
    --  Initialize serial port.

022406b8ac94c06b601593fc4725bec1da1e902a
Reto Buerki <reet@codelabs.ch>
2015-02-20 09:34:37 +0100

    Call gnattest runner with --exit-status option
    The default (on) lets the runner fail with a non-zero exit status if a
    test fails. The GNATTEST_EXIT environment variable can be set to
    override the default behavior.

--- tools/component.mk
+++ tools/component.mk
@@ -1,8 +1,9 @@
 include ../../Makeconf
 
-GNATTEST_RUNNER = $(OBJ_DIR)/tests/gnattest/harness/test_runner
-GNATTEST_DRIVER = $(OBJ_DIR)/tests/gnattest/harness/test_driver
-TESTS_DIR       = $(CURDIR)/tests
+GNATTEST_EXIT   ?= on
+GNATTEST_RUNNER  = $(OBJ_DIR)/tests/gnattest/harness/test_runner
+GNATTEST_DRIVER  = $(OBJ_DIR)/tests/gnattest/harness/test_driver
+TESTS_DIR        = $(CURDIR)/tests
 
 SRC_FILES  = $(wildcard $(SRC_DIR)/*)
 SRC_FILES += $(wildcard $(TESTS_DIR)/additional/*)
@@ -23,7 +24,7 @@ build_tests: $(TEST_TARGETS) $(OBJ_DIR)/.harness_stamp
 		-largs -fprofile-generate
 
 tests: build_tests
-	$(GNATTEST_RUNNER)
+	$(GNATTEST_RUNNER) --exit-status=$(GNATTEST_EXIT)
 
 install: $(COMPONENT)
 	install -m 755 -D bin/$(COMPONENT) $(PREFIX)/bin/$(COMPONENT)

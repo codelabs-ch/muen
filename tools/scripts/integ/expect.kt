4ba64520de6f34d3d8b332f792ced7fe05f6d914
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-03-09 11:48:34 +0100

    Integration: Adjust Kontron expect strings
    Assert that the SATA controller is succesfully passed through and the
    disk is properly recognized and ready to be mounted. Since the proper
    IRQ routing is also verified, interrupt routing to PCI Pins other than
    Pin A is now also checked during integration.

--- tools/scripts/integ/expect.kt
+++ tools/scripts/integ/expect.kt
@@ -1,5 +1,6 @@
 serial.out;pci 0000:00:19.0: \[8086:1502\] type 00 class 0x020000
 serial.out;pci 0000:00:1f.0: \[8086:10d3\] type 00 class 0x020000
+serial.out;pci 0000:00:1f.0: \[8086:1e03\] type 00 class 0x010601
 serial.out;pci 0000:00:19.0: BAR 0: assigned \[mem 0xf7f00000-0xf7f1ffff\]
 serial.out;pci 0000:00:19.0: BAR 1: assigned \[mem 0xf7f39000-0xf7f39fff\]
 serial.out;pci 0000:00:19.0: BAR 2: assigned \[io  0x1000-0x101f\]
@@ -10,5 +11,13 @@ serial.out;e1000e 0000:00:19.0 eth0: MAC: 10, PHY: 11, PBA No: FFFFFF-0FF
 serial.out;e1000e 0000:00:1f.0 eth1: MAC: 3, PHY: 8, PBA No: FFFFFF-0FF
 serial.out;00:19.0 Ethernet controller: Intel Corporation 82579LM Gigabit Network Connection (rev 04)
 serial.out;00:1f.0 Ethernet controller: Intel Corporation 82574L Gigabit Network Connection
+serial.out;ahci 0000:00:1f.0: version 3.0
+serial.out;PCI: setting IRQ 11 as level-triggered
+serial.out;ahci 0000:00:1f.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x2 impl SATA mode
+serial.out;ahci 0000:00:1f.0: flags: 64bit ncq pm led clo pio slum part ems apst
+serial.out;ahci 0000:00:1f.0: setting latency timer to 64
+serial.out;ata2: SATA max UDMA/133 abar m2048@0xf7f36000 port 0xf7f36180 irq 11
+serial.out;ata2: SATA link up 3.0 Gbps (SStatus 123 SControl 300)
+serial.out;sd 1:0:0:0: \[sda\] Attached SCSI disk
 serial.out;64 bytes from 192.168.254.1: seq=.* ttl=.* time=.* ms
 serial.out;*** '/usr/bin/integtest_kt' DONE

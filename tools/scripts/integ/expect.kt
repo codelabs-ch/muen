d68621c509007c0b561d337e305f3980345ba492
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-05-29 12:03:23 +0200

    integ: Update muen-msi assert messages
    Adjust assert message since all muen-msi log messages now start with
    uppercase.

--- tools/scripts/integ/expect.kt
+++ tools/scripts/integ/expect.kt
@@ -14,19 +14,19 @@ serial.out;00:19.0 Ethernet controller: Intel Corporation 82579LM Gigabit Networ
 serial.out;00:1f.0 Ethernet controller: Intel Corporation 82574L Gigabit Network Connection
 serial.out;ahci 0000:00:1f.0: version 3.0
 serial.out;PCI: setting IRQ 11 as level-triggered
-serial.out;ahci 0000:00:1f.0: programming MSI address 0xfee003f8 with IRTE handle 31/0
-serial.out;ahci 0000:00:1f.0: irq 11 for MSI
+serial.out;ahci 0000:00:1f.0: Programming MSI address 0xfee003f8 with IRTE handle 31/0
+serial.out;ahci 0000:00:1f.0: IRQ 11 for MSI
 serial.out;ahci 0000:00:1f.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x2 impl SATA mode
 serial.out;ahci 0000:00:1f.0: flags: 64bit ncq pm led clo pio slum part ems apst
 serial.out;ata2: SATA max UDMA/133 abar m2048@0xf7f36000 port 0xf7f36180 irq 11
 serial.out;ata2: SATA link up 3.0 Gbps (SStatus 123 SControl 300)
-serial.out;e1000e 0000:00:1e.0: programming MSI address 0xfee00518 with IRTE handle 40/0
-serial.out;e1000e 0000:00:1e.0: irq 42 for MSI-X
-serial.out;e1000e 0000:00:1e.0: programming MSI address 0xfee00518 with IRTE handle 40/1
-serial.out;e1000e 0000:00:1e.0: irq 43 for MSI-X
-serial.out;e1000e 0000:00:1e.0: programming MSI address 0xfee00518 with IRTE handle 40/2
-serial.out;e1000e 0000:00:1e.0: irq 44 for MSI-X
-serial.out;xhci_hcd 0000:00:14.0: no MSI handle configured
+serial.out;e1000e 0000:00:1e.0: Programming MSI address 0xfee00518 with IRTE handle 40/0
+serial.out;e1000e 0000:00:1e.0: IRQ 42 for MSI-X
+serial.out;e1000e 0000:00:1e.0: Programming MSI address 0xfee00518 with IRTE handle 40/1
+serial.out;e1000e 0000:00:1e.0: IRQ 43 for MSI-X
+serial.out;e1000e 0000:00:1e.0: Programming MSI address 0xfee00518 with IRTE handle 40/2
+serial.out;e1000e 0000:00:1e.0: IRQ 44 for MSI-X
+serial.out;xhci_hcd 0000:00:14.0: No MSI handle configured
 serial.out;sd 1:0:0:0: \[sda\] Attached SCSI disk
 serial.out;64 bytes from 192.168.254.1: seq=.* ttl=.* time=.* ms
 serial.out;*** '/usr/bin/integtest_kt' DONE

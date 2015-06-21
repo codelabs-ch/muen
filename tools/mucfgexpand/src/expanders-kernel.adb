e9e8e8816b19081fe7e3dfb55c9b461441431328
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-06-21 20:10:01 +0200

    Mucfgexpand: Use kernel data section constants
    Replace hard-coded values with new constants.

--- tools/mucfgexpand/src/expanders-kernel.adb
+++ tools/mucfgexpand/src/expanders-kernel.adb
@@ -84,7 +84,8 @@ is
                  (Policy        => Data,
                   Logical_Name  => "data",
                   Physical_Name => "kernel_data",
-                  Address       => "16#0011_0000#",
+                  Address       => Mutools.Utils.To_Hex
+                    (Number => Config.Kernel_Data_Section_Addr),
                   Writable      => True,
                   Executable    => False));
             Muxml.Utils.Append_Child
--- tools/mucfgexpand/src/expanders-memory.adb
+++ tools/mucfgexpand/src/expanders-memory.adb
@@ -86,8 +86,10 @@ is
       Mutools.XML_Utils.Add_Memory_Region
         (Policy      => Data,
          Name        => "kernel_data",
-         Address     => "16#0011_0000#",
-         Size        => "16#1000#",
+         Address     => Mutools.Utils.To_Hex
+           (Number => Config.Kernel_Data_Section_Addr),
+         Size        => Mutools.Utils.To_Hex
+           (Number => Config.Kernel_Data_Section_Size),
          Caching     => "WB",
          Alignment   => "16#1000#",
          File_Name   => "kernel",

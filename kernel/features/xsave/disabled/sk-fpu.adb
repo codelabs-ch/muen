5e8024cce11b1fb7f5c0b341faeaf9b7c2c42dbc
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-06-28 22:46:47 +0200

    Kernel: Implement FPU.Has_Valid_State (XSAVE disabled)
    If the XSAVE feature is disabled in the policy the FX[SAVE|RSTOR]
    instructions are used instead.
    
    The implementation of the FPU.Has_Valid_State function returns True if
    SSE/SSE2/SSE3/SSSE3 extensions and support for the FXSAVE and FXRSTOR
    instructions are present, see Intel SDM Vol. 3A, chapter 9.6.

--- kernel/features/xsave/disabled/sk-fpu.adb
+++ kernel/features/xsave/disabled/sk-fpu.adb
@@ -16,6 +16,10 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+with SK.KC;
+with SK.CPU;
+with SK.Constants;
+
 package body SK.FPU
 with
    Refined_State => (State => Subject_FPU_States)
@@ -61,10 +65,47 @@ is
 
    -------------------------------------------------------------------------
 
+   --  Returns True if SSE/SSE2/SSE3/SSSE3 extensions and support for the
+   --  FXSAVE and FXRSTOR instructions are present, see Intel SDM Vol. 3A,
+   --  chapter 9.6.
    function Has_Valid_State return Boolean
    is
+      Valid_State : Boolean;
+      Unused_EAX, Unused_EBX, ECX, EDX : SK.Word32;
    begin
-      return False;
+      Unused_EAX := 1;
+      ECX        := 0;
+
+      pragma Warnings
+        (GNATprove, Off, "unused assignment to ""Unused_E*X""",
+         Reason => "Only parts of the CPUID result is needed");
+      CPU.CPUID
+        (EAX => Unused_EAX,
+         EBX => Unused_EBX,
+         ECX => ECX,
+         EDX => EDX);
+      pragma Warnings (GNATprove, On, "unused assignment to ""Unused_E*X""");
+
+      Valid_State := Bit_Test
+        (Value => Word64 (EDX),
+         Pos   => Constants.CPUID_FEATURE_FXSR) and then
+        Bit_Test
+          (Value => Word64 (EDX),
+           Pos   => Constants.CPUID_FEATURE_SSE) and then
+        Bit_Test
+          (Value => Word64 (EDX),
+           Pos   => Constants.CPUID_FEATURE_SSE2) and then
+        Bit_Test
+          (Value => Word64 (ECX),
+           Pos   => Constants.CPUID_FEATURE_SSE3) and then
+        Bit_Test
+          (Value => Word64 (ECX),
+           Pos   => Constants.CPUID_FEATURE_SSSE3);
+
+      pragma Debug (not Valid_State,
+                    KC.Put_Line (Item => "FXSAVE features missing"));
+
+      return Valid_State;
    end Has_Valid_State;
 
 end SK.FPU;

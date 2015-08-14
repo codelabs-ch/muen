0ce7f63b29f6d7f3237efcdbf2847fbcd938b068
Reto Buerki <reet@codelabs.ch>
2015-08-14 18:14:12 +0200

    Libmutime: Add auto-generated test stubs

---
+++ components/libmutime/tests/mutime-test_data-tests.adb
@@ -0,0 +1,75 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Mutime.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Mutime.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Time_Of (Gnattest_T : in out Test);
+   procedure Test_Time_Of_a0832e (Gnattest_T : in out Test) renames Test_Time_Of;
+--  id:2.2/a0832e6f81ca9164/Time_Of/1/0/
+   procedure Test_Time_Of (Gnattest_T : in out Test) is
+   --  mutime.ads:43:4:Time_Of
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Time_Of;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Split (Gnattest_T : in out Test);
+   procedure Test_Split_3c222d (Gnattest_T : in out Test) renames Test_Split;
+--  id:2.2/3c222d0aebc09370/Split/1/0/
+   procedure Test_Split (Gnattest_T : in out Test) is
+   --  mutime.ads:53:4:Split
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Split;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Get_Month_And_Day (Gnattest_T : in out Test);
+   procedure Test_Get_Month_And_Day_7c71b5 (Gnattest_T : in out Test) renames Test_Get_Month_And_Day;
+--  id:2.2/7c71b57f9ae57aa9/Get_Month_And_Day/1/0/
+   procedure Test_Get_Month_And_Day (Gnattest_T : in out Test) is
+   --  mutime.ads:72:4:Get_Month_And_Day
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Get_Month_And_Day;
+--  end read only
+
+end Mutime.Test_Data.Tests;
---
+++ components/libmutime/tests/mutime-test_data-tests.ads
@@ -0,0 +1,22 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Mutime.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Mutime.Test_Data.Test
+   with null record;
+
+   procedure Test_Time_Of_a0832e (Gnattest_T : in out Test);
+   --  mutime.ads:43:4:Time_Of
+
+   procedure Test_Split_3c222d (Gnattest_T : in out Test);
+   --  mutime.ads:53:4:Split
+
+   procedure Test_Get_Month_And_Day_7c71b5 (Gnattest_T : in out Test);
+   --  mutime.ads:72:4:Get_Month_And_Day
+
+end Mutime.Test_Data.Tests;
+--  end read only

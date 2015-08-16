961fefa3f176133fd997ed07d161e987604768d4
Reto Buerki <reet@codelabs.ch>
2015-08-17 01:40:22 +0200

    Time: Add Tm.Rtc package
    The package provides the Read_Time procedure which reads the date and
    time from the CMOS/RTC (Real-Time Clock).

---
+++ components/time/src/tm-rtc.ads
@@ -0,0 +1,44 @@
+--
+--  Copyright (C) 2015  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2015  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with X86_64;
+
+with Interfaces;
+
+package Tm.Rtc
+is
+
+   --  Type used to store RTC values.
+   type Time_Type is record
+      Seconds      : Interfaces.Unsigned_8;
+      Minutes      : Interfaces.Unsigned_8;
+      Hours        : Interfaces.Unsigned_8;
+      Weekday      : Interfaces.Unsigned_8;
+      Day_Of_Month : Interfaces.Unsigned_8;
+      Month        : Interfaces.Unsigned_8;
+      Year         : Interfaces.Unsigned_8;
+      Status_B     : Interfaces.Unsigned_8;
+   end record;
+
+   --  Read time from CMOS RTC.
+   procedure Read_Time (T : out Time_Type)
+   with
+      Global  => (In_Out => X86_64.State),
+      Depends => (T => X86_64.State, X86_64.State =>+ null);
+
+end Tm.Rtc;

8e0bf307ebabb6f157a3582df90dc2e2ee6d0ee6
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-09-04 17:12:12 +0200

    Kernel: Add SK.Tau0_Interface package
    The package provides the Get_Major_Frame procedure, which returns the
    current major frame ID as specified by Tau0. The precondition checks
    that the value is only read by the BSP. The BSP is the only core which
    takes care of switching major frame IDs if required.
    
    Future extensions of the kernel<->Tau0 interface should be added to this
    package.

---
+++ kernel/src/sk-tau0_interface.adb
@@ -0,0 +1,45 @@
+--
+--  Copyright (C) 2015  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2015  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with System;
+
+with Skp.Kernel;
+
+package body SK.Tau0_Interface
+with
+   Refined_State => (State => New_Major)
+is
+
+   New_Major : Skp.Scheduling.Major_Frame_Range
+   with
+      Atomic,
+      Async_Writers,
+      Address => System'To_Address (Skp.Kernel.Tau0_Iface_Address);
+
+   -------------------------------------------------------------------------
+
+   procedure Get_Major_Frame (ID : out Skp.Scheduling.Major_Frame_Range)
+   with
+      Refined_Global  => (Input => New_Major),
+      Refined_Depends => (ID => New_Major)
+   is
+   begin
+      ID := New_Major;
+   end Get_Major_Frame;
+
+end SK.Tau0_Interface;
---
+++ kernel/src/sk-tau0_interface.ads
@@ -0,0 +1,35 @@
+--
+--  Copyright (C) 2013  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2013  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Skp.Scheduling;
+
+with SK.CPU_Global;
+
+package SK.Tau0_Interface
+with
+   Abstract_State => (State with External => Async_Writers)
+is
+
+   --  Returns major frame ID as specified by Tau0.
+   procedure Get_Major_Frame (ID : out Skp.Scheduling.Major_Frame_Range)
+   with
+      Global  => (Input => State),
+      Depends => (ID => State),
+      Pre     => CPU_Global.Is_BSP;
+
+end SK.Tau0_Interface;

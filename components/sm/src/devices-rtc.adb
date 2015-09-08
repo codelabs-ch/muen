58812ae083d22cc3644886c1c30f27e28252223a
Reto Buerki <reet@codelabs.ch>
2015-09-08 15:49:39 +0200

    SM: Use new Cmos_Rtc package

--- components/sm/src/devices-rtc.adb
+++ components/sm/src/devices-rtc.adb
@@ -22,6 +22,8 @@ with Mutime.Utils;
 
 with SK;
 
+with Cmos_Rtc;
+
 package body Devices.RTC
 with
    Refined_State => (State => (Current_Register, Current_Time))
@@ -29,21 +31,8 @@ is
 
    use Types;
 
-   Port_Reg_Select : constant := 16#70#;
-   Port_Data       : constant := 16#71#;
-
    --  Time is in BCD, 24-hour format.
    Status_B : constant := 2#10#;
-
-   RTC_Reg_Seconds      : constant := 16#00#;
-   RTC_Reg_Minutes      : constant := 16#02#;
-   RTC_Reg_Hours        : constant := 16#04#;
-   RTC_Reg_Weekday      : constant := 16#06#;
-   RTC_Reg_Day_Of_Month : constant := 16#07#;
-   RTC_Reg_Month        : constant := 16#08#;
-   RTC_Reg_Year         : constant := 16#09#;
-   RTC_Reg_Status_A     : constant := 16#0a#;
-   RTC_Reg_Status_B     : constant := 16#0b#;
 
    Current_Register : SK.Byte               := 0;
    Current_Time     : Mutime.Date_Time_Type := Mutime.Epoch;
@@ -62,75 +51,87 @@ is
       use type SK.Word64;
       use type Mutime.Year_Type;
 
+      package CR renames Cmos_Rtc;
+
       RAX : constant SK.Word64 := Subject_Info.State.Regs.RAX;
    begin
       Halt := False;
 
       --  Ignore invalid requests.
 
-      if (Info.Port_Number = Port_Reg_Select and then Info.Direction = Dir_In)
-        or (Info.Port_Number = Port_Data and then Info.Direction = Dir_Out)
+      if (Info.Port_Number = CR.Port_Reg_Select
+          and then Info.Direction = Dir_In)
+        or (Info.Port_Number = CR.Port_Data
+            and then Info.Direction = Dir_Out)
       then
          return;
       end if;
 
       case Info.Port_Number is
-         when Port_Reg_Select =>
+         when CR.Port_Reg_Select =>
             case RAX and 16#ff# is
-               when RTC_Reg_Status_A => Current_Register := RTC_Reg_Status_A;
-               when RTC_Reg_Seconds  => Current_Register := RTC_Reg_Seconds;
-               when RTC_Reg_Minutes  => Current_Register := RTC_Reg_Minutes;
-               when RTC_Reg_Hours    => Current_Register := RTC_Reg_Hours;
-               when RTC_Reg_Weekday  => Current_Register := RTC_Reg_Weekday;
-               when RTC_Reg_Day_Of_Month =>
-                  Current_Register := RTC_Reg_Day_Of_Month;
-               when RTC_Reg_Month    => Current_Register := RTC_Reg_Month;
-               when RTC_Reg_Year     => Current_Register := RTC_Reg_Year;
-               when RTC_Reg_Status_B => Current_Register := RTC_Reg_Status_B;
-               when others           => null;
+               when CR.Reg_Status_A =>
+                  Current_Register := CR.Reg_Status_A;
+               when CR.Reg_Seconds =>
+                  Current_Register := CR.Reg_Seconds;
+               when CR.Reg_Minutes =>
+                  Current_Register := CR.Reg_Minutes;
+               when CR.Reg_Hours =>
+                  Current_Register := CR.Reg_Hours;
+               when CR.Reg_Weekday =>
+                  Current_Register := CR.Reg_Weekday;
+               when CR.Reg_Day_Of_Month =>
+                  Current_Register := CR.Reg_Day_Of_Month;
+               when CR.Reg_Month =>
+                  Current_Register := CR.Reg_Month;
+               when CR.Reg_Year =>
+                  Current_Register := CR.Reg_Year;
+               when CR.Reg_Status_B =>
+                  Current_Register := CR.Reg_Status_B;
+               when others => null;
             end case;
-         when Port_Data =>
+         when CR.Port_Data =>
             case Current_Register is
-               when RTC_Reg_Status_A =>
+               when CR.Reg_Status_A =>
 
                   --  Client initiates RTC read, get date and time.
 
                   Current_Time := Time.Get_Date_Time;
 
                   Subject_Info.State.Regs.RAX := 0;
-               when RTC_Reg_Seconds =>
+               when CR.Reg_Seconds =>
                   Subject_Info.State.Regs.RAX := SK.Word64
                     (Mutime.Utils.To_BCD
                        (Value => Interfaces.Unsigned_8
                             (Current_Time.Second)));
-               when RTC_Reg_Minutes =>
+               when CR.Reg_Minutes =>
                   Subject_Info.State.Regs.RAX := SK.Word64
                     (Mutime.Utils.To_BCD
                        (Value => Interfaces.Unsigned_8
                             (Current_Time.Minute)));
-               when RTC_Reg_Hours =>
+               when CR.Reg_Hours =>
                   Subject_Info.State.Regs.RAX := SK.Word64
                     (Mutime.Utils.To_BCD
                        (Value => Interfaces.Unsigned_8
                             (Current_Time.Hour)));
-               when RTC_Reg_Weekday =>
+               when CR.Reg_Weekday =>
                   Subject_Info.State.Regs.RAX := 0;
-               when RTC_Reg_Day_Of_Month =>
+               when CR.Reg_Day_Of_Month =>
                   Subject_Info.State.Regs.RAX := SK.Word64
                     (Mutime.Utils.To_BCD
                        (Value => Interfaces.Unsigned_8
                             (Current_Time.Day)));
-               when RTC_Reg_Month =>
+               when CR.Reg_Month =>
                   Subject_Info.State.Regs.RAX := SK.Word64
                     (Mutime.Utils.To_BCD
                        (Value => Interfaces.Unsigned_8
                             (Current_Time.Month)));
-               when RTC_Reg_Year =>
+               when CR.Reg_Year =>
                   Subject_Info.State.Regs.RAX := SK.Word64
                     (Mutime.Utils.To_BCD
                        (Value => Mutime.Utils.Two_Digits_Type
                             (Current_Time.Year mod 100)));
-               when RTC_Reg_Status_B =>
+               when CR.Reg_Status_B =>
                   Subject_Info.State.Regs.RAX := Status_B;
                when others => null;
             end case;

f04b707b47bc307b5135b0dfb3f6f1b543a024f9
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-12-17 16:48:56 +0100

    Register subject device resource expander

--- tools/mucfgexpand/tests/stage2-expansion-test_data-tests.adb
+++ tools/mucfgexpand/tests/stage2-expansion-test_data-tests.adb
@@ -32,7 +32,7 @@ package body Stage2.Expansion.Test_Data.Tests is
          Name  => "enabled",
          Value => "true");
       Register_All (Data => Policy);
-      Assert (Condition => Procs.Get_Count = 44,
+      Assert (Condition => Procs.Get_Count = 45,
               Message   => "Count mismatch(1):" & Get_Count'Img);
       Procs.Clear;
 
@@ -42,7 +42,7 @@ package body Stage2.Expansion.Test_Data.Tests is
          Name  => "enabled",
          Value => "false");
       Register_All (Data => Policy);
-      Assert (Condition => Procs.Get_Count = 41,
+      Assert (Condition => Procs.Get_Count = 42,
               Message   => "Count mismatch(2):" & Get_Count'Img);
       Procs.Clear;
 

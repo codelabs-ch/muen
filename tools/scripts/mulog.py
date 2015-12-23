f233b4f72ed540e681da0d8f8d03285504c90e85
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-12-24 00:55:30 +0100

    scripts: Fix pep8 issues in mulog.py

--- tools/scripts/mulog.py
+++ tools/scripts/mulog.py
@@ -5,13 +5,14 @@ import re
 
 formatted = []
 
+
 # append line to parent with given log prefix
 def append(prefix, line):
-	for idx, l in reversed(list(enumerate(formatted))):
-		if l[0:5] == prefix:
-			formatted[idx] = formatted[idx] + line
-			return
-	raise Exception("Parent line '" + prefix + "' not found")
+    for idx, l in reversed(list(enumerate(formatted))):
+        if l[0:5] == prefix:
+            formatted[idx] = formatted[idx] + line
+            return
+    raise Exception("Parent line '" + prefix + "' not found")
 
 filename = sys.argv[1]
 
@@ -22,12 +23,12 @@ f.close()
 p = re.compile('[0-9a-fA-F]{4}>')
 
 for line in lines:
-	m = p.match(line)
-	if m:
-		parentPrefix = m.group().replace('>', '|')
-		append(parentPrefix, line[5:])
-	elif line[0:3] != "---":
-		formatted.append(line)
+    m = p.match(line)
+    if m:
+        parentPrefix = m.group().replace('>', '|')
+        append(parentPrefix, line[5:])
+    elif line[0:3] != "---":
+        formatted.append(line)
 
 for line in formatted:
-	print line
+    print line

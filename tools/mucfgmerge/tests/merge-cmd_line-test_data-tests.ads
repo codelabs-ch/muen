ea9fcbb5ab90dfff1f62a5ee519c77fa2b3bf773
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-01-19 13:34:14 +0100

    Mucfgmerge: Add --additional-hardware command line option
    The -a/--additional-hardware command line option allows to specify an
    extra XML file containing a policy hardware section. The content will be
    merged into the system policy on top of the hardware file given by the
    -w parameter. The argument is optional.

--- tools/mucfgmerge/tests/merge-cmd_line-test_data-tests.ads
+++ tools/mucfgmerge/tests/merge-cmd_line-test_data-tests.ads
@@ -21,5 +21,8 @@ package Merge.Cmd_Line.Test_Data.Tests is
    procedure Test_Get_Hardware_File_202173 (Gnattest_T : in out Test);
    --  merge-cmd_line.ads:36:4:Get_Hardware_File
 
+   procedure Test_Get_Additional_Hardware_File_eb3075 (Gnattest_T : in out Test);
+   --  merge-cmd_line.ads:39:4:Get_Additional_Hardware_File
+
 end Merge.Cmd_Line.Test_Data.Tests;
 --  end read only

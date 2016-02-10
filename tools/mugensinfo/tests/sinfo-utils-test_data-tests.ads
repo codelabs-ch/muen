3c58b53758505ee8a2a1e244a9690fa2f51e49ac
Reto Buerki <reet@codelabs.ch>
2016-02-10 17:30:41 +0100

    Add Utils.Get_Bounds procedure
    The procedure calculates lower and upper bounds for node list attribute
    values specified by name.

---
+++ tools/mugensinfo/tests/sinfo-utils-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Sinfo.Utils.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Sinfo.Utils.Test_Data.Test
+   with null record;
+
+   procedure Test_Get_Bounds_5acc2a (Gnattest_T : in out Test);
+   --  sinfo-utils.ads:26:4:Get_Bounds
+
+end Sinfo.Utils.Test_Data.Tests;
+--  end read only

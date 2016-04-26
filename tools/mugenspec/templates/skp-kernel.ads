5877a73be12180574ba4c87424e9f8de17f00e3c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-04-26 18:31:41 +0200

    Mugenspec: Write subject interrupts address constant
    Generate Subj_Interrupts_Addr constant in Skp.Kernel package. The
    exported value is the kernel virtual memory base address of the subject
    interrupts region mappings.

--- tools/mugenspec/templates/skp-kernel.ads
+++ tools/mugenspec/templates/skp-kernel.ads
@@ -6,6 +6,7 @@ is
    Tau0_Iface_Address        : constant := __tau0_iface_addr__;
    Subj_States_Address       : constant := __subj_states_addr__;
    Subj_Timed_Events_Address : constant := __subj_timed_events_addr__;
+   Subj_Interrupts_Address   : constant := __subj_interrupts_addr__;
    IO_Apic_Address           : constant := __ioapic_addr__;
    Subj_Sinfo_Address        : constant := __subj_sinfo_addr__;
    Subj_Sinfo_Size           : constant := __subj_sinfo_size__;

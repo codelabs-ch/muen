ba4c838f02b39bc70a0a1c9d9b808850d698e858
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-04-26 18:26:06 +0200

    Mucfgvalidate: Register subject interrupts presence check

--- tools/mucfgvalidate/tests/validate-test_data-tests.adb
+++ tools/mucfgvalidate/tests/validate-test_data-tests.adb
@@ -52,7 +52,7 @@ package body Validate.Test_Data.Tests is
                                  Name  => "enabled",
                                  Value => "true");
       Register_All (Policy => Data);
-      Assert (Condition => XML_Processors.Get_Count = 98,
+      Assert (Condition => XML_Processors.Get_Count = 99,
               Message   => "Count mismatch(1):"
               & XML_Processors.Get_Count'Img);
       XML_Processors.Clear;
@@ -62,7 +62,7 @@ package body Validate.Test_Data.Tests is
                                  Name  => "enabled",
                                  Value => "false");
       Register_All (Policy => Data);
-      Assert (Condition => XML_Processors.Get_Count = 83,
+      Assert (Condition => XML_Processors.Get_Count = 84,
               Message   => "Count mismatch(2):" & XML_Processors.Get_Count'Img);
       XML_Processors.Clear;
 

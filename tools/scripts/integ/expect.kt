6b8ce72d0d1295118756207f8ca62c4e8042c7ca
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-06-06 18:43:08 +0200

    integ: Adjust Kontron expect strings
    Auditors: reet

--- tools/scripts/integ/expect.kt
+++ tools/scripts/integ/expect.kt
@@ -37,10 +37,10 @@ serial.out;ahci 0000:00:1f.0: version 3.0
 serial.out;PCI: setting IRQ 11 as level-triggered
 serial.out;ahci 0000:00:1f.0: Programming MSI address 0xfee003f8 with IRTE handle 31/0
 serial.out;ahci 0000:00:1f.0: IRQ 11 for MSI
-serial.out;ahci 0000:00:1f.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x2 impl SATA mode
+serial.out;ahci 0000:00:1f.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps
 serial.out;ahci 0000:00:1f.0: flags: 64bit ncq pm led clo pio slum part ems apst
-serial.out;ata2: SATA max UDMA/133 abar m2048@0xf7f36000 port 0xf7f36180 irq 11
-serial.out;ata2: SATA link up 3.0 Gbps (SStatus 123 SControl 300)
+serial.out;SATA max UDMA/133 abar m2048@0xf7f36000 port 0xf7f36
+serial.out;SATA link up 3.0 Gbps (SStatus 123 SControl 300)
 serial.out;e1000e 0000:00:1e.0: Programming MSI address 0xfee00498 with IRTE handle 36/0
 serial.out;e1000e 0000:00:1e.0: IRQ 40 for MSI-X
 serial.out;e1000e 0000:00:1e.0: Programming MSI address 0xfee00498 with IRTE handle 36/1
@@ -55,6 +55,6 @@ serial.out;hub 3-0:1.0: USB hub found
 serial.out;ehci-pci 0000:00:02.0: EHCI Host Controller
 serial.out;ehci-pci 0000:00:02.0: irq 13
 serial.out;hub 4-0:1.0: USB hub found
-serial.out;sd 1:0:0:0: \[sda\] Attached SCSI disk
+serial.out;0:0:0: \[sda\] Attached SCSI disk
 serial.out;64 bytes from 192.168.254.1: seq=.* ttl=.* time=.* ms
 serial.out;*** '/usr/bin/integtest_kt' DONE

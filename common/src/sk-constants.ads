8e1b4455bf6cd3d4edd8756799051ca8a8fe3ffe
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-07-15 17:16:41 +0200

    Add VMX_EXIT_INTR_ERROR_CODE constant
    The VMCS field contains the VM-exit interruption error code.

--- common/src/sk-constants.ads
+++ common/src/sk-constants.ads
@@ -100,6 +100,7 @@ is
    VMX_INST_ERROR               : constant := 16#4400#;
    VMX_EXIT_REASON              : constant := 16#4402#;
    VMX_EXIT_INTR_INFO           : constant := 16#4404#;
+   VMX_EXIT_INTR_ERROR_CODE     : constant := 16#4406#;
    VMX_EXIT_INSTRUCTION_LEN     : constant := 16#440c#;
    CR0_MASK                     : constant := 16#6000#;
    CR4_MASK                     : constant := 16#6002#;

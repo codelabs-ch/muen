75bf3d9e41af7d403d1d6cbcc82b30ebb377f247
Reto Buerki <reet@codelabs.ch>
2016-09-15 14:22:05 +0200

    Add skeleton for new Mucfgmemhashes tool
    The tool will be used to add memory integrity hashes to a given policy.

---
+++ tools/mucfgmemhashes/src/memhashes.adb
@@ -0,0 +1,34 @@
+--
+--  Copyright (C) 2016  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2016  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Mulog;
+
+package body Memhashes
+is
+
+   -------------------------------------------------------------------------
+
+   procedure Run (Policy_In, Policy_Out, Input_Dir : String)
+   is
+   begin
+      Mulog.Log (Msg => "Looking for input files in '" & Input_Dir & "'");
+      Mulog.Log (Msg => "Processing policy '" & Policy_In & "'");
+      Mulog.Log (Msg => "Writing policy to '" & Policy_Out & "'");
+   end Run;
+
+end Memhashes;

edbef9a670bc94270e6e92641ca871bb5a85ea1b
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-10-13 18:59:46 +0200

    Add refined post-condition to FPU.Clear_State
    The post-condition states that the FPU state associated with the subject
    specified by ID is reset and all other states remain the same.

--- kernel/src/sk-fpu.adb
+++ kernel/src/sk-fpu.adb
@@ -38,7 +38,9 @@ is
    procedure Clear_State (ID : Skp.Subject_Id_Type)
    with
       Refined_Global  => (In_Out => Subject_FPU_States),
-      Refined_Depends => (Subject_FPU_States =>+ ID)
+      Refined_Depends => (Subject_FPU_States =>+ ID),
+      Refined_Post    => Subject_FPU_States =
+       Subject_FPU_States'Old'Update (ID => Null_FPU_State)
    is
    begin
       Subject_FPU_States (ID) := Null_FPU_State;

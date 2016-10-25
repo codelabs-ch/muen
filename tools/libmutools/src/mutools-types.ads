b44aa37a7ae268c19eb0342f7f5a538ff1ed5d8c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-10-25 14:41:41 +0200

    Libmutools: Drop System_Msrstore from Memory_Kind type

--- tools/libmutools/src/mutools-types.ads
+++ tools/libmutools/src/mutools-types.ads
@@ -24,8 +24,8 @@ is
 
    --  Types of physical memory.
    type Memory_Kind is
-     (System, System_Vmxon, System_Vmcs, System_Iobm, System_Msrbm,
-      System_Msrstore, System_Pt, System_Vtd_Root, System_Vtd_Context,
+     (System, System_Vmxon, System_Vmcs, System_Iobm, System_Msrbm, System_Pt,
+      System_Vtd_Root, System_Vtd_Context,
       Kernel, Kernel_Binary, Kernel_Interface, Kernel_Msrstore,
       Subject, Subject_Info, Subject_Binary, Subject_Zeropage, Subject_Initrd,
       Subject_Channel, Subject_State, Subject_Timed_Event, Subject_Interrupts,

6f73f3383cc7418882688307c871a4a6ea60aea0
Reto Buerki <reet@codelabs.ch>
2016-11-02 09:42:45 +0100

    Process monitored sinfo regions in main loop

--- components/sl/src/sl.adb
+++ components/sl/src/sl.adb
@@ -17,9 +17,65 @@
 --
 
 with SK.CPU;
+with SK.Hypercall;
+
+pragma $Release_Warnings (Off, "unit * is not referenced");
+with Debuglog.Client;
+pragma $Release_Warnings (On, "unit * is not referenced");
+
+with Musinfo.Utils;
+with Musinfo.Instance;
+
+with Loader.Process_Target;
 
 procedure Sl
 is
 begin
-   SK.CPU.Stop;
+   pragma Debug (Debuglog.Client.Put_Line (Item => "SL subject running"));
+
+   if not Musinfo.Instance.Is_Valid then
+      pragma Debug
+        (Debuglog.Client.Put_Line
+           (Item => "Error: Own sinfo data not valid, halting"));
+      SK.CPU.Stop;
+   end if;
+
+   loop
+      pragma Debug (Debuglog.Client.Put_Line
+                    (Item => "Resetting managed subject(s)"));
+
+      declare
+         Pattern : constant String := "monitor_sinfo";
+         Iter    : Musinfo.Utils.Memory_Iterator_Type
+           := Musinfo.Instance.Create_Memory_Iterator;
+         Mem     : Musinfo.Utils.Named_Memregion_Type
+           := Musinfo.Utils.Null_Named_Memregion;
+         Success : Boolean;
+      begin
+         while Musinfo.Instance.Has_Element (Iter => Iter) loop
+            Mem := Musinfo.Instance.Element (Iter => Iter);
+
+            if Musinfo.Utils.Names_Match
+              (N1    => Mem.Name,
+               N2    => Musinfo.Utils.To_Name (Str => Pattern),
+               Count => Pattern'Length)
+            then
+               Loader.Process_Target.Process
+                 (Sinfo_Mem => Mem,
+                  Success   => Success);
+               if not Success then
+                  pragma Debug
+                    (Debuglog.Client.Put_Line
+                       (Item => "Error: Reset of subject failed"));
+                  SK.CPU.Stop;
+               end if;
+            end if;
+
+            Musinfo.Instance.Next (Iter => Iter);
+            pragma Loop_Invariant (Musinfo.Instance.Belongs_To (Iter => Iter));
+         end loop;
+      end;
+
+      SK.Hypercall.Trigger_Event (Number => 0);
+   end loop;
 end Sl;

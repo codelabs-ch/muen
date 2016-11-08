0aed39274f9364f290b33f6e0f866fd77358a945
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-11-08 12:46:07 +0100

    Libmutools: Add Kernel_Vmcs to memory kind type

--- tools/libmutools/src/mutools-types.ads
+++ tools/libmutools/src/mutools-types.ads
@@ -26,7 +26,7 @@ is
    type Memory_Kind is
      (System, System_Vmxon, System_Vmcs, System_Iobm, System_Msrbm, System_Pt,
       System_Vtd_Root, System_Vtd_Context,
-      Kernel, Kernel_Binary, Kernel_Interface, Kernel_Msrstore,
+      Kernel, Kernel_Binary, Kernel_Interface, Kernel_Msrstore, Kernel_Vmcs,
       Subject, Subject_Info, Subject_Binary, Subject_Zeropage, Subject_Initrd,
       Subject_Channel, Subject_State, Subject_Timed_Event, Subject_Interrupts,
       Subject_Bios, Subject_Acpi_Rsdp, Subject_Acpi_Xsdt, Subject_Acpi_Fadt,
@@ -38,7 +38,7 @@ is
    subtype System_Memory is Memory_Kind range System .. System_Vtd_Context;
 
    --  Memory used by kernel resources.
-   subtype Kernel_Memory is Memory_Kind range Kernel .. Kernel_Msrstore;
+   subtype Kernel_Memory is Memory_Kind range Kernel .. Kernel_Vmcs;
 
    --  Memory mappable by subjects.
    subtype Subject_Memory is Memory_Kind range Subject .. Subject_Device;

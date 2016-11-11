abc78fe78e833bf2e4a21301d5360f822ea404da
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-11-11 10:54:21 +0100

    Add SK.Scheduling_Info package
    The package provides access to the per-scheduling group scheduling
    information pages. The only currently implemented operation is a setter
    for TSC start/end values for a scheduling group given by ID.

---
+++ kernel/src/sk-scheduling_info.adb
@@ -0,0 +1,72 @@
+--
+--  Copyright (C) 2016  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2016  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with System;
+
+with Interfaces;
+
+with Skp.Kernel;
+
+with Muschedinfo;
+
+package body SK.Scheduling_Info
+with
+   Refined_State => (State => Sched_Info)
+is
+
+   use type String;
+   use type Interfaces.Unsigned_64;
+
+   pragma Warnings
+     (Off,
+      "component size overrides size clause for ""Scheduling_Info_Type""",
+      Reason => "Reserved memory size is bigger than actual size of type");
+   pragma Warnings (GNAT, Off, "*padded by * bits");
+   type Sched_Info_Array is array (Skp.Scheduling.Scheduling_Group_Range)
+     of Muschedinfo.Scheduling_Info_Type
+   with
+      Independent_Components,
+      Component_Size => Page_Size * 8,
+      Alignment      => Page_Size;
+   pragma Warnings (GNAT, On, "*padded by * bits");
+   pragma Warnings
+     (On, "component size overrides size clause for ""Scheduling_Info_Type""");
+
+   --  Scheduling group info regions.
+   Sched_Info : Sched_Info_Array
+   with
+      Import,
+      Volatile,
+      Async_Readers,
+      Address => System'To_Address (Skp.Kernel.Sched_Group_Info_Address);
+
+   -------------------------------------------------------------------------
+
+   procedure Set_Scheduling_Info
+     (ID                 : Skp.Scheduling.Scheduling_Group_Range;
+      TSC_Schedule_Start : SK.Word64;
+      TSC_Schedule_End   : SK.Word64)
+   is
+   begin
+      Sched_Info (ID).TSC_Schedule_Start := Interfaces.Unsigned_64
+        (TSC_Schedule_Start);
+      Sched_Info (ID).TSC_Schedule_End   := Interfaces.Unsigned_64
+        (TSC_Schedule_End);
+   end Set_Scheduling_Info;
+
+end SK.Scheduling_Info;

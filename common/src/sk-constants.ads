c2e08be7e5a1b631bc3aeceb12883e1db148468a
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2016-12-21 11:08:47 +0100

    Add machine-check related constants

--- common/src/sk-constants.ads
+++ common/src/sk-constants.ads
@@ -38,6 +38,7 @@ is
    CR0_PE_FLAG                  : constant := 0;
    CR0_PG_FLAG                  : constant := 31;
 
+   CR4_MCE_FLAG                 : constant := 6;
    CR4_VMXE_FLAG                : constant := 13;
    CR4_SMXE_FLAG                : constant := 14;
    CR4_XSAVE_FLAG               : constant := 18;
@@ -56,6 +57,8 @@ is
    IA32_FCTRL_VMX_IN_SMX_FLAG   : constant := 1;
    IA32_FCTRL_VMX_FLAG          : constant := 2;
 
+   MCG_CTL_P_FLAG               : constant := 8;
+
    ----------
    -- MSRs --
    ----------
@@ -64,6 +67,11 @@ is
    IA32_FEATURE_CONTROL         : constant := 16#3a#;
    IA32_MISC_ENABLE             : constant := 16#1a0#;
    IA32_EFER                    : constant := 16#c000_0080#;
+
+   IA32_MCG_CAP                 : constant := 16#179#;
+   IA32_MCG_CTL                 : constant := 16#17B#;
+   IA32_MC0_CTL                 : constant := 16#400#;
+   IA32_MC0_STATUS              : constant := 16#401#;
 
    IA32_VMX_BASIC               : constant := 16#480#;
    IA32_VMX_PINBASED_CTLS       : constant := 16#481#;

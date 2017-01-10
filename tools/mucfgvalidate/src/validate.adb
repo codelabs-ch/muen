f8d6556adfba5b6811f32827c7aebc139e4013d1
Reto Buerki <reet@codelabs.ch>
2017-01-10 13:07:38 +0100

    Register system board reference validator

--- tools/mucfgvalidate/src/validate.adb
+++ tools/mucfgvalidate/src/validate.adb
@@ -186,6 +186,8 @@ is
       XML_Processors.Register
         (Process => Kernel.CPU_Memory_Section_Count'Access);
       XML_Processors.Register
+        (Process => Kernel.System_Board_Reference'Access);
+      XML_Processors.Register
         (Process => Subject.Name_Uniqueness'Access);
       XML_Processors.Register
         (Process => Subject.CPU_ID'Access);

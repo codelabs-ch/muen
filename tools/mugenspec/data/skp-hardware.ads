e9c099a1f214aa7035257f908fe730911abbbf78
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-02-19 17:31:15 +0100

    Mugenspec: Do not generate PCI config space addr
    It is not used any longer.

--- tools/mugenspec/data/skp-hardware.ads
+++ tools/mugenspec/data/skp-hardware.ads
@@ -1,8 +1,6 @@
 package Skp.Hardware
 is
 
-   MMConf_Base_Address : constant := 16#f800_0000#;
-
    Debugconsole_Port : constant := 16#50b0#;
    System_Board_Reset_Port : constant := 16#0cf9#;
    System_Board_Pm1a_Cnt_Slp_Typ : constant := 16#1c00#;

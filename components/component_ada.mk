b4c70476e8614301bd651d9933895e3371710b68
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-02-23 16:35:59 +0100

    components: Install component specs if present
    Copy the component spec XML file to the policy object cspec directory if
    present. The processed XML file generated/$(COMPONENT).xml takes
    precedence over the static source file spec/$(COMPONENT).xml.

--- components/component_ada.mk
+++ components/component_ada.mk
@@ -1,7 +1,7 @@
 include ../common_ada.mk
 include ../cspecs.mk
 
-install: $(COMPONENT)
+install: $(COMPONENT) $(INSTALL_TARGETS)
 	$(TO_RAW_CMD) $(OBJ_DIR)/$(COMPONENT) $(POLICY_OBJ_DIR)/$(COMPONENT)
 
 clean:

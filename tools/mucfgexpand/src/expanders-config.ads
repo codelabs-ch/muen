a9637d2b27fedacc6464b7573b3781122b2bfc3d
Reto Buerki <reet@codelabs.ch>
2017-02-23 16:04:37 +0100

    Increase size of per-CPU store to two pages
    This is in preparation of instantiating the interrupt tables manager in
    the per-CPU storage area.

--- tools/mucfgexpand/src/expanders-config.ads
+++ tools/mucfgexpand/src/expanders-config.ads
@@ -24,8 +24,8 @@ is
    Kernel_BSS_Section_Addr          : constant := 16#0011_1000#;
    Kernel_BSS_Section_Size          : constant := 16#0000_1000#;
    Kernel_Store_Addr                : constant := 16#0011_2000#;
-   Kernel_Store_Size                : constant := 16#0000_1000#;
-   Kernel_Stack_Addr                : constant := 16#0011_3000#;
+   Kernel_Store_Size                : constant := 16#0000_2000#;
+   Kernel_Stack_Addr                : constant := 16#0011_4000#;
    Kernel_Stack_Size                : constant := 16#0000_1000#;
    Kernel_RO_Section_Addr           : constant := 16#0011_f000#;
    Kernel_RO_Section_Size           : constant := 16#0002_1000#;

a32f83ca6c988a07cad257e7a17dd09f40331619
Reto Buerki <reet@codelabs.ch>
2017-02-23 16:01:55 +0100

    Mucfgexpand: Re-order config constants
    Ordering the constants by increasing address greatly simplifies the
    understanding of the implemented memory layout.

--- tools/mucfgexpand/src/expanders-config.ads
+++ tools/mucfgexpand/src/expanders-config.ads
@@ -19,16 +19,16 @@
 package Expanders.Config
 is
 
-   Kernel_Data_Section_Size         : constant := 16#0000_1000#;
    Kernel_Data_Section_Addr         : constant := 16#0011_0000#;
-   Kernel_BSS_Section_Size          : constant := 16#0000_1000#;
+   Kernel_Data_Section_Size         : constant := 16#0000_1000#;
    Kernel_BSS_Section_Addr          : constant := 16#0011_1000#;
-   Kernel_RO_Section_Size           : constant := 16#0002_1000#;
-   Kernel_RO_Section_Addr           : constant := 16#0011_f000#;
-   Kernel_Stack_Size                : constant := 16#0000_1000#;
-   Kernel_Stack_Addr                : constant := 16#0011_3000#;
-   Kernel_Store_Size                : constant := 16#0000_1000#;
+   Kernel_BSS_Section_Size          : constant := 16#0000_1000#;
    Kernel_Store_Addr                : constant := 16#0011_2000#;
+   Kernel_Store_Size                : constant := 16#0000_1000#;
+   Kernel_Stack_Addr                : constant := 16#0011_3000#;
+   Kernel_Stack_Size                : constant := 16#0000_1000#;
+   Kernel_RO_Section_Addr           : constant := 16#0011_f000#;
+   Kernel_RO_Section_Size           : constant := 16#0002_1000#;
    VTd_IRT_Virtual_Addr             : constant := 16#001f_e000#;
    Tau0_Interface_Virtual_Addr      : constant := 16#001f_f000#;
    Kernel_Devices_Virtual_Addr      : constant := 16#0020_0000#;

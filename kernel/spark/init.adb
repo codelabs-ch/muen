9df2721baceeaf122e862fd9d7f68ef8eaff0037
Reto Buerki <reet@codelabs.ch>
2017-02-24 11:25:12 +0100

    Adjust contracts of Init procedure

--- kernel/spark/init.adb
+++ kernel/spark/init.adb
@@ -6,6 +6,7 @@ with SK.CPU_Global;
 with SK.CPU_Registry;
 with SK.FPU;
 with SK.Interrupts;
+with SK.Interrupt_Tables;
 with SK.IO_Apic;
 with SK.Kernel;
 with SK.MP;
@@ -25,13 +26,14 @@ procedure Init
 with
    Global =>
       (Input  => (SK.Tau0_Interface.State, SK.CPU_Global.CPU_ID,
-                  SK.GDT.GDT_Pointer, SK.VMX.Exit_Address),
-       In_Out => (SK.CPU_Registry.State, SK.FPU.State, SK.Interrupts.State,
-                  SK.IO_Apic.State, SK.MP.Barrier, SK.Scheduling_Info.State,
-                  SK.Subjects.State, SK.Subjects_Events.State,
-                  SK.Subjects_Interrupts.State, SK.Subjects_MSR_Store.State,
-                  SK.VTd.State, SK.Timed_Events.State, Skp.IOMMU.State,
-                  SK.VMX.VMCS_State, X86_64.State),
+                  SK.GDT.GDT_Pointer, SK.VMX.Exit_Address,
+                  SK.Interrupt_Tables.State, SK.Interrupts.State),
+       In_Out => (SK.CPU_Registry.State, SK.FPU.State, SK.IO_Apic.State,
+                  SK.MP.Barrier, SK.Scheduling_Info.State, SK.Subjects.State,
+                  SK.Subjects_Events.State, SK.Subjects_Interrupts.State,
+                  SK.Subjects_MSR_Store.State, SK.VTd.State,
+                  SK.Timed_Events.State, Skp.IOMMU.State, SK.VMX.VMCS_State,
+                  X86_64.State),
        Output => SK.CPU_Global.State)
 is
    Subject_Registers : SK.CPU_Registers_Type;

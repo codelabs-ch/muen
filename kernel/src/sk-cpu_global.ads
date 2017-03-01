61820b9907e2351a615372f38f7ed14b22754ce6
Reto Buerki <reet@codelabs.ch>
2017-03-01 10:46:06 +0100

    Fix Depends indentation of CPU_Global.Init

--- kernel/src/sk-cpu_global.ads
+++ kernel/src/sk-cpu_global.ads
@@ -47,8 +47,8 @@ is
       Global  => (Input  => Interrupt_Tables.State,
                   Output => State,
                   In_Out => X86_64.State),
-     Depends => (State        => (Interrupt_Tables.State, X86_64.State),
-                 X86_64.State =>+ Interrupt_Tables.State);
+      Depends => (State        => (Interrupt_Tables.State, X86_64.State),
+                  X86_64.State =>+ Interrupt_Tables.State);
 
    --  Set the ID of the currently active major frame to the specified value.
    procedure Set_Current_Major_Frame (ID : Skp.Scheduling.Major_Frame_Range)

925660d800648b374323e64da6b7a0f2ae9886a2
Reto Buerki <reet@codelabs.ch>
2017-04-12 16:25:35 +0200

    Remove POLICY_CSPEC_DIR as cspec prerequisite
    As the directory access time is modified after its creation (new files
    are copied to it), this does not work as a prerequisite.

--- components/cspecs.mk
+++ components/cspecs.mk
@@ -1,6 +1,8 @@
 CSPEC_XML     = $(wildcard spec/$(COMPONENT).xml)
 CSPEC_INSTALL = $(POLICY_CSPEC_DIR)/$(COMPONENT).xml
 
+DUMMY := $(shell mkdir -p $(POLICY_CSPEC_DIR))
+
 ifneq ($(CSPEC_XML),)
 INSTALL_TARGETS += $(CSPEC_INSTALL)
 CSPEC_XML_GEN    = $(wildcard $(GEN_DIR)/$(COMPONENT).xml)
@@ -14,8 +16,5 @@ $(GEN_DIR)/.cspecs: $(MUCGENSPEC) spec/$(COMPONENT).xml $(CSPEC_TARGETS)
 	$(MUCGENSPEC) -i spec/$(COMPONENT).xml -o $(GEN_DIR)/$(COMPONENT).xml -I $(GEN_DIR) $(GEN_DIR)
 	@touch $@
 
-$(CSPEC_INSTALL): $(CSPEC_XML) $(POLICY_CSPEC_DIR)
+$(CSPEC_INSTALL): $(CSPEC_XML)
 	cp $< $@
-
-$(POLICY_CSPEC_DIR):
-	mkdir -p $@

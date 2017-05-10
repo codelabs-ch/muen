ae30df91fa51149aa24d20c281dfc37589647cac
Reto Buerki <reet@codelabs.ch>
2017-05-10 14:35:55 +0200

    Add initial SK.Crash_Audit package
    The package currently provides internal (private) data structures to
    store 3 crash audit records. Such a record provides information on why
    the kernel aborted normal operation and triggered a system restart.

---
+++ kernel/src/sk-crash_audit.ads
@@ -0,0 +1,116 @@
+--
+--  Copyright (C) 2017  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2017  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Interfaces;
+
+with Skp.Interrupts;
+
+package SK.Crash_Audit
+is
+
+private
+
+   use type Interfaces.Unsigned_64;
+
+   --  xxd -l 8 -p /dev/random
+   Crash_Magic : constant := 16#d5ab_c59c_4a9a_2a93#;
+
+   subtype Version_Str_Range is Positive range 1 .. 64;
+
+   type Version_String_Type is new String (Version_Str_Range);
+
+   Null_Version_String : constant Version_String_Type := (others => ASCII.NUL);
+
+   Max_Dumps : constant := 3;
+
+   type Dumpdata_Length is range 0 .. Max_Dumps;
+
+   subtype Dumpdata_Index is Dumpdata_Length range 1 .. Dumpdata_Length'Last;
+
+   type Header_Type is record
+      Version_Magic  : Interfaces.Unsigned_64;
+      Version_String : Version_String_Type;
+      Generation     : Interfaces.Unsigned_64;
+      Boot_Count     : Interfaces.Unsigned_64;
+      Crash_Count    : Interfaces.Unsigned_64;
+      Max_Dump_Count : Dumpdata_Index;
+      Dump_Count     : Dumpdata_Length;
+      Crc32          : Interfaces.Unsigned_32;
+      Padding        : Interfaces.Unsigned_16;
+   end record;
+
+   Null_Header : constant Header_Type
+     := (Version_Magic  => Crash_Magic,
+         Version_String => Null_Version_String,
+         Generation     => 0,
+         Boot_Count     => 1,
+         Crash_Count    => 0,
+         Max_Dump_Count => Max_Dumps,
+         Dump_Count     => 0,
+         Crc32          => 0,
+         Padding        => 0);
+
+   ---------------------
+   --  Crash Reasons  --
+   ---------------------
+
+   type Reason_Type is new Interfaces.Unsigned_64;
+
+   Reason_Undefined : constant Reason_Type := 16#0000#;
+
+   --  Exceptions.
+
+   Hardware_Exception : constant Reason_Type := 16#1000#;
+
+   type Valid_Flags_Type is record
+      Isr_Context : Boolean;
+   end record;
+
+   Null_Validity_Flags : constant Valid_Flags_Type
+     := (Isr_Context => False);
+
+   type Dumpdata_Type is record
+      TSC_Value      : Interfaces.Unsigned_64;
+      Reason         : Reason_Type;
+      APIC_ID        : Skp.Interrupts.APIC_ID_Range;
+      Field_Validity : Valid_Flags_Type;
+      Isr_Context    : Isr_Context_Type;
+   end record;
+
+   Null_Dumpdata : constant Dumpdata_Type
+     := (TSC_Value      => 0,
+         APIC_ID        => 0,
+         Reason         => Reason_Undefined,
+         Field_Validity => Null_Validity_Flags,
+         Isr_Context    => Null_Isr_Context);
+
+   type Dumpdata_Array is array (Dumpdata_Index) of Dumpdata_Type;
+
+   Null_Dumpdata_Array : constant Dumpdata_Array
+     := (others => Null_Dumpdata);
+
+   type Dump_Type is record
+      Header : Header_Type;
+      Data   : Dumpdata_Array;
+   end record;
+
+   Null_Dump : constant Dump_Type
+     := (Header => Null_Header,
+         Data   => Null_Dumpdata_Array);
+
+end SK.Crash_Audit;

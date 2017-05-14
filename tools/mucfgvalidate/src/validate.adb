7809861013cd177fd726710688e58111ce0affef
Reto Buerki <reet@codelabs.ch>
2017-05-14 23:57:40 +0200

    Register crash audit region validator

--- tools/mucfgvalidate/src/validate.adb
+++ tools/mucfgvalidate/src/validate.adb
@@ -74,6 +74,8 @@ is
       XML_Processors.Register
         (Process => Memory.Physical_Memory_Overlap'Access);
       XML_Processors.Register
+        (Process => Memory.Uncached_Crash_Audit_Presence'Access);
+      XML_Processors.Register
         (Process => Kernel.Virtual_Memory_Overlap'Access);
       XML_Processors.Register
         (Process => Subject.Virtual_Memory_Overlap'Access);

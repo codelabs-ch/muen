3e4a334bb3c78b8a8cae573a54e5262451f7b5e8
Reto Buerki <reet@codelabs.ch>
2017-05-19 15:34:56 +0200

    Introduce Crash_Audit_Virtual_Addr constant
    The constant specifies the virtual address of the upcoming crash audit
    region in the kernel: 16#0020_0000#.

--- tools/mucfgexpand/src/expanders-config.ads
+++ tools/mucfgexpand/src/expanders-config.ads
@@ -36,6 +36,7 @@ is
    Kernel_RO_Section_Addr           : constant := 16#0011_f000#;
    Kernel_RO_Section_Size           : constant := 16#0002_1000#;
    Tau0_Interface_Virtual_Addr      : constant := 16#001f_f000#;
+   Crash_Audit_Virtual_Addr         : constant := 16#0020_0000#;
    Kernel_Devices_Virtual_Addr      : constant := 16#0030_0000#;
    Subject_States_Virtual_Addr      : constant := 16#0040_0000#;
    Subject_Timed_Event_Virtual_Addr : constant := 16#0050_0000#;

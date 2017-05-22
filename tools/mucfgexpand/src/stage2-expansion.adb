709742b93db7c1d0c97eac028a8226fab2206e4a
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-05-22 15:26:05 +0200

    Register kernel per-CPU local memory expander

--- tools/mucfgexpand/src/stage2-expansion.adb
+++ tools/mucfgexpand/src/stage2-expansion.adb
@@ -63,6 +63,7 @@ is
 
       Procs.Register (Process => Memory.Add_Missing_Attributes'Access);
       Procs.Register (Process => Memory.Add_Kernel_Binary'Access);
+      Procs.Register (Process => Memory.Add_Kernel_CPU_Local_Memory'Access);
       Procs.Register (Process => Memory.Add_Stack_Store'Access);
       Procs.Register (Process => Memory.Add_Subject_States'Access);
       Procs.Register (Process => Memory.Add_Subject_Timed_Event_Pages'Access);

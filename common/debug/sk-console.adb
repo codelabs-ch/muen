d54a1a05f2c734b939483cdd19f1c5dae3d254b9
Reto Buerki <reet@codelabs.ch>
2017-06-01 11:01:11 +0200

    Remove Put_[Byte|Word*] procedures from SK.Console

--- common/debug/sk-console.adb
+++ common/debug/sk-console.adb
@@ -16,8 +16,6 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-with SK.Utils;
-
 package body SK.Console
 is
 
@@ -36,20 +34,6 @@ is
    begin
       Output_New_Line;
    end New_Line;
-
-   -------------------------------------------------------------------------
-
-   procedure Put_Byte (Item : Byte)
-   is
-      subtype Byte_Range is Positive range 1 .. 2;
-      subtype Byte_String is String (Byte_Range);
-
-      Str : Byte_String := Byte_String'(others => '0');
-   begin
-      Utils.To_Hex (Item   => Word64 (Item),
-                    Buffer => Str);
-      Put_String (Item => Str);
-   end Put_Byte;
 
    -------------------------------------------------------------------------
 
@@ -77,47 +61,5 @@ is
          Put_Char (Item => Item (I));
       end loop;
    end Put_String;
-
-   -------------------------------------------------------------------------
-
-   procedure Put_Word16 (Item : Word16)
-   is
-      subtype Word16_Range is Positive range 1 .. 4;
-      subtype Word16_String is String (Word16_Range);
-
-      Str : Word16_String := Word16_String'(others => '0');
-   begin
-      Utils.To_Hex (Item   => Word64 (Item),
-                    Buffer => Str);
-      Put_String (Item => Str);
-   end Put_Word16;
-
-   -------------------------------------------------------------------------
-
-   procedure Put_Word32 (Item : Word32)
-   is
-      subtype Word32_Range is Positive range 1 .. 8;
-      subtype Word32_String is String (Word32_Range);
-
-      Str : Word32_String := Word32_String'(others => '0');
-   begin
-      Utils.To_Hex (Item   => Word64 (Item),
-                    Buffer => Str);
-      Put_String (Item => Str);
-   end Put_Word32;
-
-   -------------------------------------------------------------------------
-
-   procedure Put_Word64 (Item : Word64)
-   is
-      subtype Word64_Range is Positive range 1 .. 16;
-      subtype Word64_String is String (Word64_Range);
-
-      Str : Word64_String := Word64_String'(others => '0');
-   begin
-      Utils.To_Hex (Item   => Item,
-                    Buffer => Str);
-      Put_String (Item => Str);
-   end Put_Word64;
 
 end SK.Console;
--- common/debug/sk-console.ads
+++ common/debug/sk-console.ads
@@ -45,16 +45,4 @@ is
    --  Output given string and append a new line.
    procedure Put_Line (Item : String);
 
-   --  Output given byte in hex.
-   procedure Put_Byte (Item : Byte);
-
-   --  Output given word in hex.
-   procedure Put_Word16 (Item : Word16);
-
-   --  Output given doubleword in hex.
-   procedure Put_Word32 (Item : Word32);
-
-   --  Output given quadword in hex.
-   procedure Put_Word64 (Item : Word64);
-
 end SK.Console;

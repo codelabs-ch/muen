959089f118f1a8a8240c30c8f748f5936d908131
Reto Buerki <reet@codelabs.ch>
2017-06-02 09:40:07 +0200

    Convert Debug_Ops.Dump_State to Img() functions

--- components/sm/debug/debug_ops.adb
+++ components/sm/debug/debug_ops.adb
@@ -18,6 +18,8 @@
 
 with Interfaces;
 
+with SK.Strings;
+
 with Debuglog.Client;
 
 with Subject_Info;
@@ -26,6 +28,8 @@ package body Debug_Ops
 with
    SPARK_Mode => Off
 is
+
+   use SK.Strings;
 
    package Ifa renames Interfaces;
 
@@ -38,143 +42,84 @@ is
    begin
       Debuglog.Client.Put
         (Item => "Halting associated subject after EXIT (");
-      Debuglog.Client.Put_Word16 (Item => Ifa.Unsigned_16 (State.Exit_Reason));
-      Debuglog.Client.Put (Item => ":");
-      Debuglog.Client.Put_Word64
-        (Item => Ifa.Unsigned_64 (State.Exit_Qualification));
-      Debuglog.Client.Put_Line (Item => ")");
+      Debuglog.Client.Put_Line
+        (Item => Img (Ifa.Unsigned_16 (State.Exit_Reason))
+         & ":" & Img (State.Exit_Qualification) & ")");
 
       if (State.IA32_EFER and 16#400#) = 0 then
-         Debuglog.Client.Put ("EIP: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.RIP));
-         Debuglog.Client.Put (" CS : ");
-         Debuglog.Client.Put_Word16 (Item => Ifa.Unsigned_16
-                                     (State.CS.Selector));
-         Debuglog.Client.Put (" EFLAGS: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.RFLAGS));
-         Debuglog.Client.New_Line;
-         Debuglog.Client.Put ("ESP: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.RSP));
-         Debuglog.Client.Put (" SS : ");
-         Debuglog.Client.Put_Word16 (Item => Ifa.Unsigned_16
-                                     (State.SS.Selector));
+         Debuglog.Client.Put_Line
+           (Item => "EIP: " & Img (Ifa.Unsigned_32 (State.RIP))
+            & " CS : " & Img (Ifa.Unsigned_16 (State.CS.Selector))
+            & " EFLAGS: " & Img (Ifa.Unsigned_32 (State.RFLAGS)));
+         Debuglog.Client.Put_Line
+           (Item => "ESP: " & Img (Ifa.Unsigned_32 (State.RSP))
+            & " SS : " & Img (Ifa.Unsigned_16 (State.SS.Selector)));
+
+         Debuglog.Client.Put_Line
+           (Item => "EAX: " & Img (Ifa.Unsigned_32 (State.Regs.RAX))
+            & " EBX: " & Img (Ifa.Unsigned_32 (State.Regs.RBX))
+            & " ECX: " & Img (Ifa.Unsigned_32 (State.Regs.RCX))
+            & " EDX: " & Img (Ifa.Unsigned_32 (State.Regs.RDX)));
+
+         Debuglog.Client.Put_Line
+           (Item => "ESI: " & Img (Ifa.Unsigned_32 (State.Regs.RSI))
+            & " EDI: " & Img (Ifa.Unsigned_32 (State.Regs.RDI))
+            & " EBP: " & Img (Ifa.Unsigned_32 (State.Regs.RBP)));
+
+         Debuglog.Client.Put_Line
+           (Item => "CR0: " & Img (Ifa.Unsigned_32 (State.CR0))
+            & " CR2: " & Img (Ifa.Unsigned_32 (State.Regs.CR2))
+            & " CR3: " & Img (Ifa.Unsigned_32 (State.CR3))
+            & " CR4: " & Img (Ifa.Unsigned_32 (State.CR4)));
+
          Debuglog.Client.New_Line;
 
-         Debuglog.Client.Put (Item => "EAX: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.Regs.RAX));
-         Debuglog.Client.Put (Item => " EBX: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.Regs.RBX));
-         Debuglog.Client.Put (Item => " ECX: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.Regs.RCX));
-         Debuglog.Client.Put (Item => " EDX: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.Regs.RDX));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "ESI: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.Regs.RSI));
-         Debuglog.Client.Put (Item => " EDI: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.Regs.RDI));
-         Debuglog.Client.Put (Item => " EBP: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.Regs.RBP));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "CR0: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.CR0));
-         Debuglog.Client.Put (Item => " CR2: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.Regs.CR2));
-         Debuglog.Client.Put (Item => " CR3: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.CR3));
-         Debuglog.Client.Put (Item => " CR4: ");
-         Debuglog.Client.Put_Word32 (Item => Ifa.Unsigned_32 (State.CR4));
-         Debuglog.Client.New_Line;
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "Shadow CR0: ");
-         Debuglog.Client.Put_Word32
-           (Item => Ifa.Unsigned_32 (State.SHADOW_CR0));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "IA32_EFER : ");
-         Debuglog.Client.Put_Word64
-           (Item => Ifa.Unsigned_64 (State.IA32_EFER));
-         Debuglog.Client.New_Line;
+         Debuglog.Client.Put_Line
+           (Item => "Shadow CR0: " & Img (Ifa.Unsigned_32 (State.SHADOW_CR0)));
+         Debuglog.Client.Put_Line
+           (Item => "IA32_EFER : " & Img (State.IA32_EFER));
       else
-         Debuglog.Client.Put ("RIP: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.RIP));
-         Debuglog.Client.Put (" CS : ");
-         Debuglog.Client.Put_Word16 (Item => Ifa.Unsigned_16
-                                     (State.CS.Selector));
-         Debuglog.Client.Put (" RFLAGS: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.RFLAGS));
-         Debuglog.Client.New_Line;
-         Debuglog.Client.Put ("RSP: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.RSP));
-         Debuglog.Client.Put (" SS : ");
-         Debuglog.Client.Put_Word16 (Item => Ifa.Unsigned_16
-                                     (State.SS.Selector));
+         Debuglog.Client.Put_Line
+           (Item => "RIP: " & Img (State.RIP)
+            & " CS : " & Img (Ifa.Unsigned_16 (State.CS.Selector))
+            & " RFLAGS: " & Img (State.RFLAGS));
+         Debuglog.Client.Put_Line
+           (Item => "RSP: " & Img (State.RSP)
+            & " SS : " & Img (Ifa.Unsigned_16 (State.SS.Selector)));
+
+         Debuglog.Client.Put_Line
+           (Item => "RAX: " & Img (State.Regs.RAX)
+            & " RBX: " & Img (State.Regs.RBX)
+            & " RCX: " & Img (State.Regs.RCX));
+         Debuglog.Client.Put_Line
+           (Item => "RDX: " & Img (State.Regs.RDX)
+            & " RSI: " & Img (State.Regs.RSI)
+            & " RDI: " & Img (State.Regs.RDI));
+         Debuglog.Client.Put_Line
+           (Item => "RBP: " & Img (State.Regs.RBP)
+            & " R08: " & Img (State.Regs.R08)
+            & " R09: " & Img (State.Regs.R09));
+         Debuglog.Client.Put_Line
+           (Item => "R10: " & Img (State.Regs.R10)
+            & " R11: " & Img (State.Regs.R11)
+            & " R12: " & Img (State.Regs.R12));
+         Debuglog.Client.Put_Line
+           (Item => "R13: " & Img (State.Regs.R13)
+            & " R14: " & Img (State.Regs.R14)
+            & " R15: " & Img (State.Regs.R15));
+
+         Debuglog.Client.Put_Line
+           (Item => "CR0: " & Img (State.CR0)
+            & " CR2: " & Img (State.Regs.CR2));
+         Debuglog.Client.Put_Line
+           (Item => "CR3: " & Img (State.CR3)
+            & " CR4: " & Img (State.CR4));
          Debuglog.Client.New_Line;
 
-         Debuglog.Client.Put (Item => "RAX: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.RAX));
-         Debuglog.Client.Put (Item => " RBX: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.RBX));
-         Debuglog.Client.Put (Item => " RCX: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.RCX));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "RDX: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.RDX));
-         Debuglog.Client.Put (Item => " RSI: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.RSI));
-         Debuglog.Client.Put (Item => " RDI: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.RDI));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "RBP: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.RBP));
-         Debuglog.Client.Put (Item => " R08: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.R08));
-         Debuglog.Client.Put (Item => " R09: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.R09));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "R10: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.R10));
-         Debuglog.Client.Put (Item => " R11: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.R11));
-         Debuglog.Client.Put (Item => " R12: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.R12));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "R13: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.R13));
-         Debuglog.Client.Put (Item => " R14: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.R14));
-         Debuglog.Client.Put (Item => " R15: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.R15));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "CR0: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.CR0));
-         Debuglog.Client.Put (Item => " CR2: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.Regs.CR2));
-         Debuglog.Client.New_Line;
-         Debuglog.Client.Put (Item => "CR3: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.CR3));
-         Debuglog.Client.Put (Item => " CR4: ");
-         Debuglog.Client.Put_Word64 (Item => Ifa.Unsigned_64 (State.CR4));
-         Debuglog.Client.New_Line;
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "Shadow CR0: ");
-         Debuglog.Client.Put_Word64
-           (Item => Ifa.Unsigned_64 (State.SHADOW_CR0));
-         Debuglog.Client.New_Line;
-
-         Debuglog.Client.Put (Item => "IA32_EFER : ");
-         Debuglog.Client.Put_Word64
-           (Item => Ifa.Unsigned_64 (State.IA32_EFER));
-         Debuglog.Client.New_Line;
+         Debuglog.Client.Put_Line
+           (Item => "Shadow CR0: " & Img (State.SHADOW_CR0));
+         Debuglog.Client.Put_Line
+           (Item => "IA32_EFER : " & Img (State.IA32_EFER));
       end if;
    end Dump_State;
 

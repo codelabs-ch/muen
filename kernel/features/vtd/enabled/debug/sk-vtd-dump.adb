e4e41e737f09cbcf7693b3dc08a5e14c4a0a44d9
Reto Buerki <reet@codelabs.ch>
2017-06-14 15:35:09 +0200

    Output VT-d fault source with no base information
    16#00#:16#1c#.16#01# looks just weird.

--- kernel/features/vtd/enabled/debug/sk-vtd-dump.adb
+++ kernel/features/vtd/enabled/debug/sk-vtd-dump.adb
@@ -103,9 +103,10 @@ is
             end if;
          end if;
 
-         KC.Put_String (Item => ", Source: " & Img (Byte (Fault.SID / 2 ** 8))
-                        & ":" & Img (Byte ((Fault.SID / 2 ** 3) and 16#1f#))
-                        & "." & Img (Byte (Fault.SID and 16#07#)));
+         KC.Put_String
+           (Item => ", Source: " & Img_Nobase (Byte (Fault.SID / 2 ** 8))
+            & ":" & Img_Nobase (Byte ((Fault.SID / 2 ** 3) and 16#1f#))
+            & "." & Img_Nobase (Byte (Fault.SID and 16#07#)));
       end if;
 
       KC.New_Line;

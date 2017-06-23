fbd8ef6d0f6f239dfe3b69bcdf2e749d7dde33d7
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-06-23 11:05:59 +0200

    Use Global_Subject_ID type in Skp.Subjects

--- tools/mugenspec/templates/skp-subjects.adb
+++ tools/mugenspec/templates/skp-subjects.adb
@@ -24,103 +24,118 @@ is
       VMX_Controls       : VMX_Controls_Type;
    end record;
 
-   type Subject_Spec_Array is array (Subject_Id_Type) of Subject_Spec_Type;
+   type Subject_Spec_Array is array (Global_Subject_ID_Type) of
+     Subject_Spec_Type;
 
    Subject_Specs : constant Subject_Spec_Array := Subject_Spec_Array'(
 __subjects__);
 
    -------------------------------------------------------------------------
 
-   function Get_CPU_Id (Subject_Id : Subject_Id_Type) return CPU_Range
+   function Get_CPU_Id (Subject_Id : Global_Subject_ID_Type) return CPU_Range
    is (Subject_Specs (Subject_Id).CPU_Id);
 
    -------------------------------------------------------------------------
 
-   function Get_CR0 (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_CR0 (Subject_Id : Global_Subject_ID_Type) return SK.Word64
    is (Subject_Specs (Subject_Id).CR0_Value);
 
    -------------------------------------------------------------------------
 
-   function Get_CR0_Mask (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_CR0_Mask (Subject_Id : Global_Subject_ID_Type) return SK.Word64
    is (Subject_Specs (Subject_Id).CR0_Mask);
 
    -------------------------------------------------------------------------
 
-   function Get_CR4 (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_CR4 (Subject_Id : Global_Subject_ID_Type) return SK.Word64
    is (Subject_Specs (Subject_Id).CR4_Value);
 
    -------------------------------------------------------------------------
 
-   function Get_CR4_Mask (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_CR4_Mask (Subject_Id : Global_Subject_ID_Type) return SK.Word64
    is (Subject_Specs (Subject_Id).CR4_Mask);
 
    -------------------------------------------------------------------------
 
-   function Get_CS_Access (Subject_Id : Subject_Id_Type) return SK.Word32
+   function Get_CS_Access
+     (Subject_Id : Global_Subject_ID_Type)
+      return SK.Word32
    is (Subject_Specs (Subject_Id).CS_Access);
 
    -------------------------------------------------------------------------
 
-   function Get_Entry_Point (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_Entry_Point
+     (Subject_Id : Global_Subject_ID_Type)
+      return SK.Word64
    is (Subject_Specs (Subject_Id).Entry_Point);
 
    -------------------------------------------------------------------------
 
-   function Get_EPT_Pointer (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_EPT_Pointer
+     (Subject_Id : Global_Subject_ID_Type)
+      return SK.Word64
    is (Subject_Specs (Subject_Id).EPT_Pointer);
 
    -------------------------------------------------------------------------
 
    function Get_Exception_Bitmap
-     (Subject_Id : Subject_Id_Type)
+     (Subject_Id : Global_Subject_ID_Type)
       return SK.Word32
    is (Subject_Specs (Subject_Id).Exception_Bitmap);
 
    -------------------------------------------------------------------------
 
    function Get_IO_Bitmap_Address
-     (Subject_Id : Subject_Id_Type)
+     (Subject_Id : Global_Subject_ID_Type)
       return SK.Word64
    is (Subject_Specs (Subject_Id).IO_Bitmap_Address);
 
    -------------------------------------------------------------------------
 
    function Get_MSR_Bitmap_Address
-     (Subject_Id : Subject_Id_Type)
+     (Subject_Id : Global_Subject_ID_Type)
       return SK.Word64
    is (Subject_Specs (Subject_Id).MSR_Bitmap_Address);
 
    -------------------------------------------------------------------------
 
-   function Get_MSR_Count (Subject_Id : Subject_Id_Type) return SK.Word32
+   function Get_MSR_Count
+     (Subject_Id : Global_Subject_ID_Type)
+      return SK.Word32
    is (Subject_Specs (Subject_Id).MSR_Count);
 
    -------------------------------------------------------------------------
 
    function Get_MSR_Store_Address
-     (Subject_Id : Subject_Id_Type)
+     (Subject_Id : Global_Subject_ID_Type)
       return SK.Word64
    is (Subject_Specs (Subject_Id).MSR_Store_Address);
 
    -------------------------------------------------------------------------
 
-   function Get_PML4_Address (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_PML4_Address
+     (Subject_Id : Global_Subject_ID_Type)
+      return SK.Word64
    is (Subject_Specs (Subject_Id).PML4_Address);
 
    -------------------------------------------------------------------------
 
-   function Get_Stack_Address (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_Stack_Address
+     (Subject_Id : Global_Subject_ID_Type)
+      return SK.Word64
    is (Subject_Specs (Subject_Id).Stack_Address);
 
    -------------------------------------------------------------------------
 
-   function Get_VMCS_Address (Subject_Id : Subject_Id_Type) return SK.Word64
+   function Get_VMCS_Address
+     (Subject_Id : Global_Subject_ID_Type)
+      return SK.Word64
    is (Subject_Specs (Subject_Id).VMCS_Address);
 
    -------------------------------------------------------------------------
 
    function Get_VMX_Controls
-     (Subject_Id : Subject_Id_Type)
+     (Subject_Id : Global_Subject_ID_Type)
       return VMX_Controls_Type
    is (Subject_Specs (Subject_Id).VMX_Controls);
 

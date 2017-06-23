dc38b3f78ed6ca9d8e057913479120798ea65f2e
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-06-23 10:48:18 +0200

    Mugenspec: Add Skp.Global_Subject_ID_Type
    The new type will eventually replace Skp.Subject_Id_Type but it is added
    in parallel to enable an iterative renaming.

--- tools/mugenspec/templates/skp.ads
+++ tools/mugenspec/templates/skp.ads
@@ -5,9 +5,11 @@ is
 
    type CPU_Range is range 0 .. CPU_Count - 1;
 
-   subtype Subject_Id_Type is Natural range __subj_range__;
+   subtype Global_Subject_ID_Type is Natural range __subj_range__;
 
-   Invalid_Subject : constant := Subject_Id_Type'Last + 1;
+   subtype Subject_Id_Type is Natural range Global_Subject_ID_Type'Range;
+
+   Invalid_Subject : constant := Global_Subject_ID_Type'Last + 1;
 
    subtype Dst_Subject_Type is Natural range 0 .. Invalid_Subject;
 

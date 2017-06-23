ec40f891efbbe1513229ed4e8c9f1f184ec4e750
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-06-23 12:10:57 +0200

    Mugenspec: Drop Subject_Id_Type from Skp package

--- tools/mugenspec/templates/skp.ads
+++ tools/mugenspec/templates/skp.ads
@@ -7,8 +7,6 @@ is
 
    subtype Global_Subject_ID_Type is Natural range __subj_range__;
 
-   subtype Subject_Id_Type is Natural range Global_Subject_ID_Type'Range;
-
    Invalid_Subject : constant := Global_Subject_ID_Type'Last + 1;
 
    subtype Dst_Subject_Type is Natural range 0 .. Invalid_Subject;

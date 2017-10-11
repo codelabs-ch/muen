4ecdaa3aabd51cc4a32bca0665ea2473a55dfc3b
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-10-11 17:43:52 +0200

    Add cyclic component library dependencies check
    The validator checks that there are no cyclic component dependencies.

--- tools/mucfgexpand/src/cfgchecks.ads
+++ tools/mucfgexpand/src/cfgchecks.ads
@@ -122,6 +122,10 @@ is
    --  Check component library references.
    procedure Component_Library_References (XML_Data : Muxml.XML_Data_Type);
 
+   --  Check component library cyclic references.
+   procedure Component_Library_Cyclic_References
+     (XML_Data : Muxml.XML_Data_Type);
+
    --  Chech that the kernel diagnostics port references a physical I/O device.
    procedure Kernel_Diagnostics_Dev_Reference (XML_Data : Muxml.XML_Data_Type);
 

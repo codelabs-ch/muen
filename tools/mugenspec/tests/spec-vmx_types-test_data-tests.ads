9323226c451807556c3cd3a621aeef6e4fae2b8f
Reto Buerki <reet@codelabs.ch>
2017-12-04 12:16:12 +0100

    Mugenspec: Implement tests for Utils.To_Number
    GNATtest GPL 2017 now automatically creates tests for the
    Utils.To_Number generic instances declared in Spec.VMX_Types.
    
    Note: The new read-only with-clauses and global variables sections in
          the tests packages do not work as they are (erroneously?)
          overwritten on each harness (re)generation.

---
+++ tools/mugenspec/tests/spec-vmx_types-test_data-tests.ads
@@ -0,0 +1,37 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Spec.VMX_Types.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Spec.VMX_Types.Test_Data.Test
+   with null record;
+
+   procedure Test_Get_Pin_Controls_725b92 (Gnattest_T : in out Test);
+   --  spec-vmx_types.ads:37:4:Get_Pin_Controls
+
+   procedure Test_Get_Proc_Controls_4f4761 (Gnattest_T : in out Test);
+   --  spec-vmx_types.ads:75:4:Get_Proc_Controls
+
+   procedure Test_Get_Proc2_Controls_d0b89f (Gnattest_T : in out Test);
+   --  spec-vmx_types.ads:122:4:Get_Proc2_Controls
+
+   procedure Test_Get_Entry_Controls_bde183 (Gnattest_T : in out Test);
+   --  spec-vmx_types.ads:155:4:Get_Entry_Controls
+
+   procedure Test_Get_Exit_Controls_779aa6 (Gnattest_T : in out Test);
+   --  spec-vmx_types.ads:183:4:Get_Exit_Controls
+
+   procedure Test_Get_CR0_a27155 (Gnattest_T : in out Test);
+   --  spec-vmx_types.ads:213:4:Get_CR0
+
+   procedure Test_Get_CR4_5f36b7 (Gnattest_T : in out Test);
+   --  spec-vmx_types.ads:252:4:Get_CR4
+
+   procedure Test_Get_Exceptions_fe16dd (Gnattest_T : in out Test);
+   --  spec-vmx_types.ads:298:4:Get_Exceptions
+
+end Spec.VMX_Types.Test_Data.Tests;
+--  end read only

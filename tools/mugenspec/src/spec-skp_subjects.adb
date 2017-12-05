ed9b0a4ff039f8a40e611c38507a23a7d238bdb5
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-12-05 16:51:37 +0100

    Mugenspec: Set CR0 mask flag default value to 1
    This makes sure that all CR0 flags not explicitly enabled are
    host-owned. As a consequence, CR0 flags that may be added by Intel in
    the future but are not (yet) supported by the Muen tools are not
    accessible to subjects.

--- tools/mugenspec/src/spec-skp_subjects.adb
+++ tools/mugenspec/src/spec-skp_subjects.adb
@@ -267,7 +267,9 @@ is
            & Mutools.Utils.To_Hex (Number => VMX.Get_CR0 (Fields => CR0_Value))
            & "," & ASCII.LF
            & Indent & "    CR0_Mask           => "
-           & Mutools.Utils.To_Hex (Number => VMX.Get_CR0 (Fields => CR0_Mask))
+           & Mutools.Utils.To_Hex (Number => VMX.Get_CR0
+                                   (Fields  => CR0_Mask,
+                                    Default => Interfaces.Unsigned_64'Last))
            & "," & ASCII.LF
            & Indent & "    CR4_Value          => "
            & Mutools.Utils.To_Hex (Number => VMX.Get_CR4 (Fields => CR4_Value))

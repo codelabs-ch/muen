155942e61680fe6bc44ade941323d93d93ecd98f
Reto Buerki <reet@codelabs.ch>
2017-12-11 14:45:30 +0100

    Add Dev_Mngr.Sender package
    The package provides a Send procedure which writes a response into the
    channel.

---
+++ components/dm/src/dev_mngr-sender.ads
@@ -0,0 +1,29 @@
+--
+--  Copyright (C) 2017  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2017  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+package Dev_Mngr.Sender
+with
+   Abstract_State => (State with External => Async_Readers)
+is
+
+   --  Copies the given response into the DM response page.
+   procedure Send (Res : Emul_Message_Type)
+   with
+      Global => (Output => State);
+
+end Dev_Mngr.Sender;

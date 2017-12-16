851ec5e97141d86dec3b85241e0e3db9fee8321d
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-12-16 16:21:23 +0100

    Add AHCI driver component skeleton

---
+++ components/ahci_drv/debug/debug_ops.adb
@@ -0,0 +1,40 @@
+--
+--  Copyright (C) 2014-2021  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014-2021  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Debuglog.Client;
+
+package body Debug_Ops
+with
+   SPARK_Mode => Off
+is
+
+   -------------------------------------------------------------------------
+
+   procedure Init
+     (Epoch : Interfaces.Unsigned_64)
+      renames Debuglog.Client.Init;
+
+   -------------------------------------------------------------------------
+
+   procedure Put_Line (Item : String) renames Debuglog.Client.Put_Line;
+
+   -------------------------------------------------------------------------
+
+   procedure Put_String (Item : String) renames Debuglog.Client.Put;
+
+end Debug_Ops;
---
+++ components/ahci_drv/debug/debug_ops.ads
@@ -0,0 +1,33 @@
+--
+--  Copyright (C) 2014-2021  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2014-2021  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Interfaces;
+
+package Debug_Ops
+is
+
+   --  Initialize debug log.
+   procedure Init (Epoch : Interfaces.Unsigned_64);
+
+   --  Output given string.
+   procedure Put_String (Item : String);
+
+   --  Output given string and append a new line.
+   procedure Put_Line (Item : String);
+
+end Debug_Ops;

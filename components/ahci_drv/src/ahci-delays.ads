1f01595b2a0db3e628a66107960cf13a9d6073d2
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-12-30 15:30:17 +0100

    Implement Ahci.Delays package
    The package provides procedures to suspend execution for a specified amount
    of time.

---
+++ components/ahci_drv/src/ahci-delays.ads
@@ -0,0 +1,28 @@
+--
+--  Copyright (C) 2017  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2017  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+package Ahci.Delays
+is
+
+   --  Suspend execution of caller for at least Usec microseconds.
+   procedure U_Delay (Usec : Natural);
+
+      --  Suspend execution of caller for at least Usec miliseconds.
+   procedure M_Delay (Msec : Natural);
+
+end Ahci.Delays;

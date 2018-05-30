d8c6bcfc19e57d4d1cd77f65e3831da9502d58f4
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2018-05-30 18:36:38 +0200

    Rename Atomic32_Type to Atomic64_Type

--- kernel/src/sk-subjects_events.adb
+++ kernel/src/sk-subjects_events.adb
@@ -87,7 +87,7 @@ is
    is
    begin
       for Subj_ID in Skp.Global_Subject_ID_Type'Range loop
-         Global_Pending_Events (Subj_ID) := Atomic32_Type'(Bits => 0);
+         Global_Pending_Events (Subj_ID) := Atomic64_Type'(Bits => 0);
       end loop;
    end Initialize;
 
@@ -113,7 +113,7 @@ is
       Refined_Depends => (Global_Pending_Events =>+ Subject)
    is
    begin
-      Global_Pending_Events (Subject) := Atomic32_Type'(Bits => 0);
+      Global_Pending_Events (Subject) := Atomic64_Type'(Bits => 0);
    end Clear_Events;
 
    -------------------------------------------------------------------------
--- kernel/src/sk-subjects_events.ads
+++ kernel/src/sk-subjects_events.ads
@@ -61,7 +61,7 @@ is
 
 private
 
-   type Atomic32_Type is record
+   type Atomic64_Type is record
       Bits : Word64 with Atomic;
    end record
    with
@@ -70,11 +70,11 @@ private
       Alignment => 8;
 
    pragma Compile_Time_Error
-     ((Atomic32_Type'Size < 2 ** Skp.Events.Event_Bits),
+     ((Atomic64_Type'Size < 2 ** Skp.Events.Event_Bits),
       "Pending event bit size too small");
 
    type Pending_Events_Array is array (Skp.Global_Subject_ID_Type)
-     of Atomic32_Type
+     of Atomic64_Type
    with
       Independent_Components;
 

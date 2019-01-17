40b1b0a6dad374538fb99466fda455e38c37afb1
Reto Buerki <reet@codelabs.ch>
2019-01-17 09:52:03 +0100

    Add start/shutdown script for GUDE EPC power connector
    The script starts/stops a target host plugged into the power slot one of
    a GUDE Expert Power Control (EPC) NET edge connector.

---
+++ deploy/start_shutdown_epc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Start or stop target host using the GUDE Expert Power Control (EPC) NET power
+# connector [1]. The target is assumed to be plugged in port 1.
+#
+# [1] - http://wiki.gude.info/FAQ_EPC_CmdLine#Beispiele
+
+DEVICE=$1
+ACTION=$2
+WGET="wget --quiet --delete-after"
+URL="http://$DEVICE/ov.html?cmd=1&p=1&s="
+
+if [ "$#" -ne 2 ]; then
+	echo "$0 <ip> <start|shutdown>"
+	exit 2
+fi
+
+SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source $SCRIPTDIR/args.sh
+
+case $ACTION in
+	start)
+		$WGET "${URL}1"
+		;;
+	shutdown)
+		$WGET "${URL}0"
+		;;
+esac

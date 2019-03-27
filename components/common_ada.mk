d0fb9d071da13796a32f3d82114f8b5753b5f409
Reto Buerki <reet@codelabs.ch>
2019-03-27 19:17:41 +0100

    Adjust stackcheck targets
    In order to run these targets in parallel, they must depend on the
    associated binary. Since this dependency is phony, a stackcheck target
    also becomes a phony target. Therefore, drop stackcheck stamp files as
    they do no longer work.
    
    As a result of this change, stackcheck targets are now always executed
    when present in a Makefile. As they are quite fast, this should be ok.

--- components/common_ada.mk
+++ components/common_ada.mk
@@ -3,16 +3,14 @@ include ../../Makeconf
 BUILD_OPTS += --RTS=$(TOP_DIR)/rts/obj
 
 STACK_DEPS  = $(wildcard $(OBJ_DIR)/*.ci)
-STACK_CHECK = $(OBJ_DIR)/.stackcheck_ok
 
-all: $(COMPONENT) $(STACK_CHECK)
+all: stackcheck
 
 $(COMPONENT): $(COMPONENT_TARGETS)
 	gprbuild $(BUILD_OPTS) -P$(COMPONENT) -Xstacksize=$(COMPONENT_STACK_SIZE)
 
-$(STACK_CHECK): $(STACK_DEPS)
+stackcheck: $(COMPONENT) $(STACK_DEPS)
 	$(MUCHECKSTACK) -P$(COMPONENT) -l$(COMPONENT_STACK_SIZE)
-	@touch $@
 
 prepare: $(COMPONENT_TARGETS)
 

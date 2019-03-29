e00ce9666e0662982bc70ac838a7628b8594196a
Reto Buerki <reet@codelabs.ch>
2019-03-29 12:31:00 +0100

    Adjust exec.mk configuration
    Always define $(E) wrapper, as it is also called in VERBOSE_OUTPUT mode.
    Use -Otarget sync in verbose mode to make sure parallel make output is
    readable.
    
    Also, drop unneeded -O sync option in quiet mode.

--- projects/exec.mk
+++ projects/exec.mk
@@ -1,6 +1,9 @@
 DIR := $(patsubst %/,%,$(dir $(realpath $(filter %exec.mk, $(MAKEFILE_LIST)))))
 
+E := $(DIR)/execute
+
 ifeq (,$(VERBOSE_OUTPUT))
-	MAKEFLAGS += -s -Oline
-	E := $(DIR)/execute
+	MAKEFLAGS += -s
+else
+	MAKEFLAGS += -Otarget
 endif

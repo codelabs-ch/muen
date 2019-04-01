27ce140e53524a270e6df1af1da66c2d1ad64f15
Reto Buerki <reet@codelabs.ch>
2019-04-01 10:39:51 +0200

    Make the make -O granularity configurable
    Use the BUILD_OUTPUT_SYNC variable to override the make output sync
    granularity, default is 'target'.

--- projects/exec.mk
+++ projects/exec.mk
@@ -2,8 +2,10 @@ DIR := $(patsubst %/,%,$(dir $(realpath $(filter %exec.mk, $(MAKEFILE_LIST)))))
 
 E := $(DIR)/execute
 
+BUILD_OUTPUT_SYNC ?= target
+
 ifeq (,$(BUILD_OUTPUT_VERBOSE))
 	MAKEFLAGS += -s
 else
-	MAKEFLAGS += -Otarget
+	MAKEFLAGS += -O$(BUILD_OUTPUT_SYNC)
 endif

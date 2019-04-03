db376a77c8816117450cf8a68c81ae053154b0e6
Reto Buerki <reet@codelabs.ch>
2019-04-03 19:27:18 +0200

    Add note about GNU make output sync option in exec.mk

--- projects/exec.mk
+++ projects/exec.mk
@@ -2,6 +2,9 @@ DIR := $(patsubst %/,%,$(dir $(realpath $(filter %exec.mk, $(MAKEFILE_LIST)))))
 
 E := $(DIR)/execute
 
+# GNU make output sync setting. Only relevant when running multiple jobs (make
+# -j) in verbose output mode. Possible output sync granularity values: none,
+#  line, target, recurse.
 BUILD_OUTPUT_SYNC ?= target
 
 ifeq (,$(BUILD_OUTPUT_VERBOSE))

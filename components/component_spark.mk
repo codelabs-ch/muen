54e5b51bd61eccd7d7d14957b64db50b8aed209d
Reto Buerki <reet@codelabs.ch>
2019-05-09 14:47:10 +0200

    Unify handling of components / libraries
    There are only two small differences between components and libraries
    now:
    
    - mk  : Libraries only install $(INSTALL_TARGETS) but no binary
    - gpr : No Linker switches for library projects
    
    Move common parts to new common.[gpr|mk] files and include them in
    component|library.[gpr|mk]. Append build mode to library lib/obj path
    like it is done for SPARK 'components'.
    
    Explicitly disable proof for libmuchannel (generics, proofed at
    instantiation) and libdebuglog (technically an Ada library).

--- components/component_spark.mk
+++ components/component_spark.mk
@@ -1,33 +1,7 @@
-COMP_BIN = $(OBJ_DIR)/debug/$(COMPONENT)
-
-all: $(ALL)
-
-include ../../Makeconf
-include ../../Makespark
-include ../cspecs.mk
-include ../proofs.mk
+include ../common.mk
 
 $(POLICY_OBJ_DIR)/$(COMPONENT): $(COMP_BIN) $(INSTALL_TARGETS)
 	 @$(E) $(COMPONENT) Install "$(TO_RAW_CMD) $< $@"
 
-STACK_SIZE = $(COMPONENT_STACK_SIZE)
-
-all: $(STACK_CHECK)
-
-SPARK_OPTS += $(PROOF_OPTS)
-
-$(OBJ_DIR)/%/$(COMPONENT): $(COMPONENT_TARGETS) FORCE
-	@$(E) $(COMPONENT) "Build ($*)" \
-		"gprbuild $(BUILD_OPTS) -P$(COMPONENT) -Xbuild=$* -Xstacksize=$(STACK_SIZE) $(PROOF_OPTS)"
-
-$(OBJ_DIR)/$(COMPONENT): $(OBJ_DIR)/debug/$(COMPONENT) $(OBJ_DIR)/release/$(COMPONENT)
-
-prepare: $(COMPONENT_TARGETS)
-
 clean:
-	@rm -rf $(OBJ_DIR)
-	@rm -rf $(GEN_DIR)
-
-FORCE:
-
-.PHONY: FORCE
+	@rm -rf $(OBJ_DIR) $(GEN_DIR)
--- components/library_spark.mk
+++ components/library_spark.mk
@@ -1,24 +1,4 @@
-include ../../Makeconf
-
-BUILD_OPTS += --RTS=$(TOP_DIR)/rts/obj
-
-STACK_DEPS  = $(wildcard $(OBJ_DIR)/*.ci)
-
-all: stackcheck
-
-$(COMPONENT): $(COMPONENT_TARGETS)
-	@$(E) $(COMPONENT) Build \
-		"gprbuild $(BUILD_OPTS) -P$(COMPONENT) -Xstacksize=$(COMPONENT_STACK_SIZE)"
-
-stackcheck: $(COMPONENT) $(STACK_DEPS)
-	@$(E) $(COMPONENT) "Check stack" \
-		"$(MUCHECKSTACK) -P$(COMPONENT) -l$(COMPONENT_STACK_SIZE)"
-
-prepare: $(COMPONENT_TARGETS)
-
-check: stackcheck
-
-include ../cspecs.mk
+include ../common.mk
 
 $(POLICY_OBJ_DIR)/$(COMPONENT): $(INSTALL_TARGETS)
 

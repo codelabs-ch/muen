4d6d7c6aa01ade04627246f10acf7a63e1529212
Reto Buerki <reet@codelabs.ch>
2019-05-09 12:08:21 +0200

    Merge common_ada.mk into library_spark.mk

--- components/library_spark.mk
+++ components/library_spark.mk
@@ -1,4 +1,23 @@
-include ../common_ada.mk
+include ../../Makeconf
+
+BUILD_OPTS += --RTS=$(TOP_DIR)/rts/obj
+
+STACK_DEPS  = $(wildcard $(OBJ_DIR)/*.ci)
+
+all: stackcheck
+
+$(COMPONENT): $(COMPONENT_TARGETS)
+	@$(E) $(COMPONENT) Build \
+		"gprbuild $(BUILD_OPTS) -P$(COMPONENT) -Xstacksize=$(COMPONENT_STACK_SIZE)"
+
+stackcheck: $(COMPONENT) $(STACK_DEPS)
+	@$(E) $(COMPONENT) "Check stack" \
+		"$(MUCHECKSTACK) -P$(COMPONENT) -l$(COMPONENT_STACK_SIZE)"
+
+prepare: $(COMPONENT_TARGETS)
+
+check: stackcheck
+
 include ../cspecs.mk
 
 $(POLICY_OBJ_DIR)/$(COMPONENT): $(INSTALL_TARGETS)

1d2ecaf4e8d3911656139ed9f5b3b2234187b7dd
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-05-17 10:48:13 +0200

    Generate appendPageMR[File|Fill] commands
    Add content to file-backed/filled memory regions by generating the
    corresponding commands.

--- tools/mugentau0cmds/src/cmd_stream-roots-memory.adb
+++ tools/mugentau0cmds/src/cmd_stream-roots-memory.adb
@@ -26,6 +26,8 @@ with McKae.XML.XPath.XIA;
 with Mutools.Constants;
 with Mutools.Utils;
 
+with Muxml.Utils;
+
 with Cmd_Stream.XML_Utils;
 
 package body Cmd_Stream.Roots.Memory
@@ -49,6 +51,92 @@ is
       Last_Level   : Natural;
       Base_Address : Interfaces.Unsigned_64;
       Size         : Interfaces.Unsigned_64);
+
+   --  Generate command stream for file or fill content of memory region.
+   procedure Add_Content
+     (Stream_Doc   : Muxml.XML_Data_Type;
+      Content_Node : DOM.Core.Node;
+      Region_Attr  : XML_Utils.Attribute_Type;
+      Base_Address : Interfaces.Unsigned_64;
+      Size         : Interfaces.Unsigned_64);
+
+   -------------------------------------------------------------------------
+
+   procedure Add_Content
+     (Stream_Doc   : Muxml.XML_Data_Type;
+      Content_Node : DOM.Core.Node;
+      Region_Attr  : XML_Utils.Attribute_Type;
+      Base_Address : Interfaces.Unsigned_64;
+      Size         : Interfaces.Unsigned_64)
+   is
+      use type Interfaces.Unsigned_64;
+
+      type Content_Type is (File, Fill);
+
+      Content_Str : constant String
+        := DOM.Core.Elements.Get_Tag_Name (Elem => Content_Node);
+      Content : constant Content_Type
+        := Content_Type'Value (Content_Str);
+      End_Addr : constant Interfaces.Unsigned_64
+        := Base_Address + Size;
+      Cur_Addr : Interfaces.Unsigned_64 := Base_Address;
+
+      Offset_Str : constant String
+        := DOM.Core.Elements.Get_Attribute
+          (Elem => Content_Node,
+           Name => "offset");
+      Content_Attrs : XML_Utils.Attribute_Array
+        (1 .. (if Content = File then 4 else 3));
+      Cmd_Name : constant String
+        := "appendPageMR" & (if Content = File then "File" else "Fill");
+
+      Cur_Offset : Interfaces.Unsigned_64;
+   begin
+      Content_Attrs (1) := Region_Attr;
+      Content_Attrs (2).Attr := U ("page");
+      if Content = File then
+         Content_Attrs (3) := (Attr  => U ("filename"),
+                               Value => U (DOM.Core.Elements.Get_Attribute
+                                 (Elem => Content_Node,
+                                  Name => "filename")));
+         Content_Attrs (4).Attr := U ("offset");
+         Cur_Offset := (if Offset_Str = "none" then 0
+                        else Interfaces.Unsigned_64'Value (Offset_Str));
+      else
+         declare
+            Fill_Str : constant String
+              := DOM.Core.Elements.Get_Attribute
+                (Elem => Content_Node,
+                 Name => "pattern");
+            Fill_Byte : constant String
+              := (if Fill_Str = "none" then "00"
+                  else Fill_Str (Fill_Str'First + 3 .. Fill_Str'First + 4));
+            Fill_Quad : constant String := "16#"
+              & Fill_Byte & Fill_Byte & "_" & Fill_Byte & Fill_Byte & "_"
+              & Fill_Byte & Fill_Byte & "_" & Fill_Byte & Fill_Byte & "#";
+         begin
+            Content_Attrs (3) := (Attr  => U ("fill"),
+                                  Value => U (Fill_Quad));
+         end;
+      end if;
+
+      while Cur_Addr < End_Addr loop
+         Content_Attrs (2).Value := U (Mutools.Utils.To_Hex
+                                       (Number => Cur_Addr));
+
+         if Content = File then
+            Content_Attrs (4).Value := U (Mutools.Utils.To_Hex
+                                          (Number => Cur_Offset));
+            Cur_Offset := Cur_Offset + Mutools.Constants.Page_Size;
+         end if;
+
+         XML_Utils.Append_Command
+           (Stream_Doc => Stream_Doc,
+            Name       => Cmd_Name,
+            Attrs      => Content_Attrs);
+         Cur_Addr := Cur_Addr + Mutools.Constants.Page_Size;
+      end loop;
+   end Add_Content;
 
    -------------------------------------------------------------------------
 
@@ -88,6 +176,7 @@ is
       for I in 0 .. DOM.Core.Nodes.Length (List => Phys_Memory) - 1 loop
          declare
             use type Interfaces.Unsigned_64;
+            use type DOM.Core.Node;
 
             Mem_Region : constant DOM.Core.Node
               := DOM.Core.Nodes.Item
@@ -110,6 +199,10 @@ is
             Region_Attr : constant XML_Utils.Attribute_Type
               := (Attr  => U ("region"),
                   Value => U (Trim (Root_ID'Img)));
+            Content_Node : constant DOM.Core.Node
+              := Muxml.Utils.Get_Element
+                (Doc   => Mem_Region,
+                 XPath => "*[self::file or self::fill]");
 
             Level : Natural := 1;
          begin
@@ -144,6 +237,14 @@ is
                            Last_Level   => Level,
                            Base_Address => Phys_Addr,
                            Size         => Size);
+            end if;
+
+            if Content_Node /= null then
+               Add_Content (Stream_Doc   => Stream_Doc,
+                            Content_Node => Content_Node,
+                            Region_Attr  => Region_Attr,
+                            Base_Address => Phys_Addr,
+                            Size         => Size);
             end if;
          end;
       end loop;

e529ac1bb27b278ff5e6779660d3f95f7dda1dd2
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-05-28 17:06:59 +0200

    Annotate SK.CPU.VMX package

--- kernel/src/sk-cpu-vmx.ads
+++ kernel/src/sk-cpu-vmx.ads
@@ -18,6 +18,10 @@
 
 with X86_64;
 
+--D @Interface
+--D This package implements subprograms corresponding to lowlevel Intel VT-x
+--D instructions. They are required for the management of VMX data structures
+--D such as VMXON and VMCS regions, see Intel SDM Vol. 3C, chapter 30.
 package SK.CPU.VMX
 is
 

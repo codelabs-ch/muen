93039481683ae825b0f437f71b42c69f86c1e0b4
Reto Buerki <reet@codelabs.ch>
2019-06-19 14:14:21 +0200

    Add doc to device validators

--- tools/libmucfgcheck/src/mucfgcheck-device.ads
+++ tools/libmucfgcheck/src/mucfgcheck-device.ads
@@ -20,80 +20,108 @@ with Muxml;
 
 package Mucfgcheck.Device
 is
+   --D @Section Id => validation-dev, Label => Devices, Parent => validation, Priority => 0
+   --D @Text Section => validation-dev, Priority => 0
+   --D The following checks are performed to guarantee that hardware devices
+   --D are correctly configured in the system policy.
+   --D @UL Id => validators_dev, Section => validation-dev, Priority => 0
 
-   --  Validate that devices referenced by logical devices exists.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that devices referenced by logical devices exists.
    procedure Physical_Device_References (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that device names (including device aliases/classes) are
-   --  unique.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that device names (including device aliases/classes) are
+   --D unique.
    procedure Physical_Device_Name_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all physical IRQs are unique.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that all physical IRQs are unique.
    procedure Physical_IRQ_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that physical device IRQs referenced by logical IRQs exists.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that physical device IRQs referenced by logical IRQs exists.
    procedure Physical_IRQ_References (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that ISA IRQs fulfill their constraints.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that ISA IRQs fulfill their constraints.
    procedure Physical_IRQ_Constraints_ISA (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that PCI LSI IRQs fulfill their constraints.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that PCI LSI IRQs fulfill their constraints.
    procedure Physical_IRQ_Constraints_PCI_LSI (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that PCI MSI IRQs fulfill their constraints.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that PCI MSI IRQs fulfill their constraints.
    procedure Physical_IRQ_Constraints_PCI_MSI (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that PCI MSI IRQs are consecutive.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that PCI MSI IRQs are consecutive.
    procedure Physical_IRQ_MSI_Consecutiveness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that physical IRQ names are unique per device.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that physical IRQ names are unique per device.
    procedure Device_IRQ_Name_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all I/O start ports are smaller than end ports.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that all I/O start ports are smaller than end ports.
    procedure IO_Port_Start_Smaller_End (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that physical I/O ports referenced by logical I/O ports exists.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that physical I/O ports referenced by logical I/O ports exists.
    procedure IO_Port_References (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all physical I/O ports are unique.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that all physical I/O ports are unique.
    procedure IO_Port_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that physical I/O port names are unique per device.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that physical I/O port names are unique per device.
    procedure Device_IO_Port_Name_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that device memory names are unique.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that device memory names are unique.
    procedure Device_Memory_Name_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that device memory referenced by logical device memory exists.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that device memory referenced by logical device memory exists.
    procedure Device_Memory_References (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that PCI device bus, device, function triplets are unique.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that PCI device bus, device, function triplets are unique.
    procedure PCI_Device_BDF_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that logical device references of each subject do not refer to
-   --  the same physical device.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that logical device references of each subject do not refer to
+   --D the same physical device.
    procedure Device_Reference_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that PCI device reference bus, device, function triplets are
-   --  unique per subject.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that PCI device reference bus, device, function triplets are
+   --D unique per subject.
    procedure Device_Reference_BDF_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all device references specifying a bus, device, function
-   --  triplet are references to physical PCI devices.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that all device references specifying a bus, device, function
+   --D triplet are references to physical PCI devices.
    procedure PCI_Device_References (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all device references to PCI multi-function devices belong
-   --  to the same subject and have the same logical device number.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that all device references to PCI multi-function devices belong
+   --D to the same subject and have the same logical device number.
    procedure PCI_Multifunction_Device_Refs (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all device references not specifying a bus, device,
-   --  function triplet are references to physical legacy (non-PCI) devices.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that all device references not specifying a bus, device,
+   --D function triplet are references to physical legacy (non-PCI) devices.
    procedure Legacy_Device_References (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all logical PCI devices specify bus number zero.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that all logical PCI devices specify bus number zero.
    procedure Device_References_PCI_Bus_Number (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all IOMMU memory-mapped IO regions have a size of 4K.
+   --D @Item List => validators_dev, Priority => 0
+   --D Validate that all IOMMU memory-mapped IO regions have a size of 4K.
    procedure IOMMU_Region_Size (XML_Data : Muxml.XML_Data_Type);
 
 end Mucfgcheck.Device;

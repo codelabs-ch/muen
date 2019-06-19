b6caaa077590e6e72d810567486cb807bb9ac2a0
Reto Buerki <reet@codelabs.ch>
2019-06-19 15:18:01 +0200

    Add doc to device domain validators

--- tools/libmucfgcheck/src/mucfgcheck-device_domains.ads
+++ tools/libmucfgcheck/src/mucfgcheck-device_domains.ads
@@ -20,30 +20,43 @@ with Muxml;
 
 package Mucfgcheck.Device_Domains
 is
+   --D @Section Id => validation-devdom, Label => Device Domains, Parent => validation, Priority => 0
+   --D @Text Section => validation-devdom, Priority => 0
+   --D The following checks are performed to guarantee that IOMMU device
+   --D domains are correctly configured in the system policy.
+   --D @UL Id => validators_devdom, Section => validation-devdom, Priority => 0
 
-   --  Validate that domain device references are unique.
+   --D @Item List => validators_devdom, Priority => 0
+   --D Validate that domain device references are unique.
    procedure Device_Reference_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that no virtual memory regions of a domain overlap.
+   --D @Item List => validators_devdom, Priority => 0
+   --D Validate that no virtual memory regions of a domain overlap.
    procedure Domain_Memory_Overlap (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that domain memory references are unique.
+   --D @Item List => validators_devdom, Priority => 0
+   --D Validate that domain memory references are unique.
    procedure Memory_Reference_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that domain memory referenced by subjects is mapped at the same
-   --  virtual address.
+   --D @Item List => validators_devdom, Priority => 0
+   --D Validate that domain memory referenced by subjects is mapped at the same
+   --D virtual address.
    procedure Memory_Mapping_Address_Equality (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate memory type of physical memory referenced by domains.
+   --D @Item List => validators_devdom, Priority => 0
+   --D Validate memory type of physical memory referenced by domains.
    procedure Domain_Memory_Type (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that each device referenced by a device domain is a PCI device.
+   --D @Item List => validators_devdom, Priority => 0
+   --D Validate that each device referenced by a device domain is a PCI device.
    procedure PCI_Device_References (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that each device domain has a physical PT memory region.
+   --D @Item List => validators_devdom, Priority => 0
+   --D Validate that each device domain has a physical PT memory region.
    procedure Domain_PT_Region_Presence (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that each PCI bus has a physical VT-d context memory region.
+   --D @Item List => validators_devdom, Priority => 0
+   --D Validate that each PCI bus has a physical VT-d context memory region.
    procedure PCI_Bus_Context_Region_Presence (XML_Data : Muxml.XML_Data_Type);
 
 end Mucfgcheck.Device_Domains;

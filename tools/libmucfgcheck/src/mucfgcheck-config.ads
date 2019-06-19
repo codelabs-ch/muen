152e287120fc37b3c6cb75ebf59362b53cc42ef8
Reto Buerki <reet@codelabs.ch>
2019-06-19 12:47:16 +0200

    Libmucfgcheck: Add doc to config validators

--- tools/libmucfgcheck/src/mucfgcheck-config.ads
+++ tools/libmucfgcheck/src/mucfgcheck-config.ads
@@ -20,26 +20,38 @@ with Muxml;
 
 package Mucfgcheck.Config
 is
+   --D @Section Id => validation-conf, Label => Configuration, Parent => validation, Priority => 0
+   --D @Text Section => validation-conf, Priority => 0
+   --D The following checks are performed to guarantee correctness of
+   --D configuration options in the system policy.
+   --D @UL Id => validators_conf, Section => validation-conf, Priority => 0
 
-   --  Validate config variable name uniqueness.
+   --D @Item List => validators_conf, Priority => 0
+   --D Validate config variable name uniqueness.
    procedure Name_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Check that all booleans defined in config contain a valid value.
+   --D @Item List => validators_conf, Priority => 0
+   --D Check that all booleans defined in config contain a valid value.
    procedure Config_Boolean_Values (XML_Data : Muxml.XML_Data_Type);
 
-   --  Check that all integers defined in config contain a valid value.
+   --D @Item List => validators_conf, Priority => 0
+   --D Check that all integers defined in config contain a valid value.
    procedure Config_Integer_Values (XML_Data : Muxml.XML_Data_Type);
 
-   --  Check that all expression config variable references are valid.
+   --D @Item List => validators_conf, Priority => 0
+   --D Check that all expression config variable references are valid.
    procedure Expression_Config_Var_Refs (XML_Data : Muxml.XML_Data_Type);
 
-   --  Check that all integers defined in expressions contain a valid value.
+   --D @Item List => validators_conf, Priority => 0
+   --D Check that all integers defined in expressions contain a valid value.
    procedure Expression_Integer_Values (XML_Data : Muxml.XML_Data_Type);
 
-   --  Check that all booleans defined in expressions contain a valid value.
+   --D @Item List => validators_conf, Priority => 0
+   --D Check that all booleans defined in expressions contain a valid value.
    procedure Expression_Boolean_Values (XML_Data : Muxml.XML_Data_Type);
 
-   --  Check that all conditional config variable references are valid.
+   --D @Item List => validators_conf, Priority => 0
+   --D Check that all conditional config variable references are valid.
    procedure Conditional_Config_Var_Refs (XML_Data : Muxml.XML_Data_Type);
 
 end Mucfgcheck.Config;

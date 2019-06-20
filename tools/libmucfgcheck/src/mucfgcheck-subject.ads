e86dbb23f76593c1148e981d755fe7f1d0b788dd
Reto Buerki <reet@codelabs.ch>
2019-06-20 11:54:25 +0200

    Add doc to subject validators

--- tools/libmucfgcheck/src/mucfgcheck-subject.ads
+++ tools/libmucfgcheck/src/mucfgcheck-subject.ads
@@ -21,94 +21,125 @@ with Muxml;
 package Mucfgcheck.Subject
 is
 
-   --  Validate subject name uniqueness.
+   --D @Section Id => validation-subj, Label => Subjects, Parent => validation, Priority => 0
+   --D @Text Section => validation-subj, Priority => 0
+   --D The following checks are performed to verify the correctness of the
+   --D subject configuration in the system policy.
+   --D @UL Id => validators_subj, Section => validation-subj, Priority => 0
+
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate subject name uniqueness.
    procedure Name_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate subject CPU ID.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate subject CPU ID.
    procedure CPU_ID (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate uniqueness of global subject IDs.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate uniqueness of global subject IDs.
    procedure Global_ID_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate pre-CPU uniqueness of local subject IDs.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate pre-CPU uniqueness of local subject IDs.
    procedure Local_ID_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate memory types of memory mappings (ie. allow access by subjects).
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate memory types of memory mappings (ie. allow access by subjects).
    procedure Memory_Types (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that no subject references an IOMMU device.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that no subject references an IOMMU device.
    procedure No_IOMMU_Device_References (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that all subjects are runnable, i.e. directly referenced in
-   --  the scheduling plan or target of a switch event of a subject that is
-   --  itself scheduled.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that all subjects are runnable, i.e. directly referenced in
+   --D the scheduling plan or target of a switch event of a subject that is
+   --D itself scheduled.
    procedure Runnability (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that subject scheduling group IDs match values as determined by
-   --  the scheduling plan and handover events.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that subject scheduling group IDs match values as determined by
+   --D the scheduling plan and handover events.
    procedure Scheduling_Group_IDs (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that logical names of subject devices are unique.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that logical names of subject devices are unique.
    procedure Logical_Device_Name_Uniqueness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that IRQ vector numbers of PCI device references with MSI
-   --  enabled are consecutive.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that IRQ vector numbers of PCI device references with MSI
+   --D enabled are consecutive.
    procedure Logical_IRQ_MSI_Consecutiveness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that logical names of subject unmask IRQ events conform to the
-   --  naming scheme ("unmask_irq_$IRQNR") and that the unmask number matches
-   --  the physical IRQ.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that logical names of subject unmask IRQ events conform to the
+   --D naming scheme ("unmask_irq_$IRQNR") and that the unmask number matches
+   --D the physical IRQ.
    procedure Logical_Unmask_Event (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that no virtual memory regions of a subject overlap.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that no virtual memory regions of a subject overlap.
    procedure Virtual_Memory_Overlap (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that multiple initramfs regions are consecutive.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that multiple initramfs regions are consecutive.
    procedure Initramfs_Consecutiveness (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that no subject has write access to the crash audit region.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that no subject has write access to the crash audit region.
    procedure Crash_Audit_Write_Access (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that subject device mmconf mappings are correct.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that subject device mmconf mappings are correct.
    procedure Device_Mmconf_Mappings (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that shared PCI devices specify the same PCI element.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that shared PCI devices specify the same PCI element.
    procedure Shared_Device_Same_PCI_Element (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the VMX controls conform to the checks specified in Intel
-   --  SDM Vol. 3C, "26.2.1 Checks on VMX Controls".
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the VMX controls conform to the checks specified in Intel
+   --D SDM Vol. 3C, "26.2.1 Checks on VMX Controls".
    procedure VMX_Controls_Entry_Checks (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the Pin-Based VM-Execution controls meet the requirements
-   --  for the execution of Muen.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the Pin-Based VM-Execution controls meet the requirements
+   --D for the execution of Muen.
    procedure VMX_Controls_Pin_Requirements (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the Processor-Based VM-Execution Controls meet the
-   --  requirements for the execution of Muen.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the Processor-Based VM-Execution Controls meet the
+   --D requirements for the execution of Muen.
    procedure VMX_Controls_Proc_Requirements (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the secondary Processor-Based VM-Execution Controls meet
-   --  the requirements for the execution of Muen.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the secondary Processor-Based VM-Execution Controls meet
+   --D the requirements for the execution of Muen.
    procedure VMX_Controls_Proc2_Requirements (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the VM-Exit Controls meet the requirements for the
-   --  execution of Muen.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the VM-Exit Controls meet the requirements for the
+   --D execution of Muen.
    procedure VM_Exit_Controls_Requirements (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the VM-Entry Controls meet the requirements for the
-   --  execution of Muen.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the VM-Entry Controls meet the requirements for the
+   --D execution of Muen.
    procedure VM_Entry_Controls_Requirements (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the VMX CR0 guest/host masks meet the requirements for the
-   --  execution of Muen.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the VMX CR0 guest/host masks meet the requirements for the
+   --D execution of Muen.
    procedure VMX_CR0_Mask_Requirements (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the VMX CR4 guest/host masks meet the requirements for the
-   --  execution of Muen.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the VMX CR4 guest/host masks meet the requirements for the
+   --D execution of Muen.
    procedure VMX_CR4_Mask_Requirements (XML_Data : Muxml.XML_Data_Type);
 
-   --  Validate that the VMX Exception bitmap meet the requirements for the
-   --  execution of Muen.
+   --D @Item List => validators_subj, Priority => 0
+   --D Validate that the VMX Exception bitmap meet the requirements for the
+   --D execution of Muen.
    procedure VMX_Exception_Bitmap_Requirements
      (XML_Data : Muxml.XML_Data_Type);
 

8291b3ddc2ca525eb899f249fc26b22111a4c4fc
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-06-28 17:28:57 +0200

    Simplify Layouts.Map_Page procedure
    Since Add_Entry does not raise an exception when adding a new entry that
    is identical to an existing one, the Map_Page procedure does not need to
    explicitly check for this condition. This is also a minor performance
    optimization.

--- tools/libpaging/src/paging-layouts.adb
+++ tools/libpaging/src/paging-layouts.adb
@@ -138,8 +138,6 @@ is
       Writable         :        Boolean;
       Executable       :        Boolean)
    is
-      use type Entries.Table_Entry_Type;
-
       Indexes : Table_Index_Array (1 .. Mem_Layout.Levels) := (others => 0);
 
       Table_Idx : Table_Range := 0;
@@ -168,10 +166,7 @@ is
       for I in Paging_Level range 2 .. Level loop
          Table_Idx := Table_Idx * 512 + Table_Range (Indexes (I - 1));
 
-         if not Maps.Contains (Map          => Mem_Layout.Structures (I),
-                               Table_Number => Table_Idx,
-                               Entry_Index  => Indexes (I))
-         then
+         begin
             Maps.Add_Entry
               (Map          => Mem_Layout.Structures (I),
                Table_Number => Table_Idx,
@@ -188,27 +183,13 @@ is
                   Maps_Page   => Level = I,
                   Global      => False,
                   Caching     => (if Level = I then Caching else WB)));
-         elsif Level = I and then Entries.Create
-           (Dst_Index   =>
-              (if I /= Level then
-                    Table_Idx * 512 + Table_Range (Indexes (I))
-               else 0),
-            Dst_Address => (if Level = I then Physical_Address else 0),
-            Readable    => True,
-            Writable    => Level /= I or Writable,
-            Executable  => Level /= I or Executable,
-            Maps_Page   => Level = I,
-            Global      => False,
-            Caching     => (if Level = I then Caching else WB))
-             /= Maps.Get_Entry
-               (Map          => Mem_Layout.Structures (I),
-                Table_Number => Table_Idx,
-                Entry_Index  => Indexes (I))
-         then
-            raise Mapping_Present with "Multiple mappings of VMA "
-              & Mutools.Utils.To_Hex (Number => Virtual_Address)
-              & " with different attributes present";
-         end if;
+
+         exception
+            when Tables.Duplicate_Entry =>
+               raise Mapping_Present with "Multiple mappings of VMA "
+                 & Mutools.Utils.To_Hex (Number => Virtual_Address)
+                 & " with different attributes present";
+         end;
       end loop;
    end Map_Page;
 

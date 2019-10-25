eb5dc8e4a522ca7ff00f4d4ea08ce226c56fd634
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-10-25 17:45:12 +0200

    Move kernel stack to 0x117000

--- tools/mucfgexpand/src/expanders-config.ads
+++ tools/mucfgexpand/src/expanders-config.ads
@@ -25,7 +25,7 @@ is
    Kernel_Data_Section_Size         : constant := 16#0000_2000#;
    Kernel_BSS_Section_Addr          : constant := 16#0011_2000#;
    Kernel_BSS_Section_Size          : constant := 16#0000_2000#;
-   Kernel_Stack_Addr                : constant := 16#0011_5000#;
+   Kernel_Stack_Addr                : constant := 16#0011_7000#;
    Kernel_Stack_Size                : constant := 16#0000_1000#;
    Kernel_Interrupt_Stack_Addr      : constant := 16#0011_9000#;
    Kernel_Interrupt_Stack_Size      : constant := 16#0000_1000#;

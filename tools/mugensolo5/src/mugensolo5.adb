bccce8c7eba9c29a56556a1f20ed1c7ba9e0578f
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-10-26 00:26:04 +0200

    Handle Solo5.Generator.Missing_Binary exception in Mugensolo5

--- tools/mugensolo5/src/mugensolo5.adb
+++ tools/mugensolo5/src/mugensolo5.adb
@@ -37,7 +37,9 @@ begin
 exception
    when Mutools.Cmd_Line.Invalid_Cmd_Line =>
       Ada.Command_Line.Set_Exit_Status (Code => Ada.Command_Line.Failure);
-   when E : Muxml.XML_Input_Error | Muxml.Validation_Error =>
+   when E : Solo5.Generator.Missing_Binary
+      | Muxml.XML_Input_Error
+      | Muxml.Validation_Error =>
       Mulog.Log (Level => Mulog.Error,
                  Msg   => "Processing failed, aborting");
       Mulog.Log (Level => Mulog.Error,

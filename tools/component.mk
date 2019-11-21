0dfe80705b3e06e45ce1b5d3b406bb888a2cee53
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-11-21 14:52:20 +0100

    Add EXTRA_BUILD_OPTS to tools/component.mk
    This allows to specify additional gprbuild arguments which must come
    last, e.g. -cargs or -largs.

--- tools/component.mk
+++ tools/component.mk
@@ -5,7 +5,7 @@ all: $(COMPONENT)
 prepare: $(COMPONENT_TARGETS)
 
 $(COMPONENT): prepare
-	@$(E) $(COMPONENT) Build "gprbuild $(BUILD_OPTS) -P$@"
+	@$(E) $(COMPONENT) Build "gprbuild $(BUILD_OPTS) -P$@ $(EXTRA_BUILD_OPTS)"
 
 install: $(COMPONENT)
 	@$(E) $(COMPONENT) Install \

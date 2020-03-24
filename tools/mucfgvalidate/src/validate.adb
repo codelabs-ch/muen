dfabc120acbefc64ca27f02b5e77e9b4ed466175
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2020-03-24 16:17:39 +0100

    Mucfgvalidate: Register Pin Exec Control Reqs check

--- tools/mucfgvalidate/src/validate.adb
+++ tools/mucfgvalidate/src/validate.adb
@@ -238,6 +238,8 @@ is
       XML_Processors.Register
         (Process => Subject.VMX_Controls_Entry_Checks'Access);
       XML_Processors.Register
+        (Process => Subject.VMX_Controls_Pin_Requirements'Access);
+      XML_Processors.Register
         (Process => Events.Physical_Event_Name_Uniqueness'Access);
       XML_Processors.Register
         (Process => Events.Source_Targets'Access);

7dcf9697e43e5b3a364e96f84960db6e261771b3
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2020-03-26 14:25:19 +0100

    Register VMX CR0 guest/host mask requirements check

--- tools/mucfgvalidate/tests/validate-test_data-tests.adb
+++ tools/mucfgvalidate/tests/validate-test_data-tests.adb
@@ -70,7 +70,7 @@ package body Validate.Test_Data.Tests is
                    File => "data/test_policy.xml");
 
       Register_All;
-      Assert (Condition => XML_Processors.Get_Count = 143,
+      Assert (Condition => XML_Processors.Get_Count = 144,
               Message   => "Count mismatch:"
               & XML_Processors.Get_Count'Img);
       XML_Processors.Clear;

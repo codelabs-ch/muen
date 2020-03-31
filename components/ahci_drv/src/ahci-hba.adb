3a09fa625b76a08143bd5053a7c50f2555f9fcfe
Ralf Hubert <ralf.hubert@secunet.com>
2020-03-31 14:14:05 +0200

    ahci_drv: add HBA Reset
    Since the bootloader already uses the HBA we do not know the state of the HBA.
    Add HBA reset to have a clean starting point.

--- components/ahci_drv/src/ahci-hba.adb
+++ components/ahci_drv/src/ahci-hba.adb
@@ -16,6 +16,9 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+with Interfaces;
+use type Interfaces.Unsigned_32;
+
 package body Ahci.HBA
 is
 
@@ -23,11 +26,41 @@ is
 
    procedure Enable
    is
+      Ctrl     : Global_HBA_Control_Type := Instance.Global_Host_Control;
+      Bohc     : BIOS_HO_Status_Type := Instance.BIOS_HO_Status_Ctrl;
+      Caps_Ext : constant HBA_Caps_Ext_Type := Instance.Host_Capabilities_Ext;
+   begin
+      Ctrl.AE := True;
+      --  Ctrl.IE := True;
+      Instance.Global_Host_Control := Ctrl;
+
+      if Caps_Ext.BOH then
+         --  request OS Ownership
+         Bohc.OOS := True;
+         Instance.BIOS_HO_Status_Ctrl := Bohc;
+         Wait_Owner : loop
+            Bohc := Instance.BIOS_HO_Status_Ctrl;
+            exit Wait_Owner when Bohc.OOS and not Bohc.BOS;
+         end loop Wait_Owner;
+      end if;
+   end Enable;
+
+   -------------------------------------------------------------------------
+
+   procedure Reset
+   is
       Ctrl : Global_HBA_Control_Type := Instance.Global_Host_Control;
    begin
       Ctrl.AE := True;
-      Ctrl.IE := True;
       Instance.Global_Host_Control := Ctrl;
-   end Enable;
+
+      Ctrl.HR := True;
+      Instance.Global_Host_Control := Ctrl;
+
+      Wait_Reset : loop
+         Ctrl := Instance.Global_Host_Control;
+         exit Wait_Reset when Ctrl.HR = False;
+      end loop Wait_Reset;
+   end Reset;
 
 end Ahci.HBA;

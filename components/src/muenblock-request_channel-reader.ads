0dd3d64c49af93f9ab89955d216803db9de47bd0
Ralf Hubert <ralf.hubert@secunet.com>
2020-04-14 06:47:35 +0200

    add muenblock interface
    The muenblock interface in meant to be used to communicate with the
    linux muenblock modules.

---
+++ components/src/muenblock-request_channel-reader.ads
@@ -0,0 +1,4 @@
+with Muchannel.Readers;
+
+package Muenblock.Request_Channel.Reader is
+   new Muenblock.Request_Channel.Readers;
---
+++ components/src/muenblock-response_channel-reader.ads
@@ -0,0 +1,4 @@
+with Muchannel.Readers;
+
+package Muenblock.Response_Channel.Reader is
+   new Muenblock.Response_Channel.Readers;

d436c6312fe343c984e5e8e418ee2146e588b9d7
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2020-04-21 15:24:23 +0200

    Proof for hypercalls with CPL = 0 only
    Add precondition to Handle_Hypercall to prove that it is only called
    when the subject current privilege level is zero.

--- kernel/src/sk-scheduler.adb
+++ kernel/src/sk-scheduler.adb
@@ -541,7 +541,8 @@ is
         (Input  => (Current_Minor_Frame_ID, Global_Current_Major_Frame_ID,
                     Scheduling_Plan, CPU_Info.APIC_ID),
          In_Out => (Scheduling_Groups, Crash_Audit.State, IO_Apic.State,
-                    Subjects.State, Subjects_Events.State, X86_64.State))
+                    Subjects.State, Subjects_Events.State, X86_64.State)),
+      Pre    => Subjects.Is_CPL_0 (ID => Current_Subject)
    is
       --  XXX: Only current wavefronts report that use type clause has no
       --       effect. To keep compatibility with earlier toolchains disable

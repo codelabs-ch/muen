2e05803ee41fc8bf95e2d1063502f1a8e94b68bb
Ralf Hubert <ralf.hubert@secunet.com>
2020-06-05 10:03:48 +0200

    ahci-ports: add timeout parameter to execute
    The SYNC command might take more than 30 sec to complete, so we need to
    pass the timeout depending on the command.

--- components/ahci_drv/src/ata.adb
+++ components/ahci_drv/src/ata.adb
@@ -209,8 +209,9 @@ is
                      Sectors => Length / 512,
                      Features => 1);
 
-      Ahci.Ports.Execute (ID, Success);
-
+      Ahci.Ports.Execute (ID       => ID,
+                          Timeout  => 30,
+                          Success  => Success);
       pragma Debug (not Success, Debug_Ops.Dump_Cmd_List (ID, 8));
       pragma Debug (not Success, Debug_Ops.Dump_Cmd_Table (ID, 40));
 
@@ -287,8 +288,10 @@ is
                      Start => Start,
                      Sectors => Sectors,
                      Features => 0);
-      Ahci.Ports.Execute (ID, Success);
 
+      Ahci.Ports.Execute (ID       => ID,
+                          Timeout  => 30,
+                          Success  => Success);
       pragma Debug (not Success, Debug_Ops.Dump_Cmd_List (ID, 8));
       pragma Debug (not Success, Debug_Ops.Dump_Cmd_Table (ID, 40));
 
@@ -463,7 +466,9 @@ is
                      Sectors => 0,
                      Features => 0);
 
-      Ahci.Ports.Execute (Port_ID, Success);
+      Ahci.Ports.Execute (ID       => Port_ID,
+                          Timeout  => 30,
+                          Success  => Success);
 
       if Success then
          FW := Ata_Identify_Response.FW;

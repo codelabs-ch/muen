f9732a376b6565b240a2aa2a23d92f24ab384af3
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2020-06-24 16:00:12 +0200

    Add Libmucontrol library
    The library can be used by clients of the component control interfaces
    to acquire commands and communicate the runtime status.

---
+++ components/libmucontrol/src/mucontrol-command-instance.adb
@@ -0,0 +1,73 @@
+--
+--  Copyright (C) 2020  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2020  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--  All rights reserved.
+--
+--  Redistribution and use in source and binary forms, with or without
+--  modification, are permitted provided that the following conditions are met:
+--
+--    * Redistributions of source code must retain the above copyright notice,
+--      this list of conditions and the following disclaimer.
+--
+--    * Redistributions in binary form must reproduce the above copyright
+--      notice, this list of conditions and the following disclaimer in the
+--      documentation and/or other materials provided with the distribution.
+--
+--  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+--  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+--  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+--  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+--  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+--  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+--  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+--  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+--  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+--  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+--  POSSIBILITY OF SUCH DAMAGE.
+--
+
+with System;
+
+with Libmucontrol_Component.Memory;
+
+package body Mucontrol.Command.Instance
+is
+
+   package Cspec renames Libmucontrol_Component.Memory;
+
+   Command_Page : Command_Page_Type
+   with
+      Async_Writers,
+      Size    => Cspec.Control_Size * 8,
+      Address => System'To_Address (Cspec.Control_Address);
+
+   -------------------------------------------------------------------------
+
+   function Get return Command_Type
+   is
+   begin
+      return Command_Page.Command;
+   end Get;
+
+   -------------------------------------------------------------------------
+
+   function Is_Self_Governed return Boolean
+   is
+      Current_Cmd : constant Command_Type := Command_Page.Command;
+   begin
+      return Current_Cmd = CMD_SELF_CTRL;
+   end Is_Self_Governed;
+
+   -------------------------------------------------------------------------
+
+   procedure Wait_For_Next (Cmd : in out Command_Type)
+   is
+      Cur_Cmd : constant Command_Type := Cmd;
+   begin
+      loop
+         Cmd := Command_Page.Command;
+         exit when Cmd /= Cur_Cmd;
+      end loop;
+   end Wait_For_Next;
+
+end Mucontrol.Command.Instance;
---
+++ components/libmucontrol/src/mucontrol-status-instance.adb
@@ -0,0 +1,90 @@
+--
+--  Copyright (C) 2020  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2020  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--  All rights reserved.
+--
+--  Redistribution and use in source and binary forms, with or without
+--  modification, are permitted provided that the following conditions are met:
+--
+--    * Redistributions of source code must retain the above copyright notice,
+--      this list of conditions and the following disclaimer.
+--
+--    * Redistributions in binary form must reproduce the above copyright
+--      notice, this list of conditions and the following disclaimer in the
+--      documentation and/or other materials provided with the distribution.
+--
+--  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+--  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+--  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+--  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+--  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+--  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+--  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+--  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+--  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+--  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+--  POSSIBILITY OF SUCH DAMAGE.
+--
+
+with System;
+
+with Libmucontrol_Component.Memory;
+
+package body Mucontrol.Status.Instance
+is
+
+   package Cspec renames Libmucontrol_Component.Memory;
+
+   Status_Page : Status_Page_Type
+   with
+      Async_Readers,
+      Size    => Cspec.Status_Size * 8,
+      Address => System'To_Address (Cspec.Status_Address);
+
+   -------------------------------------------------------------------------
+
+   procedure Error (Diagnostic : Interfaces.Unsigned_64)
+   is
+   begin
+      Status_Page.Diagnostics := Diagnostic;
+      Status_Page.Status := STATE_ERROR;
+   end Error;
+
+   -------------------------------------------------------------------------
+
+   function Get return Status_Type
+   is (Status_Page.Status);
+
+   -------------------------------------------------------------------------
+
+   function Get_Watchdog return Interfaces.Unsigned_64
+   is (Status_Page.Watchdog);
+
+   -------------------------------------------------------------------------
+
+   procedure Initialize
+   is
+   begin
+      Status_Page := (Status      => STATE_INITIAL,
+                      Watchdog    => 0,
+                      Diagnostics => 0,
+                      Reserved    => (others => 0));
+   end Initialize;
+
+   -------------------------------------------------------------------------
+
+   procedure Set (New_Status : Status_Type)
+   is
+   begin
+      Status_Page.Status := New_Status;
+   end Set;
+
+   -------------------------------------------------------------------------
+
+   procedure Set_Watchdog (Value : Interfaces.Unsigned_64)
+   is
+   begin
+      Status_Page.Watchdog := Value;
+   end Set_Watchdog;
+
+end Mucontrol.Status.Instance;

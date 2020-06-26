1a16b084e8aecf9237e4cab8cb89e4c5e6191820
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2020-06-26 18:55:40 +0200

    SL: Drop current Sinfo offset related subprograms
    Since the offset is now static, there is no need to keep track of the
    offset at runtime.

--- components/sl/src/loader-globals.ads
+++ components/sl/src/loader-globals.ads
@@ -18,31 +18,14 @@
 
 with System;
 
-with Interfaces;
-
 with Musinfo;
 
 package Loader.Globals
 is
-   use type Interfaces.Unsigned_64;
 
    Target_Sinfo : constant Musinfo.Subject_Info_Type
    with
       Import,
       Address => System'To_Address (Target_Sinfo_Address);
-
-   --  Set sinfo offset to specified value.
-   procedure Set_Current_Sinfo_Offset (O : Interfaces.Unsigned_64)
-   with
-      Post => Get_Current_Sinfo_Offset = O;
-
-   --  Get current sinfo offset value.
-   function Get_Current_Sinfo_Offset return Interfaces.Unsigned_64;
-
-private
-
-   Offset : Interfaces.Unsigned_64 := 0;
-
-   function Get_Current_Sinfo_Offset return Interfaces.Unsigned_64 is (Offset);
 
 end Loader.Globals;

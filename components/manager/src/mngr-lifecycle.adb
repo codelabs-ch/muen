1a2b59216fe95cf737b44730c8ef4601233c4ea2
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2020-07-08 19:27:38 +0200

    Add Mngr.Lifecycle package
    The package provides subprograms to keep track of the lifecycle state of
    managed subjects.

---
+++ components/manager/src/mngr-lifecycle.adb
@@ -0,0 +1,275 @@
+--
+--  Copyright (C) 2020  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2020  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with SK.Strings;
+
+with Debuglog.Client;
+
+with Mucontrol.Command;
+with Mucontrol.Status;
+
+with Mngr.Commands;
+with Mngr.Config;
+with Mngr.Status;
+
+package body Mngr.Lifecycle
+is
+
+   use type  Mucontrol.Status.Status_Type;
+
+   --  Returns True if the given subject status designates an error.
+   function Has_Error
+     (Subject_Status : Mucontrol.Status.Status_Type)
+      return Boolean
+   is ((Subject_Status and Mucontrol.Status.STATE_ERROR) > 0);
+
+   --  Returns True if all runtime dependencies for the subject specified by ID
+   --  are met.
+   function Run_Dependencies_Satisfied
+     (ID : Managed_Subjects_Range)
+      return Boolean;
+
+   --  Log the given state as string.
+   procedure Put (State : Run_State_Type);
+
+   -------------------------------------------------------------------------
+
+   procedure Put (State : Run_State_Type)
+   is
+   begin
+      case State is
+         when FSM_Start        => Debuglog.Client.Put (Item => "Start");
+         when FSM_Initial      => Debuglog.Client.Put (Item => "Initial");
+         when FSM_Syncing      => Debuglog.Client.Put (Item => "Syncing");
+         when FSM_Erasing      => Debuglog.Client.Put (Item => "Erasing");
+         when FSM_Preparing    => Debuglog.Client.Put (Item => "Preparing");
+         when FSM_Validating   => Debuglog.Client.Put (Item => "Validating");
+         when FSM_Running      => Debuglog.Client.Put (Item => "Running");
+         when FSM_Finished     => Debuglog.Client.Put (Item => "Finished");
+         when FSM_Error        => Debuglog.Client.Put (Item => "Error");
+         when FSM_Self_Control => Debuglog.Client.Put (Item => "Self-Control");
+      end case;
+   end Put;
+
+   -------------------------------------------------------------------------
+
+   procedure Dump_Command (ID : Managed_Subjects_Range)
+   is
+      Cur_Cmd         : constant Interfaces.Unsigned_64
+        := Interfaces.Unsigned_64 (Commands.Get_Command (ID => ID));
+      Cur_Epoch       : constant Interfaces.Unsigned_64
+        := Commands.Get_Epoch (ID => ID);
+      Cur_WD_Interval : constant Interfaces.Unsigned_64
+        := Commands.Get_Watchdog_Interval (ID => ID);
+   begin
+      Debuglog.Client.Put (Item => SK.Strings.Img (SK.Byte (ID)));
+      Debuglog.Client.Put (Item => ": Command ");
+      Debuglog.Client.Put (Item => SK.Strings.Img (Cur_Cmd));
+      Debuglog.Client.Put (Item => ", Epoch ");
+      Debuglog.Client.Put (Item => SK.Strings.Img (Cur_Epoch));
+      Debuglog.Client.Put (Item => ", WD Interval ");
+      Debuglog.Client.Put_Line (Item => SK.Strings.Img (Cur_WD_Interval));
+   end Dump_Command;
+
+   -------------------------------------------------------------------------
+
+   procedure Dump_Status (ID : Managed_Subjects_Range)
+   is
+      Cur_Status      : constant Interfaces.Unsigned_64
+        := Interfaces.Unsigned_64 (Status.Get_Status (ID => ID));
+      Cur_Diagnostics : constant Interfaces.Unsigned_64
+        := Interfaces.Unsigned_64 (Status.Get_Diagnostics (ID => ID));
+      Cur_WD_Value    : constant Interfaces.Unsigned_64
+        := Status.Get_Watchdog (ID => ID);
+   begin
+      Debuglog.Client.Put (Item => SK.Strings.Img (SK.Byte (ID)));
+      Debuglog.Client.Put (Item => ": Status  ");
+      Debuglog.Client.Put (Item => SK.Strings.Img (Cur_Status));
+      Debuglog.Client.Put (Item => ", Diagnostics ");
+      Debuglog.Client.Put (Item => SK.Strings.Img (Cur_Diagnostics));
+      Debuglog.Client.Put (Item => ", WD ");
+      Debuglog.Client.Put_Line (Item => SK.Strings.Img (Cur_WD_Value));
+   end Dump_Status;
+
+   -------------------------------------------------------------------------
+
+   procedure Log_Transition
+     (ID        : Managed_Subjects_Range;
+      Old_State : Run_State_Type;
+      New_State : Run_State_Type)
+   is
+   begin
+      Debuglog.Client.Put (Item => SK.Strings.Img (SK.Byte (ID)));
+      Debuglog.Client.Put (Item => ": ");
+      Put (State => Old_State);
+      Debuglog.Client.Put (Item => "->");
+      Put (State => New_State);
+      Debuglog.Client.New_Line;
+   end Log_Transition;
+
+   -------------------------------------------------------------------------
+
+   function Run_Dependencies_Satisfied
+     (ID : Managed_Subjects_Range)
+      return Boolean
+   is
+      use type Config.Subject_Dependency_Type;
+
+      Result : Boolean;
+   begin
+      Result := True;
+      for Dep of Config.Instance (ID).Run_Deps loop
+         if Dep /= Config.No_Dep
+           and then States (Dep.Dep_ID).Current_State /= Dep.Dep_State
+         then
+            Result := False;
+         end if;
+      end loop;
+
+      return Result;
+   end Run_Dependencies_Satisfied;
+
+   -------------------------------------------------------------------------
+
+   function Get_Next_State
+     (ID             : Managed_Subjects_Range;
+      Subject_Status : Mucontrol.Status.Status_Type)
+     return Run_State_Type
+   is
+      Next_State : Run_State_Type := States (ID).Current_State;
+   begin
+      case States (ID).Current_State is
+         when FSM_Start    =>
+            Next_State := FSM_Initial;
+         when FSM_Initial  =>
+            if Config.Instance (ID).Self_Governed then
+               if Run_Dependencies_Satisfied (ID => ID) then
+                  Next_State := FSM_Self_Control;
+               end if;
+            else
+               Next_State := FSM_Syncing;
+            end if;
+         when FSM_Syncing =>
+            if Subject_Status = Mucontrol.Status.STATE_SYNCED then
+               if Config.Instance (ID).Initial_Erase then
+                  Next_State := FSM_Erasing;
+               else
+                  Next_State := FSM_Preparing;
+               end if;
+            end if;
+         when FSM_Erasing =>
+            if Subject_Status = Mucontrol.Status.STATE_ERASED then
+               Next_State := FSM_Preparing;
+            elsif Has_Error (Subject_Status => Subject_Status) then
+               Next_State := FSM_Error;
+            end if;
+         when FSM_Preparing =>
+            if Subject_Status = Mucontrol.Status.STATE_PREPARED then
+               Next_State := FSM_Validating;
+            elsif Has_Error (Subject_Status => Subject_Status) then
+               Next_State := FSM_Error;
+            end if;
+         when FSM_Validating =>
+            if Subject_Status = Mucontrol.Status.STATE_VALIDATED
+              and then Run_Dependencies_Satisfied (ID => ID)
+            then
+               Next_State := FSM_Running;
+            elsif Has_Error (Subject_Status => Subject_Status) then
+               Next_State := FSM_Error;
+            end if;
+         when FSM_Running
+            | FSM_Self_Control =>
+            if Subject_Status = Mucontrol.Status.STATE_FINISHED then
+               Next_State := FSM_Finished;
+            elsif Has_Error (Subject_Status => Subject_Status) then
+               Next_State := FSM_Error;
+            end if;
+         when FSM_Finished
+            | FSM_Error =>
+            null;
+      end case;
+
+      return Next_State;
+   end Get_Next_State;
+
+   -------------------------------------------------------------------------
+
+   procedure Perform_Transition
+     (ID        : Managed_Subjects_Range;
+      New_State : Run_State_Type)
+   is
+      use type Interfaces.Unsigned_64;
+   begin
+      case New_State is
+         when FSM_Initial =>
+            States (ID).Current_Epoch := States (ID).Current_Epoch + 1;
+            Commands.Initialize
+              (ID          => ID,
+               Command     => Mucontrol.Command.CMD_NOP,
+               Epoch       => States (ID).Current_Epoch,
+               WD_Interval => Config.Instance (ID).WD_Interval);
+         when FSM_Syncing =>
+            Commands.Set_Command (ID    => ID,
+                                  Value => Mucontrol.Command.CMD_SYNC);
+         when FSM_Erasing =>
+            Commands.Set_Command (ID    => ID,
+                                  Value => Mucontrol.Command.CMD_ERASE);
+         when FSM_Preparing =>
+            Commands.Set_Command (ID    => ID,
+                                  Value => Mucontrol.Command.CMD_PREPARE);
+         when FSM_Validating =>
+            Commands.Set_Command (ID    => ID,
+                                  Value => Mucontrol.Command.CMD_VALIDATE);
+         when FSM_Running =>
+            Commands.Set_Command (ID    => ID,
+                                  Value => Mucontrol.Command.CMD_RUN);
+         when FSM_Self_Control =>
+            Commands.Set_Command (ID    => ID,
+                                  Value => Mucontrol.Command.CMD_SELF_CTRL);
+         when FSM_Error =>
+            Dump_Status (ID => ID);
+            Dump_Command (ID => ID);
+         when FSM_Start
+            | FSM_Finished =>
+            null;
+      end case;
+   end Perform_Transition;
+
+   -------------------------------------------------------------------------
+
+   procedure Process (ID : Managed_Subjects_Range)
+   is
+      Cur_Status : Mucontrol.Status.Status_Type;
+      New_State  : Run_State_Type;
+   begin
+      Cur_Status := Status.Get_Status (ID => ID);
+      New_State  := Get_Next_State
+        (ID             => ID,
+         Subject_Status => Cur_Status);
+
+      if New_State /= States (ID).Current_State then
+         Perform_Transition (ID        => ID,
+                             New_State => New_State);
+         Log_Transition (ID        => ID,
+                         Old_State => States (ID).Current_State,
+                         New_State => New_State);
+         States (ID).Current_State := New_State;
+      end if;
+   end Process;
+
+end Mngr.Lifecycle;

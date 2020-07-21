31adfeecacaeb2a1b99557bb7d26451f022de41f
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2020-07-21 11:50:17 +0200

    Add Mngr.Slot_Control package
    The provided function returns the current era for a given managed
    subject ID. Currently only the era for ID 1 is returned. For all others
    the current era is reported as zero.

---
+++ components/manager/src/mngr-slot_control.adb
@@ -0,0 +1,38 @@
+--
+--  Copyright (C) 2020  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2020  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Mngr.Config;
+
+package body Mngr.Slot_Control
+is
+
+   -------------------------------------------------------------------------
+
+   function Get_Current_Era
+     (ID : Managed_Subjects_Range)
+      return Interfaces.Unsigned_64
+   is
+      Cur_Era : Interfaces.Unsigned_64 := 0;
+   begin
+      if Config.Instance (ID).Slot_ID = 1 then
+         Cur_Era := Slot_Control_1.Era;
+      end if;
+      return Cur_Era;
+   end Get_Current_Era;
+
+end Mngr.Slot_Control;
---
+++ components/manager/src/mngr-slot_control.ads
@@ -0,0 +1,57 @@
+--
+--  Copyright (C) 2020  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2020  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Interfaces;
+
+private with System;
+
+private with Manager_Component.Memory;
+
+package Mngr.Slot_Control
+is
+
+   --  Returns the current era for the managed subject specified by ID.
+   function Get_Current_Era
+     (ID : Managed_Subjects_Range)
+      return Interfaces.Unsigned_64
+   with Volatile_Function;
+
+private
+
+   package Cspecs renames Manager_Component.Memory;
+
+   Padding_Start : constant := 9;
+
+   type Padding_Type is array (Padding_Start .. Cspecs.Slot_Control_1_Size)
+     of Interfaces.Unsigned_8
+   with Size => (Cspecs.Slot_Control_1_Size - Padding_Start + 1) * 8;
+
+   type Slot_Control_Type is record
+      Era     : Interfaces.Unsigned_64 with Atomic;
+      Padding : Padding_Type;
+   end record
+   with Object_Size => Cspecs.Slot_Control_1_Size * 8;
+
+   Slot_Control_1 : Slot_Control_Type
+   with
+      Volatile,
+      Async_Writers,
+      Address => System'To_Address (Cspecs.Slot_Control_1_Address),
+      Size    => Cspecs.Slot_Control_1_Size * 8;
+
+end Mngr.Slot_Control;

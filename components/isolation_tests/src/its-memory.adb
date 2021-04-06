f33abab0084991d6729fb8c8bf15f56520e89c54
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2021-04-07 00:47:23 +0200

    Convert memory tests to use ITS.Results package

--- components/isolation_tests/src/its-memory.adb
+++ components/isolation_tests/src/its-memory.adb
@@ -22,6 +22,7 @@ with SK.Constants;
 with SK.Strings;
 
 with ITS.Log_Buffer;
+with ITS.Results;
 with ITS.Subject_State;
 with ITS.Utils;
 
@@ -54,51 +55,42 @@ is
         & " where the write access was attempted.";
       Log_ID          : Log_Buffer.Ext_Log_Entries_Range;
       Result          : SK.Subject_State_Type;
+      Success         : Boolean;
    begin
       ITS.Subject_State.Result_State := SK.Null_Subject_State;
 
       Log_Buffer.Start_Entry (ID => Log_ID);
-      Log_Buffer.New_Line;
-      Log_Buffer.Put_Line (Str => Title);
-      for I in Natural range 1 .. Title'Length loop
-         Log_Buffer.Put (Str => "-");
-      end loop;
-      Log_Buffer.New_Line;
-
-      Log_Buffer.Put_Line (Str => "== Description");
-      Log_Buffer.Put_Line (Str => Description);
-      Log_Buffer.New_Line;
-      Log_Buffer.Put_Line (Str => "== Expected");
-      Log_Buffer.Put_Line (Str => Expected_Result);
-      Log_Buffer.New_Line;
-      Log_Buffer.Put_Line (Str => "== Test Output");
-      Log_Buffer.Put_Line (Str => "Write access to "
+      Log_Buffer.Put_Line (Str => "Writing 0x0 to memory address "
                            & SK.Strings.Img (Item => Memory_Address) & ".");
-      Log_Buffer.Put (Str => "Result: ");
+      Log_Buffer.New_Line;
       ITS.Utils.Write_Byte (Address => Memory_Address,
                             Value   => 0);
       Result := ITS.Subject_State.Result_State;
 
-      if Result.Exit_Reason = SK.Constants.EXIT_REASON_EXCEPTION_NMI
-        and then Result.Exit_Qualification = Memory_Address
-      then
-         Log_Buffer.Put_Line (Str => "PASSED");
-      else
-         Log_Buffer.Put_Line (Str => "FAILED");
-         if Result.Exit_Reason /= SK.Constants.EXIT_REASON_EPT_VIOLATION
-         then
-            Log_Buffer.Put_Line
-              (Str => "Exit Reason mismatch: "
-               & SK.Strings.Img (Item => SK.Byte'Mod (Result.Exit_Reason)));
-         end if;
-         if Result.Exit_Qualification /= Memory_Address
-         then
-            Log_Buffer.Put_Line
-              (Str => "Exit Qualification mismatch: "
-               & SK.Strings.Img (Item => Result.Exit_Qualification));
-         end if;
-      end if;
+      Log_Buffer.Put_Line (Str => ".:[ Assertions ]:.");
       Log_Buffer.New_Line;
+      Log_Buffer.Put_Line (Str => "> Exit Reason");
+      Log_Buffer.Put_Line
+        (Str => "  Expected : "
+         & SK.Strings.Img
+           (Item => SK.Word32 (SK.Constants.EXIT_REASON_EXCEPTION_NMI)));
+      Log_Buffer.Put_Line
+        (Str => "  Result   : " & SK.Strings.Img (Item => Result.Exit_Reason));
+      Log_Buffer.New_Line;
+      Log_Buffer.Put_Line (Str => "> Exit Qualification");
+      Log_Buffer.Put_Line (Str => "  Expected : "
+                           & SK.Strings.Img (Item => Memory_Address));
+      Log_Buffer.Put_Line
+        (Str => "  Result   : "
+         & SK.Strings.Img (Item => Result.Exit_Qualification));
+      Success := Result.Exit_Reason = SK.Constants.EXIT_REASON_EXCEPTION_NMI
+        and then Result.Exit_Qualification = Memory_Address;
+
+      Results.Append (Title       => Title,
+                      Description => Description,
+                      Expected    => Expected_Result,
+                      Success     => Success,
+                      Log_Entry   => Log_ID);
    end Write_To_Read_Only_Region;
 
    -------------------------------------------------------------------------
@@ -122,51 +114,42 @@ is
         & " where the write access was attempted.";
       Log_ID          : Log_Buffer.Ext_Log_Entries_Range;
       Result          : SK.Subject_State_Type;
+      Success         : Boolean;
    begin
       ITS.Subject_State.Result_State := SK.Null_Subject_State;
 
       Log_Buffer.Start_Entry (ID => Log_ID);
-      Log_Buffer.New_Line;
-      Log_Buffer.Put_Line (Str => Title);
-      for I in Natural range 1 .. Title'Length loop
-         Log_Buffer.Put (Str => "-");
-      end loop;
-      Log_Buffer.New_Line;
-
-      Log_Buffer.Put_Line (Str => "== Description");
-      Log_Buffer.Put_Line (Str => Description);
-      Log_Buffer.New_Line;
-      Log_Buffer.Put_Line (Str => "== Expected");
-      Log_Buffer.Put_Line (Str => Expected_Result);
-      Log_Buffer.New_Line;
-      Log_Buffer.Put_Line (Str => "== Test Output");
-      Log_Buffer.Put_Line (Str => "Write access to "
+      Log_Buffer.Put_Line (Str => "Writing 0xfa to memory address "
                            & SK.Strings.Img (Item => Memory_Address) & ".");
-      Log_Buffer.Put (Str => "Result: ");
       ITS.Utils.Write_Byte (Address => Memory_Address,
                             Value   => 16#fa#);
+      Log_Buffer.New_Line;
       Result := ITS.Subject_State.Result_State;
 
-      if Result.Exit_Reason = SK.Constants.EXIT_REASON_EXCEPTION_NMI
-        and then Result.Exit_Qualification = Memory_Address
-      then
-         Log_Buffer.Put_Line (Str => "PASSED");
-      else
-         Log_Buffer.Put_Line (Str => "FAILED");
-         if Result.Exit_Reason /= SK.Constants.EXIT_REASON_EPT_VIOLATION
-         then
-            Log_Buffer.Put_Line
-              (Str => "Exit Reason mismatch: "
-               & SK.Strings.Img (Item => SK.Byte'Mod (Result.Exit_Reason)));
-         end if;
-         if Result.Exit_Qualification /= Memory_Address
-         then
-            Log_Buffer.Put_Line
-              (Str => "Exit Qualification mismatch: "
-               & SK.Strings.Img (Item => Result.Exit_Qualification));
-         end if;
-      end if;
+      Log_Buffer.Put_Line (Str => ".:[ Assertions ]:.");
       Log_Buffer.New_Line;
+      Log_Buffer.Put_Line (Str => "> Exit Reason");
+      Log_Buffer.Put_Line
+        (Str => "  Expected : "
+         & SK.Strings.Img
+           (Item => SK.Word32 (SK.Constants.EXIT_REASON_EXCEPTION_NMI)));
+      Log_Buffer.Put_Line (Str => "  Result   : "
+                           & SK.Strings.Img (Item => Result.Exit_Reason));
+      Log_Buffer.New_Line;
+      Log_Buffer.Put_Line (Str => "> Exit Qualification");
+      Log_Buffer.Put_Line
+        (Str => "  Expected : " & SK.Strings.Img (Item => Memory_Address));
+      Log_Buffer.Put_Line
+        (Str => "  Result   : "
+         & SK.Strings.Img (Item => Result.Exit_Qualification));
+      Success := Result.Exit_Reason = SK.Constants.EXIT_REASON_EXCEPTION_NMI
+        and then Result.Exit_Qualification = Memory_Address;
+
+      Results.Append (Title       => Title,
+                      Description => Description,
+                      Expected    => Expected_Result,
+                      Success     => Success,
+                      Log_Entry   => Log_ID);
    end Write_To_Unmapped_Region;
 
 end ITS.Memory;

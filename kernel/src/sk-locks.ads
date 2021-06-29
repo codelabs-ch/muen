0ce16630fe8680f11638cc4a44f6bcc6fe686c86
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2021-06-29 17:40:06 +0200

    Add annotations to SK.Locks package

--- kernel/src/sk-locks.ads
+++ kernel/src/sk-locks.ads
@@ -16,6 +16,9 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+--D @Interface
+--D This package implements a spinlock which can be used to synchronize access
+--D to a shared resource like the kernel console for debug builds.
 package SK.Locks
 is
 
@@ -48,10 +51,13 @@ private
       Locked => 1);
    for Lock_State_Type'Size use 32;
 
-   --  Spin lock is implemented as limited record with a field holding the lock
-   --  value since such data types are guaranteed to be passed by reference,
-   --  see Ada RM 6.2, 7/3.
+   --D @Interface
+   --D Spin lock is implemented as limited record with a field holding the lock
+   --D value since such data types are guaranteed to be passed by reference,
+   --D see Ada RM 6.2, 7/3.
    type Spin_Lock_Type is limited record
+      --D @Interface
+      --D Current state of the spin lock.
       State : Lock_State_Type;
    end record;
 

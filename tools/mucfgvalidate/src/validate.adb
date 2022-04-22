a662758d220f0bf40c8a5d3c270332e6edb8ca40
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2022-04-22 12:18:03 +0200

    Mucfgvalidate: Register channel mappings validator

--- tools/mucfgvalidate/src/validate.adb
+++ tools/mucfgvalidate/src/validate.adb
@@ -151,6 +151,8 @@ is
       XML_Processors.Register
         (Process => Memory.Kernel_Sched_Group_Info_Mappings'Access);
       XML_Processors.Register
+        (Process => Memory.Subject_Channel_Mappings'Access);
+      XML_Processors.Register
         (Process => MSR.Start_Smaller_End'Access);
       XML_Processors.Register
         (Process => MSR.Check_Whitelist'Access);

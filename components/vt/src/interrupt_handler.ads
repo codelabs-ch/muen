6726b3f0bf432521cc6f46cad391ab8f3ba908ff
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2022-09-08 15:55:43 +0200

    VT: Adapt interrupt handler routine
    The subprogram now takes a full ISR context instead of just the vector
    number.

--- components/vt/src/interrupt_handler.ads
+++ components/vt/src/interrupt_handler.ads
@@ -1,6 +1,6 @@
 --
---  Copyright (C) 2013  Reto Buerki <reet@codelabs.ch>
---  Copyright (C) 2013  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--  Copyright (C) 2013-2022  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2013-2022  Adrian-Ken Rueegsegger <ken@codelabs.ch>
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
@@ -16,16 +16,16 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-with SK;
+with SK.Exceptions;
 
 package Interrupt_Handler
 is
 
    --  Interrupt handler.
-   procedure Handle_Interrupt (Vector : SK.Byte)
-     with
-       Export,
-       Convention => C,
-       Link_Name  => "dispatch_interrupt";
+   procedure Handle_Interrupt (Context : SK.Exceptions.Isr_Context_Type)
+   with
+      Export,
+      Convention => C,
+      Link_Name  => "dispatch_interrupt";
 
 end Interrupt_Handler;

2bb202344ec307d433ad49747e93f07ead770f3d
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2022-09-09 12:40:02 +0200

    Add Backtraces package
    The package provides a basic implementation for analysing a stack and
    creating a call trace based on the information.

---
+++ components/src/backtraces.ads
@@ -0,0 +1,33 @@
+--
+--  Copyright (C) 2022  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2022  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Interfaces;
+
+package Backtraces
+is
+
+   type Call_Trace_Type is array (Natural range <>) of Interfaces.Unsigned_64;
+
+   --  Create backtrace of System V ABI 64-bit call stack with given RBP value.
+   procedure Analyse_Stack
+     (RIP        :     Interfaces.Unsigned_64;
+      RBP        :     Interfaces.Unsigned_64;
+      Traces     : out Call_Trace_Type;
+      Last_Index : out Natural);
+
+end Backtraces;

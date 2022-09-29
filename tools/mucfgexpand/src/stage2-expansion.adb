a552e6a66aa5a03ec51aaba11181d4272c77064f
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2022-09-29 16:38:40 +0200

    Register scheduling group ID expander

--- tools/mucfgexpand/src/stage2-expansion.adb
+++ tools/mucfgexpand/src/stage2-expansion.adb
@@ -54,6 +54,7 @@ is
       Procs.Register (Process => Channels.Remove_Global_Channels'Access);
 
       Procs.Register (Process => Scheduling.Add_Partition_IDs'Access);
+      Procs.Register (Process => Scheduling.Add_Group_IDs'Access);
 
       --  Add tau0 and potential Mugenschedcfg idle subjects prior to
       --  subject-related memory expanders (state, VMCS and bitmaps).

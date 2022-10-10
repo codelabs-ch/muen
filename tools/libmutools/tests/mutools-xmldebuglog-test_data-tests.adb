ebd64375849c3b85fe41b3f8abd82cabef96d89a
Johannes Greiner <johannes.greiner@secunet.com>
2022-10-10 13:29:12 +0000

    Mark omitted unittests with stubs
    Adds tests stubs with dummy tests. Dedicated tests for Start_Element in the
    dom-reader_with_location and the debug-functionality in mutools-xmldebuglog
    will not be implemented at the moment. The stubs make these gaps more visible.

---
+++ tools/libmutools/tests/mutools-xmldebuglog-test_data-tests.adb
@@ -0,0 +1,421 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Mutools.Xmldebuglog.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+with System.Assertions;
+
+--  begin read only
+--  id:2.2/00/
+--
+--  This section can be used to add with clauses if necessary.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+package body Mutools.Xmldebuglog.Test_Data.Tests is
+
+--  begin read only
+--  id:2.2/01/
+--
+--  This section can be used to add global variables and other elements.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+
+--  begin read only
+   procedure Test_Move_Origin_To_Log (Gnattest_T : in out Test);
+   procedure Test_Move_Origin_To_Log_7f3a53 (Gnattest_T : in out Test) renames Test_Move_Origin_To_Log;
+--  id:2.2/7f3a533fdafd20c7/Move_Origin_To_Log/1/0/
+   procedure Test_Move_Origin_To_Log (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Move_Origin_To_Log;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Add_Debug_Infos_As_Comments (Gnattest_T : in out Test);
+   procedure Test_Add_Debug_Infos_As_Comments_f9d371 (Gnattest_T : in out Test) renames Test_Add_Debug_Infos_As_Comments;
+--  id:2.2/f9d371565bf5b090/Add_Debug_Infos_As_Comments/1/0/
+   procedure Test_Add_Debug_Infos_As_Comments (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Add_Debug_Infos_As_Comments;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Add_Transaction_Log_As_Comment (Gnattest_T : in out Test);
+   procedure Test_Add_Transaction_Log_As_Comment_bd868b (Gnattest_T : in out Test) renames Test_Add_Transaction_Log_As_Comment;
+--  id:2.2/bd868be90bc940e3/Add_Transaction_Log_As_Comment/1/0/
+   procedure Test_Add_Transaction_Log_As_Comment (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Add_Transaction_Log_As_Comment;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Transaction_Log_To_String (Gnattest_T : in out Test);
+   procedure Test_Transaction_Log_To_String_c3bdb3 (Gnattest_T : in out Test) renames Test_Transaction_Log_To_String;
+--  id:2.2/c3bdb3608d92c97a/Transaction_Log_To_String/1/0/
+   procedure Test_Transaction_Log_To_String (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Transaction_Log_To_String;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Node_Backtrace_To_String (Gnattest_T : in out Test);
+   procedure Test_Node_Backtrace_To_String_59f76b (Gnattest_T : in out Test) renames Test_Node_Backtrace_To_String;
+--  id:2.2/59f76b1a8870d7cd/Node_Backtrace_To_String/1/0/
+   procedure Test_Node_Backtrace_To_String (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Node_Backtrace_To_String;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Get_Log_For_Error_Message (Gnattest_T : in out Test);
+   procedure Test_Get_Log_For_Error_Message_5e1fe2 (Gnattest_T : in out Test) renames Test_Get_Log_For_Error_Message;
+--  id:2.2/5e1fe2082a6d1c13/Get_Log_For_Error_Message/1/0/
+   procedure Test_Get_Log_For_Error_Message (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Get_Log_For_Error_Message;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Get_Xpath (Gnattest_T : in out Test);
+   procedure Test_Get_Xpath_b6823f (Gnattest_T : in out Test) renames Test_Get_Xpath;
+--  id:2.2/b6823f56c83155ed/Get_Xpath/1/0/
+   procedure Test_Get_Xpath (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Get_Xpath;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Add_Usetemplate_Transaction (Gnattest_T : in out Test);
+   procedure Test_Add_Usetemplate_Transaction_5ed628 (Gnattest_T : in out Test) renames Test_Add_Usetemplate_Transaction;
+--  id:2.2/5ed62807a800c369/Add_Usetemplate_Transaction/1/0/
+   procedure Test_Add_Usetemplate_Transaction (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Add_Usetemplate_Transaction;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Add_Conditional_Transaction (Gnattest_T : in out Test);
+   procedure Test_Add_Conditional_Transaction_1af119 (Gnattest_T : in out Test) renames Test_Add_Conditional_Transaction;
+--  id:2.2/1af1195c87211879/Add_Conditional_Transaction/1/0/
+   procedure Test_Add_Conditional_Transaction (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Add_Conditional_Transaction;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Add_Amend_Transaction (Gnattest_T : in out Test);
+   procedure Test_Add_Amend_Transaction_4aca65 (Gnattest_T : in out Test) renames Test_Add_Amend_Transaction;
+--  id:2.2/4aca65e6844ff1ba/Add_Amend_Transaction/1/0/
+   procedure Test_Add_Amend_Transaction (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Add_Amend_Transaction;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Add_Log_For_Node (Gnattest_T : in out Test);
+   procedure Test_Add_Log_For_Node_2de797 (Gnattest_T : in out Test) renames Test_Add_Log_For_Node;
+--  id:2.2/2de797922ebbc5f2/Add_Log_For_Node/1/0/
+   procedure Test_Add_Log_For_Node (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Add_Log_For_Node;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Remove_Log_Of_Node (Gnattest_T : in out Test);
+   procedure Test_Remove_Log_Of_Node_95a50e (Gnattest_T : in out Test) renames Test_Remove_Log_Of_Node;
+--  id:2.2/95a50e3335ff3a30/Remove_Log_Of_Node/1/0/
+   procedure Test_Remove_Log_Of_Node (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Remove_Log_Of_Node;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Remove_Log_Of_Subtree (Gnattest_T : in out Test);
+   procedure Test_Remove_Log_Of_Subtree_4dcc81 (Gnattest_T : in out Test) renames Test_Remove_Log_Of_Subtree;
+--  id:2.2/4dcc8116fa69973e/Remove_Log_Of_Subtree/1/0/
+   procedure Test_Remove_Log_Of_Subtree (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Remove_Log_Of_Subtree;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Copy_Log_Entry (Gnattest_T : in out Test);
+   procedure Test_Copy_Log_Entry_34228a (Gnattest_T : in out Test) renames Test_Copy_Log_Entry;
+--  id:2.2/34228ac956e75032/Copy_Log_Entry/1/0/
+   procedure Test_Copy_Log_Entry (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Copy_Log_Entry;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Gather_Backtrace_Info (Gnattest_T : in out Test);
+   procedure Test_Gather_Backtrace_Info_4146bd (Gnattest_T : in out Test) renames Test_Gather_Backtrace_Info;
+--  id:2.2/4146bde43f5399a1/Gather_Backtrace_Info/1/0/
+   procedure Test_Gather_Backtrace_Info (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Gather_Backtrace_Info;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Hash (Gnattest_T : in out Test);
+   procedure Test_Hash_1a4149 (Gnattest_T : in out Test) renames Test_Hash;
+--  id:2.2/1a4149a6a5bbe8f9/Hash/1/0/
+   procedure Test_Hash (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Hash;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Origin_To_String (Gnattest_T : in out Test);
+   procedure Test_Origin_To_String_cdeaf7 (Gnattest_T : in out Test) renames Test_Origin_To_String;
+--  id:2.2/cdeaf7034e6348c5/Origin_To_String/1/0/
+   procedure Test_Origin_To_String (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Origin_To_String;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Transaction_To_String (Gnattest_T : in out Test);
+   procedure Test_Transaction_To_String_543271 (Gnattest_T : in out Test) renames Test_Transaction_To_String;
+--  id:2.2/5432713bdf4a2e4f/Transaction_To_String/1/0/
+   procedure Test_Transaction_To_String (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Transaction_To_String;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Parse_Origin_Attribute (Gnattest_T : in out Test);
+   procedure Test_Parse_Origin_Attribute_8d4892 (Gnattest_T : in out Test) renames Test_Parse_Origin_Attribute;
+--  id:2.2/8d489214027dd6fc/Parse_Origin_Attribute/1/0/
+   procedure Test_Parse_Origin_Attribute (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Parse_Origin_Attribute;
+--  end read only
+
+--  begin read only
+--  id:2.2/02/
+--
+--  This section can be used to add elaboration code for the global state.
+--
+begin
+--  end read only
+   null;
+--  begin read only
+--  end read only
+end Mutools.Xmldebuglog.Test_Data.Tests;
---
+++ tools/libmuxml/tests/schema-dom_readers_with_location-tree_reader_with_location_test_data-tree_reader_with_location_tests.adb
@@ -0,0 +1,61 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Schema.Dom_Readers_With_Location.Tree_Reader_With_Location_Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+with System.Assertions;
+
+--  begin read only
+--  id:2.2/00/
+--
+--  This section can be used to add with clauses if necessary.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+package body Schema.Dom_Readers_With_Location.Tree_Reader_With_Location_Test_Data.Tree_Reader_With_Location_Tests is
+
+--  begin read only
+--  id:2.2/01/
+--
+--  This section can be used to add global variables and other elements.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+
+--  begin read only
+   procedure Test_Start_Element (Gnattest_T : in out Test_Tree_Reader_With_Location);
+   procedure Test_Start_Element_ad957f (Gnattest_T : in out Test_Tree_Reader_With_Location) renames Test_Start_Element;
+--  id:2.2/ad957ffba9b48e0e/Start_Element/1/0/
+   procedure Test_Start_Element (Gnattest_T : in out Test_Tree_Reader_With_Location) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Start_Element;
+--  end read only
+
+--  begin read only
+--  id:2.2/02/
+--
+--  This section can be used to add elaboration code for the global state.
+--
+begin
+--  end read only
+   null;
+--  begin read only
+--  end read only
+end Schema.Dom_Readers_With_Location.Tree_Reader_With_Location_Test_Data.Tree_Reader_With_Location_Tests;

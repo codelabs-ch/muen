fc54b20687f25078b27685a849ebb70b5f002cd2
Reto Buerki <reet@codelabs.ch>
2022-12-09 16:19:33 +0100

    projects: Add --no-subprojects to GNATTEST_OPTS
    Required for gnattest from libadalang_tools_23.0.0.

--- projects/tests.mk
+++ projects/tests.mk
@@ -1,6 +1,6 @@
 GNATTEST_EXIT   ?= on
 GNATTEST_PASSED ?= show
-GNATTEST_OPTS   ?= -q --test-duration --omit-sloc
+GNATTEST_OPTS   ?= -q --test-duration --omit-sloc --no-subprojects
 GNATTEST_RUNNER  = $(OBJ_DIR)/tests/gnattest/harness/test_runner
 GNATTEST_DRIVER  = $(OBJ_DIR)/tests/gnattest/harness/test_driver
 TESTS_DIR        = $(CURDIR)/tests

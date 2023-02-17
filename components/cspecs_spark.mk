19086760f1b836d549ecc252039d5fb60fae9986
Johannes Greiner <johannes.greiner@secunet.com>
2023-02-17 08:12:11 +0100

    Toolchain: Integrate va-alloc tools in makefiles
    Integrates mucfgcvaalloc and mucfgvaalloc into the makefiles.

--- components/cspecs_spark.mk
+++ components/cspecs_spark.mk
@@ -2,4 +2,4 @@ include ../cspecs.mk
 
 $(GEN_DIR)/$(COMPONENT).xml: spec/$(COMPONENT).xml $(MUCGENSPEC) $(CSPEC_TARGETS) | $(GEN_DIR)
 	@$(E) $(COMPONENT) "Generate cspecs" \
-		"$(MUCGENSPEC) -i $< -o $@ -I $(GEN_DIR) $(GEN_DIR)"
+		"$(MUCFGCVAALLOC) $< $@ -I $(GEN_DIR) && $(MUCGENSPEC) -i $@ $(GEN_DIR)"

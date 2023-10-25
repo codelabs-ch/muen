28a055aff7fea68661c9b55d198b03df00692b2e
Reto Buerki <reet@codelabs.ch>
2023-10-25 11:00:30 +0200

    Add skeleton of Mucfgucode tool
    The tool will be used to enable Intel microcode (ucode) updates on Muen.

---
+++ tools/mucfgucode/src/ucode.adb
@@ -0,0 +1,43 @@
+--
+--  Copyright (C) 2023  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2023  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+with Muxml;
+with Mulog;
+
+package body Ucode
+is
+
+   -------------------------------------------------------------------------
+
+   procedure Run
+     (Policy     : String;
+      Ucode_Dir  : String;
+      Output_Dir : String)
+   is
+      pragma Unreferenced (Ucode_Dir, Output_Dir);
+
+      Data : Muxml.XML_Data_Type;
+   begin
+      Mulog.Log (Msg => "Processing policy '" & Policy & "'");
+
+      Muxml.Parse (Data => Data,
+                   Kind => Muxml.Format_Src,
+                   File => Policy);
+   end Run;
+
+end Ucode;

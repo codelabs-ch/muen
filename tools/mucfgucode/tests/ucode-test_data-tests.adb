576e4fadde96c96978c81dbe3b3f5747e08c1385
Reto Buerki <reet@codelabs.ch>
2023-10-25 16:41:21 +0200

    Implement Run test

--- tools/mucfgucode/tests/ucode-test_data-tests.adb
+++ tools/mucfgucode/tests/ucode-test_data-tests.adb
@@ -14,7 +14,12 @@ with System.Assertions;
 --  This section can be used to add with clauses if necessary.
 --
 --  end read only
-
+with Ada.Directories;
+with DOM.Core.Nodes;
+with DOM.Core.Elements;
+with McKae.XML.XPath.XIA;
+with Muxml;
+with Test_Utils;
 --  begin read only
 --  end read only
 package body Ucode.Test_Data.Tests is
@@ -38,12 +43,64 @@ package body Ucode.Test_Data.Tests is
 
       pragma Unreferenced (Gnattest_T);
 
+      ----------------------------------------------------------------------
+
+      procedure Positive
+      is
+         Out_Dir : constant String := "obj/run";
+      begin
+         if not Ada.Directories.Exists (Name => Out_Dir) then
+            Ada.Directories.Create_Directory (New_Directory => Out_Dir);
+         end if;
+         Ada.Directories.Copy_File
+           (Source_Name => "data/test_policy.xml",
+            Target_Name => "obj/test_policy.xml");
+         --  Dummy file, should be replaced with real ucode.
+         Ada.Directories.Copy_File
+           (Source_Name => "data/test_policy.xml",
+            Target_Name => "obj/run/f68");
+
+         Run (Policy     => "obj/test_policy.xml",
+              Ucode_Dir  => "data/ucode",
+              Output_Dir => Out_Dir); 
+
+         Assert (Condition => Ada.Directories.Exists 
+                   (Name => Out_Dir & "/f68"),
+                 Message   => "MCU not copied");
+
+         declare
+            Data  : Muxml.XML_Data_Type;
+            Nodes : DOM.Core.Node_List;
+            Node  : DOM.Core.Node;
+         begin
+            Muxml.Parse (Data => Data,
+                         Kind => Muxml.Format_Src,
+                         File => "obj/test_policy.xml");
+            Nodes := McKae.XML.XPath.XIA.XPath_Query
+              (N     => Data.Doc,
+               XPath => "/system/memory/memory[@type='kernel_microcode']"); 
+            Assert (Condition => DOM.Core.Nodes.Length (Nodes) = 1,
+                    Message   => "One memory node expected");
+            Node := DOM.Core.Nodes.Item (List => Nodes, Index => 0);
+            Assert (Condition => DOM.Core.Elements.Get_Attribute 
+                     (Elem => Node,
+                      Name => "size") = "16#1000#",
+                    Message => "Size mismatch");
+            Assert (Condition => DOM.Core.Nodes.Length
+                     (McKae.XML.XPath.XIA.XPath_Query
+                       (N => Node, XPath => "file[@filename='f68']")) = 1, 
+                    Message   => "Filename node mismatch");
+            Assert (Condition => Test_Utils.Equal_Files
+                    (Filename1 => "data/ucode/microcode",
+                     Filename2 => Out_Dir & "/f68"),
+                    Message   => "Microcode file invalid");
+         end;
+
+         Ada.Directories.Delete_Tree (Directory => Out_Dir);
+      end Positive;
+
    begin
-
-      AUnit.Assertions.Assert
-        (Gnattest_Generated.Default_Assert_Value,
-         "Test not implemented.");
-
+      Positive;
 --  begin read only
    end Test_Run;
 --  end read only

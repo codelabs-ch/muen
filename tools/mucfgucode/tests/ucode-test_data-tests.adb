3eb221da46d7d001a0691e9676b5a718d870267e
Reto Buerki <reet@codelabs.ch>
2023-10-25 11:00:30 +0200

    Add skeleton of Mucfgucode tool
    The tool will be used to enable Intel microcode (ucode) updates on Muen.

---
+++ tools/mucfgucode/tests/ucode-test_data-tests.adb
@@ -0,0 +1,61 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Ucode.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+with System.Assertions;
+
+--  begin read only
+--  id:2.2/00/
+--
+--  This section can be used to add with clauses if necessary.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+package body Ucode.Test_Data.Tests is
+
+--  begin read only
+--  id:2.2/01/
+--
+--  This section can be used to add global variables and other elements.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+
+--  begin read only
+   procedure Test_Run (Gnattest_T : in out Test);
+   procedure Test_Run_e5a2dd (Gnattest_T : in out Test) renames Test_Run;
+--  id:2.2/e5a2dd86b12d7902/Run/1/0/
+   procedure Test_Run (Gnattest_T : in out Test) is
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Run;
+--  end read only
+
+--  begin read only
+--  id:2.2/02/
+--
+--  This section can be used to add elaboration code for the global state.
+--
+begin
+--  end read only
+   null;
+--  begin read only
+--  end read only
+end Ucode.Test_Data.Tests;

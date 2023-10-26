4b42c3bc233ae642ee482d167dc24302c083d3ee
Reto Buerki <reet@codelabs.ch>
2023-10-26 11:00:30 +0200

    Kernel: Add SK.MCU package stubs
    Provide bodies for the enabled/disabled case and select which body to
    use depending on the setting in policy.gpr.

---
+++ kernel/src/sk-mcu.ads
@@ -0,0 +1,29 @@
+--
+--  Copyright (C) 2023  Reto Buerki <reet@codelabs.ch>
+--  Copyright (C) 2023  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+--D @Interface
+--D This package provides Intel microcode update (MCU) facilities.
+--D This package does nothing if the policy does not specify an Intel microcode
+--D update blob.
+package SK.MCU
+is
+
+   --  Perform MCU.
+   procedure Process;
+
+end SK.MCU;

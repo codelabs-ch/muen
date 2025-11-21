f1fd35b0d53f2a9dc1968aea765893038259b37d
Reto Buerki <reet@codelabs.ch>
2025-11-21 09:37:32 +0100

    Delete Makefiles

--- components/library_spark.mk
+++
@@ -1,6 +0,0 @@
-include ../common.mk
-
-$(POLICY_OBJ_DIR)/$(COMPONENT): $(INSTALL_TARGETS)
-
-clean:
-	@rm -rf $(OBJ_DIR) $(GEN_DIR) lib
--- components/proofs.mk
+++
@@ -1,6 +0,0 @@
-COMPLETE_PROOFS := $(shell expr $(GNATPROVE_DATE) \>= 20140901)
-ifeq ($(COMPLETE_PROOFS),1)
-PROOF_OPTS = -Xproofs=complete
-else
-PROOF_OPTS = -Xproofs=limited
-endif
